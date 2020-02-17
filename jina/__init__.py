72384782921fb918b10acde4291002e72c92a534
maxnwang <maxnwang@tencent.com>
2020-02-17 11:34:24 +0800

    fix: fix a typo in comments

--- jina/__init__.py
+++ jina/__init__.py
@@ -3,7 +3,7 @@
 __version__ = '0.0.1'
 
 # do not change this line manually
-# this is managed by shell/build-proto.sh and updated on every execution
+# this is managed by proto/build-proto.sh and updated on every execution
 __proto_version__ = '0.0.5'
 
 from datetime import datetime
