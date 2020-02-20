a5deb0ccf9ffb453aebce0acde8125358a2f3517
Han Xiao <han.xiao@jina.ai>
2020-02-20 11:02:04 +0100

    feat(executor): sharding feature continued

--- tests/test_workspace.py
+++ tests/test_workspace.py
@@ -1,5 +1,6 @@
 import os
 
+import numpy as np
 from jina.executors import BaseExecutor
 from tests import JinaTestCase
 
@@ -33,7 +34,7 @@ class MyTestCase(JinaTestCase):
         all_subspace = set()
         for j in range(3):
             a = BaseExecutor.load_config('yaml/test-compound-indexer.yml', True, j)
-            for c in a.components:
+            for c in a:
                 c.touch()
                 print(c.save_abspath)
                 print(c.index_abspath)
@@ -52,3 +53,31 @@ class MyTestCase(JinaTestCase):
             all_subspace.add(a.current_workspace)
 
         self.assertTrue(len(all_subspace) == 3)
+
+    def test_compound_indexer_rw(self):
+        all_vecs = np.random.random([6, 5])
+        for j in range(3):
+            a = BaseExecutor.load_config('yaml/test-compound-indexer2.yml', True, j)
+            self.assertEqual(a[0], a['test_meta'])
+            self.assertFalse(a[0].is_updated)
+            self.assertFalse(a.is_updated)
+            a[0].add(j)
+            self.assertTrue(a[0].is_updated)
+            self.assertTrue(a.is_updated)
+            self.assertFalse(a[1].is_updated)
+            a[1].add(np.array([j * 2, j * 2 + 1]), all_vecs[(j * 2, j * 2 + 1), :])
+            self.assertTrue(a[1].is_updated)
+            a.save()
+            # the compound executor itself is not modified, therefore should not generate a save
+            self.assertFalse(os.path.exists(a.save_abspath))
+            self.assertTrue(os.path.exists(a[0].save_abspath))
+            self.assertTrue(os.path.exists(a[0].index_abspath))
+            self.assertTrue(os.path.exists(a[1].save_abspath))
+            self.assertTrue(os.path.exists(a[1].index_abspath))
+            self.add_tmpfile(a[0].save_abspath, a[1].save_abspath, a[0].index_abspath, a[1].index_abspath,
+                             a.current_workspace)
+
+        # now load them
+        for j in range(3):
+            b = BaseExecutor.load_config('yaml/test-compound-indexer2.yml', True, j)
+            print(b['test_numpy'].int2ext_key)
