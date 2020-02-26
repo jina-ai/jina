c035c94b9b94483750c35faea48f8dc64e97cef5
Han Xiao <han.xiao@jina.ai>
2020-02-26 10:12:21 +0100

    fix(imports): fix #26 by moving yaml loading inside get

--- jina/executors/requests.py
+++ jina/executors/requests.py
@@ -1,14 +1,15 @@
-import copy
 from typing import Dict
-
-from pkg_resources import resource_stream
-
-from ..helper import yaml
-
-with resource_stream('jina', '/'.join(('resources', 'executors.requests.default.yml'))) as fp:
-    _defaults = yaml.load(fp)  # do not expand variables at here, i.e. DO NOT USE expand_dict(yaml.load(fp))
 
 
 def get_default_reqs() -> Dict:
     """Get a copy of default meta variables"""
+    import copy
+
+    from pkg_resources import resource_stream
+
+    from ..helper import yaml
+
+    with resource_stream('jina', '/'.join(('resources', 'executors.requests.default.yml'))) as fp:
+        _defaults = yaml.load(fp)  # do not expand variables at here, i.e. DO NOT USE expand_dict(yaml.load(fp))
+
     return copy.deepcopy(_defaults)
