c10a3bb4cf56a0734d172f1e658799f8caa1d936
Han Xiao <han.xiao@jina.ai>
2020-03-03 21:25:54 +0100

    feat(flow): add container support for flow

--- tests/test_container.py
+++ tests/test_container.py
@@ -47,9 +47,9 @@ class MyTestCase(JinaTestCase):
         with f.build() as fl:
             fl.index(raw_bytes=random_docs(10), in_proto=True, callback=print)
 
-    # def test_flow_with_container(self):
-    #     f = (Flow()
-    #          .add(name='dummyEncoder', image=container_name))
-    #
-    #     with f.build() as fl:
-    #         fl.index(raw_bytes=random_docs(10), in_proto=True, callback=print)
+    def test_flow_with_container(self):
+        f = (Flow()
+             .add(name='dummyEncoder', image=container_name))
+
+        with f.build() as fl:
+            fl.index(raw_bytes=random_docs(10), in_proto=True, callback=print)
