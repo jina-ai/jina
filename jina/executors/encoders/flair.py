412cc46ff895de355e7497a6c95effe7c303e57e
Nan Wang@Home.MBP <Nan.Wang.28@gmail.com>
2020-03-11 21:38:20 +0800

    feat(encoder): implement the post_init part

--- jina/executors/encoders/flair.py
+++ jina/executors/encoders/flair.py
@@ -1,44 +1,22 @@
 from typing import Union, Tuple, List
+import os
+import pickle
 import numpy as np
 import torch
 
-from flair.embeddings import WordEmbeddings, FlairEmbeddings, BytePairEmbeddings, ELMoEmbeddings, \
-    FastTextEmbeddings, PooledFlairEmbeddings
-from flair.embeddings import DocumentPoolEmbeddings, Sentence
-
 from . import BaseTextEncoder
+from ...excepts import BadPersistantFile
 
 
 class FlairTextEncoder(BaseTextEncoder):
     def __init__(self,
-                 embeddings: Union[Tuple[str], List[str]] = ['word:glove', 'flair:news-forward', 'flair:news-backword'],
+                 embeddings: Union[Tuple[str], List[str]] = ('word:glove', 'flair:news-forward', 'flair:news-backward'),
                  pooling_strategy: str = 'reduce-mean',
-                 max_length: int = 64,
                  *args,
                  **kwargs):
         super().__init__(*args, **kwargs)
-        embeddings_list = []
-        for e in embeddings:
-            sub_name = e[(e.find(':')+1):]
-            emb = None
-            try:
-                if e.startswith('flair:'):
-                    emb = FlairEmbeddings(sub_name)
-                elif e.startswith('word:'):
-                    emb = WordEmbeddings(sub_name)
-                elif e.startswith('byte-pair:'):
-                    emb = BytePairEmbeddings(sub_name)
-                elif e.startswith('elmo:'):   # depends on AllenNLP
-                    emb = ELMoEmbeddings(sub_name)
-                elif e.startswith('fasttext:'):  # load one's own pretrained model
-                    emb = FastTextEmbeddings(sub_name)
-                elif e.startswith('pooledflair:'):
-                    emb = PooledFlairEmbeddings(sub_name)
-            except ValueError:
-                self.logger.critical("embedding not found: {}".format(e))
-                continue
-            if emb is not None:
-                embeddings_list.append(emb)
+        self.embeddings = embeddings
+        self.encoder_abspath = ""
         self.pooling_strategy = None
         self.model = None
         if pooling_strategy == "reduce-mean":
@@ -48,15 +26,60 @@ class FlairTextEncoder(BaseTextEncoder):
         elif pooling_strategy == "reduce-min":
             self.pooling_strategy = 'min'
         else:
-            self.logger.critical("unknown pooling_strategy: {}".format(pooling_strategy))
+            self.logger.error("unknown pooling_strategy: {}".format(pooling_strategy))
             raise NotImplementedError
+
+    def post_init(self):
+        from flair.embeddings import WordEmbeddings, FlairEmbeddings, BytePairEmbeddings, PooledFlairEmbeddings
+        # ELMoEmbeddings, FastTextEmbeddings,
+
+        from flair.embeddings import DocumentPoolEmbeddings
+
+        if self.encoder_abspath:
+            if not os.path.exists(self.encoder_abspath):
+                self.logger.error("encoder path not found: {}".format(self.encoder_abspath))
+                raise FileNotFoundError
+            try:
+                with open(self.encoder_abspath, 'rb') as fp:
+                    self.model = pickle.load(fp)
+                    self.logger.info('load flair encoder model from {}'.format(self.encoder_abspath))
+            except EOFError:
+                raise BadPersistantFile('broken file {} can not be loaded'.format(self.encoder_abspath))
+        embeddings_list = []
+        for e in self.embeddings:
+            model_name, model_subname = e.split(':', maxsplit=1)
+            emb = None
+            try:
+                if model_name == 'flair':
+                    emb = FlairEmbeddings(model_subname)
+                elif model_name == 'word':
+                    emb = WordEmbeddings(model_subname)
+                elif model_name == 'byte-pair':
+                    emb = BytePairEmbeddings(model_subname)
+                elif model_name == 'pooledflair':
+                    emb = PooledFlairEmbeddings(model_subname)
+            except ValueError:
+                self.logger.error("embedding not found: {}".format(e))
+                continue
+            if emb is not None:
+                embeddings_list.append(emb)
         if embeddings_list:
             self.model = DocumentPoolEmbeddings(embeddings_list, pooling=self.pooling_strategy)
 
     def encode(self, data: 'np.ndarray', *args, **kwargs) -> 'np.ndarray':
+        from flair.embeddings import Sentence
         c_batch = []
         for c_idx in range(data.shape[0]):
             c_text = Sentence(data[c_idx])
             c_batch.append(c_text)
         self.model.embed(c_batch)
         return torch.stack([c_text.get_embedding() for c_text in c_batch]).detach().numpy()
+
+    def __getstate__(self):
+        save_path = os.path.join(self.current_workspace, "flair")
+        self.encoder_abspath = os.path.join(save_path, "model.bin")
+        if not os.path.exists(save_path):
+            self.logger.info("create folder for saving flair models: {}".format(save_path))
+            os.mkdir(save_path)
+        with open(self.encoder_abspath, 'wb') as f:
+            pickle.dump(self, f)
--- tests/test_exec_encoder_flair.py
+++ tests/test_exec_encoder_flair.py
@@ -1,9 +1,13 @@
 import unittest
-from jina.executors.encoders.flair import FlairTextEncoder
 import numpy as np
+import os
+
+from tests import JinaTestCase
+from jina.executors.encoders.flair import FlairTextEncoder
+from jina.executors import BaseExecutor
 
 
-class MyTestCase(unittest.TestCase):
+class MyTestCase(JinaTestCase):
     model_subname_dict = {
         'word:': ['glove', 'extvec', 'crawl', 'twitter', 'news'],
         'flair': ['multi-forward', 'multi-backward',
@@ -12,7 +16,7 @@ class MyTestCase(unittest.TestCase):
                   'news-forward-fast', 'news-backward-fast',
                   'mix-forward', 'mix-backward'],
         'byte-pair': ['en', 'de', 'fr'],
-        'elmo': ['small', 'medium', 'large', 'original', 'pubmed'],
+        # 'elmo': ['small', 'medium', 'large', 'original', 'pubmed'],
         # 'fasttext': ['/path/to/custom_fasttext_embeddings.bin']
     }
     model_subname_dict['pooledflair'] = model_subname_dict['flair']
@@ -37,6 +41,39 @@ class MyTestCase(unittest.TestCase):
                 test_data = np.array(['it is a good day!', 'the dog sits on the floor.'])
                 encoded_data = encoder.encode(test_data)
                 self.assertEqual(encoded_data.shape[0], 2, '{} failed'.format(model_name))
+
+    def test_save_and_load(self):
+        encoder = FlairTextEncoder(
+            embeddings=('word:glove', ), pooling_strategy='reduce-mean', workspace=os.environ['TEST_WORKDIR'])
+        encoder.save_config()
+        self.assertTrue(os.path.exists(encoder.config_abspath))
+
+        encoder.touch()
+        encoder.save()
+        self.assertTrue(os.path.exists(encoder.save_abspath))
+        encoder_loaded = BaseExecutor.load(encoder.save_abspath)
+
+        self.assertEqual(encoder_loaded.embeddings, encoder.embeddings)
+        self.assertEqual(encoder_loaded.pooling_strategy, encoder.pooling_strategy)
+
+        self.tmp_files.append(encoder.config_abspath)
+        self.tmp_files.append(encoder.save_abspath)
+        self.tmp_files.append(encoder_loaded.config_abspath)
+        self.tmp_files.append(encoder_loaded.save_abspath)
+        self.tmp_files.append(encoder.encoder_abspath)
+
+    def test_save_and_load_config(self):
+        encoder = FlairTextEncoder(
+            embeddings=('word:glove',), pooling_strategy='reduce-mean', workspace=os.environ['TEST_WORKDIR'])
+        encoder.save_config()
+        self.assertTrue(os.path.exists(encoder.config_abspath))
+
+        encoder_loaded = BaseExecutor.load_config(encoder.config_abspath)
+        self.assertEqual(encoder_loaded.embeddings, encoder.embeddings)
+        self.assertEqual(encoder_loaded.pooling_strategy, encoder.pooling_strategy)
+
+        self.tmp_files.append(encoder_loaded.config_abspath)
+        self.tmp_files.append(encoder_loaded.save_abspath)
 
 
 if __name__ == '__main__':
