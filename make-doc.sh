f9d79852a2877eb308a6d8ed98a7d69be16fce6f
Han Xiao <han.xiao@jina.ai>
2020-03-14 09:12:26 +0100

    docs: auto generate doc and push it to another repo

--- make-doc.sh
+++ make-doc.sh
@@ -6,6 +6,7 @@ DOC_DIR=docs
 HTML_DIR=${DOC_DIR}/_build/html
 
 
+
 cd ${DOC_DIR} && rm -rf api && pip install -r requirements.txt && make clean && cd -
 
 # require docker installed https://github.com/pseudomuto/protoc-gen-doc
@@ -18,6 +19,7 @@ cd ${DOC_DIR} && make html && cd -
 
 if [[ $1 == "commit" ]]; then
   cd ${HTML_DIR}
+  git init
   git config --local user.email "dev-team@jina.ai"
   git config --local user.name "Jina Doc Bot"
   git add .
