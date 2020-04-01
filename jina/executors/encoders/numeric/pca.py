6feb20fbeaf3be7c50ccd7bfdfb76d001d16ae0e
Nan Wang <nan.wang@jina.ai>
2020-04-01 11:57:08 +0800

    feat(encoder): add the PCAEncoder and refactoring unittests

--- jina/executors/encoders/numeric/pca.py
+++ jina/executors/encoders/numeric/pca.py
@@ -4,6 +4,7 @@ from ...decorators import batching, require_train
 
 from .. import BaseNumericEncoder
 
+from sklearn.decomposition import PCA
 
 class IncrementalPCAEncoder(BaseNumericEncoder):
     """
@@ -15,17 +16,17 @@ class IncrementalPCAEncoder(BaseNumericEncoder):
     """
     def __init__(self,
                  output_dim: int,
-                 whiten: bool = False,
                  num_features: int = None,
+                 whiten: bool = False,
                  save_path: str = '',
                  *args,
                  **kwargs):
         """
 
         :param output_dim: the output size.
-        :param whiten: If whiten is false, the data is already considered to be whitened, and no whitening is performed.
         :param num_features: the number of input features.  If ``num_features`` is None, then ``num_features`` is
             inferred from the data
+        :param whiten: If whiten is false, the data is already considered to be whitened, and no whitening is performed.
         :param encoder_abspath: the absolute saving path of the encoder. If a valid path is given, the encoder will be
             loaded from the given path.
         """
@@ -53,7 +54,6 @@ class IncrementalPCAEncoder(BaseNumericEncoder):
         num_samples, num_features = data.shape
         if not self.num_features:
             self.num_features = num_features
-        self._check_num_features(num_features)
         if num_samples < 5 * num_features:
             self.logger.warning(
                 'the batch size (={}) is suggested to be 5 * num_features(={}) to provide a balance between '
@@ -64,15 +64,12 @@ class IncrementalPCAEncoder(BaseNumericEncoder):
     @require_train
     @batching
     def encode(self, data: 'np.ndarray', *args, **kwargs) -> 'np.ndarray':
+        """
+        :param data: a `B x T` numpy ``ndarray``, `B` is the size of the batch
+        :return: a `B x D` numpy ``ndarray``
+        """
         _, num_features = data.shape
-        self._check_num_features(num_features)
         return self.model.transform(data)
-
-    def _check_num_features(self, num_features):
-        if self.num_features != num_features:
-            raise ValueError(
-                'the number of features must be consistent. ({} != {})'.format(num_features, self.num_features)
-            )
 
     def __getstate__(self):
         if not self.encoder_abspath:
@@ -80,10 +77,92 @@ class IncrementalPCAEncoder(BaseNumericEncoder):
         if os.path.exists(self.encoder_abspath):
             self.logger.warning(
                 'the existed model file will be overrided: {}".format(save_path)')
-        self.logger.info(
-            'the model is saved at: {}'.format(self.encoder_abspath))
         import pickle
         with open(self.encoder_abspath, 'wb') as f:
             pickle.dump(self.model, f)
+        self.logger.info(
+            'the model is saved at: {}'.format(self.encoder_abspath))
         return super().__getstate__()
 
+
+class PCAEncoder(BaseNumericEncoder):
+    """
+    :class:`PCAEncoder` encodes data from an ndarray in size `B x T` into an ndarray in size `B x D`.
+
+    .. note::
+        :class:`PCAEncoder` must be trained before calling ``encode()``. This encoder can NOT be trained in the batch mode.
+    """
+    def __init__(self,
+                 output_dim: int,
+                 num_features: int,
+                 whiten: bool = False,
+                 save_path: str = '',
+                 *args,
+                 **kwargs):
+        """
+
+        :param output_dim: the output size.
+        :param whiten: If whiten is false, the data is already considered to be whitened, and no whitening is performed.
+        :param num_features: the number of input features.  If ``num_features`` is None, then ``num_features`` is
+            inferred from the data
+        :param encoder_abspath: the absolute saving path of the encoder. If a valid path is given, the encoder will be
+            loaded from the given path.
+        """
+        super().__init__(*args, **kwargs)
+        self.output_dim = output_dim
+        self.whiten = whiten
+        self.num_features = num_features
+        self.encoder_abspath = save_path
+        self.is_trained = False
+        self.mean = None
+        self._num_samples = None
+
+    def __getstate__(self):
+        if not self.encoder_abspath:
+            self.encoder_abspath = os.path.join(self.current_workspace, "pca.bin")
+        if os.path.exists(self.encoder_abspath):
+            self.logger.warning(
+                'the existed model file will be overrided: {}".format(save_path)')
+        import faiss
+        faiss.write_VectorTransform(self.model, self.encoder_abspath)
+        self.logger.info(
+            'the model is saved at: {}'.format(self.encoder_abspath))
+        return super().__getstate__()
+
+    @staticmethod
+    def _calc_std(data, n_samples):
+        return np.sqrt(data ** 2 / (n_samples - 1))
+
+
+    def post_init(self):
+        self.model = None
+        import faiss
+        if os.path.exists(self.encoder_abspath):
+            self.model = faiss.read_VectorTransform(self.encoder_abspath)
+            self.std = self._calc_std(
+                faiss.vector_to_array(self.model.eigenvalues)[:self.output_dim], self._num_samples)
+            self.logger.info('load existing model from {}'.format(self.encoder_abspath))
+        else:
+            self.model = faiss.PCAMatrix(self.num_features, self.output_dim)
+
+    def train(self, data: 'np.ndarray', *args, **kwargs):
+        import faiss
+        self._num_samples, num_features = data.shape
+        if not self.num_features:
+            self.num_features = num_features
+        self.mean = np.mean(data, axis=0)
+        self.model.train((data - self.mean).astype('float32'))
+        self.std = self._calc_std(faiss.vector_to_array(self.model.eigenvalues)[:self.output_dim], self._num_samples)
+        self.is_trained = True
+
+    @require_train
+    @batching
+    def encode(self, data: 'np.ndarray', *args, **kwargs) -> 'np.ndarray':
+        """
+        :param data: a `B x T` numpy ``ndarray``, `B` is the size of the batch
+        :return: a `B x D` numpy ``ndarray``
+        """
+        output = self.model.apply_py((data - self.mean).astype('float32'))
+        if self.whiten:
+            output /= self.std
+        return output
--- tests/test_exec_encoder_numeric_pca.py
+++ tests/test_exec_encoder_numeric_pca.py
@@ -4,62 +4,67 @@ import numpy as np
 import os
 
 from . import JinaTestCase
-from jina.executors.encoders.numeric.pca import IncrementalPCAEncoder
+import jina.executors.encoders.numeric.pca as pca  #import IncrementalPCAEncoder, PCAEncoder
+from jina.executors.encoders.numeric.pca import IncrementalPCAEncoder, PCAEncoder
 from jina.executors import BaseExecutor
 
 
 class MyTestCase(JinaTestCase):
     num_features = 28
     output_dim = 2
+    model_list = ('IncrementalPCAEncoder', 'PCAEncoder')
 
-    def test_encoding_results(self):
-        encoder = IncrementalPCAEncoder(
-            output_dim=self.output_dim, whiten=True, num_features=self.num_features)
+    def _test_encoding_results(self, encoder):
         train_data = np.random.rand(1000, self.num_features)
         encoder.train(train_data)
         self.assertTrue(encoder.is_trained)
-
         test_data = np.random.rand(10, self.num_features)
         encoded_data = encoder.encode(test_data)
         self.assertEqual(encoded_data.shape, (test_data.shape[0], self.output_dim))
         self.assertIs(type(encoded_data), np.ndarray)
 
-    def test_save_and_load(self):
-        encoder = IncrementalPCAEncoder(
-            output_dim=self.output_dim, whiten=True, num_features=self.num_features)
+    def test_encoding_results(self):
+        for m in self.model_list:
+            encoder = getattr(pca, m)(
+                output_dim=self.output_dim, whiten=True, num_features=self.num_features)
+            self._test_encoding_results(encoder)
+
+    def _test_save_and_load(self, encoder):
         train_data = np.random.rand(1000, self.num_features)
         encoder.train(train_data)
         test_data = np.random.rand(10, self.num_features)
         encoded_data_control = encoder.encode(test_data)
-
         encoder.touch()
         encoder.save()
         self.assertTrue(os.path.exists(encoder.save_abspath))
         encoder_loaded = BaseExecutor.load(encoder.save_abspath)
         encoded_data_test = encoder_loaded.encode(test_data)
-
-        self.assertEqual(
-            encoder_loaded.model.n_samples_seen_,
-            encoder.model.n_samples_seen_)
         np.testing.assert_array_equal(
             encoded_data_test, encoded_data_control)
         self.add_tmpfile(
             encoder.config_abspath, encoder.save_abspath, encoder_loaded.config_abspath, encoder_loaded.save_abspath,
             encoder.encoder_abspath)
 
-    def test_save_and_load_config(self):
-        encoder = IncrementalPCAEncoder(
-            output_dim=self.output_dim, whiten=True, num_features=self.num_features)
+    def test_save_and_load(self):
+        for m in self.model_list:
+            encoder = getattr(pca, m)(
+                output_dim=self.output_dim, whiten=True, num_features=self.num_features)
+            self._test_save_and_load(encoder)
+
+    def _test_save_and_load_config(self, encoder):
         encoder.save_config()
         self.assertTrue(os.path.exists(encoder.config_abspath))
-
         encoder_loaded = BaseExecutor.load_config(encoder.config_abspath)
-
         self.assertEqual(
             encoder_loaded.output_dim,
             encoder.output_dim)
-
         self.add_tmpfile(encoder_loaded.config_abspath, encoder_loaded.save_abspath)
+
+    def test_save_and_load_config(self):
+        for m in self.model_list:
+            encoder = getattr(pca, m)(
+                output_dim=self.output_dim, whiten=True, num_features=self.num_features)
+            self._test_save_and_load_config(encoder)
 
 
 if __name__ == '__main__':
