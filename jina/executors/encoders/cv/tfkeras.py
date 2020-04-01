97c240d97eb28ae5fa69e65a17bb93f0cca7b853
Han Xiao <han.xiao@jina.ai>
2020-04-01 17:03:41 +0200

    docs: add toc for to page generator
    Signed-off-by: Han Xiao <han.xiao@jina.ai>

--- jina/executors/encoders/cv/tfkeras.py
+++ jina/executors/encoders/cv/tfkeras.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from .. import BaseImageEncoder
+from ...decorators import batching, as_ndarray
 
 
 class KerasImageEncoder(BaseImageEncoder):
@@ -8,10 +9,11 @@ class KerasImageEncoder(BaseImageEncoder):
     :class:`KerasImageEncoder` encodes data from a ndarray, potentially B x (Channel x Height x Width) into a
         ndarray of `B x D`.
     Internally, :class:`KerasImageEncoder` wraps the models from `tensorflow.keras.applications`.
-    https://github.com/PaddlePaddle/PaddleHub
+    https://keras.io/applications/
     """
 
-    def __init__(self, model_name: str = 'MobileNetV2', pool_strategy: str = 'avg', *args, **kwargs):
+    def __init__(self, model_name: str = 'MobileNetV2', img_shape: int = 96,
+                 pool_strategy: str = 'avg', channel_axis: int = -1, *args, **kwargs):
         """
 
         :param model_name: the name of the model. Supported models include
@@ -28,23 +30,34 @@ class KerasImageEncoder(BaseImageEncoder):
             - `avg` means that global average pooling will be applied to the output of the last convolutional block, and
                  thus the output of the model will be a 2D tensor.
             - `max` means that global max pooling will be applied.
+        :param channel_axis: the axis id of the channel, -1 indicate the color channel info at the last axis.
+                If given other, then ``np.moveaxis(data, channel_axis, -1)`` is performed before :meth:`encode`.
         """
         super().__init__(*args, **kwargs)
         self.model_name = model_name
         self.pool_strategy = pool_strategy
+        self.img_shape = img_shape
+        self.channel_axis = channel_axis
 
     def post_init(self):
         import tensorflow as tf
-        self.model = getattr(tf.keras.applications, self.model_name)(
-            input_shape=(224, 224, 3),
+        model = getattr(tf.keras.applications, self.model_name)(
+            input_shape=(self.img_shape, self.img_shape, 3),
             include_top=False,
             pooling=self.pool_strategy,
             weights='imagenet')
 
+        model.trainable = False
+        self.model = model
+
+    @batching
+    @as_ndarray
     def encode(self, data: 'np.ndarray', *args, **kwargs) -> 'np.ndarray':
         """
 
         :param data: a `B x (Channel x Height x Width)` numpy ``ndarray``, `B` is the size of the batch
         :return: a `B x D` numpy ``ndarray``, `D` is the output dimension
         """
-        return self.model(np.moveaxis(data, 1, -1)).numpy()
+        if self.channel_axis != -1:
+            data = np.moveaxis(data, self.channel_axis, -1)
+        return self.model(data)
