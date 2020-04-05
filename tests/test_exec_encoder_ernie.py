a73690e916e9a67610c71c6b42ad11b18be51417
Nan Wang <nan.wang@jina.ai>
2020-04-05 18:33:23 +0800

    refactor(encoder): refactoring the paddlehub encoder for nlp

--- tests/test_exec_encoder_ernie.py
+++ tests/test_exec_encoder_ernie.py
@@ -4,23 +4,22 @@ import unittest
 import numpy as np
 
 from jina.executors import BaseExecutor
-from jina.executors.encoders.nlp.ernie import ErnieTextEncoder
+from jina.executors.encoders.nlp.paddlehub import TextPaddlehubEncoder
 from tests import JinaTestCase
 
 
 class MyTestCase(JinaTestCase):
     @unittest.skipUnless('JINA_TEST_PRETRAINED' in os.environ, 'skip the pretrained test if not set')
     def test_encoding_results(self):
-        encoder = ErnieTextEncoder(max_length=10, workspace=os.environ['TEST_WORKDIR'])
+        encoder = TextPaddlehubEncoder(max_length=10, workspace=os.environ['TEST_WORKDIR'])
         test_data = np.array(['it is a good day!', 'the dog sits on the floor.'])
         encoded_data = encoder.encode(test_data)
         self.assertEqual(encoded_data.shape[0], 2)
         self.assertIs(type(encoded_data), np.ndarray)
-        self.add_tmpfile(encoder.vocab_abspath)
 
     @unittest.skipUnless('JINA_TEST_PRETRAINED' in os.environ, 'skip the pretrained test if not set')
     def test_save_and_load(self):
-        encoder = ErnieTextEncoder(
+        encoder = TextPaddlehubEncoder(
             max_length=10, workspace=os.environ['TEST_WORKDIR'])
         encoder.save_config()
         self.assertTrue(os.path.exists(encoder.config_abspath))
@@ -33,22 +32,23 @@ class MyTestCase(JinaTestCase):
         encoder_loaded = BaseExecutor.load(encoder.save_abspath)
         encoded_data_test = encoder_loaded.encode(test_data)
 
-        self.assertEqual(encoder_loaded.vocab_abspath, encoder.vocab_abspath)
+        self.assertEqual(encoder_loaded.max_seq_length, encoder.max_seq_length)
+        np.testing.assert_array_equal(encoded_data_control, encoded_data_test)
 
         self.add_tmpfile(
-            encoder.config_abspath, encoder.save_abspath, encoder_loaded.config_abspath, encoder_loaded.save_abspath, encoder.vocab_abspath)
+            encoder.config_abspath, encoder.save_abspath, encoder_loaded.config_abspath, encoder_loaded.save_abspath)
 
     @unittest.skipUnless('JINA_TEST_PRETRAINED' in os.environ, 'skip the pretrained test if not set')
     def test_save_and_load_config(self):
-        encoder = ErnieTextEncoder(
+        encoder = TextPaddlehubEncoder(
             max_length=10, workspace=os.environ['TEST_WORKDIR'])
         encoder.save_config()
         self.assertTrue(os.path.exists(encoder.config_abspath))
 
         encoder_loaded = BaseExecutor.load_config(encoder.config_abspath)
-        self.assertEqual(encoder_loaded.vocab_abspath, encoder.vocab_abspath)
+        self.assertEqual(encoder_loaded.max_seq_length, encoder.max_seq_length)
 
-        self.add_tmpfile(encoder_loaded.config_abspath, encoder_loaded.save_abspath, encoder.vocab_abspath)
+        self.add_tmpfile(encoder_loaded.config_abspath, encoder_loaded.save_abspath)
 
 
 if __name__ == '__main__':
