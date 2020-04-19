986f2b9fcd787ea2857322bd98984f5fe872d37d
Han Xiao <han.xiao@jina.ai>
2020-04-19 15:48:06 +0200

    docs: add hello world to docs

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,11 @@
+import os
 import subprocess
 import unittest
 
+from pkg_resources import resource_filename
+
+from jina.flow import Flow
+from jina.main.parser import set_hw_parser
 from tests import JinaTestCase
 
 
@@ -13,6 +18,20 @@ class MyTestCase(JinaTestCase):
 
     def test_helloworld(self):
         subprocess.check_call(['jina', 'hello-world'])
+
+    def test_helloworld_flow(self):
+        args = set_hw_parser().parse_args([])
+
+        os.environ['RESOURCE_DIR'] = resource_filename('jina', 'resources')
+        os.environ['SHARDS'] = str(args.shards)
+        os.environ['REPLICAS'] = str(args.replicas)
+        os.environ['HW_WORKDIR'] = args.workdir
+        os.environ['WITH_LOGSERVER'] = str(args.logserver)
+
+        a = Flow.load_config(resource_filename('jina', '/'.join(('resources', 'helloworld.flow.index.yml'))))
+        a.build()
+        for p in a._pod_nodes.values():
+            print(f'{p.name}, {p.needs}')
 
 
 if __name__ == '__main__':
