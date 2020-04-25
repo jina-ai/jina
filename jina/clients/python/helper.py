177c1a65faf19133b8a511158947f5e545b647a8
Han Xiao <han.xiao@jina.ai>
2020-04-25 17:57:33 +0200

    fix: add num_req to profile api

--- jina/clients/python/helper.py
+++ jina/clients/python/helper.py
@@ -28,7 +28,7 @@ class ProgressBar:
         """
         self.bar_len = bar_len
         self.task_name = task_name
-        self.proc_doc = 0
+        self.num_docs = 0
         self.logger = logger
 
     def update(self, progress: int = None, *args, **kwargs) -> None:
@@ -36,46 +36,48 @@ class ProgressBar:
 
         :param progress: the number of unit to increment
         """
-        self.num_bars += 1
+        self.num_reqs += 1
         sys.stdout.write('\r')
         elapsed = time.perf_counter() - self.start_time
-        num_bars = self.num_bars % self.bar_len
-        num_bars = self.bar_len if not num_bars and self.num_bars else max(num_bars, 1)
+        num_bars = self.num_reqs % self.bar_len
+        num_bars = self.bar_len if not num_bars and self.num_reqs else max(num_bars, 1)
         if progress:
-            self.proc_doc += progress
+            self.num_docs += progress
 
         sys.stdout.write(
             '{:>10} [{:<{}}] 📃 {:6d} ⏱️ {:3.1f}s 🐎 {:3.1f}/s {:6d} batch'.format(
                 colored(self.task_name, 'cyan'),
                 colored('=' * num_bars, 'green'),
                 self.bar_len + 9,
-                self.proc_doc,
+                self.num_docs,
                 elapsed,
-                self.proc_doc / elapsed,
-                self.num_bars
+                self.num_docs / elapsed,
+                self.num_reqs
             ))
         if num_bars == self.bar_len:
             sys.stdout.write('\n')
         sys.stdout.flush()
         profile_logger.debug({'num_bars': num_bars,
-                              'num_req': self.num_bars,
+                              'num_reqs': self.num_reqs,
                               'bar_len': self.bar_len,
+                              'progress': num_bars / self.bar_len,
                               'task_name': self.task_name,
-                              'speed': (self.proc_doc if self.proc_doc > 0 else self.num_bars) / elapsed,
-                              'speed_unit': ('Documents' if self.proc_doc > 0 else 'Batches'),
+                              'qps': self.num_reqs / elapsed,
+                              'speed': (self.num_docs if self.num_docs > 0 else self.num_reqs) / elapsed,
+                              'speed_unit': ('Documents' if self.num_docs > 0 else 'Requests'),
                               'elapsed': elapsed})
 
     def __enter__(self):
         self.start_time = time.perf_counter()
-        self.num_bars = -1
-        self.proc_doc = 0
+        self.num_reqs = -1
+        self.num_docs = 0
         self.update()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         elapsed = time.perf_counter() - self.start_time
-        if self.proc_doc > 0:
-            speed = self.proc_doc / elapsed
+        if self.num_docs > 0:
+            speed = self.num_docs / elapsed
         else:
-            speed = self.num_bars / elapsed
+            speed = self.num_reqs / elapsed
         sys.stdout.write('\t%s\n' % colored(f'✅ done in ⏱ {elapsed:3.1f}s 🐎 {speed:3.1f}/s', 'green'))
