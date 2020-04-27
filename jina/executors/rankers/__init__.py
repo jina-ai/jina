54de20f3e3589b98d97f0d22e032ba9e21cb70f8
Nan Wang <nan.wang@jina.ai>
2020-04-27 21:52:17 +0800

    fix(rankers): add the warnings

--- jina/executors/rankers/__init__.py
+++ jina/executors/rankers/__init__.py
@@ -106,7 +106,7 @@ class MinRanker(BaseRanker):
     :class:`MinRanker` calculates the score of the matched doc form the matched chunks. For each matched doc, the score
         is `1 / (1 + s)`, where `s` is the minimal score from all the matched chunks belonging to this doc.
 
-    .. warning: Here we suppose that the smaller chunk score means the more similar.
+    .. warning:: Here we suppose that the smaller chunk score means the more similar.
     """
 
     def _get_score(self, match_idx, query_chunk_meta, match_chunk_meta, *args, **kwargs):
