4d11e1131cdf5f388dc81c7e0febcd32c10447b1
phamtrancsek12 <phamtrancsek12@gmail.com>
2020-05-17 14:02:33 +0800

    refactor(executors): rename numeric crafter

--- jina/executors/crafters/numeric/io.py
+++ jina/executors/crafters/numeric/io.py
@@ -2,9 +2,9 @@ from typing import Dict, List
 import numpy as np
 from jina.executors.crafters import BaseSegmenter
 
-class ArrayConverter(BaseSegmenter):
+class ArrayReader(BaseSegmenter):
     """
-    :class:`ArrayConverter` convert the string of numbers into a numpy array and save to the Chunk.
+    :class:`ArrayReader` convert the string of numbers into a numpy array and save to the Chunk.
         Numbers are split on the provided delimiter, default is comma (,)
     """
 
--- tests/executors/crafters/numeric/test_io.py
+++ tests/executors/crafters/numeric/test_io.py
@@ -1,18 +1,18 @@
 import unittest
 import numpy as np
 
-from jina.executors.crafters.numeric.convert import ArrayConverter
+from jina.executors.crafters.numeric.io import ArrayReader
 from tests import JinaTestCase
 
 
 class MyTestCase(JinaTestCase):
-    def test_array_converter(self):
+    def test_array_reader(self):
         size = 8
         sample_array = np.random.rand(size).astype("float32")
         raw_bytes = ",".join([str(x) for x in sample_array]).encode("utf8")
 
-        converter = ArrayConverter()
-        crafted_chunk = converter.craft(raw_bytes, 0)[0]
+        reader = ArrayReader()
+        crafted_chunk = reader.craft(raw_bytes, 0)[0]
 
         np.testing.assert_array_equal(crafted_chunk["blob"], sample_array)
 
