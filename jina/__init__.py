186e2fe9c26334cd3ec002c9f993a082fbeb9e51
Han Xiao <han.xiao@jina.ai>
2020-05-21 11:09:47 +0200

    feat(proto): add data uri as content of doc

--- jina/__init__.py
+++ jina/__init__.py
@@ -7,7 +7,7 @@ __version__ = '0.1.13'
 
 # do not change this line manually
 # this is managed by proto/build-proto.sh and updated on every execution
-__proto_version__ = '0.0.21'
+__proto_version__ = '0.0.22'
 
 import platform
 import sys
