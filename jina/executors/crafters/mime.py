fa7ade6aab76767563fc39178c733da725c3a5d1
Han Xiao <han.xiao@jina.ai>
2020-05-21 16:01:27 +0200

    test: add test for data uri crafter

--- jina/executors/crafters/mime.py
+++ jina/executors/crafters/mime.py
@@ -3,15 +3,55 @@ import mimetypes
 from . import BaseDocCrafter
 
 
-class DataURICrafter(BaseDocCrafter):
-    def __init__(self, mimetype: str, charset: str = 'utf-8', base64: bool = False, *args,
+class File2DataURICrafter(BaseDocCrafter):
+    def __init__(self, charset: str = 'utf-8', base64: bool = False, *args, **kwargs):
+        """ Build MIME type document from file name. The MIME type is guessed from the file name extension
+
+        :param charset: charset may be any character set registered with IANA
+        :param base64: used to encode arbitrary octet sequences into a form that satisfies the rules of 7bit. Designed to be efficient for non-text 8 bit and binary data. Sometimes used for text data that frequently uses non-US-ASCII characters.
+        :param args:
+        :param kwargs:
+        """
+        super().__init__(*args, **kwargs)
+        self.charset = charset
+        self.base64 = base64
+
+    def craft(self, raw_bytes, *args, **kwargs):
+        p = raw_bytes.decode()
+        mimetype, _ = mimetypes.guess_type(p)
+        if not mimetype:
+            self.logger.warning(f'can not determine the '
+                                f'media type from the filename and extension of {p}, '
+                                f'the result data URI may not be readable')
+            mimetype = ''
+        with open(p, 'rb') as fp:
+            raw_bytes = fp.read()
+
+        return dict(data_uri=self.make_datauri(mimetype, raw_bytes))
+
+    def make_datauri(self, mimetype, raw_bytes):
+        parts = ['data:', mimetype]
+        if self.charset is not None:
+            parts.extend([';charset=', self.charset])
+        if self.base64:
+            parts.append(';base64')
+            from base64 import encodebytes as encode64
+            encoded_data = encode64(raw_bytes).decode(self.charset).replace('\n', '').strip()
+        else:
+            from urllib.parse import quote_from_bytes
+            encoded_data = quote_from_bytes(raw_bytes)
+        parts.extend([',', encoded_data])
+        return ''.join(parts)
+
+
+class Bin2DataURICrafter(File2DataURICrafter):
+    def __init__(self, mimetype: str, *args,
                  **kwargs):
-        """ Build MIME type document from binary raw_bytes. It takes binary input and converts it to MIME-specific data URI.
+        """ Build MIME type document from binary raw_bytes.
+        It takes binary input and converts it to MIME-specific data URI. Therefore :attr:`mimetype` must be given.
 
         :param mimetype: media type defined in https://en.wikipedia.org/wiki/Media_type you can also give a file extension
                         only and let it guess the mimetype
-        :param charset: charset may be any character set registered with IANA
-        :param base64: used to encode arbitrary octet sequences into a form that satisfies the rules of 7bit. Designed to be efficient for non-text 8 bit and binary data. Sometimes used for text data that frequently uses non-US-ASCII characters.
         :param args:
         :param kwargs:
         """
@@ -25,19 +65,5 @@ class DataURICrafter(BaseDocCrafter):
             raise NotImplementedError(f'i can not determine the MIME type from '
                                       f'mimetype={mimetype}, please check your input')
 
-        self.charset = charset
-        self.base64 = base64
-
     def craft(self, raw_bytes, *args, **kwargs):
-        parts = ['data:', self.mimetype]
-        if self.charset is not None:
-            parts.extend([';charset=', self.charset])
-        if self.base64:
-            parts.append(';base64')
-            from base64 import encodebytes as encode64
-            encoded_data = encode64(raw_bytes).decode(self.charset).replace('\n', '').strip()
-        else:
-            from urllib.parse import quote_from_bytes
-            encoded_data = quote_from_bytes(raw_bytes)
-        parts.extend([',', encoded_data])
-        return dict(data_uri=''.join(parts))
+        return dict(data_uri=self.make_datauri(self.mimetype, raw_bytes))
--- tests/executors/crafters/test_mime.py
+++ tests/executors/crafters/test_mime.py
@@ -4,26 +4,35 @@ from jina.flow import Flow
 from tests import JinaTestCase
 
 num_docs = 100
-image_src = '../../../**/*.png'
 
 
-def input_fn():
+def input_fn(pattern='../../../**/*.png'):
     idx = 0
-    for g in glob.glob(image_src, recursive=True)[:num_docs]:
+    for g in glob.glob(pattern, recursive=True)[:num_docs]:
         with open(g, 'rb') as fp:
             yield fp.read()
             idx += 1
 
 
+def input_fn2(pattern='../../*.*'):
+    for g in glob.glob(pattern, recursive=True)[:num_docs]:
+        yield g.encode()
+
+
 class MyTestCase(JinaTestCase):
     def test_dummy_seg(self):
-        f = Flow().add(yaml_path='!DataURICrafter\nwith: {mimetype: png}')
+        f = Flow().add(yaml_path='!Bin2DataURICrafter\nwith: {mimetype: png}')
         with f:
             f.index(input_fn=input_fn(), output_fn=print)
 
-        f = Flow().add(yaml_path='!DataURICrafter\nwith: {mimetype: png, base64: true}')
+        f = Flow().add(yaml_path='!Bin2DataURICrafter\nwith: {mimetype: png, base64: true}')
         with f:
             f.index(input_fn=input_fn(), output_fn=print)
+
+    def test_any_file(self):
+        f = Flow().add(yaml_path='!File2DataURICrafter\nwith: {base64: true}')
+        with f:
+            f.index(input_fn=input_fn2, output_fn=print)
 
     # def test_dummy_seg_random(self):
     #     f = Flow().add(yaml_path='../../yaml/dummy-seg-random.yml')
