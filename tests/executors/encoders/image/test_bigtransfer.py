276e1420bfddbb2f881a7a18659a5d716b3deff9
Han Xiao <han.xiao@jina.ai>
2020-05-24 23:03:36 +0200

    fix: fix typo in tests

--- tests/executors/encoders/image/test_bigtransfer.py
+++ tests/executors/encoders/image/test_bigtransfer.py
@@ -8,7 +8,7 @@ class MyTestCase(ImageTestCase):
     def _get_encoder(self, metas):
         self.target_output_dim = 8192
         self.input_dim = 48
-        return BitImageEncoder(
+        return BiTImageEncoder(
             model_path='/tmp/bit_models/Imagenet21k/R152x4/feature_vectors', channel_axis=1, metas=metas)
 
 
