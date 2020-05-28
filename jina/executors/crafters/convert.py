80adf9c98d3075dac9eacbd48a3b03349274c50a
Han Xiao <han.xiao@jina.ai>
2020-05-28 17:43:49 +0200

    refactor(helloworld): use data uri instead of metainfo

--- jina/executors/crafters/convert.py
+++ jina/executors/crafters/convert.py
@@ -1,9 +1,14 @@
 __copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
+import base64
 import os
+import struct
 import urllib.parse
 import urllib.request
+import zlib
+
+import numpy as np
 
 from . import BaseDocCrafter
 
@@ -81,3 +86,39 @@ class Buffer2DataURI(FilePath2DataURI):
 
     def craft(self, buffer: bytes, mime_type: str, *args, **kwargs):
         return dict(data_uri=self.make_datauri(mime_type, buffer))
+
+
+class NumpyBuffer2PNGDataURI(FilePath2DataURI):
+    """Simple DocCrafter used in :command:`jina hello-world`,
+        it reads ``buffer`` into base64 png and stored in ``data_uri``"""
+
+    def __init__(self, width: int = 28, height: int = 28, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.width = width
+        self.height = height
+
+    def craft(self, buffer: bytes, *args, **kwargs):
+        doc = np.frombuffer(buffer, dtype=np.uint8)
+        pixels = []
+        for p in doc[::-1]:
+            pixels.extend([255 - int(p), 255 - int(p), 255 - int(p), 255])
+        buf = bytearray(pixels)
+
+        # reverse the vertical line order and add null bytes at the start
+        width_byte_4 = self.width * 4
+        raw_data = b''.join(
+            b'\x00' + buf[span:span + width_byte_4]
+            for span in range((self.height - 1) * width_byte_4, -1, - width_byte_4))
+
+        def png_pack(png_tag, data):
+            chunk_head = png_tag + data
+            return (struct.pack('!I', len(data)) +
+                    chunk_head +
+                    struct.pack('!I', 0xFFFFFFFF & zlib.crc32(chunk_head)))
+
+        png_bytes = b''.join([
+            b'\x89PNG\r\n\x1a\n',
+            png_pack(b'IHDR', struct.pack('!2I5B', self.width, self.height, 8, 6, 0, 0, 0)),
+            png_pack(b'IDAT', zlib.compress(raw_data, 9)),
+            png_pack(b'IEND', b'')])
+        return dict(data_uri='data:image/png;base64,' + base64.b64encode(png_bytes).decode())
--- jina/helloworld/helper.py
+++ jina/helloworld/helper.py
@@ -1,13 +1,10 @@
 __copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
-import base64
 import gzip
 import os
-import struct
 import urllib.request
 import webbrowser
-import zlib
 
 import numpy as np
 from pkg_resources import resource_filename
@@ -20,32 +17,6 @@ from ..logging import default_logger
 def load_mnist(path):
     with gzip.open(path, 'rb') as fp:
         return np.frombuffer(fp.read(), dtype=np.uint8, offset=16).reshape([-1, 784])
-
-
-def write_png(buf, width=28, height=28):
-    pixels = []
-    for p in buf[::-1]:
-        pixels.extend([255 - int(p), 255 - int(p), 255 - int(p), 255])
-    buf = bytearray(pixels)
-
-    # reverse the vertical line order and add null bytes at the start
-    width_byte_4 = width * 4
-    raw_data = b''.join(
-        b'\x00' + buf[span:span + width_byte_4]
-        for span in range((height - 1) * width_byte_4, -1, - width_byte_4))
-
-    def png_pack(png_tag, data):
-        chunk_head = png_tag + data
-        return (struct.pack("!I", len(data)) +
-                chunk_head +
-                struct.pack("!I", 0xFFFFFFFF & zlib.crc32(chunk_head)))
-
-    png_bytes = b''.join([
-        b'\x89PNG\r\n\x1a\n',
-        png_pack(b'IHDR', struct.pack("!2I5B", width, height, 8, 6, 0, 0, 0)),
-        png_pack(b'IDAT', zlib.compress(raw_data, 9)),
-        png_pack(b'IEND', b'')])
-    return base64.b64encode(png_bytes)
 
 
 def input_fn(fp, index=True, num_doc=None):
@@ -66,10 +37,10 @@ result_html = []
 
 def print_result(resp):
     for d in resp.search.docs:
-        vi = 'data:image/png;base64,' + d.meta_info.decode()
+        vi = d.data_uri
         result_html.append(f'<tr><td><img src="{vi}"/></td><td>')
         for kk in d.topk_results:
-            kmi = 'data:image/png;base64,' + kk.match_doc.meta_info.decode()
+            kmi = kk.match_doc.data_uri
             result_html.append(f'<img src="{kmi}" style="opacity:{kk.score.value}"/>')
             # k['score']['explained'] = json.loads(kk.score.explained)
         result_html.append('</td></tr>\n')
