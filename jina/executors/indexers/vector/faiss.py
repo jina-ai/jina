54ecb422a997f5d6bff6b21f4a0a98b973a4ea18
Nan Wang <nan.wang@jina.ai>
2020-06-16 22:29:49 +0800

    fix: fix the query bug in faiss

--- jina/executors/indexers/vector/faiss.py
+++ jina/executors/indexers/vector/faiss.py
@@ -4,6 +4,7 @@ __license__ = "Apache-2.0"
 from typing import Tuple
 
 import numpy as np
+import gzip
 
 from .numpy import NumpyIndexer
 
@@ -19,28 +20,59 @@ class FaissIndexer(NumpyIndexer):
         Faiss package dependency is only required at the query time.
     """
 
-    def __init__(self, index_key: str, *args, **kwargs):
+    def __init__(self, index_key: str, train_filepath: str, *args, **kwargs):
         """
         Initialize an Faiss Indexer
 
         :param index_key: index type supported by ``faiss.index_factory``
+        :param train_filename: the training data
         """
         super().__init__(*args, **kwargs)
         self.index_key = index_key
+        self.train_filepath = train_filepath
+
+    def post_init(self):
+        super().post_init()
+        import faiss
+        self._index = faiss.index_factory(self.num_dim, self.index_key)
 
     def get_query_handler(self):
         """Load all vectors (in numpy ndarray) into Faiss indexers """
-        import faiss
-        data = super().get_query_handler()
-        _index = faiss.index_factory(self.num_dim, self.index_key)
-        _index.add(data)
-        return _index
+        if not self.is_trained:
+            _train_data = self._load_training_data(self.train_filepath)
+            if _train_data is None:
+                self.logger.warning('loading training data failed.')
+                return None
+            self.train(_train_data)
+        _index_data = super().get_query_handler()
+        if _index_data is None:
+            self.logger.warning('loading indexing data failed.')
+            return None
+        if _index_data.ndim != 2:
+            self.logger.warning('the index data should be 2D tensor, {} != 2'.format(_index_data.ndim))
+            return None
+        self._index.add(_index_data.astype('float32'))
+        return self._index
 
     def query(self, keys: 'np.ndarray', top_k: int, *args, **kwargs) -> Tuple['np.ndarray', 'np.ndarray']:
         if keys.dtype != np.float32:
             raise ValueError('vectors should be ndarray of float32')
-
         dist, ids = self.query_handler.search(keys, top_k)
-
-        # ids is already a numpy array
         return self.int2ext_key[ids], dist
+
+    def train(self, data: 'np.ndarray', *args, **kwargs):
+        _num_samples, _num_dim = data.shape
+        if not self.num_dim:
+            self.num_dim = _num_dim
+        if self.num_dim != _num_dim:
+            raise ValueError('training data should have the same number of features as the index, {} != {}'.format(
+                self.num_dim, _num_dim))
+        if _num_samples < 100 * _num_dim:
+            self.logger.warning(
+                'training sample size (={}) is suggested to be 10 * num_dim (={})'.format(
+                    _num_samples, self.num_dim))
+        self._index.train(data)
+        self.is_trained = True
+
+    def _load_training_data(self, train_filepath):
+        return self._load_numpy(train_filepath)
