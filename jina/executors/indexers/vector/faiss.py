5ec57d6dddf85c5edff632f620c3fdba35b27072
Nan Wang <nan.wang@jina.ai>
2020-06-16 23:20:39 +0800

    fix: fix the query bug in faiss

--- jina/executors/indexers/vector/faiss.py
+++ jina/executors/indexers/vector/faiss.py
@@ -31,19 +31,9 @@ class FaissIndexer(NumpyIndexer):
         self.index_key = index_key
         self.train_filepath = train_filepath
 
-    def post_init(self):
-        super().post_init()
-        import faiss
-        self._index = faiss.index_factory(self.num_dim, self.index_key)
-
     def get_query_handler(self):
         """Load all vectors (in numpy ndarray) into Faiss indexers """
-        if not self.is_trained:
-            _train_data = self._load_training_data(self.train_filepath)
-            if _train_data is None:
-                self.logger.warning('loading training data failed.')
-                return None
-            self.train(_train_data)
+        import faiss
         _index_data = super().get_query_handler()
         if _index_data is None:
             self.logger.warning('loading indexing data failed.')
@@ -51,6 +41,13 @@ class FaissIndexer(NumpyIndexer):
         if _index_data.ndim != 2:
             self.logger.warning('the index data should be 2D tensor, {} != 2'.format(_index_data.ndim))
             return None
+        self._index = faiss.index_factory(self.num_dim, self.index_key)
+        if not self.is_trained:
+            _train_data = self._load_training_data(self.train_filepath)
+            if _train_data is None:
+                self.logger.warning('loading training data failed.')
+                return None
+            self.train(_train_data)
         self._index.add(_index_data.astype('float32'))
         return self._index
 
