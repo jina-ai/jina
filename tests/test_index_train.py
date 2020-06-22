6e07549ef9407d785bbf9be295794288183e7b48
fhaase2 <44052928+fhaase2@users.noreply.github.com>
2020-06-22 19:23:32 +0200

    fix(indexer): is trained set on train call

--- tests/test_index_train.py
+++ tests/test_index_train.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-from jina.executors.indexers.vector.numpy import NumpyIndexer
+from jina.executors.indexers import BaseIndexer
 from tests import JinaTestCase
 
 
-class DummyIndexer(NumpyIndexer):
+class DummyIndexer(BaseIndexer):
 
     def train(self, data: 'np.ndarray', *args, **kwargs):
         pass
@@ -15,7 +15,7 @@ class MyTestCase(JinaTestCase):
     def test_calling_train_sets_is_trained(self):
         data = np.random.rand(1, 2)
         i = DummyIndexer(index_filename='test2.bin')
-        i.save()
+
         # Call train method on indexer
         i.train(data)
         # After train was called, is_trained must be true
