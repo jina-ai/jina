661f280287d623c88f4a8fda1d18e0b827cfee35
Han Xiao <han.xiao@jina.ai>
2020-06-25 20:13:27 +0200

    fix(driver): remove pub driver with post_hook

--- tests/test_hub.py
+++ tests/test_hub.py
@@ -1,9 +1,14 @@
+import os
+import unittest
+
 from jina.hubapi.docker import HubIO
 from jina.main.parser import set_hub_build_parser, set_hub_pushpull_parser
 from tests import JinaTestCase
 
 
 class MyTestCase(JinaTestCase):
+
+    @unittest.skipIf('GITHUB_WORKFLOW' in os.environ, 'skip the stdin test on github workflow')
     def test_hub_build_pull(self):
         args = set_hub_build_parser().parse_args(['./hub-mwu', '--pull', '--push'])
         HubIO(args).build()
