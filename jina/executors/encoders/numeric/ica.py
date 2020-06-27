2730422bd71a034d3f96b505655466becda9f213
root <root@localhost.localdomain>
2020-06-27 18:22:58 +0800

    Add ICA to numeric encoder

--- jina/executors/encoders/numeric/ica.py
+++ jina/executors/encoders/numeric/ica.py
@@ -11,7 +11,7 @@ class FastICAEncoder(BaseNumericEncoder):
     """
     :class:`FastICAEncoder` encodes data from an ndarray in size `B x T` into an ndarray in size `B x D`.
 
-    .. note::
+    .. note:: 
         :class:`FastICAEncoder` must be trained before calling ``encode()``.
     """
 
--- tests/executors/encoders/numeric/test_ica.py
+++ tests/executors/encoders/numeric/test_ica.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jun 26 20:35:11 2020
-
-@author: James
-"""
-
 import unittest
 
 import numpy as np
