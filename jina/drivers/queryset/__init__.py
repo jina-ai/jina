04b9a41617116360c82aa70890e5913201626682
Han Xiao <han.xiao@jina.ai>
2020-07-13 09:27:36 +0200

    revert(driver): remove if clause for #640

--- jina/drivers/queryset/__init__.py
+++ jina/drivers/queryset/__init__.py
@@ -1,9 +1,43 @@
-from typing import List
+from typing import Iterator, Optional
 
 from .. import BaseDriver
+from ...proto import jina_pb2
 
 
-class QuerySetDriver(BaseDriver):
+class BaseFilterDriver(BaseDriver):
+    def __init__(self, raw_filter=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if raw_filter:
+            self.driver_filter = self.parse(raw_filter)
+
+    def __call__(self, *args, **kwargs):
+        f = getattr(self.req, 'raw_filter', None)
+        query_filter = self.parse(f) if f else None
+        filtered = self.execute(self.req.docs, self.driver_filter, query_filter)
+        if filtered is None:
+            # filter is done in-place.
+            pass
+        elif isinstance(filtered, Iterator):
+            for d in filtered:
+                _d = self.req.docs.add()
+                _d.CopyFrom(d)
+
+    def parse(self, raw_filter):
+        raise NotImplementedError
+
+    def execute(self, docs: Iterator['jina_pb2.Document'], driver_filter, query_filter) -> Optional[
+        Iterator['jina_pb2.Document']]:
+        """
+
+        :param docs: the document to be filtered
+        :param driver_filter: the parsed filter assigned to the driver
+        :param query_filter: the parsed filter carried on the query
+        :return: either an iterator of document,
+        """
+        raise NotImplementedError
+
+
+class QuerySetDriver(BaseFilterDriver):
     """
     A QuerySet represents a collection of objects from your database.
     It can have zero, one or many filters. Filters narrow down the query results
@@ -13,30 +47,14 @@ class QuerySetDriver(BaseDriver):
     and a filter is a limiting clause such as WHERE or LIMIT.
     """
 
-    def __init__(self, queryset_raw: List[str] = None, combine_policy: str = 'append', *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if isinstance(queryset_raw, List) and queryset_raw:
-            self.queryset_raw = queryset_raw
-        elif isinstance(queryset_raw, str):
-            self.queryset_raw = [queryset_raw]
-        else:
-            self.queryset_raw = []
-        self.combine_policy = combine_policy
 
-    def __call__(self, *args, **kwargs):
-        q = getattr(self.req, 'queryset_raw', None)
-        if isinstance(q, str):
-            q = [q]
-        if q:
-            if self.combine_policy == 'append':
-                self.queryset_raw.extend(q)
-            elif self.combine_policy == 'prepend':
-                self.queryset_raw = q + self.queryset_raw
-            elif self.combine_policy == 'override':
-                self.queryset_raw = q
+class GraphQLDriver(BaseFilterDriver):
+    """
+    GraphQL as in-memory filter
+    """
 
-        for d in self.execute():
-            pass
 
-    def execute(self):
-        pass
+class CythonFilterDriver(BaseFilterDriver):
+    """
+    Cython implementation of the in-memory proto filter
+    """
