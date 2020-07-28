387f5ee313b18dfd4e72087b861f4835b23cbf56
Han Xiao <han.xiao@jina.ai>
2020-07-28 15:32:36 +0200

    test(flow): add test for double merging pathways

--- tests/unit/flow/test_flow_merge.py
+++ tests/unit/flow/test_flow_merge.py
@@ -1,5 +1,6 @@
 import os
 
+import pytest
 from jina.executors.crafters import BaseSegmenter
 from jina.flow import Flow
 from jina.proto import jina_pb2
@@ -23,18 +24,34 @@ class DummySegment(BaseSegmenter):
         return [dict(buffer=f'aa{self._label}'.encode()), dict(buffer=f'bb{self._label}'.encode())]
 
 
-class MyTestCase(JinaTestCase):
+class MergeFlowTest(JinaTestCase):
     def validate(self, req):
         chunk_ids = [c.id for d in req.index.docs for c in d.chunks]
         self.assertTrue(len(chunk_ids), len(set(chunk_ids)))
-        self.assertTrue(len(chunk_ids), 2)
+        self.assertTrue(len(chunk_ids), 8)
 
-    def test_dummy_seg(self):
+    @pytest.mark.skip('this should fail as explained in https://github.com/jina-ai/jina/pull/730')
+    def test_this_will_fail(self):
         f = (Flow().add(name='a11', uses='DummySegment')
              .add(name='a12', uses='DummySegment', needs='gateway')
              .add(name='r1', uses='_merge_all', needs=['a11', 'a12'])
              .add(name='a21', uses='DummySegment', needs='gateway')
              .add(name='a22', uses='DummySegment', needs='gateway')
+             .add(name='r2', uses='_merge_all', needs=['a21', 'a22'])
+             .add(uses='_merge_all', needs=['r1', 'r2']))
+
+        with f:
+            f.index(input_fn=random_docs(10), output_fn=self.validate)
+
+    def test_this_should_work(self):
+        f = (Flow()
+             .add(name='a1', uses='_forward')
+             .add(name='a11', uses='DummySegment', needs='a1')
+             .add(name='a12', uses='DummySegment', needs='a1')
+             .add(name='r1', uses='_merge_all', needs=['a11', 'a12'])
+             .add(name='a2', uses='_forward', needs='gateway')
+             .add(name='a21', uses='DummySegment', needs='a2')
+             .add(name='a22', uses='DummySegment', needs='a2')
              .add(name='r2', uses='_merge_all', needs=['a21', 'a22'])
              .add(uses='_merge_all', needs=['r1', 'r2']))
 
