33f667d1d89422b457b0b0c6f903f5a7cf86c9c8
Han Xiao <han.xiao@jina.ai>
2020-07-30 19:58:05 +0200

    feat(driver): improve efficiency of concat embed

--- tests/unit/drivers/test_concat_driver.py
+++ tests/unit/drivers/test_concat_driver.py
@@ -2,7 +2,7 @@ import os
 
 import numpy as np
 
-from jina.drivers.helper import array2pb
+from jina.drivers.helper import array2pb, pb2array
 from jina.flow import Flow
 from jina.proto.jina_pb2 import Document
 from tests import JinaTestCase
@@ -10,23 +10,35 @@ from tests import JinaTestCase
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 
 
-class ConcatDriverTestCase(JinaTestCase):
-    def test_concat(self):
-        def input_fn():
-            doc1 = Document()
-            doc1.id = 1
-            doc1.embedding.CopyFrom(array2pb(np.random.random([7])))
-            c = doc1.chunks.add()
-            c.id = 3
-            c.embedding.CopyFrom(array2pb(np.random.random([5])))
-            doc2 = Document()
-            doc2.id = 2
-            doc2.embedding.CopyFrom(array2pb(np.random.random([3])))
-            d = doc2.chunks.add()
-            d.id = 4
-            d.embedding.CopyFrom(array2pb(np.random.random([9])))
-            return [doc1, doc2]
+def input_fn():
+    doc1 = Document()
+    doc1.id = 1
+    doc1.embedding.CopyFrom(array2pb(np.random.random([7])))
+    c = doc1.chunks.add()
+    c.id = 3
+    c.embedding.CopyFrom(array2pb(np.random.random([5])))
+    doc2 = Document()
+    doc2.id = 2
+    doc2.embedding.CopyFrom(array2pb(np.random.random([3])))
+    d = doc2.chunks.add()
+    d.id = 4
+    d.embedding.CopyFrom(array2pb(np.random.random([9])))
+    return [doc1, doc2]
 
+
+class ConcatDriverTestCase(JinaTestCase):
+
+    def test_direct_concat(self):
+        doc1, doc2 = input_fn()
+
+        t1 = np.concatenate([pb2array(doc1.embedding), pb2array(doc2.embedding)], axis=0)
+        doc1.embedding.buffer += doc2.embedding.buffer
+        doc1.embedding.shape[0] += doc2.embedding.shape[0]
+        t2 = pb2array(doc1.embedding)
+
+        np.testing.assert_almost_equal(t1, t2)
+
+    def test_concat_embed_driver(self):
         def validate(req):
             self.assertEqual(len(req.docs), 2)
             self.assertEqual(req.docs[0].embedding.shape, [14])
