bea5e4652cf028c96b791900f3504d15a9498410
Joan Fontanals Martinez <joan.martinez@jina.ai>
2020-08-04 11:38:55 +0200

    feat(images): add location info to sliding window

--- jina/executors/crafters/image/segmenter.py
+++ jina/executors/crafters/image/segmenter.py
@@ -1,4 +1,4 @@
-from typing import Tuple, Dict, List, Union
+from typing import Tuple, Dict, List, Union, Generator
 
 import numpy as np
 
@@ -25,7 +25,7 @@ class RandomImageCropper(BaseSegmenter):
         """
         super().__init__(*args, **kwargs)
         self.target_size = target_size
-        self.num_pathes = num_patches
+        self.num_patches = num_patches
         self.channel_axis = channel_axis
 
     def craft(self, blob: 'np.ndarray', *args, **kwargs) -> List[Dict]:
@@ -37,11 +37,11 @@ class RandomImageCropper(BaseSegmenter):
         """
         raw_img = _load_image(blob, self.channel_axis)
         result = []
-        for i in range(self.num_pathes):
+        for i in range(self.num_patches):
             _img, top, left = _crop_image(raw_img, self.target_size, how='random')
             img = _restore_channel_axis(np.asarray(_img), self.channel_axis)
             result.append(
-                dict(offset=0, weight=1., blob=np.asarray(img).astype('float32')), location=(top, left))
+                dict(offset=0, weight=1., blob=np.asarray(img).astype('float32'), location=(top, left)))
         return result
 
 
@@ -81,19 +81,20 @@ class FiveImageCropper(BaseSegmenter):
             raise ValueError(f'target_size should be an integer or a tuple of two integers: {self.target_size}')
         _tl, top_tl, left_tl = _crop_image(raw_img, self.target_size, 0, 0)
         tl = _restore_channel_axis(np.asarray(_tl), self.channel_axis)
-        _tr, top_tr, left_tr = _crop_image(raw_img, self.target_size, image_width - target_w, 0)
+        _tr, top_tr, left_tr = _crop_image(raw_img, self.target_size, top=0, left=image_width - target_w)
         tr = _restore_channel_axis(np.asarray(_tr), self.channel_axis)
-        _bl, top_bl, left_bl = _crop_image(raw_img, self.target_size, 0, image_height - target_h)
+        _bl, top_bl, left_bl = _crop_image(raw_img, self.target_size, top=image_height - target_h, left=0)
         bl = _restore_channel_axis(np.asarray(_bl), self.channel_axis)
-        _br, top_br, left_br = _crop_image(raw_img, self.target_size, image_width - target_w, image_height - target_h)
+        _br, top_br, left_br = _crop_image(raw_img, self.target_size, top=image_height - target_h,
+                                           left=image_width - target_w)
         br = _restore_channel_axis(np.asarray(_br), self.channel_axis)
         _center, top_center, left_center = _crop_image(raw_img, self.target_size, how='center')
         center = _restore_channel_axis(np.asarray(_center), self.channel_axis)
         return [
             dict(offset=0, weight=1., blob=tl.astype('float32'), location=(top_tl, left_tl)),
             dict(offset=0, weight=1., blob=tr.astype('float32'), location=(top_tr, left_tr)),
-            dict(offset=0, weight=1., blob=bl.astype('float32'), location=(top_tr, left_tr)),
-            dict(offset=0, weight=1., blob=br.astype('float32'), location=(top_tr, left_tr)),
+            dict(offset=0, weight=1., blob=bl.astype('float32'), location=(top_bl, left_bl)),
+            dict(offset=0, weight=1., blob=br.astype('float32'), location=(top_br, left_br)),
             dict(offset=0, weight=1., blob=center.astype('float32'), location=(top_center, left_center)),
         ]
 
@@ -106,7 +107,7 @@ class SlidingWindowImageCropper(BaseSegmenter):
     def __init__(self,
                  target_size: int,
                  strides: Tuple[int, int],
-                 padding: str = 'VALID',
+                 padding: bool = False,
                  channel_axis: int = -1,
                  *args,
                  **kwargs):
@@ -116,10 +117,10 @@ class SlidingWindowImageCropper(BaseSegmenter):
             this. If size is an int, the output will have the same height and width as the `target_size`.
         :param strides: the strides between two neighboring sliding windows. `strides` is a sequence like (h, w), in
             which denote the strides on the vertical and the horizontal axis.
-        :param padding: If `VALID`, only patches which are fully contained in the input image are included. If `SAME`,
+        :param padding: If False, only patches which are fully contained in the input image are included. If True,
             all patches whose starting point is inside the input are included, and areas outside the input default to
             zero. The `padding` argument has no effect on the size of each patch, it determines how many patches are
-            extracted. Default is `VALID`.
+            extracted. Default is False.
         """
         super().__init__(*args, **kwargs)
         self.target_size = target_size
@@ -129,7 +130,7 @@ class SlidingWindowImageCropper(BaseSegmenter):
         self.padding = padding
         self.channel_axis = channel_axis
 
-    def _expand_img(self, img: 'np.ndarray') -> 'np.ndarray':
+    def _add_zero_padding(self, img: 'np.ndarray') -> 'np.ndarray':
         h, w, c = img.shape
         ext_h = self.target_size - h % self.stride_h
         ext_w = self.target_size - w % self.stride_w
@@ -137,6 +138,31 @@ class SlidingWindowImageCropper(BaseSegmenter):
                       ((0, ext_h), (0, ext_w), (0, 0)),
                       mode='constant',
                       constant_values=0)
+
+    def _location_generator_without_padding(self, img: 'np.ndarray') -> Generator[Tuple[int, int], None, None]:
+        height, width, _ = img.shape
+        top, left = (0, 0)
+        if isinstance(self.target_size, Tuple):
+            w_size, h_size = self.target_size
+        else:
+            w_size = h_size = self.target_size
+
+        while top + h_size <= height:
+            while left + w_size <= width:
+                yield top, left
+                left += self.stride_w
+            left = 0
+            top += self.stride_h
+
+    def _location_generator_with_padding(self, img: 'np.ndarray') -> Generator[Tuple[int, int], None, None]:
+        height, width, _ = img.shape
+        top, left = (0, 0)
+        while top <= height:
+            while left <= width:
+                yield top, left
+                left += self.stride_w
+            left = 0
+            top += self.stride_h
 
     def craft(self, blob: 'np.ndarray', *args, **kwargs) -> List[Dict]:
         """
@@ -147,29 +173,33 @@ class SlidingWindowImageCropper(BaseSegmenter):
         """
         raw_img = np.copy(blob)
         raw_img = _check_channel_axis(raw_img, self.channel_axis)
-        if self.padding == 'SAME':
-            raw_img = self._expand_img(blob)
+        if self.padding:
+            raw_img = self._add_zero_padding(blob)
         h, w, c = raw_img.shape
         row_step = raw_img.strides[0]
         col_step = raw_img.strides[1]
+
         expanded_img = np.lib.stride_tricks.as_strided(
             raw_img,
-            (
+            shape=(
                 1 + int((h - self.target_size) / self.stride_h),
                 1 + int((w - self.target_size) / self.stride_w),
                 self.target_size,
                 self.target_size,
                 c
-            ), (
+            ),
+            strides=(
                 row_step * self.stride_h,
                 col_step * self.stride_w,
                 row_step,
                 col_step,
-                1))
+                1), writeable=False)
         expanded_img = expanded_img.reshape((-1, self.target_size, self.target_size, c))
+        location_generator = self._location_generator_with_padding(raw_img) if self.padding \
+            else self._location_generator_without_padding(raw_img)
+
         results = []
-        for _blob in expanded_img:
+        for location, _blob in zip(location_generator, expanded_img):
             blob = _restore_channel_axis(_blob, self.channel_axis)
-            # TODO add location
-            results.append(dict(offset=0, weight=1.0, blob=blob.astype('float32')))
+            results.append(dict(offset=0, weight=1.0, blob=blob.astype('float32'), location=location))
         return results
--- tests/unit/executors/crafters/image/test_crop.py
+++ tests/unit/executors/crafters/image/test_crop.py
@@ -12,18 +12,22 @@ class ImageCropTestCase(JinaImageTestCase):
         width = 20
         height = 20
         crafter = ImageCropper(top=top, left=left, width=width, height=height)
-        crafted_chunk = crafter.craft(img_array, 0, 0)
+        crafted_doc = crafter.craft(img_array)
         np.testing.assert_array_equal(
-            crafted_chunk['blob'], np.asarray(img_array[top:top + height, left:left + width, :]),
+            crafted_doc['blob'], np.asarray(img_array[top:top + height, left:left + width, :]),
             'img_array: {}\ntest: {}\ncontrol:{}'.format(
                 img_array.shape,
-                crafted_chunk['blob'].shape,
+                crafted_doc['blob'].shape,
                 np.asarray(img_array[left:left + width, top:top + height, :]).shape))
+        self.assertEqual(crafted_doc['location'], (top, left))
 
     def test_center_crop(self):
         img_size = 217
         img_array = self.create_random_img_array(img_size, img_size)
         output_dim = 20
         crafter = CenterImageCropper(output_dim)
-        crafted_chunk = crafter.craft(img_array, 0, 0)
-        self.assertEqual(crafted_chunk["blob"].shape, (20, 20, 3))
+        crafted_doc = crafter.craft(img_array)
+        self.assertEqual(crafted_doc['blob'].shape, (20, 20, 3))
+        # int((img_size - output_dim) / 2)
+        (top, left) = (98, 98)
+        self.assertEqual(crafted_doc['location'], (top, left))
