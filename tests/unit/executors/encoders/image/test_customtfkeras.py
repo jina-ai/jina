6e2ed943a1d9bc97ed47be867caba6b9f9f7b554
Han Xiao <han.xiao@jina.ai>
2020-08-05 17:05:00 +0200

    fix(encoder): rename tfkeras to keras

--- tests/unit/executors/encoders/image/test_customtfkeras.py
+++ tests/unit/executors/encoders/image/test_customtfkeras.py
@@ -1,6 +1,6 @@
 import tempfile
 
-from jina.executors.encoders.image.tfkeras import CustomKerasImageEncoder
+from jina.executors.encoders.image.keras import CustomKerasImageEncoder
 from tests.unit.executors.encoders.image import ImageTestCase
 
 
