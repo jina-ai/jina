533a2234def2ff92030af0e032fa53de97c28444
Maanav Shah <shah.maanav.07@gmail.com>
2020-08-07 22:15:49 +0530

    fix(crafter): modify flip image crafters

--- jina/executors/crafters/image/flip.py
+++ jina/executors/crafters/image/flip.py
@@ -3,58 +3,37 @@ from typing import Dict
 import numpy as np
 
 from .. import BaseCrafter
-from .helper import _restore_channel_axis, _load_image
+from .helper import _load_image, _move_channel_axis
 
 
 class ImageFlipper(BaseCrafter):
     """
-    :class:`ImageFlipper` is an abstract class for horizonal and vertical image flip.
-
-    The apply function is :func:`_flip_image`, where the image will be flipped.
+    :class:`ImageFlipper` flips the image horizontally or vertically. Flip image in the left/right or up/down direction respectively.
     """
 
-    def __init__(self, channel_axis: int = -1, *args, **kwargs):
+    def __init__(self,
+                 vertical: bool = False,
+                 channel_axis: int = -1,
+                 *args,
+                 **kwargs):
         """
 
+        :param vertical: desired rotation type. ``True`` indicates the image should be flipped vertically.
         :param channel_axis: the axis id of the color channel, ``-1`` indicates the color channel info at the last axis
         """
         super().__init__(*args, **kwargs)
+        self.vertical = vertical
         self.channel_axis = channel_axis
 
     def craft(self, blob: 'np.ndarray', *args, **kwargs) -> Dict:
         """
-        Flip the input image array.
+        Flip the input image array horizontally or vertically.
 
         :param blob: the ndarray of the image with the color channel at the last axis
         :return: a chunk dict with the mage
         """
         raw_img = _load_image(blob, self.channel_axis)
-        _img = self._flip_image(raw_img)
-        img = _restore_channel_axis(_img, self.channel_axis)
+        img = np.array(raw_img).astype('float32')
+        img = np.flipud(img) if self.vertical else np.fliplr(img)
+        img = _move_channel_axis(img, -1, self.channel_axis)
         return dict(offset=0, weight=1., blob=img)
-
-    def _flip_image(self, img):
-        """The apply function of this executor.
-
-        """
-        raise NotImplementedError
-
-
-class ImageHorizontalFlipper(ImageFlipper):
-    """
-    :class:`ImageHorizontalFlipper` flips the image horizontally. Flip image in the left/right direction respectively.
-    """
-
-    def _flip_image(self, img):
-        img = np.array(img).astype('float32')
-        return np.fliplr(img)
-
-
-class ImageVerticalFlipper(ImageFlipper):
-    """
-    :class:`ImageVerticalFlipper` flips the image vertically. Flip image in the up/down direction.
-    """
-
-    def _flip_image(self, img):
-        img = np.array(img).astype('float32')
-        return np.flipud(img)
--- tests/unit/executors/crafters/image/test_flip.py
+++ tests/unit/executors/crafters/image/test_flip.py
@@ -1,14 +1,13 @@
-from jina.executors.crafters.image.flip import ImageHorizontalFlipper
-from jina.executors.crafters.image.flip import ImageVerticalFlipper
+from jina.executors.crafters.image.flip import ImageFlipper
 from tests.unit.executors.crafters.image import JinaImageTestCase
 
 import numpy as np
 
 
-class ImageHorizontalFlipperTestCase(JinaImageTestCase):
+class ImageFlipperTestCase(JinaImageTestCase):
     def test_horizontal_flip(self):
         img_size = 217
-        crafter = ImageHorizontalFlipper()
+        crafter = ImageFlipper()
         # generates a random image array of size (217, 217)
         img_array = self.create_random_img_array(img_size, img_size)
         crafted_chunk = crafter.craft(img_array)
@@ -17,11 +16,9 @@ class ImageHorizontalFlipperTestCase(JinaImageTestCase):
         # assert flipped image using numpy's fliplr method
         np.testing.assert_equal(crafted_chunk['blob'], flip_img_array)
 
-
-class ImageVerticalFlipperTestCase(JinaImageTestCase):
     def test_vertical_flip(self):
         img_size = 217
-        crafter = ImageVerticalFlipper(vertical=True)
+        crafter = ImageFlipper(vertical=True)
         # generates a random image array of size (217, 217)
         img_array = self.create_random_img_array(img_size, img_size)
         crafted_chunk = crafter.craft(img_array)
