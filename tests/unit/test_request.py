51a16411a842dd92250850e24c05d230e29129ca
Kavan72 <vadaliya.kavan@gmail.com>
2020-08-07 14:00:12 +0530

    test(request): add request generator test

---
+++ tests/unit/test_request.py
@@ -0,0 +1,15 @@
+from jina.clients.python.request import _generate
+from tests import JinaTestCase
+
+
+class RequestTestCase(JinaTestCase):
+
+    def test_request_generate(self):
+
+        def random_lines(num_lines):
+            for j in range(num_lines):
+                yield "https://github.com 'i\'m dummy doc %d'" % j
+
+        req = _generate(data=random_lines(100), batch_size=100)
+
+        self.assertEqual(len(list(req)), 1)
