6d691d457ab353f34915b8f24d3a46337cb9aed5
JoanFM <joan.martinez@jina.ai>
2020-08-19 18:35:31 +0200

    test: working test of different level depth

--- tests/integration/level_depth/test_search_different_depths.py
+++ tests/integration/level_depth/test_search_different_depths.py
@@ -27,20 +27,27 @@ class SearchDifferentLevelDepthTestCase(JinaTestCase):
             index_flow.index(index_data)
 
         def validate_level_depth_1(resp):
-            print(f'JOAN RESP: \n {resp}')
             self.assertEqual(len(resp.docs), 3)
             for doc in resp.docs:
                 self.assertEqual(doc.level_depth, 1)
                 self.assertEqual(len(doc.matches), 1)
                 self.assertEqual(doc.matches[0].id, doc.id)  # done on purpose
+                # self.assertEqual(doc.matches[0].level_depth, 0)  # tconceptually is like this, but we impose that match has same level as its matched
+
+            self.assertEqual(resp.docs[0].text, 'I am chunk 1 of doc 1,')
+            self.assertEqual(resp.docs[0].matches[0].text, 'I am chunk 0 of doc 1, I am chunk 1 of doc 1, I am chunk 2 of doc 1')
+
+            self.assertEqual(resp.docs[1].text, 'I am chunk 0 of doc 2,')
+            self.assertEqual(resp.docs[1].matches[0].text, 'I am chunk 0 of doc 2, I am chunk 1 of doc 2')
+
+            self.assertEqual(resp.docs[2].text, 'I am chunk 3 of doc 3')
+            self.assertEqual(resp.docs[2].matches[0].text, 'I am chunk 0 of doc 3, I am chunk 1 of doc 3, I am chunk 2 of doc 3, I am chunk 3 of doc 3')
 
         search_data = [
             'I am chunk 1 of doc 1,',
             'I am chunk 0 of doc 2,',
             'I am chunk 3 of doc 3',
         ]
-
-        print(f'********************** SEARCH ***********************************')
 
         search_flow = Flow().load_config(os.path.join(cur_dir, 'flow-query.yml'))
         with search_flow:
