8a2ed8cfacc710ab6cdafea3395f54dd510d8205
Han Xiao <han.xiao@jina.ai>
2020-08-20 18:47:34 +0200

    fix(test): update import to core

--- tests/unit/test_index.py
+++ tests/unit/test_index.py
@@ -6,7 +6,7 @@ import unittest
 import numpy as np
 
 from jina.enums import FlowOptimizeLevel
-from jina.hub.indexers.vector.NumpyIndexer import NumpyIndexer
+from jina.executors.indexers.vector import NumpyIndexer
 from jina.flow import Flow
 from jina.main.parser import set_flow_parser
 from jina.proto import jina_pb2
