b62c6fff15aa0f4f63f5fa28f1247ab07a058dbc
Joan Fontanals Martinez <joan.martinez@jina.ai>
2020-08-20 12:50:23 +0200

    refactor: remove tests migrated to hub

--- tests/unit/executors/rankers/test_bm25.py
+++
@@ -1,8 +0,0 @@
-from jina.hub.rankers.tfidf import BM25Ranker
-from tests.unit.executors.rankers import RankerTestCase
-
-
-class BM25TestCase(RankerTestCase):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.ranker = BM25Ranker()
