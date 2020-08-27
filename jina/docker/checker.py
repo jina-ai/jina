200d0b0b5cac41c4a9be669bcb1121db7d685786
Nan Wang <nan.wang@jina.ai>
2020-08-27 18:26:29 +0800

    fix: minor fix

--- jina/docker/checker.py
+++ jina/docker/checker.py
@@ -15,7 +15,7 @@ sver_regex = r'^(=|>=|<=|=>|=<|>|<|!=|~|~>|\^)?(?P<major>0|[1-9]\d*)\.(?P<minor>
              r'(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+' \
              r'(?:\.[0-9a-zA-Z-]+)*))?$'
 name_regex = r'^[a-zA-Z_$][a-zA-Z_\s\-$0-9]{2,30}$'
-excepts_regex = r'[\s\b](error)|(failed)\b'
+excepts_regex = r'\berror\b|\bfailed\b'
 
 
 def check_registry(registry, name, repo_prefix):
