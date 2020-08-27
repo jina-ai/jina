66dc58796e82701e6381e570fb48870c827faff0
Han Xiao <han.xiao@jina.ai>
2020-08-27 17:42:57 +0200

    fix(import): more friendly warning

--- tests/unit/executors/encoders/nlp/test_use.py
+++
@@ -1,7 +0,0 @@
-from jina.hub.encoders.nlp.use import UniversalSentenceEncoder
-from tests.unit.executors.encoders.nlp import NlpTestCase
-
-
-class UniversalSentenceEncoderTestCase(NlpTestCase):
-    def _get_encoder(self, metas):
-        return UniversalSentenceEncoder(metas=metas)
