27c3caf389608daa0dd152fe51849078f0e3630b
Han Xiao <han.xiao@jina.ai>
2020-09-01 18:48:03 +0200

    refactor: add adjacency to doc pb definiton

--- jina/__init__.py
+++ jina/__init__.py
@@ -8,7 +8,7 @@ __version__ = '0.5.1'
 
 # do not change this line manually
 # this is managed by proto/build-proto.sh and updated on every execution
-__proto_version__ = '0.0.58'
+__proto_version__ = '0.0.59'
 
 import platform
 import sys
