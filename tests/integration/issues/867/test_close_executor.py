85f61accb67dc3e68f676af177252ce29a5eaef7
Joan Fontanals Martinez <joan.martinez@jina.ai>
2020-09-07 15:35:20 +0200

    test: improve issue 867 testing

--- tests/integration/issues/867/test_close_executor.py
+++ tests/integration/issues/867/test_close_executor.py
@@ -1,10 +1,16 @@
 import time
 import os
+import pytest
+from mock import patch
+from jina.excepts import BadPersistantFile
+from jina.peapods.pea import BasePea
 from jina.executors import BaseExecutor
 from jina.flow import Flow
 
+
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 save_abs_path = os.path.join(cur_dir, 'slow-save-executor.bin')
+
 
 class SlowSaveExecutor(BaseExecutor):
     """
@@ -45,4 +51,37 @@ def test_close_and_load_executor():
     assert hasattr(exec, 'test')
     assert exec.test == 10
     assert exec.save_abspath == save_abs_path
+    os.remove(save_abs_path)
+
+
+class OldErrorPea(BasePea):
+    """
+    This Pea tries to simulate the behavior of Pea before issue was fixed
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.daemon = True
+
+    def loop_teardown(self):
+        """Stop the request loop """
+        if hasattr(self, 'executor'):
+            if not self.args.exit_no_dump:
+                self.save_executor(dump_interval=0)
+            self.executor.close()
+        if hasattr(self, 'zmqlet'):
+            self.zmqlet.close()
+
+    def _handle_terminate_signal(self, msg):
+        self.zmqlet.send_message(msg)
+        self.zmqlet.close()
+
+
+@patch(target='jina.peapods.pea.BasePea', new=OldErrorPea)
+def test_close_and_load_executor_daemon_failed():
+    with Flow().add(uses='!SlowSaveExecutor', daemon=True).build() as f:
+        pass
+
+    with pytest.raises(BadPersistantFile):
+        BaseExecutor.load(save_abs_path)
+
     os.remove(save_abs_path)
