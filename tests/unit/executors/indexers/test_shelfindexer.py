c4534033f830b2be34e102113bdea97f4411268e
Han Xiao <han.xiao@jina.ai>
2020-09-10 11:42:38 +0200

    feat(indexer): add shelfpbindexer for data persistance

--- tests/unit/executors/indexers/test_shelfindexer.py
+++ tests/unit/executors/indexers/test_shelfindexer.py
@@ -5,26 +5,28 @@ from tests import random_docs
 
 docs = list(random_docs(10))
 
-with ShelfPbIndexer('test-shelf') as spi:
-    spi.add(docs)
 
-with ShelfPbIndexer('test-shelf') as spi:
-    assert spi.query(1) == docs[1]
-    assert spi.query(11) is None
-    print(spi.query(1))
+def test_shelf():
+    with ShelfPbIndexer('test-shelf') as spi:
+        spi.add(docs)
 
-f = Flow(callback_on_body=True).add(uses='shelfpb.yml')
-
-with f:
-    f.index(docs)
-
-d = jina_pb2.Document()
-d.id = 1
+    with ShelfPbIndexer('test-shelf') as spi:
+        assert spi.query(1) == docs[1]
+        assert spi.query(11) is None
+        print(spi.query(1))
 
 
-def validate(req):
-    assert req.docs[0].embedding == docs[0].embedding
+def test_shelf_in_flow():
+    f = Flow(callback_on_body=True).add(uses='shelfpb.yml')
 
+    with f:
+        f.index(docs)
 
-with f:
-    f.search([d], output_fn=validate)
+    d = jina_pb2.Document()
+    d.id = 1
+
+    def validate(req):
+        assert req.docs[0].embedding == docs[0].embedding
+
+    with f:
+        f.search([d], output_fn=validate)
