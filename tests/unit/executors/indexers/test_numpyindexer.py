dbb82cb2b3826df0d9e6ffa4e2431c32b3125f21
Han Xiao <han.xiao@jina.ai>
2020-09-15 18:10:45 +0200

    feat(indexer): add memmap numpy indexer

--- tests/unit/executors/indexers/test_numpyindexer.py
+++ tests/unit/executors/indexers/test_numpyindexer.py
@@ -1,26 +1,28 @@
 import os
-import shutil
 
 import numpy as np
+import pytest
 
 from jina.executors.indexers import BaseIndexer
-from jina.executors.indexers.mmap import MmapNumpyIndexer
 from jina.executors.indexers.vector import NumpyIndexer
-
 # fix the seed here
 from tests import rm_files
 
 np.random.seed(500)
 retr_idx = None
-vec_idx = np.random.randint(0, high=100, size=[10])
-vec = np.random.random([10, 10])
-query = np.array(np.random.random([10, 10]), dtype=np.float32)
+num_data = 100
+num_dim = 64
+num_query = 10
+vec_idx = np.random.randint(0, high=num_data, size=[num_data])
+vec = np.random.random([num_data, num_dim])
+query = np.array(np.random.random([num_query, num_dim]), dtype=np.float32)
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 
 
-
-def test_numpy_indexer():
-    with NumpyIndexer(index_filename='np.test.gz') as indexer:
+@pytest.mark.parametrize('batch_size, compress_level', [(None, 0), (None, 1), (2, 0), (2, 1)])
+def test_numpy_indexer(batch_size, compress_level):
+    with NumpyIndexer(index_filename='np.test.gz', compress_level=compress_level) as indexer:
+        indexer.batch_size = batch_size
         indexer.add(vec_idx, vec)
         indexer.save()
         assert os.path.exists(indexer.index_abspath)
@@ -36,46 +38,20 @@ def test_numpy_indexer():
         else:
             np.testing.assert_almost_equal(retr_idx, idx)
         assert idx.shape == dist.shape
-        assert idx.shape == (10, 4)
+        assert idx.shape == (num_query, 4)
 
     rm_files([index_abspath, save_abspath])
 
 
-def test_memnumpy_indexer_known():
+@pytest.mark.parametrize('batch_size, compress_level', [(None, 0), (None, 1), (16, 0), (16, 1)])
+def test_numpy_indexer_known(batch_size, compress_level):
     vectors = np.array([[1, 1, 1],
                         [10, 10, 10],
                         [100, 100, 100],
                         [1000, 1000, 1000]])
     keys = np.array([4, 5, 6, 7]).reshape(-1, 1)
-    with MmapNumpyIndexer(index_filename='np.test.gz') as indexer:
-        indexer.add(keys, vectors)
-        indexer.save()
-        assert os.path.exists(indexer.index_abspath)
-        index_abspath = indexer.index_abspath
-        save_abspath = indexer.save_abspath
-
-    queries = np.array([[1, 1, 1],
-                        [10, 10, 10],
-                        [100, 100, 100],
-                        [1000, 1000, 1000]])
-    with BaseIndexer.load(save_abspath) as indexer:
-        assert isinstance(indexer, MmapNumpyIndexer)
-        idx, dist = indexer.query(queries, top_k=2)
-        np.testing.assert_equal(idx, np.array([[4, 5], [5, 4], [6, 5], [7, 6]]))
-        assert idx.shape == dist.shape
-        assert idx.shape == (4, 2)
-        np.testing.assert_equal(indexer.query_by_id([7, 4]), vectors[[3, 0]])
-
-    rm_files([index_abspath, save_abspath])
-
-
-def test_numpy_indexer_known():
-    vectors = np.array([[1, 1, 1],
-                        [10, 10, 10],
-                        [100, 100, 100],
-                        [1000, 1000, 1000]])
-    keys = np.array([4, 5, 6, 7]).reshape(-1, 1)
-    with NumpyIndexer(index_filename='np.test.gz') as indexer:
+    with NumpyIndexer(index_filename='np.test.gz', compress_level=compress_level) as indexer:
+        indexer.batch_size = batch_size
         indexer.add(keys, vectors)
         indexer.save()
         assert os.path.exists(indexer.index_abspath)
@@ -97,8 +73,10 @@ def test_numpy_indexer_known():
     rm_files([index_abspath, save_abspath])
 
 
-def test_scipy_indexer():
-    with NumpyIndexer(index_filename='np.test.gz', backend='scipy') as indexer:
+@pytest.mark.parametrize('batch_size, compress_level', [(None, 0), (None, 1), (16, 0), (16, 1)])
+def test_scipy_indexer(batch_size, compress_level):
+    with NumpyIndexer(index_filename='np.test.gz', backend='scipy', compress_level=compress_level) as indexer:
+        indexer.batch_size = batch_size
         indexer.add(vec_idx, vec)
         indexer.save()
         assert os.path.exists(indexer.index_abspath)
@@ -114,6 +92,6 @@ def test_scipy_indexer():
         else:
             np.testing.assert_almost_equal(retr_idx, idx)
         assert idx.shape == dist.shape
-        assert idx.shape == (10, 4)
+        assert idx.shape == (num_query, 4)
 
     rm_files([index_abspath, save_abspath])
