02425806d8be48b709d4eebe7c620982bc57f3de
Joan Fontanals Martinez <joan.martinez@jina.ai>
2020-09-15 12:18:17 +0200

    test: add more tests to issue936

--- tests/integration/issues/issue936/test_flow_with_sse_no_deadlock.py
+++ tests/integration/issues/issue936/test_flow_with_sse_no_deadlock.py
@@ -2,6 +2,7 @@ import pytest
 from jina.flow import Flow
 from jina.peapods import Pod
 from jina.main.parser import set_pod_parser
+import time
 
 """
 Github issue: https://github.com/jina-ai/jina/issues/936
@@ -30,21 +31,45 @@ def test_flow_with_sse_no_deadlock_one_pod():
     f = Flow(logserver=True). \
         add(uses='BaseExecutor', parallel=1, name='crafter')
     with f:
+        time.sleep(1)
         assert hasattr(f, '_sse_logger')
         pass
 
 
 @pytest.mark.repeat(10)
 def test_pod_with_sse_no_deadlock():
-    args = set_pod_parser().parse_args(['--parallel', '4', '--log-sse'])
+    args = set_pod_parser().parse_args(['--parallel', '2', '--log-sse'])
     p = Pod(args)
     with p:
+        time.sleep(1)
         pass
 
 
 @pytest.mark.repeat(10)
-def test_pod_with_sse_no_deadlock_thread():
-    args = set_pod_parser().parse_args(['--parallel', '4', '--runtime', 'thread', '--log-sse'])
+def test_pod_with_sse_no_deadlock_log_remote():
+    args = set_pod_parser().parse_args(['--parallel', '2', '--log-remote'])
     p = Pod(args)
     with p:
+        time.sleep(1)
+        pass
+
+
+@pytest.mark.repeat(10)
+def test_pod_with_sse_no_deadlock_log_file():
+    import os
+    os.environ['JINA_LOG_FILE'] = 'TXT'
+    args = set_pod_parser().parse_args(['--parallel', '2'])
+    p = Pod(args)
+    with p:
+        time.sleep(1)
+        pass
+    del os.environ['JINA_LOG_FILE']
+
+
+@pytest.mark.repeat(10)
+def test_pod_with_sse_no_deadlock_thread():
+    args = set_pod_parser().parse_args(['--parallel', '2', '--runtime', 'thread', '--log-sse'])
+    p = Pod(args)
+    with p:
+        time.sleep(1)
         pass
