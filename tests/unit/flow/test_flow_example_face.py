6cef93b7d45e35e01d59c8125b07c898f62f4424
Han Xiao <han.xiao@jina.ai>
2020-09-16 17:37:33 +0200

    test: refactor flow test with pytest format

--- tests/unit/flow/test_flow_example_face.py
+++ tests/unit/flow/test_flow_example_face.py
@@ -2,145 +2,143 @@ import os
 
 from jina.enums import SocketType
 from jina.flow import Flow
-from tests import JinaTestCase
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 
 
-class FlowFacesExamplesTestCase(JinaTestCase):
+def test_index():
+    f = Flow.load_config('../yaml/examples/faces/flow-index.yml')
+    with f:
+        node = f._pod_nodes['gateway']
+        assert node.head_args.socket_in == SocketType.PULL_CONNECT
+        assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
 
-    def test_index(self):
-        f = Flow.load_config('../yaml/examples/faces/flow-index.yml')
-        with f:
-            node = f._pod_nodes['gateway']
-            assert node.head_args.socket_in == SocketType.PULL_CONNECT
-            assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
+        node = f._pod_nodes['loader']
+        assert node.head_args.socket_in == SocketType.PULL_BIND
+        assert node.head_args.socket_out == SocketType.ROUTER_BIND
+        for arg in node.peas_args['peas']:
+            assert arg.socket_in == SocketType.DEALER_CONNECT
+            assert arg.socket_out == SocketType.PUSH_CONNECT
+        assert node.tail_args.socket_in == SocketType.PULL_BIND
+        assert node.tail_args.socket_out == SocketType.PUB_BIND
 
-            node = f._pod_nodes['loader']
-            assert node.head_args.socket_in == SocketType.PULL_BIND
-            assert node.head_args.socket_out == SocketType.ROUTER_BIND
-            for arg in node.peas_args['peas']:
-                assert arg.socket_in == SocketType.DEALER_CONNECT
-                assert arg.socket_out == SocketType.PUSH_CONNECT
-            assert node.tail_args.socket_in == SocketType.PULL_BIND
-            assert node.tail_args.socket_out == SocketType.PUB_BIND
+        node = f._pod_nodes['flipper']
+        assert node.head_args.socket_in == SocketType.SUB_CONNECT
+        assert node.head_args.socket_out == SocketType.ROUTER_BIND
+        for arg in node.peas_args['peas']:
+            assert arg.socket_in == SocketType.DEALER_CONNECT
+            assert arg.socket_out == SocketType.PUSH_CONNECT
+        assert node.tail_args.socket_in == SocketType.PULL_BIND
+        assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
 
-            node = f._pod_nodes['flipper']
-            assert node.head_args.socket_in == SocketType.SUB_CONNECT
-            assert node.head_args.socket_out == SocketType.ROUTER_BIND
-            for arg in node.peas_args['peas']:
-                assert arg.socket_in == SocketType.DEALER_CONNECT
-                assert arg.socket_out == SocketType.PUSH_CONNECT
-            assert node.tail_args.socket_in == SocketType.PULL_BIND
-            assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
+        node = f._pod_nodes['normalizer']
+        assert node.head_args.socket_in == SocketType.PULL_BIND
+        assert node.head_args.socket_out == SocketType.ROUTER_BIND
+        for arg in node.peas_args['peas']:
+            assert arg.socket_in == SocketType.DEALER_CONNECT
+            assert arg.socket_out == SocketType.PUSH_CONNECT
+        assert node.tail_args.socket_in == SocketType.PULL_BIND
+        assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
 
-            node = f._pod_nodes['normalizer']
-            assert node.head_args.socket_in == SocketType.PULL_BIND
-            assert node.head_args.socket_out == SocketType.ROUTER_BIND
-            for arg in node.peas_args['peas']:
-                assert arg.socket_in == SocketType.DEALER_CONNECT
-                assert arg.socket_out == SocketType.PUSH_CONNECT
-            assert node.tail_args.socket_in == SocketType.PULL_BIND
-            assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
+        node = f._pod_nodes['encoder']
+        assert node.head_args.socket_in == SocketType.PULL_BIND
+        assert node.head_args.socket_out == SocketType.ROUTER_BIND
+        for arg in node.peas_args['peas']:
+            assert arg.socket_in == SocketType.DEALER_CONNECT
+            assert arg.socket_out == SocketType.PUSH_CONNECT
+        assert node.tail_args.socket_in == SocketType.PULL_BIND
+        assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
 
-            node = f._pod_nodes['encoder']
-            assert node.head_args.socket_in == SocketType.PULL_BIND
-            assert node.head_args.socket_out == SocketType.ROUTER_BIND
-            for arg in node.peas_args['peas']:
-                assert arg.socket_in == SocketType.DEALER_CONNECT
-                assert arg.socket_out == SocketType.PUSH_CONNECT
-            assert node.tail_args.socket_in == SocketType.PULL_BIND
-            assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
+        node = f._pod_nodes['chunk_indexer']
+        assert node.head_args.socket_in == SocketType.PULL_BIND
+        assert node.head_args.socket_out == SocketType.PUSH_CONNECT
+        assert node.peas_args['peas'][0].socket_in == node.head_args.socket_in
+        assert node.peas_args['peas'][0].socket_out == node.head_args.socket_out
+        assert node.tail_args.socket_in == SocketType.PULL_BIND
+        assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
 
-            node = f._pod_nodes['chunk_indexer']
-            assert node.head_args.socket_in == SocketType.PULL_BIND
-            assert node.head_args.socket_out == SocketType.PUSH_CONNECT
-            assert node.peas_args['peas'][0].socket_in == node.head_args.socket_in
-            assert node.peas_args['peas'][0].socket_out == node.head_args.socket_out
-            assert node.tail_args.socket_in == SocketType.PULL_BIND
-            assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
+        node = f._pod_nodes['doc_indexer']
+        assert node.head_args.socket_in == SocketType.SUB_CONNECT
+        assert node.head_args.socket_out == SocketType.PUSH_CONNECT
+        assert node.peas_args['peas'][0].socket_in == node.head_args.socket_in
+        assert node.peas_args['peas'][0].socket_out == node.head_args.socket_out
+        assert node.tail_args.socket_in == SocketType.SUB_CONNECT
+        assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
 
-            node = f._pod_nodes['doc_indexer']
-            assert node.head_args.socket_in == SocketType.SUB_CONNECT
-            assert node.head_args.socket_out == SocketType.PUSH_CONNECT
-            assert node.peas_args['peas'][0].socket_in == node.head_args.socket_in
-            assert node.peas_args['peas'][0].socket_out == node.head_args.socket_out
-            assert node.tail_args.socket_in == SocketType.SUB_CONNECT
-            assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
+        node = f._pod_nodes['join_all']
+        assert node.head_args.socket_in == SocketType.PULL_BIND
+        assert node.head_args.socket_out == SocketType.PUSH_BIND
+        assert node.peas_args['peas'][0].socket_in == node.head_args.socket_in
+        assert node.peas_args['peas'][0].socket_out == node.head_args.socket_out
+        assert node.tail_args.socket_in == SocketType.PULL_BIND
+        assert node.tail_args.socket_out == SocketType.PUSH_BIND
+        f.dry_run()
 
-            node = f._pod_nodes['join_all']
-            assert node.head_args.socket_in == SocketType.PULL_BIND
-            assert node.head_args.socket_out == SocketType.PUSH_BIND
-            assert node.peas_args['peas'][0].socket_in == node.head_args.socket_in
-            assert node.peas_args['peas'][0].socket_out == node.head_args.socket_out
-            assert node.tail_args.socket_in == SocketType.PULL_BIND
-            assert node.tail_args.socket_out == SocketType.PUSH_BIND
-            f.dry_run()
 
-    def test_query(self):
-        f = Flow.load_config('../yaml/examples/faces/flow-query.yml')
-        with f:
-            node = f._pod_nodes['gateway']
-            assert node.head_args.socket_in == SocketType.PULL_CONNECT
-            assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
+def test_query():
+    f = Flow.load_config('../yaml/examples/faces/flow-query.yml')
+    with f:
+        node = f._pod_nodes['gateway']
+        assert node.head_args.socket_in == SocketType.PULL_CONNECT
+        assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
 
-            node = f._pod_nodes['loader']
-            assert node.head_args.socket_in == SocketType.PULL_BIND
-            assert node.head_args.socket_out == SocketType.ROUTER_BIND
-            for arg in node.peas_args['peas']:
-                assert arg.socket_in == SocketType.DEALER_CONNECT
-                assert arg.socket_out == SocketType.PUSH_CONNECT
-            assert node.tail_args.socket_in == SocketType.PULL_BIND
-            assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
+        node = f._pod_nodes['loader']
+        assert node.head_args.socket_in == SocketType.PULL_BIND
+        assert node.head_args.socket_out == SocketType.ROUTER_BIND
+        for arg in node.peas_args['peas']:
+            assert arg.socket_in == SocketType.DEALER_CONNECT
+            assert arg.socket_out == SocketType.PUSH_CONNECT
+        assert node.tail_args.socket_in == SocketType.PULL_BIND
+        assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
 
-            node = f._pod_nodes['flipper']
-            assert node.head_args.socket_in == SocketType.PULL_BIND
-            assert node.head_args.socket_out == SocketType.ROUTER_BIND
-            for arg in node.peas_args['peas']:
-                assert arg.socket_in == SocketType.DEALER_CONNECT
-                assert arg.socket_out == SocketType.PUSH_CONNECT
-            assert node.tail_args.socket_in == SocketType.PULL_BIND
-            assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
+        node = f._pod_nodes['flipper']
+        assert node.head_args.socket_in == SocketType.PULL_BIND
+        assert node.head_args.socket_out == SocketType.ROUTER_BIND
+        for arg in node.peas_args['peas']:
+            assert arg.socket_in == SocketType.DEALER_CONNECT
+            assert arg.socket_out == SocketType.PUSH_CONNECT
+        assert node.tail_args.socket_in == SocketType.PULL_BIND
+        assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
 
-            node = f._pod_nodes['normalizer']
-            assert node.head_args.socket_in == SocketType.PULL_BIND
-            assert node.head_args.socket_out == SocketType.ROUTER_BIND
-            for arg in node.peas_args['peas']:
-                assert arg.socket_in == SocketType.DEALER_CONNECT
-                assert arg.socket_out == SocketType.PUSH_CONNECT
-            assert node.tail_args.socket_in == SocketType.PULL_BIND
-            assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
+        node = f._pod_nodes['normalizer']
+        assert node.head_args.socket_in == SocketType.PULL_BIND
+        assert node.head_args.socket_out == SocketType.ROUTER_BIND
+        for arg in node.peas_args['peas']:
+            assert arg.socket_in == SocketType.DEALER_CONNECT
+            assert arg.socket_out == SocketType.PUSH_CONNECT
+        assert node.tail_args.socket_in == SocketType.PULL_BIND
+        assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
 
-            node = f._pod_nodes['encoder']
-            assert node.head_args.socket_in == SocketType.PULL_BIND
-            assert node.head_args.socket_out == SocketType.ROUTER_BIND
-            for arg in node.peas_args['peas']:
-                assert arg.socket_in == SocketType.DEALER_CONNECT
-                assert arg.socket_out == SocketType.PUSH_CONNECT
-            assert node.tail_args.socket_in == SocketType.PULL_BIND
-            assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
+        node = f._pod_nodes['encoder']
+        assert node.head_args.socket_in == SocketType.PULL_BIND
+        assert node.head_args.socket_out == SocketType.ROUTER_BIND
+        for arg in node.peas_args['peas']:
+            assert arg.socket_in == SocketType.DEALER_CONNECT
+            assert arg.socket_out == SocketType.PUSH_CONNECT
+        assert node.tail_args.socket_in == SocketType.PULL_BIND
+        assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
 
-            node = f._pod_nodes['chunk_indexer']
-            assert node.head_args.socket_in == SocketType.PULL_BIND
-            assert node.head_args.socket_out == SocketType.PUSH_CONNECT
-            assert node.peas_args['peas'][0].socket_in == node.head_args.socket_in
-            assert node.peas_args['peas'][0].socket_out == node.head_args.socket_out
-            assert node.tail_args.socket_in == SocketType.PULL_BIND
-            assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
+        node = f._pod_nodes['chunk_indexer']
+        assert node.head_args.socket_in == SocketType.PULL_BIND
+        assert node.head_args.socket_out == SocketType.PUSH_CONNECT
+        assert node.peas_args['peas'][0].socket_in == node.head_args.socket_in
+        assert node.peas_args['peas'][0].socket_out == node.head_args.socket_out
+        assert node.tail_args.socket_in == SocketType.PULL_BIND
+        assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
 
-            node = f._pod_nodes['ranker']
-            assert node.head_args.socket_in == SocketType.PULL_BIND
-            assert node.head_args.socket_out == SocketType.PUSH_CONNECT
-            assert node.peas_args['peas'][0].socket_in == node.head_args.socket_in
-            assert node.peas_args['peas'][0].socket_out == node.head_args.socket_out
-            assert node.tail_args.socket_in == SocketType.PULL_BIND
-            assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
+        node = f._pod_nodes['ranker']
+        assert node.head_args.socket_in == SocketType.PULL_BIND
+        assert node.head_args.socket_out == SocketType.PUSH_CONNECT
+        assert node.peas_args['peas'][0].socket_in == node.head_args.socket_in
+        assert node.peas_args['peas'][0].socket_out == node.head_args.socket_out
+        assert node.tail_args.socket_in == SocketType.PULL_BIND
+        assert node.tail_args.socket_out == SocketType.PUSH_CONNECT
 
-            node = f._pod_nodes['doc_indexer']
-            assert node.head_args.socket_in == SocketType.PULL_BIND
-            assert node.head_args.socket_out == SocketType.PUSH_BIND
-            assert node.peas_args['peas'][0].socket_in == node.head_args.socket_in
-            assert node.peas_args['peas'][0].socket_out == node.head_args.socket_out
-            assert node.tail_args.socket_in == SocketType.PULL_BIND
-            assert node.tail_args.socket_out == SocketType.PUSH_BIND
-            f.dry_run()
+        node = f._pod_nodes['doc_indexer']
+        assert node.head_args.socket_in == SocketType.PULL_BIND
+        assert node.head_args.socket_out == SocketType.PUSH_BIND
+        assert node.peas_args['peas'][0].socket_in == node.head_args.socket_in
+        assert node.peas_args['peas'][0].socket_out == node.head_args.socket_out
+        assert node.tail_args.socket_in == SocketType.PULL_BIND
+        assert node.tail_args.socket_out == SocketType.PUSH_BIND
+        f.dry_run()
