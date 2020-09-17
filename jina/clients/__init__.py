86811e7891cf25d8285cd57a86ce228e3659a056
GNRam <gnrameshwara@gmail.com>
2020-09-17 22:33:59 +0530

    fix: import missing package

--- jina/clients/__init__.py
+++ jina/clients/__init__.py
@@ -1,6 +1,8 @@
 __copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
+if False:
+    import PyClient
 
 def py_client(**kwargs) -> 'PyClient':
     """A simple Python client for connecting to the gateway.
