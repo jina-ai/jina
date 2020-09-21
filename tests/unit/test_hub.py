0d2462b4ccafcff5fe0238e501b7006def932c51
Han Xiao <han.xiao@jina.ai>
2020-09-21 19:28:53 +0200

    ci: fix #975

--- tests/unit/test_hub.py
+++ tests/unit/test_hub.py
@@ -12,7 +12,7 @@ cur_dir = os.path.dirname(os.path.abspath(__file__))
 @pytest.mark.timeout(360)
 def test_hub_build_pull():
     args = set_hub_build_parser().parse_args(
-        [os.path.join(cur_dir, 'hub-mwu'), '--pull', '--push', '--test-uses', '--raise-error'])
+        [os.path.join(cur_dir, 'hub-mwu'), '--push', '--test-uses', '--raise-error'])
     HubIO(args).build()
 
     args = set_hub_pushpull_parser().parse_args(['jinahub/pod.dummy_mwu_encoder'])
@@ -25,13 +25,13 @@ def test_hub_build_pull():
 @pytest.mark.timeout(360)
 def test_hub_build_uses():
     args = set_hub_build_parser().parse_args(
-        [os.path.join(cur_dir, 'hub-mwu'), '--pull', '--test-uses', '--raise-error'])
+        [os.path.join(cur_dir, 'hub-mwu'), '--test-uses', '--raise-error'])
     HubIO(args).build()
     # build again it shall not fail
     HubIO(args).build()
 
     args = set_hub_build_parser().parse_args(
-        [os.path.join(cur_dir, 'hub-mwu'), '--pull', '--test-uses', '--daemon', '--raise-error'])
+        [os.path.join(cur_dir, 'hub-mwu'), '--test-uses', '--daemon', '--raise-error'])
     HubIO(args).build()
     # build again it shall not fail
     HubIO(args).build()
@@ -57,16 +57,15 @@ def test_hub_build_push():
 def test_hub_build_failures():
     for j in ['bad-dockerfile', 'bad-pythonfile', 'missing-dockerfile', 'missing-manifest']:
         args = set_hub_build_parser().parse_args(
-            [os.path.join(cur_dir, 'hub-mwu-bad', j), '--pull'])
+            [os.path.join(cur_dir, 'hub-mwu-bad', j)])
         assert not HubIO(args).build()['is_build_success']
 
 
 def test_hub_build_no_pymodules():
     args = set_hub_build_parser().parse_args(
-        [os.path.join(cur_dir, 'hub-mwu-bad', 'fail-to-start'), '--pull'])
+        [os.path.join(cur_dir, 'hub-mwu-bad', 'fail-to-start')])
     assert HubIO(args).build()['is_build_success']
 
     args = set_hub_build_parser().parse_args(
-        [os.path.join(cur_dir, 'hub-mwu-bad', 'fail-to-start'), '--pull', '--test-uses'])
+        [os.path.join(cur_dir, 'hub-mwu-bad', 'fail-to-start'), '--test-uses'])
     assert not HubIO(args).build()['is_build_success']
-
