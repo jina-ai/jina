04e1de60d4d432433b32a6e4eabb6f3e96f185db
Susana Guzman <zuxi.w13@gmail.com>
2020-09-30 15:52:19 +0200

    test: test img sizes

--- tests/unit/flow/test_flow_visualization.py
+++ tests/unit/flow/test_flow_visualization.py
@@ -1,9 +1,16 @@
+import os
+from PIL import Image
 from jina.flow import Flow
 
+
 def test_visualization():
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+
     flow = (Flow().add(name='pod_a')
-                    .add(name='pod_b', needs='gateway')
-                    .join(needs=['pod_a', 'pod_b']))
+            .add(name='pod_b', needs='gateway')
+            .join(needs=['pod_a', 'pod_b']))
 
     flow.mermaidstr_to_jpg()
-
+    flow_original = Image.open(os.path.join(cur_dir, 'flow_original.jpg'))
+    flow_created = Image.open(os.path.join(cur_dir, 'flow.jpg'))
+    assert flow_original.size == flow_created.size
