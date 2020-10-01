9d2c100163159ac15ac0fb0b9ad983b8c30d016f
Susana Guzman <zuxi.w13@gmail.com>
2020-10-01 17:53:13 +0200

    test: add test with yml file

--- tests/unit/flow/test_flow_visualization.py
+++ tests/unit/flow/test_flow_visualization.py
@@ -1,9 +1,8 @@
 import os
 import numpy as np
-
 from PIL import Image
-
 from jina.flow import Flow
+import glob
 
 
 def test_visualization():
@@ -18,3 +17,21 @@ def test_visualization():
         with Image.open(os.path.join(cur_dir, 'flow.jpg')) as flow_created:
             assert flow_original.size == flow_created.size
             np.testing.assert_array_almost_equal(flow_original, flow_created)
+
+    for i in glob.glob("*.jpg"):
+        os.remove(i)
+
+
+def test_visualization_with_yml_file():
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+
+    f = Flow.load_config(os.path.join(cur_dir, '../yaml/test_flow_visualization.yml'))
+
+    f.mermaidstr_to_jpg()
+    with Image.open(os.path.join(cur_dir, 'flow_original_yml.jpg')) as flow_original:
+        with Image.open(os.path.join(cur_dir, 'flow.jpg')) as flow_created:
+            assert flow_original.size == flow_created.size
+            np.testing.assert_array_almost_equal(flow_original, flow_created)
+
+    for i in glob.glob("*.jpg"):
+        os.remove(i)
