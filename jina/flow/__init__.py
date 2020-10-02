f7b44558bfd2fa63f35e0ab515006f5f86478266
Susana Guzman <zuxi.w13@gmail.com>
2020-10-02 14:33:28 +0200

    stlye: double to single quotes

--- jina/flow/__init__.py
+++ jina/flow/__init__.py
@@ -802,7 +802,7 @@ class Flow(ExitStack):
         i = 0
         for k in self._pod_nodes.items():
             for need_pod in self._pod_needs[i]:
-                curr_line = "Pod" + need_pod + "[" + need_pod + "]" + " --> " + "Pod" + k[0] + "[" + k[0] + "]"
+                curr_line = 'Pod' + need_pod + '[' + need_pod + ']' + ' --> ' + 'Pod' + k[0] + '[' + k[0] + ']'
                 mermaid_graph.append(curr_line)
             i = i + 1
         mermaid_str = 'graph TD\n' + '\n'.join(mermaid_graph)
@@ -818,6 +818,7 @@ class Flow(ExitStack):
 
         import base64
         mermaid_str = self.flow_visualization(**kwargs)
+        print('mermaid_str*** ', mermaid_str)
         encoded_str = base64.b64encode(bytes(mermaid_str, 'utf-8')).decode('utf-8')
         return 'https://mermaidjs.github.io/mermaid-live-editor/#/view/' + encoded_str
 
