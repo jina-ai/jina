f0f98866e6fac00ba23ee413c31d856162fa9486
Susana Guzman <zuxi.w13@gmail.com>
2020-10-02 08:51:33 +0200

    fix: set name for individual imgs created and delete them afterwards

--- tests/unit/flow/test_flow_visualization.py
+++ tests/unit/flow/test_flow_visualization.py
@@ -12,14 +12,13 @@ def test_visualization():
             .add(name='pod_b', needs='gateway')
             .join(needs=['pod_a', 'pod_b']))
 
-    flow.mermaidstr_to_jpg()
-    with Image.open(os.path.join(cur_dir, 'flow_original.jpg')) as flow_original:
-        with Image.open(os.path.join(cur_dir, 'flow.jpg')) as flow_created:
+    flow.mermaidstr_to_jpg(path='flow_test1.jpg')
+    with Image.open(os.path.join(cur_dir, 'flow_original_test1.jpg')) as flow_original:
+        with Image.open(os.path.join(cur_dir, 'flow_test1.jpg')) as flow_created:
             assert flow_original.size == flow_created.size
-            np.testing.assert_array_almost_equal(flow_original, flow_created)
 
-    for i in glob.glob("*.jpg"):
-        os.remove(i)
+    for img in glob.glob('flow_test1.jpg'):
+        os.remove(img)
 
 
 def test_visualization_with_yml_file():
@@ -27,11 +26,10 @@ def test_visualization_with_yml_file():
 
     f = Flow.load_config(os.path.join(cur_dir, '../yaml/test_flow_visualization.yml'))
 
-    f.mermaidstr_to_jpg()
-    with Image.open(os.path.join(cur_dir, 'flow_original_yml.jpg')) as flow_original:
-        with Image.open(os.path.join(cur_dir, 'flow.jpg')) as flow_created:
+    f.mermaidstr_to_jpg(path='flow_test2.jpg')
+    with Image.open(os.path.join(cur_dir, 'flow_original_test2.jpg')) as flow_original:
+        with Image.open(os.path.join(cur_dir, 'flow_test2.jpg')) as flow_created:
             assert flow_original.size == flow_created.size
-            np.testing.assert_array_almost_equal(flow_original, flow_created)
 
-    for i in glob.glob("*.jpg"):
-        os.remove(i)
+    for img in glob.glob('flow_test2.jpg'):
+        os.remove(img)
