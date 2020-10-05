ab2409c4ea85245507a4d3668adc209f8b7fb9b1
bwanglzu <kingbolanda@live.com>
2020-10-05 21:53:36 +0200

    test: take out autouse from conftest

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -6,13 +6,13 @@ import pytest
 
 from jina.executors.metas import get_default_metas
 
-@pytest.fixture()
+@pytest.fixture(scope='function')
 def random_workspace_name():
     """Generate a random workspace name with digits and letters."""
     rand = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
     return f'JINA_TEST_WORKSPACE_{rand}'
 
-@pytest.fixture(scope='function', autouse=True)
+@pytest.fixture(scope='function')
 def test_metas(tmpdir, random_workspace_name):
     os.environ[random_workspace_name] = str(tmpdir)
     metas = get_default_metas()
