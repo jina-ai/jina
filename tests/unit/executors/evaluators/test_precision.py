d3cd83bff2093bfc15c5dd735e77427e30604428
bwanglzu <kingbolanda@live.com>
2020-10-07 09:51:28 +0200

    test: refactor precision evaluator test

--- tests/unit/executors/evaluators/test_precision.py
+++ tests/unit/executors/evaluators/test_precision.py
@@ -1,94 +1,59 @@
+import pytest
+
 from jina.proto import jina_pb2
 from jina.executors.evaluators.precision import PrecisionEvaluator
 
-
-def test_precision_evaluator():
-    def matches():
-        match0 = jina_pb2.Document()
-        match0.tags['id'] = 0
-        match1 = jina_pb2.Document()
-        match1.tags['id'] = 1
-        match2 = jina_pb2.Document()
-        match2.tags['id'] = 2
-        match3 = jina_pb2.Document()
-        match3.tags['id'] = 3
-        match4 = jina_pb2.Document()
-        match4.tags['id'] = 4
-        return [match0, match1, match2, match3, match4]
-
-    def groundtruth():
-        match0 = jina_pb2.Document()
-        match0.tags['id'] = 1
-        match1 = jina_pb2.Document()
-        match1.tags['id'] = 0
-        match2 = jina_pb2.Document()
-        match2.tags['id'] = 20
-        match3 = jina_pb2.Document()
-        match3.tags['id'] = 30
-        match4 = jina_pb2.Document()
-        match4.tags['id'] = 40
-        return [match0, match1, match2, match3, match4]
-
-    evaluator = PrecisionEvaluator(eval_at=2, id_tag='id')
-    assert evaluator.evaluate(matches=matches(), groundtruth=groundtruth()) == 1.0
-
-    evaluator = PrecisionEvaluator(eval_at=4, id_tag='id')
-    assert evaluator.evaluate(matches=matches(), groundtruth=groundtruth()) == 0.5
-
-    evaluator = PrecisionEvaluator(eval_at=5, id_tag='id')
-    assert evaluator.evaluate(matches=matches(), groundtruth=groundtruth()) == 0.4
-
-    evaluator = PrecisionEvaluator(eval_at=100, id_tag='id')
-    assert evaluator.evaluate(matches=matches(), groundtruth=groundtruth()) == 0.4
+@pytest.fixture(scope='function')
+def jina_pb2_document():
+    class JinaPb2DocumentFactory(object):
+        def create(self, tags_id):
+            document = jina_pb2.Document()
+            document.tags['id'] = tags_id
+            return document
+    return JinaPb2DocumentFactory()
 
 
-def test_precision_evaluator_no_groundtruth():
-    def matches():
-        match0 = jina_pb2.Document()
-        match0.tags['id'] = 0
-        match1 = jina_pb2.Document()
-        match1.tags['id'] = 1
-        match2 = jina_pb2.Document()
-        match2.tags['id'] = 2
-        match3 = jina_pb2.Document()
-        match3.tags['id'] = 3
-        match4 = jina_pb2.Document()
-        match4.tags['id'] = 4
-        return [match0, match1, match2, match3, match4]
+@pytest.mark.parametrize(
+    'eval_at, expected',
+    [
+        (0, 0.0),
+        (2, 1.0),
+        (4, 0.5),
+        (5, 0.4),
+        (100, 0.4)
+    ]
+)
+def test_precision_evaluator(jina_pb2_document, eval_at, expected):
+    matches = [
+        jina_pb2_document.create(0),
+        jina_pb2_document.create(1),
+        jina_pb2_document.create(2),
+        jina_pb2_document.create(3),
+        jina_pb2_document.create(4),
+    ]
 
-    def groundtruth():
-        return []
+    groundtruth = [
+        jina_pb2_document.create(1),
+        jina_pb2_document.create(0),
+        jina_pb2_document.create(20),
+        jina_pb2_document.create(30),
+        jina_pb2_document.create(40),
+    ]
+
+    evaluator = PrecisionEvaluator(eval_at=eval_at, id_tag='id')
+    assert evaluator.evaluate(matches=matches, groundtruth=groundtruth) == expected
+
+
+def test_precision_evaluator_no_groundtruth(jina_pb2_document):
+    matches = [
+        jina_pb2_document.create(0),
+        jina_pb2_document.create(1),
+        jina_pb2_document.create(2),
+        jina_pb2_document.create(3),
+        jina_pb2_document.create(4),
+    ]
+
+    groundtruth = []
 
     evaluator = PrecisionEvaluator(eval_at=2, id_tag='id')
-    assert evaluator.evaluate(matches=matches(), groundtruth=groundtruth()) == 0.0
-
-
-def test_precision_evaluator_eval_at_0():
-    def matches():
-        match0 = jina_pb2.Document()
-        match0.tags['id'] = 0
-        match1 = jina_pb2.Document()
-        match1.tags['id'] = 1
-        match2 = jina_pb2.Document()
-        match2.tags['id'] = 2
-        match3 = jina_pb2.Document()
-        match3.tags['id'] = 3
-        match4 = jina_pb2.Document()
-        match4.tags['id'] = 4
-        return [match0, match1, match2, match3, match4]
-
-    def groundtruth():
-        match0 = jina_pb2.Document()
-        match0.tags['id'] = 1
-        match1 = jina_pb2.Document()
-        match1.tags['id'] = 0
-        match2 = jina_pb2.Document()
-        match2.tags['id'] = 20
-        match3 = jina_pb2.Document()
-        match3.tags['id'] = 30
-        match4 = jina_pb2.Document()
-        match4.tags['id'] = 40
-        return [match0, match1, match2, match3, match4]
-
-    evaluator = PrecisionEvaluator(eval_at=0, id_tag='id')
-    assert evaluator.evaluate(matches=matches(), groundtruth=groundtruth()) == 0.0
+    assert evaluator.evaluate(matches=matches, groundtruth=groundtruth) == 0.0
