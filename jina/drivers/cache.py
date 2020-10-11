85969a565b54c7497c11ab0e38f133ad35f963e9
Han Xiao <artex.xh@gmail.com>
2020-10-12 00:51:25 +0200

    feat(driver): add bloom filter driver

--- jina/drivers/cache.py
+++ jina/drivers/cache.py
@@ -1,10 +1,9 @@
 __copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
+import struct
 from typing import Iterable
-
-import numpy as np
-
+import os
 from . import BaseRecursiveDriver
 from ..proto import uid
 
@@ -12,38 +11,78 @@ if False:
     from ..proto import jina_pb2
 
 
-class UniqueDocDriver(BaseRecursiveDriver):
-    """ Remove docs that previously have seen in the cache.
-
-    :class:UniqueDocDriver: follows a simple rule: it stores all new doc.id in a cache file.
-    If a doc.id hits the cache, then it is removed from the request.
+class BloomFilterDriver(BaseRecursiveDriver):
+    """ Bloom filter to test whether an element is a member of a set.
+    It is used to speed up answers in a key-value storage system.
+    Values are stored on a disk which has slow access times. Bloom filter decisions are much faster.
     """
 
-    def __init__(self, ids_file: str, *args, **kwargs):
+    def __init__(self, bit_array: int = 0, num_hash: int = 8, *args, **kwargs):
         """
 
-        :param ids_file: a file path to store observed doc ids
+        :param bit_array: a bit array of m bits, all set to 0.
+        :param num_hash: number of hash functions, can only be 4, 8.
+            larger value, slower, but more memory efficient
         :param args:
         :param kwargs:
         """
         super().__init__(*args, **kwargs)
-        self._ids_history = ids_file
+        self._bit_array = bit_array
+        # unpack int64 (8 bytes) to eight uint8 (1 bytes)
+        # to simulate a group of hash functions in bloom filter
+        if num_hash == 4:
+            # 8 bytes/4 = 2 bytes = H (unsigned short)
+            fmt = 'H' * 4
+        elif num_hash == 8:
+            fmt = 'B' * 8
+        else:
+            raise ValueError(f'"num_hash" must be 4 or 8 but given {num_hash}')
+        self._hash_funcs = lambda x: struct.unpack(fmt, uid.id2bytes(x))
+
+    def __contains__(self, doc_id: str):
+        for _r in self._hash_funcs(doc_id):
+            if not (self._bit_array & (1 << _r)):
+                return False
+        return True
+
+    def on_hit(self, doc: 'jina_pb2.Document'):
+        """Function to call when doc exists"""
+        raise NotImplementedError
+
+    def on_miss(self, doc: 'jina_pb2.Document'):
+        """Function to call when doc is missing"""
+        pass
+
+    def _add(self, doc_id: str):
+        for _r in self._hash_funcs(doc_id):
+            self._bit_array |= (1 << _r)
+
+    def _flush(self):
+        """Write the bloom filter by writing ``_bit_array`` back"""
+        pass
 
     def _apply_all(self, docs: Iterable['jina_pb2.Document'], *args, **kwargs) -> None:
-        if len({d.id for d in docs}) != len(docs):
-            raise ValueError('doc id is not unique')
+        for idx, doc in enumerate(docs):
+            if doc.id in self:
+                self.on_hit(doc)
+            else:
+                self._add(doc.id)
+                self.on_miss(doc)
+        self._flush()
 
-        hit_idx = []
-        with open(self._ids_history, 'ab+') as fp:
-            fp.seek(0)
-            _ids = np.frombuffer(fp.read(), dtype=np.int64)
-            fp.seek(0, 2)
-            for idx, doc in enumerate(docs):
-                if uid.id2hash(doc.id) not in _ids:
-                    fp.write(uid.id2bytes(doc.id))
-                else:
-                    hit_idx.append(idx)
 
-        # delete hit docs in reverse
-        for j in reversed(hit_idx):
-            del docs[j]
+class EnvBloomFilterDriver(BloomFilterDriver):
+    """
+    A :class:`BloomFilterDriver` that stores ``bit_array`` in OS environment.
+
+    Just an example how to share & persist ``bit_array``
+
+    """
+
+    def __init__(self, env_name: str = 'JINA_BLOOMFILTER_1', *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._env_name = env_name
+        self._bit_array = int(os.environ.get(env_name, '0'), 2)
+
+    def _flush(self):
+        os.environ[self._env_name] = bin(self._bit_array)[2:]
--- tests/unit/drivers/test_cache_driver.py
+++ tests/unit/drivers/test_cache_driver.py
@@ -1,30 +1,41 @@
-import numpy as np
+import pytest
 
-from jina.drivers.cache import UniqueDocDriver
+from jina.drivers.cache import BloomFilterDriver, EnvBloomFilterDriver
 from tests import random_docs, rm_files
+import os
 
 
-def test_cache_driver_twice():
-    cache_file = 'test-cache.bin'
-
+@pytest.mark.parametrize('num_hash', [4, 8])
+def test_cache_driver_twice(num_hash):
     docs = list(random_docs(10))
-    driver = UniqueDocDriver(cache_file)
+    driver = BloomFilterDriver(num_hash=num_hash)
     driver._apply_all(docs)
-    assert len(docs) == 10
-    # duplicate docs
-    driver._apply_all(docs)
-    assert len(docs) == 0
+
+    with pytest.raises(NotImplementedError):
+        # duplicate docs
+        driver._apply_all(docs)
 
     # new docs
     docs = list(random_docs(10))
     driver._apply_all(docs)
-    assert len(docs) == 10
-
-    # in total there should be 20 unique doc ids
-    with open(cache_file, 'rb') as fp:
-        ids = np.frombuffer(fp.read(), dtype=np.int64)
-        assert len(ids) == 20
-
-    rm_files(['test-cache.bin'])
 
 
+@pytest.mark.parametrize('num_hash', [4, 8])
+def test_cache_driver_env(num_hash):
+    docs = list(random_docs(10))
+    driver = EnvBloomFilterDriver(num_hash=num_hash)
+    assert os.environ.get(driver._env_name, None) is None
+    driver._apply_all(docs)
+
+    with pytest.raises(NotImplementedError):
+        # duplicate docs
+        driver._apply_all(docs)
+
+    # now start a new one
+    # should fail again, as bloom filter is persisted in os.env
+    with pytest.raises(NotImplementedError):
+        driver = EnvBloomFilterDriver(num_hash=num_hash)
+        driver._apply_all(docs)
+
+    assert os.environ.get(driver._env_name, None) is not None
+    print(os.environ.pop(driver._env_name))
