4128378e69e67a5255bcf79b2dbfa641cd1f6f8f
bwanglzu <kingbolanda@live.com>
2020-10-13 23:04:32 +0200

    test: refactor remote test

--- tests/unit/test_remote.py
+++ tests/unit/test_remote.py
@@ -1,33 +1,18 @@
-import time
-import unittest
+import pytest
 
-from jina.parser import set_gateway_parser, set_pea_parser
 from jina.peapods.pod import GatewayPod
 from jina.peapods.remote import PeaSpawnHelper
-from tests import JinaTestCase
+from jina.parser import set_gateway_parser, set_pea_parser
 
 
-class MyTestCase(JinaTestCase):
-    def tearDown(self) -> None:
-        time.sleep(2)
-        super().tearDown()
+def test_remote_not_allowed():
+    f_args = set_gateway_parser().parse_args([])
+    p_args = set_pea_parser().parse_args(['--host', 'localhost', '--port-expose', str(f_args.port_expose)])
+    with GatewayPod(f_args):
+        PeaSpawnHelper(p_args).start()
 
-    def test_remote_not_allowed(self):
-        f_args = set_gateway_parser().parse_args([])
-
-        p_args = set_pea_parser().parse_args(['--host', 'localhost', '--port-expose', str(f_args.port_expose)])
-        with GatewayPod(f_args):
-            PeaSpawnHelper(p_args).start()
-
-    def test_cont_gateway(self):
-        f1_args = set_gateway_parser().parse_args(['--allow-spawn'])
-        f2_args = set_gateway_parser().parse_args([])
-        with GatewayPod(f1_args):
-            pass
-
-        with GatewayPod(f2_args):
-            pass
-
-
-if __name__ == '__main__':
-    unittest.main()
+@pytest.mark.parametrize('args', [['--allow-spawn'], []])
+def test_cont_gateway(args):
+    parsed_args = set_gateway_parser().parse_args(args)
+    with GatewayPod(parsed_args):
+        pass
