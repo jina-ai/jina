dfe1a702069e81ba84baaa53e8af7aa9f049bc9a
Joan Fontanals Martinez <joan.martinez@jina.ai>
2020-10-19 10:18:47 +0200

    test: refactor ranking evaluation test

--- tests/unit/drivers/test_rankingevaluation_driver.py
+++ tests/unit/drivers/test_rankingevaluation_driver.py
@@ -22,10 +22,14 @@ class MockPrecisionEvaluator(BaseRankingEvaluator):
 
         divisor = min(self.eval_at, len(groundtruth_ids))
         if divisor == 0.0:
-            """TODO: Agree on a behavior"""
             return 0.0
         else:
             return ret / divisor
+
+
+@pytest.fixture
+def mock_precision_evaluator():
+    return MockPrecisionEvaluator()
 
 
 class SimpleEvaluateDriver(RankingEvaluationDriver):
@@ -34,28 +38,34 @@ class SimpleEvaluateDriver(RankingEvaluationDriver):
         return self._exec_fn
 
 
-def test_ranking_evaluate_driver():
-    def create_document_ground_truth_pairs(num_docs):
-        def add_matches(doc: jina_pb2.Document, num_matches):
-            for idx in range(num_matches):
-                match = doc.matches.add()
-                match.tags['id'] = idx
+@pytest.fixture
+def simple_evaluate_driver():
+    return SimpleEvaluateDriver()
 
-        pairs = []
-        for idx in range(num_docs):
-            doc = jina_pb2.Document()
-            gt = jina_pb2.Document()
-            add_matches(doc, num_docs)
-            add_matches(gt, num_docs)
-            pairs.append(DocGroundtruthPair(doc=doc, groundtruth=gt))
-        return pairs
 
-    pairs = create_document_ground_truth_pairs(10)
-    driver = SimpleEvaluateDriver()
-    executor = MockPrecisionEvaluator()
-    driver.attach(executor=executor, pea=None)
-    driver._apply_all(pairs)
-    for pair in pairs:
+@pytest.fixture
+def ground_truth_pairs():
+    num_docs = 10
+
+    def add_matches(doc: jina_pb2.Document, num_matches):
+        for idx in range(num_matches):
+            match = doc.matches.add()
+            match.tags['id'] = idx
+
+    pairs = []
+    for idx in range(num_docs):
+        doc = jina_pb2.Document()
+        gt = jina_pb2.Document()
+        add_matches(doc, num_docs)
+        add_matches(gt, num_docs)
+        pairs.append(DocGroundtruthPair(doc=doc, groundtruth=gt))
+    return pairs
+
+
+def test_ranking_evaluate_driver(mock_precision_evaluator, simple_evaluate_driver, ground_truth_pairs):
+    simple_evaluate_driver.attach(executor=mock_precision_evaluator, pea=None)
+    simple_evaluate_driver._apply_all(ground_truth_pairs)
+    for pair in ground_truth_pairs:
         doc = pair.doc
         assert len(doc.evaluations) == 1
         assert doc.evaluations[0].op_name == 'SimpleEvaluateDriver-MockPrecision@2'
@@ -79,37 +89,45 @@ class SimpleChunkEvaluateDriver(RankingEvaluationDriver):
         return self.eval_request
 
 
-def test_ranking_evaluate_driver_matches_in_chunks():
+@pytest.fixture
+def simple_chunk_evaluate_driver():
+    return SimpleChunkEvaluateDriver()
+
+
+@pytest.fixture
+def eval_request():
+    num_docs = 10
+    num_matches = 1
+
+    def add_matches(doc: jina_pb2.Document):
+        for idx in range(num_matches):
+            match = doc.matches.add()
+            match.tags['id'] = idx
+
+    req = jina_pb2.Request.IndexRequest()
+    for idx in range(num_docs):
+        doc = req.docs.add()
+        gt = req.groundtruths.add()
+        chunk_doc = doc.chunks.add()
+        chunk_gt = gt.chunks.add()
+        chunk_doc.granularity = 1
+        chunk_gt.granularity = 1
+        add_matches(chunk_doc)
+        add_matches(chunk_gt)
+    return req
+
+
+def test_ranking_evaluate_driver_matches_in_chunks(simple_chunk_evaluate_driver, mock_precision_evaluator,
+                                                   eval_request):
     # this test proves that we can evaluate matches at chunk level,
     # proving that the driver can traverse in a parallel way docs and groundtruth
-    def create_eval_request(num_docs, num_matches):
-        def add_matches(doc: jina_pb2.Document):
-            for idx in range(num_matches):
-                match = doc.matches.add()
-                match.tags['id'] = idx
+    simple_chunk_evaluate_driver.attach(executor=mock_precision_evaluator, pea=None)
+    simple_chunk_evaluate_driver.eval_request = eval_request
+    simple_chunk_evaluate_driver()
 
-        req = jina_pb2.Request.IndexRequest()
-        for idx in range(num_docs):
-            doc = req.docs.add()
-            gt = req.groundtruths.add()
-            chunk_doc = doc.chunks.add()
-            chunk_gt = gt.chunks.add()
-            chunk_doc.granularity = 1
-            chunk_gt.granularity = 1
-            add_matches(chunk_doc)
-            add_matches(chunk_gt)
-        return req
-
-    req = create_eval_request(10, 1)
-    driver = SimpleChunkEvaluateDriver()
-    executor = MockPrecisionEvaluator()
-    driver.attach(executor=executor, pea=None)
-    driver.eval_request = req
-    driver()
-
-    assert len(req.docs) == len(req.groundtruths)
-    assert len(req.docs) == 10
-    for doc in req.docs:
+    assert len(eval_request.docs) == len(eval_request.groundtruths)
+    assert len(eval_request.docs) == 10
+    for doc in eval_request.docs:
         assert len(doc.evaluations) == 0  # evaluation done at chunk level
         assert len(doc.chunks) == 1
         chunk = doc.chunks[0]
@@ -118,30 +136,33 @@ def test_ranking_evaluate_driver_matches_in_chunks():
         assert chunk.evaluations[0].value == 1.0
 
 
-def test_evaluate_assert_doc_groundtruth_structure():
-    def create_eval_request_with_unmatching_structure(num_docs, num_matches):
-        def add_matches(doc: jina_pb2.Document):
-            for idx in range(num_matches):
-                match = doc.matches.add()
-                match.tags['id'] = idx
+@pytest.fixture
+def eval_request_with_unmatching_struct():
+    num_docs = 10
+    num_matches = 1
 
-        req = jina_pb2.Request.SearchRequest()
-        for idx in range(num_docs):
-            doc = req.docs.add()
-            gt = req.groundtruths.add()
-            chunk_doc = doc.chunks.add()
-            chunk_gt = gt.chunks.add()
-            chunk_doc.granularity = 1
-            chunk_gt.granularity = 1
-            add_matches(chunk_doc)
-            add_matches(chunk_gt)
-            chunk_gt_wrong = gt.chunks.add()
-        return req
+    def add_matches(doc: jina_pb2.Document):
+        for idx in range(num_matches):
+            match = doc.matches.add()
+            match.tags['id'] = idx
 
-    req = create_eval_request_with_unmatching_structure(10, 1)
-    driver = SimpleChunkEvaluateDriver()
-    executor = MockPrecisionEvaluator()
-    driver.attach(executor=executor, pea=None)
-    driver.eval_request = req
+    req = jina_pb2.Request.SearchRequest()
+    for idx in range(num_docs):
+        doc = req.docs.add()
+        gt = req.groundtruths.add()
+        chunk_doc = doc.chunks.add()
+        chunk_gt = gt.chunks.add()
+        chunk_doc.granularity = 1
+        chunk_gt.granularity = 1
+        add_matches(chunk_doc)
+        add_matches(chunk_gt)
+        chunk_gt_wrong = gt.chunks.add()
+    return req
+
+
+def test_evaluate_assert_doc_groundtruth_structure(simple_chunk_evaluate_driver, mock_precision_evaluator,
+                                                   eval_request_with_unmatching_struct):
+    simple_chunk_evaluate_driver.attach(executor=mock_precision_evaluator, pea=None)
+    simple_chunk_evaluate_driver.eval_request = eval_request_with_unmatching_struct
     with pytest.raises(AssertionError):
-        driver()
+        simple_chunk_evaluate_driver()
