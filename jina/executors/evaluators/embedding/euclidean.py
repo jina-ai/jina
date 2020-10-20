dc75eb7b2cd16bd5049579a93c084db2f71fd363
Han Xiao <artex.xh@gmail.com>
2020-10-20 23:23:53 +0200

    refactor: flatten evaluator inheirtance

--- jina/executors/evaluators/embedding/euclidean.py
+++ jina/executors/evaluators/embedding/euclidean.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 from ..embedding import BaseEmbeddingEvaluator, expand_vector
-from ...indexers.vector import _euclidean, _ext_B, _ext_A
 
 
 class EuclideanEvaluator(BaseEmbeddingEvaluator):
@@ -22,3 +21,38 @@ class EuclideanEvaluator(BaseEmbeddingEvaluator):
         actual = expand_vector(actual)
         desired = expand_vector(desired)
         return _euclidean(_ext_A(actual), _ext_B(desired))
+
+
+# duplicate on purpose, to be migrated to the Hub
+def _get_ones(x, y):
+    return np.ones((x, y))
+
+
+def _ext_A(A):
+    nA, dim = A.shape
+    A_ext = _get_ones(nA, dim * 3)
+    A_ext[:, dim:2 * dim] = A
+    A_ext[:, 2 * dim:] = A ** 2
+    return A_ext
+
+
+def _ext_B(B):
+    nB, dim = B.shape
+    B_ext = _get_ones(dim * 3, nB)
+    B_ext[:dim] = (B ** 2).T
+    B_ext[dim:2 * dim] = -2.0 * B.T
+    del B
+    return B_ext
+
+
+def _euclidean(A_ext, B_ext):
+    sqdist = A_ext.dot(B_ext).clip(min=0)
+    return np.sqrt(sqdist)
+
+
+def _norm(A):
+    return A / np.linalg.norm(A, ord=2, axis=1, keepdims=True)
+
+
+def _cosine(A_norm_ext, B_norm_ext):
+    return A_norm_ext.dot(B_norm_ext).clip(min=0) / 2
