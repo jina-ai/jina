64d34d53a4ec4e282ca79300c6bf0c2fee787834
bwanglzu <kingbolanda@live.com>
2020-10-20 22:35:08 +0200

    feat: add modality tests

--- tests/unit/drivers/test_multimodal_driver.py
+++ tests/unit/drivers/test_multimodal_driver.py
@@ -15,9 +15,16 @@ def embedding():
             return np.random.random([dim])
     return EmbeddingFactory()
 
+@pytest.fixture(scope='function')
+def embeddings(embedding):
+    return [
+        embedding.create(dim=16, seed=1),
+        embedding.create(dim=16, seed=2),
+        embedding.create(dim=24, seed=3)
+    ]
 
 @pytest.fixture(scope='function')
-def doc_with_multimodal_chunks(embedding):
+def doc_with_multimodal_chunks(embeddings):
     doc = Document()
     chunk1 = doc.chunks.add()
     chunk2 = doc.chunks.add()
@@ -28,10 +35,9 @@ def doc_with_multimodal_chunks(embedding):
     chunk1.id = uid.new_doc_id(chunk1)
     chunk2.id = uid.new_doc_id(chunk2)
     chunk3.id = uid.new_doc_id(chunk3)
-    # visual features has the same embedding dim.
-    chunk1.embedding.CopyFrom(array2pb(embedding.create(dim=16, seed=1)))
-    chunk2.embedding.CopyFrom(array2pb(embedding.create(dim=16, seed=2)))
-    chunk3.embedding.CopyFrom(array2pb(embedding.create(dim=24, seed=3)))
+    chunk1.embedding.CopyFrom(array2pb(embeddings[0]))
+    chunk2.embedding.CopyFrom(array2pb(embeddings[1]))
+    chunk3.embedding.CopyFrom(array2pb(embeddings[2]))
     return doc
 
 @pytest.fixture(scope='function')
@@ -40,24 +46,23 @@ def flow():
          .add(name='b', needs='gateway')
          .join(needs=['a', 'b'], uses='- !MultimodalDriver | {}'))
 
-def test_multimodal_driver(doc_with_multimodal_chunks, flow, embedding):
+def test_multimodal_driver(flow, embeddings, doc_with_multimodal_chunks):
     def input_fn():
         return [doc_with_multimodal_chunks]
 
     def validate(req):
         doc = req.docs[0]
-        embedding1 = embedding.create(dim=16, seed=1)
-        embedding2 = embedding.create(dim=16, seed=2)
-        embedding3 = embedding.create(dim=24, seed=3)
         chunk1 = doc.chunks[0]
         chunk2 = doc.chunks[1]
         chunk3 = doc.chunks[2]
 
         assert len(req.docs) == 1
         assert len(doc.chunks) == 3
-        assert chunk1.embedding.shape == [embedding1.shape[0]]
-        assert chunk2.embedding.shape == [embedding2.shape[0]]
-        assert chunk3.embedding.shape == [embedding3.shape[0]]
+        assert chunk1.embedding.shape == [embeddings[0].shape[0]]
+        assert chunk2.embedding.shape == [embeddings[1].shape[0]]
+        assert chunk3.embedding.shape == [embeddings[2].shape[0]]
+        assert chunk1.modality == chunk2.modality == 'visual'
+        assert chunk3.modality == 'textual'
         # TODO add np testing after resolve question.
 
     with flow:
