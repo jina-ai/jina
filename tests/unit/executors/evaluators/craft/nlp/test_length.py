02b5d718ea248b0f73dc45fb406a8c703949d6e8
Han Xiao <artex.xh@gmail.com>
2020-10-20 22:50:00 +0200

    refactor: flatten evaluator inheirtance

--- tests/unit/executors/evaluators/craft/nlp/test_length.py
+++ tests/unit/executors/evaluators/craft/nlp/test_length.py
@@ -12,7 +12,7 @@ from jina.executors.evaluators.text.length import TextLengthEvaluator
 )
 def test_length_evaluator(doc, gt, expected):
     evaluator = TextLengthEvaluator()
-    assert evaluator.evaluate(predicton=doc, desired=gt) == expected
+    assert evaluator.evaluate(actual=doc, desired=gt) == expected
     assert evaluator._running_stats._n == 1
     np.testing.assert_almost_equal(evaluator.mean, expected)
 
@@ -22,8 +22,8 @@ def test_cosine_evaluator_average():
     gt_content = ['aaaa', 'ccc', 'ddd']
 
     evaluator = TextLengthEvaluator()
-    assert evaluator.evaluate(predicton=doc_content[0], desired=gt_content[0]) == 1.0
-    assert evaluator.evaluate(predicton=doc_content[1], desired=gt_content[1]) == 0.0
-    assert evaluator.evaluate(predicton=doc_content[2], desired=gt_content[2]) == 0.0
+    assert evaluator.evaluate(actual=doc_content[0], desired=gt_content[0]) == 1.0
+    assert evaluator.evaluate(actual=doc_content[1], desired=gt_content[1]) == 0.0
+    assert evaluator.evaluate(actual=doc_content[2], desired=gt_content[2]) == 0.0
     assert evaluator._running_stats._n == 3
     np.testing.assert_almost_equal(evaluator.mean, 1.0 / 3)
