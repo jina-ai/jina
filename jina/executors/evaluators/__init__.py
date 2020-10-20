29c05cec62e1029119375ddbae0429e4ac54414b
Joan Fontanals Martinez <joan.martinez@jina.ai>
2020-10-20 15:14:22 +0200

    feat: add file based evaluator

--- jina/executors/evaluators/__init__.py
+++ jina/executors/evaluators/__init__.py
@@ -4,6 +4,7 @@ __license__ = "Apache-2.0"
 from typing import Any
 
 from .. import BaseExecutor
+from ..compound import CompoundExecutor
 
 
 class BaseEvaluator(BaseExecutor):
@@ -28,3 +29,53 @@ class BaseEvaluator(BaseExecutor):
 
     def evaluate(self, prediction: Any, groundtruth: Any, *args, **kwargs) -> float:
         raise NotImplementedError
+
+
+class FileBasedEvaluator(CompoundExecutor):
+    """A Frequently used pattern for combining A :class:`BaseKVIndexer` and :class:`BaseEvaluator`.
+     It will be equipped with predefined ``requests.on`` behaviors:
+
+         -  At evaluation time(query or index)
+             - 1. Checks for the incoming document, gets its value from the `BaseKVIndexer` and fills the `groundtruth of the request
+             - 2. Filter the documents that do not have a corresponding groundtruth
+             - 2. The BaseEvaluator works as if the `groundtruth` had been provided by the client as it comes in the request.
+
+     One can use the :class:`FileBasedEvaluator` via
+
+     .. highlight:: yaml
+     .. code-block:: yaml
+
+         !FileBasedEvaluator
+         components:
+           - !BinaryPbIndexer
+             with:
+               index_filename: ground_truth.gz
+             metas:
+               name: groundtruth_index  # a customized name
+               workspace: $TEST_WORKDIR
+           - !BaseEvaluator
+             metas:
+               name: evaluator  # a customized name
+         metas:
+           name: file_based_evaluator
+           workspace: $TEST_WORKDIR
+
+     Without defining any ``requests.on`` logic. When load from this YAML, it will be auto equipped with
+
+     .. highlight:: yaml
+     .. code-block:: yaml
+
+         on:
+           [SearchRequest, IndexRequest]:
+             - !KVSearchDriver
+               with:
+                 executor: groundtruth_index
+             - !FilterQL
+                with:
+                 lookup: TODO: Adapt this
+             - !BaseEvaluationDriver
+               with:
+                 executor: BaseKVIndexer
+           ControlRequest:
+             - !ControlReqDriver {}
+     """
