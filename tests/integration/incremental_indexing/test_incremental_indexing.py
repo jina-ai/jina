39a03d2500e018284fe54a441fe833a42a3c86bd
Nan Wang@Home.MBP <Nan.Wang.28@gmail.com>
2020-10-20 13:18:37 +0800

    test: clean up

--- tests/integration/incremental_indexing/test_incremental_indexing.py
+++ tests/integration/incremental_indexing/test_incremental_indexing.py
@@ -9,43 +9,13 @@ from tests.integration.incremental_indexing import random_workspace, get_duplica
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 
 
-def test_incremental_indexing_vecindexers(random_workspace):
-    total_docs = 10
-    duplicate_docs, num_uniq_docs = get_duplicate_docs(num_docs=total_docs)
-
-    f = (Flow()
-         .add(uses=os.path.join(cur_dir, 'inc_vectorindexer.yml'), name='vec_idx'))
-
-    with f:
-        f.index(duplicate_docs)
-
-    with BaseExecutor.load(random_workspace / 'vec_idx.bin') as vector_indexer:
-        assert isinstance(vector_indexer, NumpyIndexer)
-        assert vector_indexer.size == num_uniq_docs
-
-
-def test_incremental_indexing_docindexers(random_workspace):
-    total_docs = 10
-    duplicate_docs, num_uniq_docs = get_duplicate_docs(num_docs=total_docs)
-
-    f = (Flow()
-         .add(uses=os.path.join(cur_dir, 'inc_docindexer.yml'), shards=1))
-
-    with f:
-        f.index(duplicate_docs)
-
-    with BaseExecutor.load(random_workspace / 'doc_idx.bin') as doc_indexer:
-        assert isinstance(doc_indexer, BinaryPbIndexer)
-        assert doc_indexer.size == num_uniq_docs
-
-
 def test_incremental_indexing_sequential_indexers(random_workspace):
     total_docs = 20
     duplicate_docs, num_uniq_docs = get_duplicate_docs(num_docs=total_docs)
 
     f = (Flow()
-         .add(uses=(cur_dir / 'inc_vectorindexer.yml'), shards=1)
-         .add(uses=(cur_dir / 'inc_docindexer.yml'), shards=1))
+         .add(uses=os.path.join(cur_dir, 'uniq_vectorindexer.yml'))
+         .add(uses=os.path.join(cur_dir, 'uniq_docindexer.yml')))
 
     with f:
         f.index(duplicate_docs[:10])
@@ -56,6 +26,30 @@ def test_incremental_indexing_sequential_indexers(random_workspace):
         assert vector_indexer._size == num_uniq_docs
 
     with BaseExecutor.load(random_workspace / 'doc_idx.bin') as doc_indexer:
+        assert isinstance(doc_indexer, BinaryPbIndexer)
+        assert doc_indexer._size == num_uniq_docs
+
+
+def test_incremental_indexing_parallel_indexers(random_workspace):
+    total_docs = 1000
+    duplicate_docs, num_uniq_docs = get_duplicate_docs(num_docs=total_docs)
+
+    f = (Flow()
+         .add(uses=os.path.join(cur_dir, 'uniq_vectorindexer.yml'),
+              name='inc_vec')
+         .add(uses=os.path.join(cur_dir, 'uniq_docindexer.yml'),
+              name='inc_doc',
+              needs=['gateway'])
+         .add(uses='_merge', needs=['inc_vec', 'inc_doc']))
+    with f:
+        f.index(duplicate_docs[:500])
+        f.index(duplicate_docs)
+
+    with BaseExecutor.load((random_workspace / 'vec_idx.bin')) as vector_indexer:
+        assert isinstance(vector_indexer, NumpyIndexer)
+        assert vector_indexer._size == num_uniq_docs
+
+    with BaseExecutor.load((random_workspace / 'doc_idx.bin')) as doc_indexer:
         assert isinstance(doc_indexer, BinaryPbIndexer)
         assert doc_indexer._size == num_uniq_docs
 
@@ -74,6 +68,7 @@ def test_incremental_indexing_sequential_indexers_with_shards(random_workspace):
               uses_before='_unique',
               shards=num_shards,
               separated_workspace=True))
+
     with f:
         f.index(duplicate_docs[:500])
         f.index(duplicate_docs)
@@ -93,30 +88,6 @@ def test_incremental_indexing_sequential_indexers_with_shards(random_workspace):
             assert isinstance(doc_indexer, BinaryPbIndexer)
             doc_idx_size += doc_indexer._size
     assert doc_idx_size == num_uniq_docs
-
-
-def test_incremental_indexing_parallel_indexers(random_workspace):
-    total_docs = 1000
-    duplicate_docs, num_uniq_docs = get_duplicate_docs(num_docs=total_docs)
-
-    f = (Flow()
-         .add(uses=os.path.join(cur_dir, 'inc_vectorindexer.yml'),
-              name='inc_vec')
-         .add(uses=os.path.join(cur_dir, 'inc_docindexer.yml'),
-              name='inc_doc',
-              needs=['gateway'])
-         .add(uses='_merge', needs=['inc_vec', 'inc_doc']))
-    with f:
-        f.index(duplicate_docs[:500])
-        f.index(duplicate_docs)
-
-    with BaseExecutor.load((random_workspace / 'vec_idx.bin')) as vector_indexer:
-        assert isinstance(vector_indexer, NumpyIndexer)
-        assert vector_indexer._size == num_uniq_docs
-
-    with BaseExecutor.load((random_workspace / 'doc_idx.bin')) as doc_indexer:
-        assert isinstance(doc_indexer, BinaryPbIndexer)
-        assert doc_indexer._size == num_uniq_docs
 
 
 def test_incremental_indexing_parallel_indexers_with_shards(random_workspace):
