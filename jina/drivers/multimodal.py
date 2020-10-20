183d5d3f6e3f234015299f574227f0a19db5c85f
bwanglzu <kingbolanda@live.com>
2020-10-20 13:37:31 +0200

    feat: inherit from reduce driver

--- jina/drivers/multimodal.py
+++ jina/drivers/multimodal.py
@@ -1,12 +1,12 @@
 __copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
-from typing import Iterable
+from typing import Iterable, Dict
 
-from . import BaseRecursiveDriver
+from .reduce import ReduceDriver
 from .helper import pb2array
 
-class MultimodalDriver(BaseRecursiveDriver):
+class MultimodalDriver(ReduceDriver):
     """
     TODO add docstring
     each document have multiple chunks
@@ -17,34 +17,35 @@ class MultimodalDriver(BaseRecursiveDriver):
         # traversal chunks from chunk level.
         super().__init__(traversal_paths=traversal_paths, *args, **kwargs)
 
+    def reduce(self, *args, **kwargs) -> None:
+        doc_pointers = {}
+        # traverse apply on ALL requests collected to collect embeddings
+        # reversed since the last response should collect the chunks/matches
+        for r in reversed(self.prev_reqs):
+            self._traverse_apply(r.docs, doc_pointers=doc_pointers, *args, **kwargs)
+
     def _apply_all(
             self,
             docs: Iterable['jina_pb2.Document'],
             context_doc: 'jina_pb2.Document',
             field: str,
+            doc_pointers: Dict,
             *args,
             **kwargs
     ) -> None:
-        """"""
-        # extract all the required fields from the chunks of a document (buffer, blob, text,
-        # or directly embedding)
+        # docs are chunks get by traversal rec
         for doc in docs:
-            doc_instance = {}
-            doc_instance['id'] = doc.id
-            chunks = doc.chunks
-            for chunk in chunks:
-                modality = chunk.modality
-                doc_instance[modality] = {}
-                doc_instance[modality][chunk.id]['content'] = self._extract_content(chunk)
-                doc_instance[modality][chunk.id]['embedding'] = self._extract_embedding(chunk)
-            # Set the return value to the context_doc embedding.
+            modality = doc.modality
+            content = self._extract_chunk_level_content(doc) # one of buffer, blob or text.
+            embedding = self._extract_chunk_level_embedding(doc) # directly embedding
 
 
-    def _extract_content(self, chunk):
-        return chunk.text or chunk.buffer or (chunk.blob and pb2array(chunk.blob))
 
-    def _extract_embedding(self, chunk):
-        return (chunk.embedding.buffer or None) and pb2array(chunk.embedding)
+    def _extract_chunk_level_content(self, doc):
+        return doc.text or doc.buffer or (doc.blob and pb2array(doc.blob))
+
+    def _extract_chunk_level_embedding(self, doc):
+        return (doc.embedding.buffer or None) and pb2array(doc.embedding)
 
 
 
