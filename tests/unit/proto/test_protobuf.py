53beedf41002366827272acaa6a9608af56b2165
Wang Bo <kingbolanda@live.com>
2020-10-22 10:12:21 +0200

    test: take out jinatestcase from protobuf test (#1121)
    * test: take out jinatestcase from protobuf test

--- tests/unit/proto/test_protobuf.py
+++ tests/unit/proto/test_protobuf.py
@@ -1,31 +1,29 @@
-import unittest
+import pytest
 
 from jina.proto.jina_pb2 import Document
-from tests import JinaTestCase
 from google.protobuf.json_format import MessageToJson, Parse
 
+@pytest.fixture(scope='function')
+def document():
+    d = Document()
+    d.tags['int'] = 1  # will convert to float!!!
+    d.tags['str'] = 'blah'
+    d.tags['float'] = 0.1234
+    d.tags['bool'] = True
+    d.tags['nested'] = {'bool': True}
+    return d
 
-class ProtobufTestCase(JinaTestCase):
-
-    def test_tags(self):
-        d = Document()
-        d.tags['int'] = 1  # will convert to float!!!
-        d.tags['str'] = 'blah'
-        d.tags['float'] = 0.1234
-        d.tags['bool'] = True
-        d.tags['nested'] = {'bool': True}
-        jd = MessageToJson(d)
-        d2 = Parse(jd, Document())
-        self.assertTrue(isinstance(d2.tags['int'], float))
-        self.assertTrue(isinstance(d2.tags['str'], str))
-        self.assertTrue(isinstance(d2.tags['float'], float))
-        self.assertTrue(isinstance(d2.tags['bool'], bool))
-        self.assertTrue(isinstance(d2.tags['nested']['bool'], bool))
-        # can be used as a dict
-        for k, v in d2.tags['nested'].items():
-            print(f'{k}:{v}')
+def test_tags(document):
+    jd = MessageToJson(document)
+    d2 = Parse(jd, Document())
+    assert isinstance(d2.tags['int'], float)
+    assert isinstance(d2.tags['str'], str)
+    assert isinstance(d2.tags['float'], float)
+    assert isinstance(d2.tags['bool'], bool)
+    assert isinstance(d2.tags['nested']['bool'], bool)
+    # can be used as a dict
+    for _, _ in d2.tags['nested'].items():
+        continue
 
 
 
-if __name__ == '__main__':
-    unittest.main()
