d39be424446e54374383397da2a5b91858963d70
Maximilian Werk <maximilian.werk@jina.ai>
2020-10-22 10:41:31 +0200

    fix: fixed the recall calcuation and tests

--- tests/unit/executors/evaluators/rank/test_recall.py
+++ tests/unit/executors/evaluators/rank/test_recall.py
@@ -3,12 +3,14 @@ import pytest
 from jina.executors.evaluators.rank.recall import RecallEvaluator
 import numpy as np
 
+
 @pytest.mark.parametrize(
     'eval_at, expected',
     [
         (0, 0.0),
-        (2, 1.0),
-        (4, 0.5),
+        (1, 0.2),
+        (2, 0.4),
+        (3, 0.4),
         (5, 0.4),
         (100, 0.4)
     ]
@@ -28,8 +30,9 @@ def test_recall_evaluator(eval_at, expected):
     'eval_at, expected_first',
     [
         (0, 0.0),
-        (2, 1.0),
-        (4, 0.5),
+        (1, 0.2),
+        (2, 0.4),
+        (3, 0.4),
         (5, 0.4),
         (100, 0.4)
     ]
@@ -37,7 +40,7 @@ def test_recall_evaluator(eval_at, expected):
 def test_recall_evaluator_average(eval_at, expected_first):
     matches_ids = [[0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4]]
 
-    groundtruth_ids = [[1, 0, 20, 30, 40], [-1, -1, -1, -1, -1],  [-1, -1, -1, -1, -1]]
+    groundtruth_ids = [[1, 0, 20, 30, 40], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1]]
 
     evaluator = RecallEvaluator(eval_at=eval_at)
     assert evaluator.evaluate(actual=matches_ids[0], desired=groundtruth_ids[0]) == expected_first
