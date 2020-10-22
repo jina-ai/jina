acb42a6f321067ac7be49d3856fc7088cd0e04b8
Pratik Bhavsar <pratik.a.bhavsar@gmail.com>
2020-10-22 22:34:11 +0530

    fix: renamed variable

--- tests/integration/issues/github_976/test_topk_rest_api.py
+++ tests/integration/issues/github_976/test_topk_rest_api.py
@@ -6,19 +6,19 @@ from jina.flow import Flow
 
 
 @pytest.fixture
-def top_k():
+def TOP_K():
     return '2'
 
 
 @pytest.fixture
-def query_dict(top_k):
-    return {'top_k': top_k, 'mode': 'search', 'data': [f'text:query']}
+def query_dict(TOP_K):
+    return {'top_k': TOP_K, 'mode': 'search', 'data': [f'text:query']}
 
 
-def test_top_k_with_rest_api(query_dict, top_k):
+def test_top_k_with_rest_api(query_dict, TOP_K):
     with Flow(rest_api=True, port_expose=45678).add(uses='_pass'):
         query = json.dumps(query_dict).encode('utf-8')
         req = request.Request('http://0.0.0.0:45678/api/search', data=query, headers={'content-type': 'application/json'})
         resp = request.urlopen(req).read().decode('utf8')
         assert json.loads(resp)['queryset'][0]['name'] == 'VectorSearchDriver'
-        assert json.loads(resp)['queryset'][0]['parameters']['top_k'] == top_k
+        assert json.loads(resp)['queryset'][0]['parameters']['top_k'] == TOP_K
