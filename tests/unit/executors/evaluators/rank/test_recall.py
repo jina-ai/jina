Fix recall calculation (#1134)

tests/unit/executors/evaluators/rank/test_recall.py
@@ -3,12 +3,14 @@ import pytest
 from jina.executors.evaluators.rank.recall import RecallEvaluator
 import numpy as np
 
+
 @pytest.mark.parametrize(
     'eval_at, expected',
     [
         (0, 0.0),
-        (2, 1.0),
-        (4, 0.5),
+        (1, 0.2),
+        (2, 0.4),
+        (3, 0.4),
         (5, 0.4),
         (100, 0.4)
     ]
@@ -16,10 +18,10 @@ import numpy as np
 def test_recall_evaluator(eval_at, expected):
     matches_ids = [0, 1, 2, 3, 4]
 
-    groundtruth_ids = [1, 0, 20, 30, 40]
+    desired_ids = [1, 0, 20, 30, 40]
 
     evaluator = RecallEvaluator(eval_at=eval_at)
-    assert evaluator.evaluate(actual=matches_ids, desired=groundtruth_ids) == expected
+    assert evaluator.evaluate(actual=matches_ids, desired=desired_ids) == expected
     assert evaluator._running_stats._n == 1
     np.testing.assert_almost_equal(evaluator.mean, expected)
 
@@ -28,8 +30,9 @@ def test_recall_evaluator(eval_at, expected):
     'eval_at, expected_first',
     [
         (0, 0.0),
-        (2, 1.0),
-        (4, 0.5),
+        (1, 0.2),
+        (2, 0.4),
+        (3, 0.4),
         (5, 0.4),
         (100, 0.4)
     ]
@@ -37,12 +40,12 @@ def test_recall_evaluator(eval_at, expected):
 def test_recall_evaluator_average(eval_at, expected_first):
     matches_ids = [[0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4]]
 
-    groundtruth_ids = [[1, 0, 20, 30, 40], [-1, -1, -1, -1, -1],  [-1, -1, -1, -1, -1]]
+    desired_ids = [[1, 0, 20, 30, 40], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1]]
 
     evaluator = RecallEvaluator(eval_at=eval_at)
-    assert evaluator.evaluate(actual=matches_ids[0], desired=groundtruth_ids[0]) == expected_first
-    assert evaluator.evaluate(actual=matches_ids[1], desired=groundtruth_ids[1]) == 0.0
-    assert evaluator.evaluate(actual=matches_ids[2], desired=groundtruth_ids[2]) == 0.0
+    assert evaluator.evaluate(actual=matches_ids[0], desired=desired_ids[0]) == expected_first
+    assert evaluator.evaluate(actual=matches_ids[1], desired=desired_ids[1]) == 0.0
+    assert evaluator.evaluate(actual=matches_ids[2], desired=desired_ids[2]) == 0.0
     assert evaluator._running_stats._n == 3
     np.testing.assert_almost_equal(evaluator.mean, expected_first / 3)
 
@@ -50,9 +53,9 @@ def test_recall_evaluator_average(eval_at, expected_first):
 def test_recall_evaluator_no_matches():
     matches_ids = []
 
-    groundtruth_ids = [1, 0, 20, 30, 40]
+    desired_ids = [1, 0, 20, 30, 40]
 
     evaluator = RecallEvaluator(eval_at=2)
-    assert evaluator.evaluate(actual=matches_ids, desired=groundtruth_ids) == 0.0
+    assert evaluator.evaluate(actual=matches_ids, desired=desired_ids) == 0.0
     assert evaluator._running_stats._n == 1
     np.testing.assert_almost_equal(evaluator.mean, 0.0)