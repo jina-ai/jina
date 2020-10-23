6cfda49d715d756c356d6c74b36a43cfa8aac76e
Pratik Bhavsar <pratik.a.bhavsar@gmail.com>
2020-10-23 18:59:45 +0530

    fix: random topk rest port

--- tests/integration/issues/github_976/test_topk_rest_api.py
+++ tests/integration/issues/github_976/test_topk_rest_api.py
@@ -1,12 +1,14 @@
-import pytest
 import json
 from urllib import request
 
+import pytest
+
 from jina.flow import Flow
+from jina.helper import random_port
 
 
 TOP_K = 2
-
+PORT = random_port()
 
 @pytest.fixture
 def query_dict():
@@ -14,9 +16,9 @@ def query_dict():
 
 
 def test_top_k_with_rest_api(query_dict):
-    with Flow(rest_api=True, port_expose=45678).add(uses='_pass'):
+    with Flow(rest_api=True, port_expose=PORT).add(uses='_pass'):
         query = json.dumps(query_dict).encode('utf-8')
-        req = request.Request('http://0.0.0.0:45678/api/search', data=query, headers={'content-type': 'application/json'})
+        req = request.Request(f'http://0.0.0.0:{PORT}/api/search', data=query, headers={'content-type': 'application/json'})
         resp = request.urlopen(req).read().decode('utf8')
         assert json.loads(resp)['queryset'][0]['name'] == 'VectorSearchDriver'
         assert json.loads(resp)['queryset'][0]['parameters']['top_k'] == TOP_K
