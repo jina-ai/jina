8b2452f516fd71e5558370742dba7acc118cc78d
Pantelis Giankoulidis <Pantelis.Giankoulidis@payever.org>
2020-11-01 12:12:47 +0200

    test(executor): Add f1 score test file

---
+++ tests/unit/executors/evaluators/rank/test_f1score.py
@@ -0,0 +1,56 @@
+import pytest
+import numpy as np
+from jina.executors.evaluators.rank.f1_score import F1Evaluator
+
+
+@pytest.mark.parametrize(
+    'eval_at, expected',
+    [
+        (0, 0.0),
+        (2, 0.5714),
+        (5, 0.4),
+        (100, 0.4)
+    ]
+)
+def test_precision_evaluator(eval_at, expected):
+    matches_ids = [0, 1, 2, 3, 4]
+
+    desired_ids = [1, 0, 20, 30, 40]
+
+    evaluator = F1Evaluator(eval_at=eval_at)
+    assert evaluator.evaluate(actual=matches_ids, desired=desired_ids) == expected
+    np.testing.assert_almost_equal(evaluator.mean, expected)
+
+
+@pytest.mark.parametrize(
+    'eval_at, expected_first',
+    [
+        (0, 0.0),
+        (2, 0.5714),
+        (5, 0.4),
+        (100, 0.4)
+    ]
+)
+def test_precision_evaluator_average(eval_at, expected_first):
+    matches_ids = [[0, 1, 2, 3, 4], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1]]
+
+    desired_ids = [[1, 0, 20, 30, 40], [1, 0, 20, 30, 40], [1, 0, 20, 30, 40]]
+
+    evaluator = F1Evaluator(eval_at=eval_at)
+    assert evaluator.evaluate(actual=matches_ids[0], desired=desired_ids[0]) == expected_first
+    assert evaluator.evaluate(actual=matches_ids[1], desired=desired_ids[1]) == 0.0
+    assert evaluator.evaluate(actual=matches_ids[2], desired=desired_ids[2]) == 0.0
+    assert evaluator._running_stats._n == 3
+    np.testing.assert_almost_equal(evaluator.mean, expected_first / 3)
+
+
+def test_precision_evaluator_no_groundtruth():
+    matches_ids = [0, 1, 2, 3, 4]
+
+    desired_ids = []
+
+    evaluator = F1Evaluator(eval_at=2)
+    assert evaluator.evaluate(actual=matches_ids, desired=desired_ids) == 0.0
+    assert evaluator._running_stats._n == 1
+    np.testing.assert_almost_equal(evaluator.mean, 0.0)
+
