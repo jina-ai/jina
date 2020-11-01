ea68b8726fa8952a628e3a256766fc172289dae3
Han Xiao <artex.xh@gmail.com>
2020-11-01 22:03:55 +0100

    feat(hub): show local manifest

--- jina/proto/message.py
+++ jina/proto/message.py
@@ -1,48 +1,64 @@
+from typing import List
+
 from . import jina_pb2
 
 _trigger_body_fields = set(kk
                            for v in [jina_pb2.Request.IndexRequest,
                                      jina_pb2.Request.SearchRequest,
                                      jina_pb2.Request.TrainRequest,
-                                     jina_pb2.Request.ControlRequest] for kk in v.DESCRIPTOR.fields_by_name.keys())
+                                     jina_pb2.Request.ControlRequest]
+                           for kk in v.DESCRIPTOR.fields_by_name.keys())
 
 _trigger_req_fields = set(jina_pb2.Request.DESCRIPTOR.fields_by_name.keys()).difference(
     {'train', 'index', 'search', 'control'})
 
 _trigger_fields = _trigger_req_fields.union(_trigger_body_fields)
+_empty_request = jina_pb2.Request()
 
 
 class LazyRequest:
     def __init__(self, request: bytes, is_compressed: bool):
         self._buffer = request
-        self._deserialized = None
+        self._deserialized = None  # type: jina_pb2.Request
         self._is_compressed = is_compressed
 
     @property
-    def is_used(self):
+    def is_used(self) -> bool:
+        """Return True when request has been r/w at least once """
         return self._deserialized is not None
 
     def __getattr__(self, name: str):
         # https://docs.python.org/3/reference/datamodel.html#object.__getattr__
-        if name in _trigger_fields:
+        if (name in _trigger_fields) or hasattr(_empty_request, name):
             if self._deserialized is None:
                 self._deserialized = jina_pb2.Request()
+                _buffer = self._buffer
                 if self._is_compressed:
                     import lz4.frame
-                    _buffer = lz4.frame.decompress(self._buffer)
-                else:
-                    _buffer = self._buffer
+                    _buffer = lz4.frame.decompress(_buffer)
                 self._deserialized.ParseFromString(_buffer)
-            if name in _trigger_body_fields:
-                req = getattr(self._deserialized, self._deserialized.WhichOneof('body'))
-                return getattr(req, name)
-            elif name in _trigger_req_fields:
-                return getattr(self._deserialized, name)
-        raise AttributeError
+        if name in _trigger_body_fields:
+            req = getattr(self._deserialized, self._deserialized.WhichOneof('body'))
+            return getattr(req, name)
+        elif hasattr(_empty_request, name):
+            return getattr(self._deserialized, name)
+        else:
+            raise AttributeError
+
+    def dump(self):
+        if self.is_used:
+            return self._deserialized.SerializeToString()
+        else:
+            # no touch, skip serialization, return original
+            return self._buffer
 
 
 class LazyMessage:
 
     def __init__(self, envelope: bytes, request: bytes):
-        self._envelope_buffer = envelope
-        self._request_buffer = request
+        self.envelope = jina_pb2.Envelope()
+        self.envelope.ParseFromString(envelope)
+        self.request = LazyRequest(request, False)
+
+    def dump(self) -> List[bytes]:
+        return [self.envelope.SerializeToString(), self.request.dump()]
--- tests/unit/proto/test_message.py
+++ tests/unit/proto/test_message.py
@@ -1,6 +1,7 @@
 import pytest
 
 from jina.clients.python.request import _generate
+from jina.proto import jina_pb2
 from jina.proto.message import LazyRequest, _trigger_fields
 from tests import random_docs
 
@@ -18,13 +19,54 @@ def test_lazy_access(field):
         assert r.is_used
 
 
+def test_no_access():
+    reqs = (LazyRequest(r.SerializeToString(), False) for r in _generate(random_docs(10)))
+    for r in reqs:
+        assert not r.is_used
+        print(r)
+        assert not r.is_used
+
+    for r in reqs:
+        assert not r.is_used
+        print(r.train)
+        assert r.is_used
+
+
 def test_lazy_nest_access():
     reqs = (LazyRequest(r.SerializeToString(), False) for r in _generate(random_docs(10)))
     for r in reqs:
         assert not r.is_used
+        # write access r.train
+        r.docs[0].id = '1'
+        # now it is read
+        assert r.is_used
 
-        # access r.train
-        print(r.docs[0].id)
 
+def test_lazy_append_access():
+    reqs = (LazyRequest(r.SerializeToString(), False) for r in _generate(random_docs(10)))
+    for r in reqs:
+        assert not r.is_used
+        # write access r.train
+        r.docs.append(jina_pb2.Document())
+        # now it is read
+        assert r.is_used
+
+
+def test_lazy_clear_access():
+    reqs = (LazyRequest(r.SerializeToString(), False) for r in _generate(random_docs(10)))
+    for r in reqs:
+        assert not r.is_used
+        # write access r.train
+        r.ClearField('index')
+        # now it is read
+        assert r.is_used
+
+
+def test_lazy_nested_clear_access():
+    reqs = (LazyRequest(r.SerializeToString(), False) for r in _generate(random_docs(10)))
+    for r in reqs:
+        assert not r.is_used
+        # write access r.train
+        r.index.ClearField('docs')
         # now it is read
         assert r.is_used
