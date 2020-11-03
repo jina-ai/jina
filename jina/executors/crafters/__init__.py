6295d3da3a080a9eae023e8221ae021b6c16c1a9
cristianmitroi <cristian.mitroi@jina.ai>
2020-11-03 09:33:41 +0100

    better word choice

--- jina/executors/crafters/__init__.py
+++ jina/executors/crafters/__init__.py
@@ -8,7 +8,7 @@ from .. import BaseExecutor
 
 
 class BaseCrafter(BaseExecutor):
-    """A :class:`BaseCrafter` encodes the content of `Document` or `Chunk`. It can be used for preprocessing,
+    """A :class:`BaseCrafter` transforms the content of `Document` or `Chunk`. It can be used for preprocessing,
     segmenting etc. It is an interface for Crafters which is a family of executors intended to apply
     transformations to single documents.
     The apply function is :func:`craft`, where the name of the arguments will be used as keys of the content.
