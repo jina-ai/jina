b439c79b9c06bfbe4d04f92be585f43e6071e928
cristian <cristian.mitroi@jina.ai>
2020-11-12 17:14:14 +0100

    ci: handling updating jina hub modules

---
+++ scripts/jina-hub-update.py
@@ -0,0 +1,21 @@
+import os
+from glob import glob
+
+
+def handle_module_update(module_dir):
+    print(f'handing module {module_dir}')
+    pass
+
+
+def main():
+    manifest_ymls = glob('**/manifest.yml')
+    print(f'Got {len(manifest_ymls)} manifests/modules to handle')
+    # TODO limited to 1 for dev/testing
+    manifest_ymls = manifest_ymls[:1]
+    for manifest_path in manifest_ymls:
+        module_dir = os.path.sep.join(manifest_path.split(os.path.sep)[:-1])
+        handle_module_update(module_dir)
+
+
+if __name__ == '__main__':
+    main()
