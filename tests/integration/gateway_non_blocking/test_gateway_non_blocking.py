72ad82a3dda111b106b6226eba20ccb582823c3f
Joan Fontanals Martinez <joan.martinez@jina.ai>
2020-12-09 10:36:53 +0100

    refactor: ensure callback called in integration tests

--- tests/integration/gateway_non_blocking/test_gateway_non_blocking.py
+++ tests/integration/gateway_non_blocking/test_gateway_non_blocking.py
@@ -7,7 +7,7 @@ cur_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 @pytest.mark.parametrize('parallel, expected_response', [(1, ['slow', 'fast']), (2, ['fast', 'slow'])])
-def test_non_blocking_gateway(parallel, expected_response):
+def test_non_blocking_gateway(parallel, expected_response, mocker):
     os.environ['JINA_NON_BLOCKING_PARALLEL'] = str(parallel)
     response = []
 
@@ -18,11 +18,14 @@ def test_non_blocking_gateway(parallel, expected_response):
     data = [
         'slow', 'fast'
     ]
+    response_mock = mocker.Mock(wrap=fill_responses)
+
     with Flow().load_config(os.path.join(cur_dir, 'flow.yml')) as f:
         f.search(input_fn=data,
-                 output_fn=fill_responses,
+                 output_fn=response_mock,
                  batch_size=1,
                  callback_on='body')
 
     del os.environ['JINA_NON_BLOCKING_PARALLEL']
+    response_mock.assert_called()
     assert response == expected_response
--- tests/integration/issues/github_976/test_topk.py
+++ tests/integration/issues/github_976/test_topk.py
@@ -13,9 +13,10 @@ from jina.types.ndarray.generic import NdArray
 @pytest.fixture
 def config(tmpdir):
     os.environ['JINA_TOPK_DIR'] = str(tmpdir)
-    os.environ['JINA_NDOCS'] = '3'
     os.environ['JINA_TOPK'] = '9'
-    os.environ['JINA_TOPK_OVERRIDE'] = '11'
+    yield
+    del os.environ['JINA_TOPK_DIR']
+    del os.environ['JINA_TOPK']
 
 
 def random_docs(num_docs, embed_dim=10, jitter=1):
@@ -27,32 +28,46 @@ def random_docs(num_docs, embed_dim=10, jitter=1):
         yield d
 
 
-def validate_results(resp):
-    assert len(resp.search.docs) == int(os.environ['JINA_NDOCS'])
-    for doc in resp.search.docs:
-        assert len(doc.matches) == int(os.environ['JINA_TOPK'])
+def test_topk(config, mocker):
+    NDOCS = 3
+    TOPK = int(os.getenv('JINA_TOPK'))
 
+    def validate_results(resp):
+        assert len(resp.search.docs) == NDOCS
+        for doc in resp.search.docs:
+            assert len(doc.matches) == TOPK
 
-def test_topk(config):
+    response_mock = mocker.Mock(wraps=validate_results)
+
     with Flow.load_config('flow.yml') as index_flow:
         index_flow.index(input_fn=random_docs(100))
+
     with Flow.load_config('flow.yml') as search_flow:
-        search_flow.search(input_fn=random_docs(int(os.environ['JINA_NDOCS'])),
-                           output_fn=validate_results)
+        search_flow.search(input_fn=random_docs(NDOCS),
+                           output_fn=response_mock)
+
+    response_mock.assert_called()
 
 
-def validate_override_results(resp):
-    assert len(resp.search.docs) == int(os.environ['JINA_NDOCS'])
-    for doc in resp.search.docs:
-        assert len(doc.matches) == int(os.environ['JINA_TOPK_OVERRIDE'])
+def test_topk_override(config, mocker):
+    NDOCS = 3
+    TOPK_OVERRIDE = 11
 
+    def validate_override_results(resp):
+        assert len(resp.search.docs) == NDOCS
+        for doc in resp.search.docs:
+            assert len(doc.matches) == TOPK_OVERRIDE
 
-def test_topk_override(config):
+    response_mock = mocker.Mock(wraps=validate_override_results)
+
     # Making queryset
-    top_k_queryset = QueryLang(VectorSearchDriver(top_k=int(os.environ['JINA_TOPK_OVERRIDE']), priority=1))
+    top_k_queryset = QueryLang(VectorSearchDriver(top_k=TOPK_OVERRIDE, priority=1))
 
     with Flow.load_config('flow.yml') as index_flow:
         index_flow.index(input_fn=random_docs(100))
+
     with Flow.load_config('flow.yml') as search_flow:
-        search_flow.search(input_fn=random_docs(int(os.environ['JINA_NDOCS'])),
-                           output_fn=validate_override_results, queryset=[top_k_queryset])
+        search_flow.search(input_fn=random_docs(NDOCS),
+                           output_fn=response_mock, queryset=[top_k_queryset])
+
+    response_mock.assert_called()
