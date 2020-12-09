b237d5b4c3caf78091f8aed009154ef3d4e383b7
bwanglzu <kingbolanda@live.com>
2020-12-09 11:38:27 +0100

    test: refactor type sets test

--- tests/unit/types/sets/test_querylangset.py
+++ tests/unit/types/sets/test_querylangset.py
@@ -5,27 +5,33 @@ def test_init():
     pass
 
 def test_insert():
-    """Test insert :attr:`ql` to :attr:`_querylangs_proto` at :attr:`index`."""
+    """Test insert :attr:`ql` to :class:`QueryLangSet` at :attr:`index`."""
     pass
 
 def test_get_set_success():
-    """Test :meth:`__setitem__` and :meth:`__getitem__`  in :class`QueryLangSet`.
-    If the key is instance of int, update :attr:`_querylangs_proto`, else, update :attr:`_querylangs_map`.
+    """
+    Test :meth:`__setitem__` and :meth:`__getitem__`  in :class`QueryLangSet`.
+    :attr:`key` might blongs to type `int` or `str`.
+
+    .. note::
+            Please parameterize this test with pytest.mark.parameterize.
     """
     pass
 
 def test_get_set_fail():
     """Test :meth:`__setitem__` and :meth:`__getitem__`  in :class`QueryLangSet`.
-    Should raise `IndexError` given invalid key.
+
+    .. note::
+            Please assert pytest.rases `IndexError`
     """
     pass
 
 def test_delete():
-    """Test :meth:`__del__`, should remove value from :attr:`_querylangs_proto` given an index."""
+    """Test :meth:`__del__`, should remove value from :class:`QueryLangSet` given an index."""
     pass
 
 def test_length():
-    """Test :meth:`__len__`, should return the length of :attr:`_querylangs_proto`."""
+    """Test :meth:`__len__`, should return the length of :class:`QueryLangSet`."""
     pass
 
 def test_iter():
@@ -41,19 +47,27 @@ def test_append_success():
     """
     pass
 
+def test_append_fail():
+    """Test :meth:`append` with an invalid input.
+
+    .. note::
+            Please assert pytest.rases `TypeError`
+    """
+    pass
+
 def test_extend():
     """Test :meth:`extend`, extend an iterable to :class:`QueryLangSet`."""
     pass
 
 def test_clear():
-    """Test :meth:`clear`, ensure length of :attr:`_querylangs_proto` is 0."""
+    """Test :meth:`clear`, ensure length of :attr:`_querylangs_proto` is 0 after clear."""
     pass
 
 def test_reverse():
-    """Test :meth:`reverse`, reverse the items in :attr:`_querylangs_proto`.
+    """Test :meth:`reverse`, reverse the items in :class:`QueryLangSet`.
 
     .. note::
-        reverse the same :attr:`_querylangs_proto` twice and assert they're identical.
+        reverse the same :class:`QueryLangSet` twice and assert they're identical.
     """
     pass
 
