e1551b43f734da9c770bc3d7973522cbdd044d68
bwanglzu <kingbolanda@live.com>
2020-12-14 22:49:51 +0100

    test: refactor driver test

--- tests/unit/drivers/querylang/queryset/test_lookup.py
+++ tests/unit/drivers/querylang/queryset/test_lookup.py
@@ -177,28 +177,28 @@ def test_lookup_leaf_None():
     assert not leaf.evaluate(mock1)
 
 def test_docs_filter():
-    s = random_docs(10)
-    ss = QuerySet(s).filter(tags__id__lt=5, tags__id__gt=3)
-    ssr = list(ss)
-    assert len(ssr) == 1
-    for d in ssr:
+    docs = random_docs(10)
+    filtered_docs = QuerySet(docs).filter(tags__id__lt=5, tags__id__gt=3)
+    filtered_docs = list(filtered_docs)
+    assert len(filtered_docs) == 1
+    for d in filtered_docs:
         assert (3 < d.tags['id'] < 5)
 
 
 def test_docs_filter_equal():
-    s = random_docs(10)
-    ss = QuerySet(s).filter(tags__id=4)
-    ssr = list(ss)
-    assert len(ssr) == 1
-    for d in ssr:
+    docs = random_docs(10)
+    filtered_docs = QuerySet(docs).filter(tags__id=4)
+    filtered_docs = list(filtered_docs)
+    assert len(filtered_docs) == 1
+    for d in filtered_docs:
         assert int(d.tags['id']) == 4
         assert len(d.chunks) == 5
 
 
 def test_nested_chunks_filter():
-    s = random_docs(10)
-    ss = QuerySet(s).filter(Q(chunks__filter=Q(tags__id__lt=35, tags__id__gt=33)))
-    ssr = list(ss)
-    assert len(ssr) == 1
-    for d in ssr:
+    docs = random_docs(10)
+    filtered_docs = QuerySet(docs).filter(Q(chunks__filter=Q(tags__id__lt=35, tags__id__gt=33)))
+    filtered_docs = list(filtered_docs)
+    assert len(filtered_docs) == 1
+    for d in filtered_docs:
         assert len(d.chunks) == 5
