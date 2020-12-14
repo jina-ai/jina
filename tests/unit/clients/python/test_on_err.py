95178b0ebc767e2a7f506fa21c55bbb11b70ad5a
Han Xiao <artex.xh@gmail.com>
2020-12-14 15:13:41 +0100

    fix(client): handle request exception on client (#1458)
    * fix(client): handle request exception on client

--- tests/unit/clients/python/test_on_err.py
+++ tests/unit/clients/python/test_on_err.py
@@ -14,3 +14,10 @@ def test_on_error(mocker):
         f.index_ndarray(np.random.random([5, 4]), output_fn=response_mock, continue_on_error=True)
 
     response_mock.assert_called()
+
+
+def test_on_bad_iterator():
+    # this should not stuck the server as request_generator's error is handled on the client side
+    f = Flow().add()
+    with f:
+        f.index([1, 2, 3])
