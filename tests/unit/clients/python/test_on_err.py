47af2845045643d4bac6d02a751ecffb7f0f9b6c
Han Xiao <han.xiao@jina.ai>
2020-12-14 18:02:35 +0100

    test: add client bad iterator test

--- tests/unit/clients/python/test_on_err.py
+++ tests/unit/clients/python/test_on_err.py
@@ -12,10 +12,10 @@ def test_client_on_error():
     # In this particular test, when you write two tests in a row, you are testing the following case:
     #
     # You are testing exception in client's callback, not error in client's request generator
-    # The exception breaks the async for req in stub.Call(req_iter) on the client
-    # Server probably has something hold in the stream
-    # Restart the client, keep server untouched.
-    # Now, server stucks (because it considers the last connection wasn't end yet)
+    # 1. The exception breaks the `async for req in stub.Call(req_iter)` on the client
+    # 2. Server probably has something hold in the stream
+    # 3. Restart the client, keep server untouched.
+    # 4. Now, server stucks (because it considers the last connection wasn't end yet)
     def validate(x):
         raise NotImplementedError
 
@@ -24,7 +24,9 @@ def test_client_on_error():
         try:
             f.index_ndarray(np.random.random([5, 4]), output_fn=validate, continue_on_error=False)
         except BadClientCallback:
+            # bad client callback will break the `async for req in stub.Call(req_iter)`
             t = 1
+        # now query the gateway again, make sure gateway's channel is still usable
         f.index_ndarray(np.random.random([5, 4]), output_fn=validate, continue_on_error=True)
         assert t == 1
 
