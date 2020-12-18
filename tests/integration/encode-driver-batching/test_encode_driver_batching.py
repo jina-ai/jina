0aee87fc4c4e8af858988575e6054db7cf5dd610
Joan Fontanals Martinez <joan.martinez@jina.ai>
2020-12-18 16:09:16 +0100

    test: add proper integration test

--- tests/integration/encode-driver-batching/test_encode_driver_batching.py
+++ tests/integration/encode-driver-batching/test_encode_driver_batching.py
@@ -1,9 +1,9 @@
+import os
 from typing import Any
 
 import pytest
 import numpy as np
 
-from jina.executors import BaseExecutor
 from jina.executors.encoders import BaseEncoder
 from jina.drivers.encode import EncodeDriver
 from jina.flow import Flow
@@ -13,28 +13,53 @@ from jina import Document
 class MockEncoder(BaseEncoder):
     def __init__(self,
                  driver_batch_size: int,
+                 num_docs_in_same_request: int,
                  total_num_docs: int,
                  *args, **kwargs):
+        """
+        :param driver_batch_size: the batch_size at which to accumulate for encode driver
+        :param num_docs_in_same_request: batch size of the request
+        :param total_num_docs: the total_num_docs that will be sent in the test, important to know how large
+        the last batches should be
+        :param kwargs: accepts all keyword arguments of `jina client` CLI
+        """
         super().__init__(*args, **kwargs)
         self.driver_batch_size = driver_batch_size
+        self.num_processed_docs = 0
         if self.driver_batch_size:
+            self.num_docs_in_same_request = num_docs_in_same_request
+            self.total_batches_in_request = int(self.num_docs_in_same_request / self.driver_batch_size)
             self.total_num_docs = total_num_docs
-            self.total_passes = int(self.total_num_docs / self.driver_batch_size)
-            self.id_pass = 0
+            self.total_num_requests = self.total_num_docs / self.num_docs_in_same_request
+            self.request_id = 0
 
     @property
-    def is_last_batch(self):
-        return self.id_pass == self.total_passes
+    def total_documents_left_to_process(self):
+        return self.total_num_docs - self.num_processed_docs
+
+    @property
+    def left_to_run_in_request(self):
+        return self.num_docs_in_same_request - self.num_processed_in_request
+
+    @property
+    def num_processed_in_request(self):
+        return self.num_processed_docs % ((self.request_id + 1) * self.num_docs_in_same_request)
+
+    @property
+    def next_expected_driver_batch_size(self):
+        if not self.driver_batch_size:
+            return None
+        else:
+            if self.left_to_run_in_request >= self.driver_batch_size:
+                return min(self.driver_batch_size, self.total_documents_left_to_process)
+            else:
+                return min(self.left_to_run_in_request, self.total_documents_left_to_process)
 
     def encode(self, data: Any, *args, **kwargs) -> Any:
-        # encodes 10 * data into the encoder, so return data
-        if self.driver_batch_size:
-            if not self.is_last_batch:
-                assert len(data) == self.driver_batch_size
-            else:
-                left_to_run_in_request = self.total_num_docs - (self.driver_batch_size * self.total_passes)
-                assert len(data) == left_to_run_in_request
-            self.id_pass += 1
+        assert len(data) == self.next_expected_driver_batch_size
+        self.num_processed_docs += len(data)
+        if self.num_processed_in_request > self.num_docs_in_same_request:
+            self.request_id += 1
         return data
 
 
@@ -51,31 +76,43 @@ def document_generator(num_docs, num_chunks, num_chunks_chunks):
 
 
 @pytest.mark.parametrize('request_batch_size', [100, 200, 500])
-@pytest.mark.parametrize('driver_batch_size',  [8, 64, 128])
-@pytest.mark.parametrize('num_chunks',  [0, 8, 64, 64, 128])
-@pytest.mark.parametrize('traversal_paths', [('rcc', 'cc')])
-def test_encode_driver_batching(request_batch_size, driver_batch_size, num_chunks, traversal_paths, mocker):
+@pytest.mark.parametrize('driver_batch_size', [8, 64, 128])
+# @pytest.mark.parametrize('num_chunks', [0, 8, 64, 64, 128])
+@pytest.mark.parametrize('traversal_paths', [('r',)])
+def test_encode_driver_batching(request_batch_size, driver_batch_size, traversal_paths, tmpdir, mocker):
+    num_docs = 1315
+    num_chunks = 0
+    num_chunks_chunks = 0
 
     def validate_response(resp):
         assert len(resp.search.docs) == request_batch_size
         for doc in resp.search.docs:
             assert doc.embedding is not None
 
-    num_docs = 6598
-    num_chunks_chunks = 3
-    encoder = MockEncoder(driver_batch_size=driver_batch_size, total_num_docs=num_docs)
+    def error_response(resp):
+        assert False  # no error should happen
+
+    encoder = MockEncoder(driver_batch_size=driver_batch_size,
+                          num_docs_in_same_request=request_batch_size,
+                          total_num_docs=num_docs)
+
     driver = EncodeDriver(batch_size=driver_batch_size,
                           traversal_paths=traversal_paths)
 
     encoder._drivers.clear()
-    encoder._drivers['SearchRequest'] = driver
+    encoder._drivers['SearchRequest'] = [driver]
 
-    yaml_repr = BaseExecutor._dump_instance_to_yaml(encoder)
-    print(yaml_repr)
+    executor_yml_file = os.path.join(tmpdir, 'executor.yml')
+    encoder.save_config(executor_yml_file)
 
     response_mock = mocker.Mock(wrap=validate_response)
+    error_mock = mocker.Mock(wrap=error_response)
 
-    with Flow().add(uses=yaml_repr) as f:
-        f.search(input_fn=document_generator(num_docs, num_chunks, num_chunks_chunks), output_fn=response_mock)
+    with Flow().add(uses=executor_yml_file) as f:
+        f.search(input_fn=document_generator(num_docs, num_chunks, num_chunks_chunks),
+                 batch_size=request_batch_size,
+                 output_fn=response_mock,
+                 on_error=error_mock)
 
     response_mock.assert_called()
+    error_mock.assert_not_called()
