17d07d414e1a0966c0988b5c91651d1ef4da4602
Florian Hönicke <hoenicke.florian@gmail.com>
2020-12-18 15:37:29 +0100

    fix: flow unit test

--- tests/integration/flow/test_crud.py
+++ tests/integration/flow/test_crud.py
@@ -26,46 +26,40 @@ def random_docs(num_docs, embed_dim=10, jitter=1):
 
 def test_delete(config, mocker):
     NUMBER_OF_SEARCHES = 3
-
     def validate_results(resp):
         assert len(resp.search.docs) == NUMBER_OF_SEARCHES
         for doc in resp.search.docs:
             assert len(doc.matches) == 0
 
     response_mock = mocker.Mock(wraps=validate_results)
-
     docs = list(random_docs(10))
-
     for method in [
         'index',
         'delete'
     ]:
         with Flow.load_config('flow.yml') as index_flow:
             getattr(index_flow, method)(input_fn=(d for d in docs))
-
-
     with Flow.load_config('flow.yml') as search_flow:
         search_flow.search(input_fn=random_docs(NUMBER_OF_SEARCHES),
                            output_fn=response_mock)
-
     response_mock.assert_called()
 
 def test_update(config, mocker):
     NUMBER_OF_SEARCHES = 3
-
-    def validate_results(docs_before, resp):
+    docs_before = list(random_docs(10))
+    docs_updated = list(random_docs(10))
+    def validate_results(resp):
+        print('start validation')
         assert len(resp.search.docs) == NUMBER_OF_SEARCHES
-        hash_set = {hash(str(d.embedding)) for d in docs_before}
+        hash_set_before = {hash(str(d.embedding)) for d in docs_before}
+        hash_set_updated= {hash(str(d.embedding)) for d in docs_updated}
         for doc in resp.search.docs:
             assert len(doc.matches) == 9
             for match in doc.matches:
-                assert hash(str(match.embedding)) not in hash_set
+                assert hash(str(match.embedding)) not in hash_set_before
+                assert hash(str(match.embedding)) in hash_set_updated
 
     response_mock = mocker.Mock(wraps=validate_results)
-
-    docs_before = list(random_docs(10))
-    docs_updated = list(random_docs(10))
-
     for method, docs in [
         ['index', docs_before],
         ['update', docs_updated]
