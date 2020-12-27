d0fb777ab784a6b187a6d660fd39414bc69e4a7f
Deepam Patel <deepam.patel@senquire.com>
2020-12-27 18:35:02 +0530

    feat: add batching_ranker_input decorator

--- jina/executors/decorators.py
+++ jina/executors/decorators.py
@@ -5,13 +5,14 @@ __license__ = "Apache-2.0"
 
 import inspect
 from functools import wraps
-from typing import Callable, Any, Union, Iterator, List, Optional
+from typing import Callable, Any, Union, Iterator, List, Optional, Dict
 
 import numpy as np
 
 from .metas import get_default_metas
 from ..helper import batch_iterator, typename, convert_tuple_to_list
 from ..logging import default_logger
+from itertools import islice
 
 
 def as_aggregate_method(func: Callable) -> Callable:
@@ -155,6 +156,12 @@ def store_init_kwargs(func: Callable) -> Callable:
 
     return arg_wrapper
 
+def _get_slice(data: Union[Iterator[Any], List[Any], np.ndarray], total_size: int) -> Union[Iterator[Any], List[Any], np.ndarray]:
+    if isinstance(data, Dict):
+        data = islice(data.items(), total_size)
+    else:
+        data = data[:total_size]
+    return data
 
 def _get_size(data: Union[Iterator[Any], List[Any], np.ndarray], axis: int = 0) -> int:
     if isinstance(data, np.ndarray):
@@ -361,6 +368,110 @@ def batching_multi_input(func: Callable[[Any], np.ndarray] = None,
                 args[slice_on] = batch
                 for idx in range(1, num_data):
                     args[slice_on + idx] = next(data_iterators[idx])
+
+                r = func(*args, **kwargs)
+
+                if r is not None:
+                    final_result.append(r)
+
+            return _merge_results_after_batching(final_result, merge_over_axis)
+
+        return arg_wrapper
+
+    if func:
+        return _batching(func)
+    else:
+        return _batching
+
+def batching_ranker_input(func: Callable[[Any], np.ndarray] = None,
+                         batch_size: Union[int, Callable] = None,
+                         num_batch: Optional[int] = None,
+                         split_over_axis: int = 0,
+                         merge_over_axis: int = 0,
+                         slice_on: Union[int, List[int]] = 1,
+                         num_data: int = None) -> Any:
+    """Split the input of a function into small batches and call :func:`func` on each batch
+    , collect the merged result and return. This is useful when the input is too big to fit into memory.
+    This can batch multiple inputs. 
+
+    :param func: function to decorate
+    :param batch_size: size of each batch
+    :param num_batch: number of batches to take, the rest will be ignored
+    :param split_over_axis: split over which axis into batches
+    :param merge_over_axis: merge over which axis into a single result
+    :param slice_on: the location of the data. When using inside a class,
+            ``slice_on`` should take ``self`` into consideration.
+    :param num_data: the number of data inside the arguments
+    :return: the merged result as if run :func:`func` once on the input.
+    ..warning:
+        data arguments will be taken starting from ``slice_on` to ``slice_on + num_data``
+    Example:
+        .. highlight:: python
+        .. code-block:: python
+            
+            class MemoruHungryRanker:
+                
+                @batching(batch_size = 64, slice_on=2)
+                def score(
+                    self, query_meta: Dict, old_match_scores: Dict, match_meta: Dict
+                ) -> 'np.ndarray': 
+                ...
+
+                @batching(batch_size = 64, slice_on=2, num_data = 1)
+                def score(
+                    self, query_meta: Dict, old_match_scores: Dict, match_meta: Dict
+                ) -> 'np.ndarray': 
+                ...
+
+                @batching(batch_size = 64, slice_on=[2])
+                def score(
+                    self, query_meta: Dict, old_match_scores: Dict, match_meta: Dict
+                ) -> 'np.ndarray': 
+                ...
+                    
+    """
+    def _batching(func):
+        @wraps(func)
+        def arg_wrapper(*args, **kwargs):
+            _slice_on = slice_on
+            _num_data = num_data
+            if _num_data is not None:
+                if isinstance(_slice_on, List):
+                    raise ValueError(f'When using num_data in @batching_ranker, an integer value '
+                'for slice_on (default=1) is expected.')
+                _slice_on = [_slice_on + i for i in range(0, _num_data)]
+            else:
+                if isinstance(_slice_on, int):
+                    _slice_on = [_slice_on]
+                    _num_data = 1
+                else:
+                    _num_data = len(_slice_on)
+            data = [args[i] for i in _slice_on]
+            # priority: decorator > class_attribute
+            # by default data is in args[1:] (self needs to be taken into account)
+            b_size = batch_size or getattr(args[0], 'batch_size', None)
+            # no batching if b_size is None
+            if b_size is None or data is None:
+                return func(*args, **kwargs)
+            args = list(args)
+            default_logger.debug(
+                f'batching enabled for {func.__qualname__} batch_size={b_size} '
+                f'num_batch={num_batch} axis={split_over_axis}')
+
+            # assume all datas have the same length
+            full_data_size = _get_size(args[_slice_on[0]], split_over_axis)
+            total_size = _get_total_size(full_data_size, b_size, num_batch)
+            final_result = []
+            yield_dict = [isinstance(args[i], Dict) for i in _slice_on]
+            data_iterators = [batch_iterator(_get_slice(args[i],total_size), b_size, split_over_axis) for i in _slice_on]
+
+            for batch in data_iterators[0]:
+                args[_slice_on[0]] = dict(batch) if yield_dict[0] else batch
+                for idx,slice_idx in enumerate(_slice_on[1:]):
+                    batch_idx = next(data_iterators[idx+1])
+                    if yield_dict[idx+1]:
+                        batch_idx = dict(batch_idx)
+                    args[slice_idx] = batch_idx
 
                 r = func(*args, **kwargs)
 
