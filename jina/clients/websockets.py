16b955089400449c0c2d6919b7d44c9ae3a47f08
Deepankar Mahapatro <deepankar.mahapatro@jina.ai>
2020-12-28 20:35:37 +0530

    feat(gateway): better websocket handling

--- jina/clients/websockets.py
+++ jina/clients/websockets.py
@@ -4,6 +4,7 @@ from .base import BaseClient
 from .helper import callback_exec
 from ..importer import ImportExtensions
 from ..logging.profile import TimeContext, ProgressBar
+from ..types.request import Request
 
 
 class WebSocketBaseClient(BaseClient):
@@ -19,32 +20,31 @@ class WebSocketBaseClient(BaseClient):
         req_iter, tname = self._get_requests(**kwargs)
         try:
             client_info = f'{self.args.host}:{self.args.port_expose}'
-            async with websockets.connect(f'ws://{client_info}/stream/{tname}') as websocket:
+            async with websockets.connect(f'ws://{client_info}/stream') as websocket:
                 self.logger.success(f'Connected to the gateway at {client_info}')
                 # To enable websockets debug logs
                 # https://websockets.readthedocs.io/en/stable/cheatsheet.html#debugging
 
                 with ProgressBar(task_name=tname) as p_bar, TimeContext(tname):
+                    # Unlike gRPC, any arbitrary function cannot be passed via websockets.
+                    # We'd need to traverse the iterator & convert each request to bytes/string
                     for next_request in req_iter:
-                        # Unlike gRPC, any arbitrary function cannot be passed via websockets.
-                        # We'd need to traverse the iterator & convert each request to bytes/string
                         await websocket.send(next_request.SerializeToString())
 
-                        # Traverse through the websocket to recv messages, instead of doing `await websocket.recv()`
+                        # When we have a stream of responses, traverse through the websocket to recv messages,
+                        # instead of doing `await websocket.recv()`.
                         # https://websockets.readthedocs.io/en/stable/faq.html#why-does-the-server-close-the-connection-after-processing-one-message
-                        async for response in websocket:
-                            if response == b'done':
-                                self.logger.debug('received done from the server. proceeding to next request')
-                                break
-                            callback_exec(response=response,
-                                          on_error=on_error,
-                                          on_done=on_done,
-                                          on_always=on_always,
-                                          continue_on_error=self.args.continue_on_error,
-                                          logger=self.logger)
-                            p_bar.update(self.args.batch_size)
+                        response_bytes = await websocket.recv()
+                        response = Request(response_bytes).to_response()
+                        callback_exec(response=response,
+                                      on_error=on_error,
+                                      on_done=on_done,
+                                      on_always=on_always,
+                                      continue_on_error=self.args.continue_on_error,
+                                      logger=self.logger)
+                        p_bar.update(self.args.batch_size)
 
         except websockets.exceptions.ConnectionClosedOK:
             self.logger.warning(f'client got disconnected from the rest server')
         except websockets.exceptions.WebSocketException as e:
-            self.logger.error(f'Got following error while streaming requests via websocket {repr(e)}')
+            self.logger.error(f'Got following error while streaming requests via websocket: {repr(e)}')
--- jina/peapods/peas/gateway/rest/app.py
+++ jina/peapods/peas/gateway/rest/app.py
@@ -1,12 +1,15 @@
-import json
+import asyncio
 from typing import Any
 
 from google.protobuf.json_format import MessageToDict
 
-from ..grpc.async_call import AsyncPrefetchCall
-from .....enums import RequestType
-from .....importer import ImportExtensions
 from ..... import clients
+from ....zmq import AsyncZmqlet
+from .....enums import RequestType
+from .....types.message import Message
+from .....types.request import Request
+from .....importer import ImportExtensions
+from ..grpc.async_call import AsyncPrefetchCall
 
 
 def get_fastapi_app(args, logger):
@@ -15,6 +18,10 @@ def get_fastapi_app(args, logger):
         from fastapi import WebSocket, WebSocketDisconnect
         from fastapi.responses import JSONResponse
         from fastapi.middleware.cors import CORSMiddleware
+        # TODO(Deepankar): starlette comes installed with fastapi. Should this be added as a separate dependency?
+        from starlette.endpoints import WebSocketEndpoint
+        if False:
+            from starlette.types import Receive, Scope, Send
 
     app = FastAPI(title='RESTGateway')
     app.add_middleware(CORSMiddleware, allow_origins=['*'])
@@ -23,9 +30,11 @@ def get_fastapi_app(args, logger):
     def error(reason, status_code):
         return JSONResponse(content={'reason': reason}, status_code=status_code)
 
+
     @app.get('/ready')
     async def is_ready():
         return JSONResponse(status_code=200)
+
 
     @app.post(path='/api/{mode}')
     async def api(mode: str, body: Any = Body(...)):
@@ -43,44 +52,46 @@ def get_fastapi_app(args, logger):
     async def get_result_in_json(request_iterator):
         return [MessageToDict(k) async for k in servicer.Call(request_iterator=request_iterator, context=None)]
 
-    @app.websocket(path='/stream/{mode}')
-    async def stream(websocket: WebSocket, mode: str):
-        if mode.upper() not in RequestType.__members__:
-            return error(reason=f'unsupported mode {mode}', status_code=405)
 
-        await websocket.accept()
-        client_info = f'{websocket.client.host}:{websocket.client.port}'
-        logger.success(f'Client {client_info} connected to stream requests via websockets')
-        try:
-            while True:
-                logger.warning('server awaiting the next request')
-                ws_receive = await websocket.receive()
-                if 'bytes' in ws_receive:
-                    request_body = ws_receive['bytes']
-                elif 'text' in ws_receive:
-                    request_body = json.loads(ws_receive['text'])
-                    # TODO(Deepankar): convert request_body to jina.types.Request if it is text based
+    @app.websocket_route(path='/stream')
+    class StreamingEndpoint(WebSocketEndpoint):
 
-                # if 'data' not in request_body:
-                #     await websocket.send_text('"data" field is empty')
+        # This disabled other encodings ('text' & 'json')
+        encoding = 'bytes'
 
-                def gen():
-                    yield request_body
-                request_iterator = gen()
+        def __init__(self, scope: 'Scope', receive: 'Receive', send: 'Send') -> None:
+            super().__init__(scope, receive, send)
+            self.args = args
+            self.name = args.name or self.__class__.__name__
 
-                # TODO(Deepankar): Using same servicer as gRPC is sub-optimal, since every `Call` creates a new zmqlet.
-                # For a single stream of requests, we can have a single zmqlet to process requests
-                async for response in servicer.Call(request_iterator=request_iterator, context=None):
-                    await websocket.send_bytes(response.SerializeToString())
-                await websocket.send_bytes(b'done')
-        except WebSocketDisconnect:
-            # TODO(Deepankar): For some strange reason, a different exception is raised when client disconnects.
-            # websockets.exceptions.ConnectionClosedOK: code = 1000 (OK), no reason
+        def handle(self, msg: 'Message') -> 'Request':
+            msg.add_route(self.name, self.args.identity)
+            return msg.response
 
-            # While WebSocketDisconnect is a high-level exception, websockets.exceptions.ConnectionClosedOK is
-            # raised by fastapi -> starlette -> uvicorn -> websockets library
-            logger.info(f'client {client_info} got disconnected!')
-        except Exception as e:
-            logger.error(f'got an exception while streaming requests {e}')
+        async def on_connect(self, websocket: WebSocket) -> None:
+            await websocket.accept()
+            self.client_info = f'{websocket.client.host}:{websocket.client.port}'
+            logger.success(f'Client {self.client_info} connected to stream requests via websockets')
+            self.zmqlet = AsyncZmqlet(args, logger)
+
+        async def on_receive(self, websocket: WebSocket, data: Any) -> None:
+            # At any point only a single request is sent in bytes instead of an iterator of requests
+            # For each such request, we send back the response in bytes
+            try:
+                asyncio.create_task(
+                    self.zmqlet.send_message(
+                        Message(None, data, 'gateway', **vars(self.args))
+                    )
+                )
+                response = await self.zmqlet.recv_message(callback=self.handle)
+                # Convert to bytes before sending the response
+                await websocket.send_bytes(response.SerializeToString())
+            except Exception as e:
+                logger.error(f'Got an exception while streaming requests: {e}')
+                return
+
+        async def on_disconnect(self, websocket: WebSocket, close_code: int) -> None:
+            self.zmqlet.close()
+            logger.info(f'Client {self.client_info} got disconnected!')
 
     return app
