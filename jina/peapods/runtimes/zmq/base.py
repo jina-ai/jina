bca558a549fc8aea7459a980a16f41c896a0e664
Han Xiao <artex.xh@gmail.com>
2020-12-29 09:37:53 +0100

    docs: fix doc website layout

--- jina/peapods/runtimes/zmq/base.py
+++ jina/peapods/runtimes/zmq/base.py
@@ -1,5 +1,6 @@
 import argparse
 from abc import ABC
+from typing import Union, Dict
 
 from ..base import BaseRuntime
 from ...zmq import Zmqlet, send_ctrl_message
@@ -22,3 +23,38 @@ class ZMQRuntime(BaseRuntime, ABC):
     def is_ready(self) -> bool:
         status = self.status
         return status and status.is_ready
+
+
+class ZMQManyRuntime(BaseRuntime, ABC):
+    def __init__(self, args: Union['argparse.Namespace', Dict]):
+        super().__init__(args)
+        self.many_ctrl_addr = []
+        if isinstance(args, Dict):
+            first_pea_args = self.args['peas'][0]
+            self.timeout_ctrl = first_pea_args.timeout_ctrl
+            self.host = first_pea_args.host
+            self.port_expose = first_pea_args.port_expose
+            for args in self.args['peas']:
+                ctrl_addr, _ = Zmqlet.get_ctrl_address(args.host, args.port_ctrl, args.ctrl_with_ipc)
+                self.many_ctrl_addr.append(ctrl_addr)
+        elif isinstance(args, argparse.Namespace):
+            self.many_ctrl_addr.append(Zmqlet.get_ctrl_address(args.host, self.args.port_ctrl, args.ctrl_with_ipc)[0])
+            self.timeout_ctrl = args.timeout_ctrl
+            self.host = args.host
+            self.port_expose = args.port_expose
+
+    def cancel(self):
+        for ctrl_addr in self.many_ctrl_addr:
+            send_ctrl_message(ctrl_addr, 'TERMINATE', timeout=self.timeout_ctrl)
+
+    @property
+    def status(self):
+        result = []
+        for ctrl_addr in self.many_ctrl_addr:
+            result.append(send_ctrl_message(ctrl_addr, 'STATUS', timeout=self.timeout_ctrl))
+        return result
+
+    @property
+    def is_ready(self) -> bool:
+        status = self.status
+        return status and all(s.is_ready for s in status)
