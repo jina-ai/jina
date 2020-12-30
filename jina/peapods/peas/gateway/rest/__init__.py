5401088a73296c747ae87372830ad696b99126c9
Yannic Kilcher <yk@users.noreply.github.com>
2020-12-30 17:54:50 +0100

    fix: fixes CORS behavior for REST API (#1568)

--- jina/peapods/peas/gateway/rest/__init__.py
+++ jina/peapods/peas/gateway/rest/__init__.py
@@ -21,7 +21,13 @@ class RESTGatewayPea(GatewayPea):
             from fastapi.middleware.cors import CORSMiddleware
 
         app = FastAPI(title=self.__class__.__name__)
-        app.add_middleware(CORSMiddleware, allow_origins=['*'])
+        app.add_middleware(
+                CORSMiddleware, 
+                allow_origins=['*'], 
+                allow_credentials=True,
+                allow_methods=['*'],
+                allow_headers=['*'],
+                )
         servicer = AsyncPrefetchCall(self.args)
 
         def error(reason, status_code):
