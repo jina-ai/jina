78add6770d854c6e73a34be5c85ac68884365d0e
Deepankar Mahapatro <deepankar.mahapatro@jina.ai>
2020-12-31 14:47:15 +0530

    test(gateway): changing rest_api param for gateway non blocking tests

--- tests/integration/gateway_non_blocking/test_gateway_non_blocking.py
+++ tests/integration/gateway_non_blocking/test_gateway_non_blocking.py
@@ -8,8 +8,9 @@ cur_dir = os.path.dirname(os.path.abspath(__file__))
 
 @pytest.mark.parametrize('parallel, expected_response', [(1, ['slow', 'fast']), (2, ['fast', 'slow'])])
 @pytest.mark.parametrize('rest_api', [False, True])
-def test_non_blocking_gateway(parallel, expected_response, rest_api, mocker):
-    os.environ['JINA_NON_BLOCKING_PARALLEL'] = str(parallel)
+def test_non_blocking_gateway(parallel, expected_response, rest_api, mocker, monkeypatch):
+    monkeypatch.setenv("JINA_NON_BLOCKING_PARALLEL", str(parallel))
+    monkeypatch.setenv("REST_API", rest_api)
     response = []
 
     def fill_responses(resp):
@@ -20,11 +21,10 @@ def test_non_blocking_gateway(parallel, expected_response, rest_api, mocker):
         'slow', 'fast'
     ]
 
-    with Flow(rest_api=rest_api).load_config(os.path.join(cur_dir, 'flow.yml')) as f:
+    with Flow().load_config(os.path.join(cur_dir, 'flow.yml')) as f:
         f.search(input_fn=data,
                  on_done=fill_responses,
                  batch_size=1,
                  callback_on='body')
 
-    del os.environ['JINA_NON_BLOCKING_PARALLEL']
     assert response == expected_response
