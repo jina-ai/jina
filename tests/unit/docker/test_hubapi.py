6f4147b11f001d80abe4e17af7e650f7c3ce7459
Rutuja Surve <rutuja.r.surve@gmail.com>
2021-01-04 15:54:16 +0530

    feat: add docker auth for hub (#1429)
    * feat: add docker auth for hub
    
    * fix: changes for access token fetch
    
    * fix: add get method for docker auth
    
    * feat: tests for docker auth hubapi
    
    * feat: mock get responses for docker auth
    
    * fix: add pytest fixture for docker auth token

--- tests/unit/docker/test_hubapi.py
+++ tests/unit/docker/test_hubapi.py
@@ -2,8 +2,13 @@ import json
 from logging import getLogger
 
 import mock
+import pytest
+import requests
 
+from jina.docker import hubapi
+from jina.docker.hubapi import JAML
 from jina.docker.hubapi import _fetch_access_token
+from jina.docker.hubapi import _docker_auth
 from jina.docker.hubapi import _list
 
 sample_manifest = {
@@ -55,3 +60,43 @@ def test_fetch_access_token(mocker):
     token_json = json.loads(token_string)
     mocker.return_value = token_json
     _fetch_access_token(logger=getLogger())
+
+
+@pytest.fixture(scope='function')
+def docker_jaml_token():
+    token_string = json.dumps({'hubapi': {'url': 'dummy_url', 'docker_auth': 'dummy_auth'}})
+    token_json = json.loads(token_string)
+    return token_json
+
+
+def test_docker_auth_success(mocker, docker_jaml_token):
+    mock_load = mocker.patch.object(hubapi.JAML, 'load', autospec=True)
+    mock_load.return_value = docker_jaml_token
+
+    mock_access_token = mocker.patch.object(hubapi, '_fetch_access_token', autospec=True)
+    mock_access_token.return_value = 'dummy_token'
+
+    mock_response = mocker.patch.object(requests, 'get', autospec=True)
+    mock_response.return_value.status_code = 200
+    mock_response.return_value.text = json.dumps({"docker_username": "amQ==", "docker_password": "amQ=="})
+
+    # Verify the fetched creds are as expected
+    fetch_cred = _docker_auth(logger=getLogger())
+    assert fetch_cred['docker_username'] == 'jd'
+    assert fetch_cred['docker_password'] == 'jd'
+
+
+def test_docker_auth_failure(mocker, docker_jaml_token):
+    mock_load = mocker.patch.object(hubapi.JAML, 'load', autospec=True)
+    mock_load.return_value = docker_jaml_token
+
+    mock_access_token = mocker.patch.object(hubapi, '_fetch_access_token', autospec=True)
+    mock_access_token.return_value = 'dummy_token'
+
+    mock_response = mocker.patch.object(requests, 'get', autospec=True)
+    mock_response.return_value.status_code = 403
+    mock_response.return_value.text = json.dumps({"message":"Missing Authentication Token"})
+
+    # If no token is fetched, docker auth fails
+    fetch_cred = _docker_auth(logger=getLogger())
+    assert fetch_cred == None
