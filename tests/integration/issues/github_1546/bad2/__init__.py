8f4bc221e2a821cef8bcd0824bafe0fb5c0c996d
Han Xiao <artex.xh@gmail.com>
2021-01-05 19:33:05 +0100

    fix: clarify py_modules usage in 1546 (#1597)
    * fix: clarify py_modules usage in 1549

---
+++ tests/integration/issues/github_1546/bad2/__init__.py
@@ -0,0 +1,9 @@
+from .helper import helper_function
+
+from jina.executors.crafters import BaseSegmenter
+
+
+class CustomCrafter2(BaseSegmenter):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        print(helper_function)
