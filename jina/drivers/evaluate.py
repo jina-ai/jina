b657e649ee7102bc4a1ac6e62368fac2d7af2259
Joan Fontanals Martinez <joan.martinez@jina.ai>
2021-01-07 12:33:34 +0100

    fix: fix eval name from driver

--- jina/drivers/evaluate.py
+++ jina/drivers/evaluate.py
@@ -55,7 +55,7 @@ class BaseEvaluateDriver(BaseExecutableDriver):
             if self._running_avg:
                 evaluation.value = self.exec.mean
 
-            if hasattr(self.exec, 'eval_at'):
+            if hasattr(self.exec, 'eval_at') and self.exec.eval_at is not None:
                 evaluation.op_name = f'{self.exec.__class__.__name__}@{self.exec.eval_at}'
             else:
                 evaluation.op_name = self.exec.__class__.__name__
