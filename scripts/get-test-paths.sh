9b258330c49fb7dbe0d00d867f22d268da955814
Han Xiao <artex.xh@gmail.com>
2021-01-08 22:33:31 +0100

    refactor: doc content hash

--- scripts/get-test-paths.sh
+++ scripts/get-test-paths.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -ex
+
 declare -a array1=( "unit/*.py" "integration/*.py" )
 declare -a array2=( $(ls -d tests/{unit,integration}/*/ | cut -f2,3 -d'/' | grep -v '__pycache__' ))
 dest=( "${array1[@]}" "${array2[@]}" )
