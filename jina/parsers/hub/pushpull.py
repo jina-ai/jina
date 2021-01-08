560ea35782b415b6cbb6fb23e7c51fbf8712f0fd
Joan Fontanals Martinez <joan.martinez@jina.ai>
2021-01-08 18:28:37 +0100

    fix: fix some bugs in hubio

--- jina/parsers/hub/pushpull.py
+++ jina/parsers/hub/pushpull.py
@@ -4,3 +4,5 @@ from ..helper import add_arg_group
 def mixin_hub_pushpull_parser(parser):
     gp = add_arg_group(parser, title='Push Pull')
     gp.add_argument('name', type=str, help='the name of the image.')
+    gp.add_argument('--no-overwrite', action='store_true', default=False,
+                    help='Do not allow overwriting existing images (based on module version and jina version)')
