7d55d389ecfc9afbb189fb675b33fbb1fcc1d154
Joan Fontanals Martinez <joan.martinez@jina.ai>
2021-01-11 15:54:02 +0100

    test: add extra jinad distributed test

---
+++ tests/jinad/integration/distributed/test_local_flow_loop_remote/test_integration.py
@@ -0,0 +1,32 @@
+import os
+
+import pytest
+
+from jina import Document
+from jina.flow import Flow
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+compose_yml = os.path.join(cur_dir, 'docker-compose.yml')
+flow_yml = os.path.join(cur_dir, 'flow.yml')
+
+
+@pytest.mark.parametrize('docker_compose', [compose_yml], indirect=['docker_compose'])
+def test_flow(docker_compose, mocker):
+    text = 'cats rules'
+    m = mocker.Mock()
+
+    def validate_output(resp):
+        m()
+        assert len(resp.index.docs) == 1
+        assert resp.index.docs[0].text == text
+
+    os.environ['JINA_CRAFTER_HOST'] = '172.28.1.1'
+    os.environ['JINA_INDEXER_HOST'] = '172.28.1.2'
+
+    with Document() as doc:
+        doc.content = text
+
+    with Flow.load_config(flow_yml) as f:
+        f.index([doc], on_done=validate_output)
+
+    m.assert_called_once()
