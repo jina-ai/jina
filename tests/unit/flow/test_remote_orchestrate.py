f70a738c496f972376f55d124b84d7fe13f0631d
Han Xiao <artex.xh@gmail.com>
2021-01-15 13:13:39 +0100

    refactor: remove sse logger

--- tests/unit/flow/test_remote_orchestrate.py
+++ tests/unit/flow/test_remote_orchestrate.py
@@ -23,13 +23,12 @@ def test_remote_pod_local_gateway(local_ip, on_public):
                                                  (get_public_ip(), True)])
 def test_remote_pod_local_pod_local_gateway(local_ip, on_public):
     remote_ip = '111.111.111.111'
-    f = Flow(expose_public=on_public).add(host=remote_ip).add()
-    f.build()
-    for k, v in f._pod_nodes.items():
+    f = Flow(expose_public=on_public).add(host=remote_ip).add().build()
+    for k, v in f:
         print(f'{v.name}\tIN: {v.address_in}\t{v.address_out}')
     assert f['pod0'].host_in == __default_host__
-    assert f['pod0'].host_out == local_ip
-    assert f['pod1'].host_in == __default_host__
+    assert f['pod0'].host_out == __default_host__
+    assert f['pod1'].host_in == remote_ip
     assert f['pod1'].host_out == __default_host__
     assert f['gateway'].host_in == __default_host__
     assert f['gateway'].host_out == remote_ip
@@ -41,14 +40,13 @@ def test_remote_pod_local_pod_remote_pod_local_gateway(local_ip, on_public):
     remote1 = '111.111.111.111'
     remote2 = '222.222.222.222'
 
-    f = Flow(expose_public=on_public).add(host=remote1).add().add(host=remote2)
-    f.build()
-    for k, v in f._pod_nodes.items():
+    f = Flow(expose_public=on_public).add(host=remote1).add().add(host=remote2).build()
+    for k, v in f:
         print(f'{v.name}\tIN: {v.address_in}\t{v.address_out}')
 
     assert f['pod0'].host_in == __default_host__
-    assert f['pod0'].host_out == local_ip
-    assert f['pod1'].host_in == __default_host__
+    assert f['pod0'].host_out == __default_host__
+    assert f['pod1'].host_in == remote1
     assert f['pod1'].host_out == remote2
     assert f['pod2'].host_in == __default_host__
     assert f['pod2'].host_out == __default_host__
@@ -74,4 +72,64 @@ def test_local_pod_remote_pod_remote_pod_local_gateway(local_ip, on_public):
     assert f['pod2'].host_in == __default_host__
     assert f['pod2'].host_out == __default_host__
     assert f['gateway'].host_in == remote2
+    assert f['gateway'].host_out == __default_host__
+
+
+def test_gateway_remote():
+    remote1 = '111.111.111.111'
+    f = Flow().add(host=remote1).build()
+
+    assert f['pod0'].host_in == __default_host__
+    assert f['pod0'].host_out == __default_host__
+    assert f['pod0'].args.socket_in.is_bind
+    assert f['pod0'].args.socket_out.is_bind
+    assert f['gateway'].host_in == remote1
+    assert f['gateway'].host_out == remote1
+
+
+def test_gateway_remote_local():
+    """
+
+    remote	IN: 0.0.0.0:61913 (PULL_BIND)	0.0.0.0:61914 (PUSH_BIND)
+    pod1	IN: 3.135.17.36:61914 (PULL_CONNECT)	0.0.0.0:61918 (PUSH_BIND)
+    gateway	IN: 0.0.0.0:61918 (PULL_CONNECT)	3.135.17.36:61913 (PUSH_CONNECT)
+
+    :return:
+    """
+    remote1 = '111.111.111.111'
+    f = Flow().add(host=remote1).add().build()
+
+    assert f['pod0'].host_in == __default_host__
+    assert f['pod0'].host_out == __default_host__
+    assert f['pod0'].args.socket_in.is_bind
+    assert f['pod0'].args.socket_out.is_bind
+    assert f['pod1'].host_in == remote1
+    assert f['pod1'].host_out == __default_host__
+    assert not f['pod1'].args.socket_in.is_bind
+    assert f['pod1'].args.socket_out.is_bind
+    assert f['gateway'].host_in == __default_host__
+    assert f['gateway'].host_out == remote1
+
+
+def test_gateway_local_remote():
+    """
+
+    pod0	IN: 0.0.0.0:62322 (PULL_BIND)	3.135.17.36:62326 (PUSH_CONNECT)
+    remote	IN: 0.0.0.0:62326 (PULL_BIND)	0.0.0.0:62327 (PUSH_BIND)
+    gateway	IN: 3.135.17.36:62327 (PULL_CONNECT)	0.0.0.0:62322 (PUSH_CONNECT)
+
+    :return:
+    """
+    remote1 = '111.111.111.111'
+    f = Flow().add().add(host=remote1).build()
+
+    assert f['pod0'].host_in == __default_host__
+    assert f['pod0'].host_out == remote1
+    assert f['pod0'].args.socket_in.is_bind
+    assert not f['pod0'].args.socket_out.is_bind
+    assert f['pod1'].host_in == __default_host__
+    assert f['pod1'].host_out == __default_host__
+    assert f['pod1'].args.socket_in.is_bind
+    assert f['pod1'].args.socket_out.is_bind
+    assert f['gateway'].host_in == remote1
     assert f['gateway'].host_out == __default_host__
