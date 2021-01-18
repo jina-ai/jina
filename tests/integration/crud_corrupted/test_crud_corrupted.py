658bea5fcb664881d2fc4915c9acb4cf286049a7
cristian <cristian.mitroi@jina.ai>
2021-01-18 10:32:15 +0100

    test: add only embedding

--- tests/integration/crud_corrupted/test_crud_corrupted.py
+++ tests/integration/crud_corrupted/test_crud_corrupted.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 
+import numpy as np
+
 from jina import Document
 from jina.executors.indexers import BaseIndexer
 from jina.flow import Flow
@@ -73,6 +75,69 @@ def test_only_tags(tmp_path, mocker):
     with f:
         f.search(input_fn=docs_search,
                  output_fn=validate_result_factory(EXPECTED_ONLY_TAGS_RESULTS))
+    mock.assert_called_once()
+
+    with f:
+        f.delete(input_fn=all_docs_indexed)
+    validate_index_size(0)
+
+    mock = mocker.Mock()
+    with f:
+        f.search(input_fn=docs_search,
+                 output_fn=validate_result_factory(0))
+    mock.assert_called_once()
+
+
+np.random.seed(0)
+
+EMBEDDING_SHAPE = (7)
+
+
+def random_docs_only_embedding(nr_docs, start=0):
+    for i in range(start, nr_docs + start):
+        d = Document()
+        d.embedding = np.random.random(EMBEDDING_SHAPE)
+        yield d
+
+
+def test_only_embedding(tmp_path, mocker):
+    config_environ(path=tmp_path)
+    flow_file = 'flow.yml'
+    docs = list(random_docs_only_embedding(NR_DOCS_INDEX))
+    docs_update = list(random_docs_only_embedding(NR_DOCS_INDEX, start=len(docs) + 1))
+    all_docs_indexed = docs.copy()
+    all_docs_indexed.extend(docs_update)
+    docs_search = list(random_docs_only_embedding(NUMBER_OF_SEARCHES, start=len(docs) + len(docs_update) + 1))
+    f = Flow.load_config(flow_file)
+
+    def validate_result_factory(num_matches):
+        def validate_results(resp):
+            mock()
+            assert len(resp.docs) == NUMBER_OF_SEARCHES
+            for doc in resp.docs:
+                assert len(doc.matches) == num_matches
+
+        return validate_results
+
+    with f:
+        f.index(input_fn=docs)
+    validate_index_size(NR_DOCS_INDEX)
+
+    mock = mocker.Mock()
+    with f:
+        f.search(input_fn=docs_search,
+                 output_fn=validate_result_factory(TOPK))
+    mock.assert_called_once()
+
+    # this won't increase the index size as the ids are new
+    with f:
+        f.update(input_fn=docs_update)
+    validate_index_size(NR_DOCS_INDEX)
+
+    mock = mocker.Mock()
+    with f:
+        f.search(input_fn=docs_search,
+                 output_fn=validate_result_factory(TOPK))
     mock.assert_called_once()
 
     with f:
