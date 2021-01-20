5a783555c29b2fa3e58addc3c4a1bdd769196f6c
Han Xiao <artex.xh@gmail.com>
2021-01-20 01:43:41 +0100

    test(daemon): add distribute tests (#1727)
    * test(daemon): add distribute tests

---
+++ tests/distributed/test_against_external_daemon/test_single_instance.py
@@ -0,0 +1,116 @@
+import os
+
+import numpy as np
+import pytest
+
+from jina import Flow
+
+CLOUD_HOST = 'cloud.jina.ai:8000'  # consider it as the staged version
+NUM_DOCS = 100
+
+
+@pytest.mark.parametrize('silent_log', [True, False])
+@pytest.mark.parametrize('parallels', [1, 2])
+def test_r_l_simple(silent_log, parallels, mocker):
+    response_mock = mocker.Mock()
+    f = (Flow(expose_public=True, parallel=parallels)
+         .add(host=CLOUD_HOST,
+              parallel=parallels,
+              silent_remote_logs=silent_log)
+         .add(parallel=parallels))
+    with f:
+        f.index(('hello' for _ in range(NUM_DOCS)), on_done=response_mock)
+
+    response_mock.assert_called()
+
+
+@pytest.mark.parametrize('silent_log', [True, False])
+@pytest.mark.parametrize('parallels', [1, 2])
+def test_l_r_simple(silent_log, parallels, mocker):
+    response_mock = mocker.Mock()
+
+    f = (Flow(expose_public=True, parallel=parallels)
+         .add(parallel=parallels)
+         .add(host=CLOUD_HOST,
+              parallel=parallels,
+              silent_remote_logs=silent_log)
+         )
+    with f:
+        f.index(('hello' for _ in range(NUM_DOCS)), on_done=response_mock)
+    response_mock.assert_called()
+
+
+@pytest.mark.parametrize('silent_log', [True, False])
+@pytest.mark.parametrize('parallels', [1, 2])
+def test_r_l_r_simple(silent_log, parallels, mocker):
+    response_mock = mocker.Mock()
+
+    f = (Flow(expose_public=True)
+         .add(host=CLOUD_HOST,
+              parallel=parallels,
+              silent_remote_logs=silent_log)
+         .add()
+         .add(host=CLOUD_HOST,
+              parallel=parallels,
+              silent_remote_logs=silent_log)
+         )
+    with f:
+        f.index(('hello' for _ in range(NUM_DOCS)), on_done=response_mock)
+    response_mock.assert_called()
+
+
+@pytest.mark.parametrize('silent_log', [True, False])
+@pytest.mark.parametrize('parallels', [1, 2])
+def test_r_r_r_simple(silent_log, parallels, mocker):
+    response_mock = mocker.Mock()
+
+    f = (Flow(expose_public=True)
+         .add(host=CLOUD_HOST,
+              parallel=parallels,
+              silent_remote_logs=silent_log)
+         .add(host=CLOUD_HOST,
+              parallel=parallels,
+              silent_remote_logs=silent_log)
+         .add(host=CLOUD_HOST,
+              parallel=parallels,
+              silent_remote_logs=silent_log)
+         )
+    with f:
+        f.index(('hello' for _ in range(NUM_DOCS)), on_done=response_mock)
+    response_mock.assert_called()
+
+
+@pytest.mark.parametrize('silent_log', [True, False])
+@pytest.mark.parametrize('parallels', [1, 2])
+def test_l_r_l_simple(silent_log, parallels, mocker):
+    response_mock = mocker.Mock()
+
+    f = (Flow(expose_public=True)
+         .add()
+         .add(host=CLOUD_HOST,
+              parallel=parallels,
+              silent_remote_logs=silent_log)
+         .add()
+         )
+    with f:
+        f.index(('hello' for _ in range(NUM_DOCS)), on_done=response_mock)
+    response_mock.assert_called()
+
+
+@pytest.mark.skipif('GITHUB_WORKFLOW' in os.environ,
+                    reason='somehow this upload test does not work on Github action, but locally it works fine!')
+@pytest.mark.parametrize('silent_log', [True, False])
+@pytest.mark.parametrize('parallels', [1, 2])
+def test_l_r_l_with_upload(silent_log, parallels, mocker):
+    response_mock = mocker.Mock()
+    f = (Flow()
+         .add()
+         .add(uses='mwu_encoder.yml',
+              host=CLOUD_HOST,
+              parallel=parallels,
+              upload_files=['mwu_encoder.py'],
+              silent_remote_logs=silent_log)
+         .add())
+    with f:
+        f.index_ndarray(np.random.random([NUM_DOCS, 100]), on_done=response_mock)
+    response_mock.assert_called()
