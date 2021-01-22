8c8077198064b5516bae77e459354c375ef0a1e7
Florian Hönicke <hoenicke.florian@gmail.com>
2021-01-22 13:19:29 +0100

    feat: optimizer command line

--- jina/parsers/__init__.py
+++ jina/parsers/__init__.py
@@ -98,6 +98,7 @@ def get_main_parser():
     from .hub import set_hub_parser
     from .logger import set_logger_parser
     from .ping import set_ping_parser
+    from .optimizer import set_optimizer_parser
 
     # create the top-level parser
     parser = set_base_parser()
@@ -120,6 +121,10 @@ def get_main_parser():
     set_flow_parser(sp.add_parser('flow',
                                   description='Start a Jina flow that consists of multiple pods',
                                   help='start a flow from a YAML file', formatter_class=_chf))
+
+    set_optimizer_parser(sp.add_parser('optimizer',
+                                  description='Start a Jina optimizer from a yaml configuration file',
+                                  help='start a optimizer from a YAML file', formatter_class=_chf))
 
     set_gateway_parser(sp.add_parser('gateway',
                                      description='Start a Jina gateway that receives client remote requests via gRPC',
