77a87aa1edee927cd99dccf2a5e8135386b9ae19
Florian Hönicke <hoenicke.florian@gmail.com>
2021-01-22 15:30:00 +0100

    refactor: crud example part 2

--- tests/integration/crud/advanced/yaml/custom_executor.py
+++ tests/integration/crud/advanced/yaml/custom_executor.py
@@ -2,9 +2,11 @@ import re
 import string
 from typing import Dict, List
 
+from jina.executors.decorators import batching, as_ndarray
+from jina.executors.encoders import BaseTextEncoder
 from jina.executors.rankers import Chunk2DocRanker
 from jina.executors.segmenters import BaseSegmenter
-
+import numpy as np
 
 class DummySentencizer(BaseSegmenter):
 
@@ -36,7 +38,7 @@ class DummySentencizer(BaseSegmenter):
             ))
         return results
 
-class DummyRanker(Chunk2DocRanker):
+class DummyMinRanker(Chunk2DocRanker):
     """
     :class:`MinRanker` calculates the score of the matched doc from the matched chunks. For each matched doc, the score
         is `1 / (1 + s)`, where `s` is the minimal score from all the matched chunks belonging to this doc.
@@ -52,3 +54,42 @@ class DummyRanker(Chunk2DocRanker):
 
     def _get_score(self, match_idx, query_chunk_meta, match_chunk_meta, *args, **kwargs):
         return self.get_doc_id(match_idx), 1. / (1. + match_idx[self.COL_SCORE].min())
+
+class DummyOneHotTextEncoder(BaseTextEncoder):
+    """
+    One-hot Encoder encodes the characters into one-hot vectors. ONLY FOR TESTING USAGES.
+    :param on_value: the default value for the locations represented by characters
+    :param off_value: the default value for the locations not represented by characters
+    """
+
+    def __init__(self,
+                 on_value: float = 1,
+                 off_value: float = 0,
+                 *args,
+                 **kwargs):
+        super().__init__(*args, **kwargs)
+        self.offset = 32
+        self.dim = 127 - self.offset + 2  # only the Unicode code point between 32 and 127 are embedded, and the rest are considered as ``UNK```
+        self.unk = self.dim
+        self.on_value = on_value
+        self.off_value = off_value
+        self.embeddings = None
+
+    def post_init(self):
+        self.embeddings = np.eye(self.dim) * self.on_value + \
+                          (np.ones((self.dim, self.dim)) - np.eye(self.dim)) * self.off_value
+
+
+    @batching
+    @as_ndarray
+    def encode(self, data: 'np.ndarray', *args, **kwargs) -> 'np.ndarray':
+        """
+
+        :param data: each row is one character, an 1d array of string type (data.dtype.kind == 'U') in size B
+        :return: an ndarray of `B x D`
+        """
+        output = []
+        for r in data:
+            r_emb = [ord(c) - self.offset if self.offset <= ord(c) <= 127 else self.unk for c in r]
+            output.append(self.embeddings[r_emb, :].sum(axis=0))
+        return np.array(output)
