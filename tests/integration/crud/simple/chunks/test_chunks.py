a800012f3dab7ab2ddacbb3ce10415a07a22b0e8
Susana Guzman <zuxi.w13@gmail.com>
2021-01-22 14:12:12 +0100

    fix: remove content_same

--- tests/integration/crud/simple/chunks/test_chunks.py
+++ tests/integration/crud/simple/chunks/test_chunks.py
@@ -17,7 +17,7 @@ def config(tmpdir):
     random.seed(0)
     np.random.seed(0)
     os.environ['JINA_CRUD_CHUNKS'] = str(tmpdir)
-    os.environ['JINA_TOPK'] = '10'
+    os.environ['JINA_TOPK'] = str(TOP_K)
     yield
     del os.environ['JINA_CRUD_CHUNKS']
     del os.environ['JINA_TOPK']
@@ -27,29 +27,20 @@ d_embedding = np.random.random([9])
 c_embedding = np.random.random([9])
 
 
-def document_generator(content_same, start, num_docs, num_chunks):
+def document_generator(start, num_docs, num_chunks):
     chunk_id = num_docs
     for idx in range(start, num_docs):
         with Document() as doc:
             doc.id = idx
-            if content_same:
-                doc.tags['id'] = idx
-                doc.text = 'I have cats'
-                doc.embedding = d_embedding
-            else:
-                doc.text = f'I have {idx} cats'
-                doc.embedding = np.random.random([9])
+            doc.tags['id'] = idx
+            doc.text = f'I have {idx} cats'
+            doc.embedding = np.random.random([9])
             for chunk_idx in range(num_chunks):
                 with Document() as chunk:
                     chunk.id = chunk_id
-                    if content_same:
-                        chunk.tags['id'] = chunk_idx
-                        chunk.text = 'I have chunky cats'
-                        chunk.embedding = c_embedding
-                    else:
-                        chunk.tags['id'] = chunk_idx
-                        chunk.text = f'I have {chunk_idx} chunky cats'
-                        chunk.embedding = np.random.random([9])
+                    chunk.tags['id'] = chunk_idx
+                    chunk.text = f'I have {chunk_idx} chunky cats'
+                    chunk.embedding = np.random.random([9])
                 chunk_id += 1
                 doc.chunks.append(chunk)
         yield doc
@@ -64,11 +55,8 @@ def validate_index_size(num_indexed_docs):
         assert index.size == num_indexed_docs
 
 
-@pytest.mark.parametrize('flow_file, content_same', [
-    ['flow_vector.yml', False],
-    ['flow_vector.yml', True]
-])
-def test_delete_vector(config, mocker, flow_file, content_same):
+@pytest.mark.parametrize('flow_file', ['flow_vector.yml'])
+def test_delete_vector(config, mocker, flow_file):
     num_searches = 10
     num_docs = 10
     num_chunks = 5
@@ -84,41 +72,38 @@ def test_delete_vector(config, mocker, flow_file, content_same):
 
     with Flow.load_config(flow_file) as index_flow:
         index_flow.index(
-            input_fn=document_generator(content_same=content_same, start=0, num_docs=num_docs, num_chunks=num_chunks))
-    validate_index_size(50) #5 chunks for each of the 10 docs
+            input_fn=document_generator(start=0, num_docs=num_docs, num_chunks=num_chunks))
+    validate_index_size(num_chunks*num_docs) #5 chunks for each of the 10 docs
 
     mock = mocker.Mock()
     with Flow.load_config(flow_file) as search_flow:
         search_flow.search(
-            input_fn=document_generator(content_same=content_same, start=0, num_docs=num_docs, num_chunks=num_chunks),
+            input_fn=document_generator(start=0, num_docs=num_docs, num_chunks=num_chunks),
             on_done=validate_result_factory(TOP_K))
     mock.assert_called_once()
 
     with Flow.load_config(flow_file) as index_flow:
         index_flow.delete(
-            input_fn=document_generator(content_same=content_same, start=0, num_docs=num_docs, num_chunks=num_chunks))
+            input_fn=document_generator(start=0, num_docs=num_docs, num_chunks=num_chunks))
     validate_index_size(0)
 
     mock = mocker.Mock()
     with Flow.load_config(flow_file) as search_flow:
         search_flow.search(
-            input_fn=document_generator(content_same=content_same, start=0, num_docs=num_docs, num_chunks=num_chunks),
+            input_fn=document_generator(start=0, num_docs=num_docs, num_chunks=num_chunks),
             on_done=validate_result_factory(0))
     mock.assert_called_once()
 
 
-@pytest.mark.parametrize('flow_file, content_same', [
-    ['flow_vector.yml', False],
-    ['flow_vector.yml', True]
-])
-def test_update_vector(config, mocker, flow_file, content_same):
+
+@pytest.mark.parametrize('flow_file', ['flow_vector.yml'])
+def test_update_vector(config, mocker, flow_file):
     num_searches = 10
     num_docs = 10
     num_chunks = 5
-    num_matches = num_docs * num_chunks
 
-    docs_before = list(document_generator(content_same=content_same, start=0, num_docs=num_docs, num_chunks=num_chunks))
-    docs_updated = list(document_generator(content_same=content_same, start=10, num_docs=20, num_chunks=num_chunks))
+    docs_before = list(document_generator(start=0, num_docs=num_docs, num_chunks=num_chunks))
+    docs_updated = list(document_generator(start=10, num_docs=20, num_chunks=num_chunks))
     ids_before = list()
     ids_updated = list()
 
@@ -142,23 +127,23 @@ def test_update_vector(config, mocker, flow_file, content_same):
 
     with Flow.load_config(flow_file) as index_flow:
         index_flow.index(input_fn=docs_before)
-    validate_index_size(num_matches) #num_docs per all its chunks, 50 in this case
+    validate_index_size(num_chunks*num_docs) #num_docs per all its chunks, 50 in this case
 
     mock = mocker.Mock()
     with Flow.load_config(flow_file) as search_flow:
         search_flow.search(
-            input_fn=document_generator(content_same=content_same, start=0, num_docs=num_docs, num_chunks=num_chunks),
+            input_fn=document_generator(start=0, num_docs=num_docs, num_chunks=num_chunks),
             on_done=validate_result_factory(has_changed=False, num_matches=TOP_K))
     mock.assert_called_once()
 
     with Flow.load_config(flow_file) as index_flow:
         index_flow.update(input_fn=docs_updated)
-    validate_index_size(num_matches) #num_docs per all its chunks, 50 in this case
+    validate_index_size(num_chunks*num_docs) #num_docs per all its chunks, 50 in this case
 
     mock = mocker.Mock()
     with Flow.load_config(flow_file) as search_flow:
         search_flow.search(
-            input_fn=document_generator(content_same=content_same, start=10, num_docs=20, num_chunks=num_chunks),
+            input_fn=document_generator(start=10, num_docs=20, num_chunks=num_chunks),
             on_done=validate_result_factory(has_changed=True, num_matches=num_docs))
     mock.assert_called_once()
 
