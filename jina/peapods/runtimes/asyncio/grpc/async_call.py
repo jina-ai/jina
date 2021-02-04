c6599eb72ea68fe284d6af4bfe98f96da696e670
Han Xiao <han.xiao@jina.ai>
2021-02-04 14:05:18 +0100

    fix(grpc): fix zmqlet lifetime in grpc

--- jina/peapods/runtimes/asyncio/grpc/async_call.py
+++ jina/peapods/runtimes/asyncio/grpc/async_call.py
@@ -1,6 +1,5 @@
 import asyncio
 
-from ....zmq import AsyncZmqlet
 from .....logging import JinaLogger
 from .....logging.profile import TimeContext
 from .....proto import jina_pb2_grpc
@@ -25,38 +24,18 @@ class AsyncPrefetchCall(jina_pb2_grpc.JinaRPCServicer):
             msg.add_route(self.name, hex(id(self)))
             return msg.response
 
-        # async for next_request in request_iterator:
-        #     await self.zmqlet.send_message(Message(None, next_request, 'gateway', **vars(self.args)))
-        #     yield await self.zmqlet.recv_message(callback=handle)
-
-        # this restricts the gateway can not be the joiner to wait
-        # as every request corresponds to one message, #send_message = #recv_message
         prefetch_task = []
-        onrecv_task = []
 
         async def prefetch_req(num_req, fetch_to):
-            for _ in range(num_req):
-                try:
-                    if hasattr(request_iterator, '__anext__'):
-                        # This code block will be executed for gRPC based invocations
-                        # An iterator gets converted to a grpc._cython.cygrpc._MessageReceiver object,
-                        # which doesn't have a __next__, only an __anext__ method.
-                        # If there's any issue with the request_iterator, __anext__() never fails, just hangs.
-                        # Adding a default timeout of 2 secs for the anext to avoid hang.
-                        # To cancel on large request (will fail/segfault on large request):
-                        # await asyncio.wait_for(request_iterator.__anext__(), timeout=2)
-                        next_request = await request_iterator.__anext__()
-                    elif hasattr(request_iterator, '__next__'):
-                        # This code block will be executed for REST based invocations
-                        next_request = next(request_iterator)
-                    else:
-                        break
-                    asyncio.create_task(
-                        self.zmqlet.send_message(Message(None, next_request, 'gateway', **vars(self.args))))
-                    fetch_to.append(asyncio.create_task(self.zmqlet.recv_message(callback=handle)))
-                except (StopIteration, StopAsyncIteration):
-                    return True
-            return False
+            nf = 0
+            async for next_request in request_iterator:
+                asyncio.create_task(
+                    self.zmqlet.send_message(Message(None, next_request, 'gateway', **vars(self.args))))
+                fetch_to.append(asyncio.create_task(self.zmqlet.recv_message(callback=handle)))
+                nf += 1
+                if nf == num_req:
+                    return False  # aiter is not empty yet
+            return True  # aiter is empty
 
         with TimeContext(f'prefetching {self.args.prefetch} requests', self.logger):
             self.logger.warning('if this takes too long, you may want to take smaller "--prefetch" or '
@@ -68,13 +47,25 @@ class AsyncPrefetchCall(jina_pb2_grpc.JinaRPCServicer):
                                   'you can use "Client.check_input(input_fn)"')
                 return
 
-        while not (self.zmqlet.msg_sent == self.zmqlet.msg_recv != 0 and is_req_empty):
-            self.logger.info(f'send: {self.zmqlet.msg_sent} '
-                             f'recv: {self.zmqlet.msg_recv} '
-                             f'pending: {self.zmqlet.msg_sent - self.zmqlet.msg_recv}')
-            onrecv_task.clear()
+        # the total num requests < self.args.prefetch
+        if is_req_empty:
             for r in asyncio.as_completed(prefetch_task):
                 yield await r
-                is_req_empty = await prefetch_req(self.args.prefetch_on_recv, onrecv_task)
-            prefetch_task.clear()
-            prefetch_task = [j for j in onrecv_task]
+        else:
+            # if there are left over (`else` clause above is unnecessary for code but for better readability)
+            onrecv_task = []
+
+            # the following code "interleaves" prefetch_task and onrecv_task, when one dries, it switches to the other
+            while not is_req_empty:
+                self.logger.info(f'send: {self.zmqlet.msg_sent} '
+                                 f'recv: {self.zmqlet.msg_recv} '
+                                 f'pending: {self.zmqlet.msg_sent - self.zmqlet.msg_recv}')
+                onrecv_task.clear()
+                for r in asyncio.as_completed(prefetch_task):
+                    yield await r
+                    if not is_req_empty:
+                        is_req_empty = await prefetch_req(self.args.prefetch_on_recv, onrecv_task)
+
+                # this list dries, clear it and feed it with on_recv_task
+                prefetch_task.clear()
+                prefetch_task = [j for j in onrecv_task]
