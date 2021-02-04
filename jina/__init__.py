5cbe03c2cfe5f7493a9c8bc01b296bfce4313dba
Florian Hönicke <hoenicke.florian@gmail.com>
2021-02-04 12:37:39 +0100

    refactor: remove groundtruth from update (#1858)

--- jina/__init__.py
+++ jina/__init__.py
@@ -57,7 +57,7 @@ __version__ = '0.9.29'
 
 # do not change this line manually
 # this is managed by proto/build-proto.sh and updated on every execution
-__proto_version__ = '0.0.77'
+__proto_version__ = '0.0.78'
 
 __uptime__ = _datetime.datetime.now().isoformat()
 
