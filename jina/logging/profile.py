94a7893ea5c6498a617273679b4935fd3beb95df
YongxuanZhang <yongxuanzhang1009@gmail.com>
2021-02-06 23:15:13 -0500

    docs(logging): update profile docs

--- jina/logging/profile.py
+++ jina/logging/profile.py
@@ -14,9 +14,11 @@ if False:
 
 
 def used_memory(unit: int = 1024 * 1024 * 1024) -> float:
-    """Get the memory usage of the current process.
+    """
+    Get the memory usage of the current process.
 
-    :param unit: unit of the memory, default in Gigabytes
+    :param unit: Unit of the memory, default in Gigabytes.
+    :return: Memory usage of the current process.
     """
     try:
         import resource
@@ -29,15 +31,17 @@ def used_memory(unit: int = 1024 * 1024 * 1024) -> float:
 
 
 def used_memory_readable() -> str:
-    """ Get the memory usage of the current process in a human-readable format
+    """
+    Get the memory usage of the current process in a human-readable format.
 
-    :return:
+    :return: Memory usage of the current process.
     """
     return get_readable_size(used_memory(1))
 
 
 def profiling(func):
-    """Decorator to mark a function for profiling. The time and memory usage will be recorded and printed.
+    """
+    Decorator to mark a function for profiling. The time and memory usage will be recorded and printed.
 
     Example:
 
@@ -68,6 +72,8 @@ def profiling(func):
 
 
 class TimeDict:
+    """Records of time information."""
+
     def __init__(self):
         self.accum_time = defaultdict(float)
         self.first_start_time = defaultdict(float)
@@ -96,6 +102,11 @@ class TimeDict:
         return self
 
     def reset(self):
+        """
+        Clear the time information.
+
+        :return: None
+        """
         if self._key_stack:
             self._pending_reset = True
         else:
@@ -111,15 +122,15 @@ class TimeDict:
 
 
 class TimeContext:
-    """Timing a code snippet with a context manager """
+    """Timing a code snippet with a context manager. """
 
     time_attrs = ['years', 'months', 'days', 'hours', 'minutes', 'seconds']
 
     def __init__(self, task_name: str, logger: 'JinaLogger' = None):
         """
 
-        :param task_name: the context/message
-        :param logger: use existing logger or use naive :func:`print`
+        :param task_name: The context/message.
+        :param logger: Use existing logger or use naive :func:`print`.
 
         Example:
 
@@ -160,7 +171,8 @@ class TimeContext:
 
 
 class ProgressBar(TimeContext):
-    """A simple progress bar
+    """
+    A simple progress bar.
 
     Example:
 
@@ -174,8 +186,10 @@ class ProgressBar(TimeContext):
     def __init__(self, bar_len: int = 20, task_name: str = '', batch_unit: str = 'batch', logger=None):
         """
 
-        :param bar_len: total length of the bar
-        :param task_name: the name of the task, will be displayed in front of the bar
+        :param bar_len: Total length of the bar.
+        :param task_name: The name of the task, will be displayed in front of the bar.
+        :param batch_unit: Unit of batch
+        :param logger: Jina logger
         """
         super().__init__(task_name, logger)
         self.bar_len = bar_len
@@ -184,9 +198,10 @@ class ProgressBar(TimeContext):
         self.batch_unit = batch_unit
 
     def update_tick(self, tick: float = .1) -> None:
-        """ Increment the progress bar by one tick, when the ticks accumulate to one, trigger one :meth:`update`
+        """
+        Increment the progress bar by one tick, when the ticks accumulate to one, trigger one :meth:`update`.
 
-        :param tick: a float unit to increment (should < 1)
+        :param tick: A float unit to increment (should < 1).
         """
         self._ticks += tick
         if self._ticks > 1:
@@ -194,9 +209,10 @@ class ProgressBar(TimeContext):
             self._ticks = 0
 
     def update(self, progress: int = None, *args, **kwargs) -> None:
-        """ Increment the progress bar by one unit
+        """
+        Increment the progress bar by one unit.
 
-        :param progress: the number of unit to increment
+        :param progress: The number of unit to increment.
         """
         self.num_reqs += 1
         sys.stdout.write('\r')
