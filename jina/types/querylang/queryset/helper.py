68f8d4c6f2d8e4d855276e5aa097582d4e73ded3
Joan Fontanals Martinez <joan.martinez@jina.ai>
2021-02-12 09:55:33 +0100

    refactor: keep relative imports

--- jina/types/querylang/queryset/helper.py
+++ jina/types/querylang/queryset/helper.py
@@ -1,7 +1,7 @@
 from functools import partial
 from typing import Callable, List, Type, Union, Iterable
 
-from jina.excepts import LookupyError
+from ....excepts import LookupyError
 
 
 def iff(precond: Callable, val: Union[int, str], f: Callable) -> bool:
