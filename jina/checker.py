cd34f4e3a3c23e7b2b75d1fe8e715e7528eca6dd
Shubhendra Singh Chauhan <withshubh@gmail.com>
2021-02-16 09:51:39 +0000

    chore: pass string format arguments as logging method parameters

--- jina/checker.py
+++ jina/checker.py
@@ -73,7 +73,7 @@ class NetworkChecker:
                 with TimeContext(f'ping {ctrl_addr} at {j} round', default_logger) as tc:
                     r = send_ctrl_message(ctrl_addr, 'STATUS', timeout=args.timeout)
                     if not r:
-                        default_logger.warning('not responding, retry (%d/%d) in 1s' % (j + 1, args.retries))
+                        default_logger.warning('not responding, retry (%d/%d) in 1s', j + 1, args.retries)
                     else:
                         total_success += 1
                         if args.print_response:
@@ -81,8 +81,7 @@ class NetworkChecker:
                 total_time += tc.duration
                 time.sleep(1)
             if total_success < args.retries:
-                default_logger.warning('message lost %.0f%% (%d/%d) ' % (
-                    (1 - total_success / args.retries) * 100, args.retries - total_success, args.retries))
+                default_logger.warning('message lost %.0f%% (%d/%d) ', (1 - total_success / args.retries) * 100, args.retries - total_success, args.retries)
             if total_success > 0:
                 default_logger.success('avg. latency: %.0f ms' % (total_time / total_success * 1000))
                 exit(0)
