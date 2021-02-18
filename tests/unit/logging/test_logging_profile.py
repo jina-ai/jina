c8e6f0071e070eae14110b09ff53d5eb894d7e35
YongxuanZhang <yongxuanzhang1009@gmail.com>
2021-02-18 09:05:31 -0500

    test(logging): fix quote

--- tests/unit/logging/test_logging_profile.py
+++ tests/unit/logging/test_logging_profile.py
@@ -1,14 +1,19 @@
-from jina.logging.profile import TimeDict
 import time
+
+from jina.logging.profile import TimeDict
 
 
 def test_logging_profile_timedict():
     td = TimeDict()
-    td("timer")
+    td('timer')
     with td:
         time.sleep(2)
 
-    assert int(td.accum_time["timer"]) == 2
-    assert str(td) == "timer: 2.0s"
+    assert int(td.accum_time['timer']) == 2
+    assert str(td) == 'timer: 2.0s'
+
     td.reset()
+    assert len(td.accum_time) == 0
+    assert len(td.first_start_time) == 0
     assert len(td.start_time) == 0
+    assert len(td.end_time) == 0
