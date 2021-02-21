4e7df6e27872d2ed7723240ab38157dd8336f6dd
Deepankar Mahapatro <deepankar.mahapatro@jina.ai>
2021-02-21 13:00:15 +0530

    fix(rest): default request size from parser

--- jina/peapods/runtimes/asyncio/rest/models.py
+++ jina/peapods/runtimes/asyncio/rest/models.py
@@ -1,9 +1,10 @@
-from jina.types.document import Document
 from typing import Callable, Dict, Any, Optional, List, Union
 
 from pydantic import Field, BaseModel, create_model
 
 from jina.enums import DataInputType
+from jina.types.document import Document
+from jina.parsers import set_client_cli_parser
 from jina.proto.jina_pb2 import DocumentProto, QueryLangProto
 
 
@@ -28,13 +29,14 @@ def build_model_from_pb(name: str, pb_model: Callable):
 JinaDocumentModel = build_model_from_pb('Document', DocumentProto)
 JinaDocumentModel.update_forward_refs()
 JinaQueryLangModel = build_model_from_pb('QueryLang', QueryLangProto)
+default_request_size = set_client_cli_parser().parse_args([]).request_size
 
 
 class JinaRequestModel(BaseModel):
     # To avoid an error while loading the request model schema on swagger, we've added an example.
     data: Union[List[JinaDocumentModel], List[Dict[str, Any]], List[str], List[bytes]] = \
         Field(..., example=[Document().dict()])
-    request_size: Optional[int] = 1
+    request_size: Optional[int] = default_request_size
     mime_type: Optional[str] = ''
     queryset: Optional[List[JinaQueryLangModel]] = None
     data_type: DataInputType = DataInputType.AUTO
