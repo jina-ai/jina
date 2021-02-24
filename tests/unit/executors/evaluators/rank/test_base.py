5167c734154a83148fb8ada3c26a7262c74de749
Nan Wang <Nan.Wang@jina.ai>
2021-02-24 23:27:49 +0800

    fix: fix unit tests

--- tests/unit/executors/evaluators/rank/test_base.py
+++ tests/unit/executors/evaluators/rank/test_base.py
@@ -8,15 +8,15 @@ class DummyRankingEvaluator(BaseRankingEvaluator):
 
 
 def test_base_ranking_evalutor():
-    from jina.jaml import JAML
-    from pkg_resources import resource_filename
-    with open(resource_filename('jina',
-                            '/'.join(('resources', 'executors.requests.BaseRankingEvaluator.yml')))) as fp:
-        config_from_resources = JAML.load(fp)
     evaluator = DummyRankingEvaluator()
     actual_eval_driver = evaluator._drivers['SearchRequest'][-1]
     assert isinstance(actual_eval_driver, RankEvaluateDriver)
     default_eval_driver = RankEvaluateDriver()
-    assert default_eval_driver.fields != actual_eval_driver.fields
-    # default value for fields in RankEvaluateDriver is overwritten by `executors.requests.BaseRankingEvaluator.yml`
-    assert actual_eval_driver.fields == config_from_resources['on']['SearchRequest']['drivers'][-1].fields
+    assert list(default_eval_driver.fields) == actual_eval_driver.fields
+    # make sure the default value for fields in RankEvaluateDriver is no longer overwritten by `executors.requests.BaseRankingEvaluator.yml`
+    from jina.jaml import JAML
+    from pkg_resources import resource_filename
+    with open(resource_filename('jina',
+                                '/'.join(('resources', 'executors.requests.BaseRankingEvaluator.yml')))) as fp:
+        config_from_resources = JAML.load(fp)
+    assert default_eval_driver.fields == config_from_resources['on']['SearchRequest']['drivers'][-1].fields
