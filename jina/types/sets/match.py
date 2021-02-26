dc0ef98e4cdaedb31d3ba0ecf4a8ae9e71ef24cb
David Buchaca Prats <davidbuchaca@gmail.com>
2021-02-26 23:51:33 +0100

    fix: removing problematic mime type assignment (#2025)

--- jina/types/sets/match.py
+++ jina/types/sets/match.py
@@ -11,11 +11,11 @@ class MatchSet(DocumentSet):
 
     :param docs_proto: Set of matches of the `reference_doc`
     :type docs_proto: :class:`Document`
-    :para reference_doc: Reference :class:`Document` for the sub-documents
+    :param reference_doc: Reference :class:`Document` for the sub-documents
     :type reference_doc: :class:`Document`
     """
+
     def __init__(self, docs_proto, reference_doc: 'Document'):
-        """Set constructor method."""
         super().__init__(docs_proto)
         self._ref_doc = reference_doc
 
@@ -24,6 +24,7 @@ class MatchSet(DocumentSet):
 
         :param document: Sub-document to be added
         :type document: :class: `Document
+        :param kwargs: Extra key value arguments
         :return: the newly added sub-document in :class:`Document` view
         :rtype: :class:`Document` view
         """
@@ -35,22 +36,25 @@ class MatchSet(DocumentSet):
         match.set_attrs(granularity=self.granularity, adjacency=self.adjacency, **kwargs)
         match.score.ref_id = self._ref_doc.id
 
-        if not match.mime_type:
-            match.mime_type = self._ref_doc.mime_type
-
         return match
 
     @property
     def reference_doc(self) -> 'Document':
-        """Get the document that this :class:`MatchSet` referring to."""
+        """Get the document that this :class:`MatchSet` referring to.
+        :return: the document the match refers to
+        """
         return self._ref_doc
 
     @property
     def granularity(self) -> int:
-        """Get granularity of all document in this set."""
+        """Get granularity of all document in this set.
+        :return: the granularity of the documents of which these are match
+        """
         return self._ref_doc.granularity
 
     @property
     def adjacency(self) -> int:
-        """Get the adjacency of all document in this set."""
+        """Get the adjacency of all document in this set.
+        :return: the adjacency of the set of matches
+        """
         return self._ref_doc.adjacency + 1
