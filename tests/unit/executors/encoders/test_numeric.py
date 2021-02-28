674db662cd004af42ed4127db00aa29a42ee2671
YongxuanZhang <yongxuanzhang1009@gmail.com>
2021-02-28 11:53:43 -0500

    test(encoders): split tests

--- tests/unit/executors/encoders/test_numeric.py
+++ tests/unit/executors/encoders/test_numeric.py
@@ -7,26 +7,26 @@ input_dim = 5
 target_output_dim = 5
 
 
-@pytest.fixture
-def model():
-    class SimpleModel:
+class SimpleModel:
+    def fit(self, data):
+        return data
 
-        def fit(self, data):
-            return data
-
-        def transform(self, data):
-            return data
-
-    simple_model = SimpleModel()
-    return simple_model
+    def transform(self, data):
+        return data
 
 
-def test_test_transformencoder(model):
+simple_model = SimpleModel()
+encoder = TransformEncoder(output_dim=target_output_dim)
+
+
+def test_transformencoder_train():
     train_data = np.random.rand(100, input_dim)
-    encoder = TransformEncoder(output_dim=target_output_dim)
-    encoder.model = model
+    encoder.model = simple_model
     encoder.train(train_data)
+    assert encoder.train
 
+
+def test_transformencoder_test():
     test_data = np.random.rand(10, input_dim)
     encoded_data = encoder.encode(test_data)
     assert encoded_data.shape == (test_data.shape[0], target_output_dim)
