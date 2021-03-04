1e537f63d0fe55f5a1972db677caed0aa40da17b
cristian mtr <cristian.mitroi@jina.ai>
2021-03-04 16:37:53 +0100

    feat: dump on delete binarypb

--- jina/executors/indexers/keyvalue.py
+++ jina/executors/indexers/keyvalue.py
@@ -2,6 +2,7 @@ __copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
 import mmap
+import os
 from typing import Iterable, Optional
 
 import numpy as np
@@ -13,7 +14,11 @@ HEADER_NONE_ENTRY = (-1, -1, -1)
 
 
 class BinaryPbIndexer(BaseKVIndexer):
-    """Simple Key-value indexer."""
+    """Simple Key-value indexer that writes to disk
+
+    :param delete_on_dump: whether to delete the entries that were marked as 'deleted'
+    """
+
     class WriteHandler:
         """
         Write file handler.
@@ -21,6 +26,7 @@ class BinaryPbIndexer(BaseKVIndexer):
         :param path: Path of the file.
         :param mode: Writing mode. (e.g. 'ab', 'wb')
         """
+
         def __init__(self, path, mode):
             self.body = open(path, mode)
             self.header = open(path + '.head', mode)
@@ -42,6 +48,7 @@ class BinaryPbIndexer(BaseKVIndexer):
         :param path: Path of the file.
         :param key_length: Length of key.
         """
+
         def __init__(self, path, key_length):
             with open(path + '.head', 'rb') as fp:
                 tmp = np.frombuffer(fp.read(),
@@ -55,6 +62,52 @@ class BinaryPbIndexer(BaseKVIndexer):
         def close(self):
             """Close the file."""
             self._body.close()
+
+    def __getstate__(self):
+        # called on pickle save
+        if self.delete_on_dump:
+            self._delete_invalid_indices()
+        d = super().__getstate__()
+        return d
+
+    def _delete_invalid_indices(self):
+        if self.query_handler:
+            self.query_handler.close()
+        if self.write_handler:
+            self.write_handler.flush()
+            self.write_handler.close()
+
+        keys = []
+        vals = []
+        # we read the valid values and write them to the intermediary file
+        read_handler = self.ReadHandler(self.index_abspath, self.key_length)
+        for key in read_handler.header.keys():
+            pos_info = read_handler.header.get(key, None)
+            if pos_info:
+                p, r, l = pos_info
+                with mmap.mmap(read_handler.body, offset=p, length=l) as m:
+                    keys.append(key)
+                    vals.append(m[r:])
+        read_handler.close()
+        if len(keys) == 0:
+            return
+
+        # intermediary file
+        tmp_file = self.index_abspath + '-tmp'
+        self._start = 0
+        filtered_data_writer = self.WriteHandler(tmp_file, 'ab')
+        # reset size
+        self._size = 0
+        self._add(keys, vals, filtered_data_writer)
+        filtered_data_writer.close()
+
+        # replace orig. file
+        # and .head file
+        head_path = self.index_abspath + '.head'
+        os.remove(self.index_abspath)
+        os.remove(head_path)
+        os.rename(tmp_file, self.index_abspath)
+        os.rename(tmp_file + '.head', head_path)
 
     def get_add_handler(self) -> 'WriteHandler':
         """
@@ -82,11 +135,14 @@ class BinaryPbIndexer(BaseKVIndexer):
         """
         return self.ReadHandler(self.index_abspath, self.key_length)
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self,
+                 delete_on_dump: bool = False,
+                 *args,
+                 **kwargs):
         super().__init__(*args, **kwargs)
-        self._total_byte_len = 0
         self._start = 0
         self._page_size = mmap.ALLOCATIONGRANULARITY
+        self.delete_on_dump = delete_on_dump
 
     def add(self, keys: Iterable[str], values: Iterable[bytes], *args, **kwargs) -> None:
         """Add the serialized documents to the index via document ids.
@@ -96,23 +152,10 @@ class BinaryPbIndexer(BaseKVIndexer):
         :param args: extra arguments
         :param kwargs: keyword arguments
         """
-        if not keys:
+        if not any(keys):
             return
 
-        for key, value in zip(keys, values):
-            l = len(value)  #: the length
-            p = int(self._start / self._page_size) * self._page_size  #: offset of the page
-            r = self._start % self._page_size  #: the remainder, i.e. the start position given the offset
-            self.write_handler.header.write(
-                np.array(
-                    (key, p, r, r + l),
-                    dtype=[('', (np.str_, self.key_length)), ('', np.int64), ('', np.int64), ('', np.int64)]
-                ).tobytes()
-            )
-            self._start += l
-            self.write_handler.body.write(value)
-            self._size += 1
-        self.write_handler.flush()
+        self._add(keys, values, writer=self.write_handler)
 
     def query(self, key: str) -> Optional[bytes]:
         """Find the serialized document to the index via document id.
@@ -164,6 +207,23 @@ class BinaryPbIndexer(BaseKVIndexer):
         keys = self._filter_nonexistent_keys(keys, self.query_handler.header.keys())
         if keys:
             self._delete(keys)
+
+    def _add(self, keys: Iterable[str], values: Iterable[bytes], writer: WriteHandler):
+        for key, value in zip(keys, values):
+            l = len(value)  #: the length
+            p = int(self._start / self._page_size) * self._page_size  #: offset of the page
+            r = self._start % self._page_size  #: the remainder, i.e. the start position given the offset
+            # noinspection PyTypeChecker
+            writer.header.write(
+                np.array(
+                    (key, p, r, r + l),
+                    dtype=[('', (np.str_, self.key_length)), ('', np.int64), ('', np.int64), ('', np.int64)]
+                ).tobytes()
+            )
+            self._start += l
+            writer.body.write(value)
+            self._size += 1
+        writer.flush()
 
 
 class DataURIPbIndexer(BinaryPbIndexer):
