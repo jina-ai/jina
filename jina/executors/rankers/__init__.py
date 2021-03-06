ac5ac2b13788bdc9612a530bd4e356c648f2fde8
Joan Fontanals <joan.martinez@jina.ai>
2021-03-06 10:56:50 +0100

    fix: update jina/executors/rankers/__init__.py
    Co-authored-by: Nan Wang <nan.wang@jina.ai>

--- jina/executors/rankers/__init__.py
+++ jina/executors/rankers/__init__.py
@@ -137,6 +137,6 @@ class Match2DocRanker(BaseRanker):
 
         :param old_match_scores: Contains old scores in a list
         :param query_meta: Dictionary containing all the query meta information requested by the `query_required_keys` class_variable.
-        :param match_meta: List containing all the matches meta information requested by the `required_keys` class_variable. Sorted in the same way as `old_match_scores`
+        :param match_meta: List containing all the matches meta information requested by the `match_required_keys` class_variable. Sorted in the same way as `old_match_scores`
         """
         raise NotImplementedError
