2a90694818443f7fc7db804b65abc9ead9320eaf
YongxuanZhang <yongxuanzhang1009@gmail.com>
2021-03-08 17:10:49 -0500

    test: refactor rank driver test

--- tests/unit/drivers/rank/test_matches2doc_rank_drivers.py
+++ tests/unit/drivers/rank/test_matches2doc_rank_drivers.py
@@ -1,8 +1,6 @@
-import numpy as np
-
-from jina import Document
 from jina.drivers.rank import Matches2DocRankDriver
 from jina.executors.rankers import Match2DocRanker
+from jina.proto import jina_pb2
 from jina.types.sets import DocumentSet
 
 
@@ -40,20 +38,19 @@ def create_document_to_score():
     # |- matches: (id: 3, parent_id: 1, score.value: 3),
     # |- matches: (id: 4, parent_id: 1, score.value: 4),
     # |- matches: (id: 5, parent_id: 1, score.value: 5),
-    doc = Document()
+    doc = jina_pb2.DocumentProto()
     doc.id = '1' * 20
-    doc.length = 5
+    doc.length = 20
     for match_id, match_score, match_length in [
         (2, 3, 16),
         (3, 6, 24),
         (4, 1, 8),
         (5, 8, 16),
     ]:
-        with Document() as match:
-            match.id = str(match_id) * match_length
-            match.length = match_score
-            match.score.value = match_score
-            doc.matches.append(match)
+        match = doc.matches.add()
+        match.id = str(match_id) * match_length
+        match.length = match_length
+        match.score.value = match_score
     return doc
 
 
@@ -64,14 +61,14 @@ def test_chunk2doc_ranker_driver_mock_exec():
     driver.attach(executor=executor, runtime=None)
     driver()
     assert len(doc.matches) == 4
-    assert doc.matches[0].id == '3' * 24
-    assert doc.matches[0].score.value == -1
-    assert doc.matches[1].id == '2' * 16
-    assert doc.matches[1].score.value == -2
+    assert doc.matches[0].id == '2' * 16
+    assert doc.matches[0].score.value == -4
+    assert doc.matches[1].id == '3' * 24
+    assert doc.matches[1].score.value == -4
     assert doc.matches[2].id == '5' * 16
-    assert doc.matches[2].score.value == -3
+    assert doc.matches[2].score.value == -4
     assert doc.matches[3].id == '4' * 8
-    assert doc.matches[3].score.value == -4
+    assert doc.matches[3].score.value == -12
     for match in doc.matches:
         assert match.score.ref_id == doc.id
         assert match.score.op_name == 'MockAbsoluteLengthRanker'
