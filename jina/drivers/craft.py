b8a70ab1df2ac1f95e5f784b0a48068b8afaffa0
Joan Fontanals Martinez <joan.martinez@jina.ai>
2021-03-10 08:14:20 +0100

    fix: raise lengthmismatch exception

--- jina/drivers/craft.py
+++ jina/drivers/craft.py
@@ -4,6 +4,7 @@ __license__ = "Apache-2.0"
 from typing import Optional
 
 from . import FlatRecursiveMixin, BaseExecutableDriver
+from ..excepts import LengthMismatchException
 
 if False:
     from .. import DocumentSet
@@ -21,24 +22,25 @@ class CraftDriver(FlatRecursiveMixin, BaseExecutableDriver):
         if not docs:
             self.logger.warning(f'an empty DocumentSet {docs}')
             return
-        
+
         contents, docs_pts = docs.extract_docs(*self.exec.required_keys)
-        
+
         if not docs_pts:
             self.logger.warning(f'no Document is extracted {docs}')
             return
-        
+
         if len(self.exec.required_keys) > 1:
             craft_dicts = self.exec_fn(*contents)
         else:
             craft_dicts = self.exec_fn(contents)
 
         if len(docs_pts) != len(craft_dicts):
-            self.logger.error(
+            msg = (
                 f'mismatched {len(docs_pts)} docs from level {docs_pts[0].granularity} '
                 f'and length of returned crafted documents: {len(craft_dicts)}, the length must be the same'
             )
-            return
-    
+            self.logger.error(msg)
+            raise LengthMismatchException(msg)
+
         for doc, crafted in zip(docs_pts, craft_dicts):
             doc.set_attrs(**crafted)
--- jina/drivers/encode.py
+++ jina/drivers/encode.py
@@ -3,6 +3,7 @@ __license__ = "Apache-2.0"
 
 from . import BaseExecutableDriver, FlatRecursiveMixin
 from ..types.sets import DocumentSet
+from ..excepts import LengthMismatchException
 
 
 class BaseEncodeDriver(BaseExecutableDriver):
@@ -21,9 +22,11 @@ class EncodeDriver(FlatRecursiveMixin, BaseEncodeDriver):
         if docs_pts:
             embeds = self.exec_fn(contents)
             if len(docs_pts) != embeds.shape[0]:
-                self.logger.error(
+                msg = (
                     f'mismatched {len(docs_pts)} docs from level {docs_pts[0].granularity} '
                     f'and a {embeds.shape} shape embedding, the first dimension must be the same'
                 )
+                self.logger.error(msg)
+                raise LengthMismatchException(msg)
             for doc, embedding in zip(docs_pts, embeds):
                 doc.embedding = embedding
