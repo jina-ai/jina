41248c4863d49a2730678a02bce2327c6018cf9b
Joan Fontanals <joan.martinez@jina.ai>
2021-03-10 08:09:03 +0100

    refactor: update jina/drivers/craft.py
    Co-authored-by: Nan Wang <nan.wang@jina.ai>

--- jina/drivers/craft.py
+++ jina/drivers/craft.py
@@ -18,19 +18,27 @@ class CraftDriver(FlatRecursiveMixin, BaseExecutableDriver):
         super().__init__(executor, method, *args, **kwargs)
 
     def _apply_all(self, docs: 'DocumentSet', *args, **kwargs):
-        if docs:
-            contents, docs_pts = docs.extract_docs(*self.exec.required_keys)
+        if not docs:
+            self.logger.warning(f'an empty DocumentSet {docs}')
+            return
+        
+        contents, docs_pts = docs.extract_docs(*self.exec.required_keys)
+        
+        if not docs_pts:
+            self.logger.warning(f'no Document is extracted {docs}')
+            return
+        
+        if len(self.exec.required_keys) > 1:
+            craft_dicts = self.exec_fn(*contents)
+        else:
+            craft_dicts = self.exec_fn(contents)
 
-            if docs_pts:
-                if len(self.exec.required_keys) > 1:
-                    craft_dicts = self.exec_fn(*contents)
-                else:
-                    craft_dicts = self.exec_fn(contents)
-
-                if len(docs_pts) != len(craft_dicts):
-                    self.logger.error(
-                        f'mismatched {len(docs_pts)} docs from level {docs_pts[0].granularity} '
-                        f'and length of returned crafted documents: {len(craft_dicts)}, the length must be the same'
-                    )
-                for doc, crafted in zip(docs_pts, craft_dicts):
-                    doc.set_attrs(**crafted)
+        if len(docs_pts) != len(craft_dicts):
+            self.logger.error(
+                f'mismatched {len(docs_pts)} docs from level {docs_pts[0].granularity} '
+                f'and length of returned crafted documents: {len(craft_dicts)}, the length must be the same'
+            )
+            return
+    
+        for doc, crafted in zip(docs_pts, craft_dicts):
+            doc.set_attrs(**crafted)
