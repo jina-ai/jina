bc062ee6486f15a204a06237d76b2a07a02029ba
Joan Fontanals <joan.martinez@jina.ai>
2021-03-12 14:29:53 +0100

    feat: add batching capability for segmenters (#2141)
    * feat: provide input in batches for Crafter
    
    * test: add single to tests
    
    * test: add integ tests for different crafters single and batching
    
    * fix: extract required keys when decorator
    
    * test: fix some flow unit tests
    
    * test: fix test
    
    * refactor: update jina/drivers/craft.py
    
    Co-authored-by: Nan Wang <nan.wang@jina.ai>
    
    * fix: update jina/executors/crafters/__init__.py
    
    * fix: raise lengthmismatch exception
    
    * feat: add batching capability for segmenters
    
    * fix: update tests/integration/crud/advanced/yaml/custom_executor.py
    
    * fix: update tests/integration/issues/github_1546/good3/__init__.py
    
    * fix: update tests/integration/mime/test_segmenter.py
    
    * fix: update tests/unit/drivers/querylang/test_querylang_drivers.py
    
    * fix: update tests/unit/drivers/test_segmenter_driver.py
    
    * fix: update tests/unit/drivers/test_reduce_all_driver.py
    
    * fix: update tests/unit/flow/test_flow_merge.py
    
    * fix: update tests/unit/flow/test_flow_multimode.py
    
    * fix: update tests/unit/drivers/querylang/test_querylang_drivers.py
    
    * fix: update tests/integration/level_depth/yaml/custom.py
    
    * test: add test in segment executor batching
    
    * fix: fix setting number of siblings
    
    * fix: fix the bug in slice_on
    
    * fix: minor refactoring
    
    * fix: fix bugs
    
    * fix: revert last commit
    
    * fix: fix failing tests
    
    * fix: not flatten default in single
    
    Co-authored-by: Nan Wang <nan.wang@jina.ai>

--- jina/drivers/segment.py
+++ jina/drivers/segment.py
@@ -4,6 +4,7 @@ __license__ = "Apache-2.0"
 from typing import Optional, Tuple
 
 from . import BaseExecutableDriver, FlatRecursiveMixin
+from ..excepts import LengthMismatchException
 from ..types.document import Document
 
 if False:
@@ -19,24 +20,44 @@ class SegmentDriver(FlatRecursiveMixin, BaseExecutableDriver):
         method: str = 'segment',
         traversal_paths: Tuple[str] = ('r',),
         *args,
-        **kwargs
+        **kwargs,
     ):
         super().__init__(
             executor, method, traversal_paths=traversal_paths, *args, **kwargs
         )
 
     def _apply_all(self, docs: 'DocumentSet', *args, **kwargs):
+        if not docs:
+            self.logger.warning(f'an empty DocumentSet {docs}')
+            return
 
-        for doc in docs:
-            _args_dict = doc.get_attrs(*self.exec.required_keys)
-            ret = self.exec_fn(**_args_dict)
-            if ret:
-                self._update(doc, ret)
+        contents, docs_pts = docs.extract_docs(*self.exec.required_keys)
+
+        if not docs_pts:
+            self.logger.warning(f'no Document is extracted {docs}')
+            return
+
+        if len(self.exec.required_keys) > 1:
+            docs_chunks = self.exec_fn(*contents)
+        else:
+            docs_chunks = self.exec_fn(contents)
+
+        if len(docs_pts) != len(docs_chunks):
+            msg = (
+                f'mismatched {len(docs_pts)} docs from level {docs_pts[0].granularity} '
+                f'and length of returned crafted documents: {len(docs_chunks)}, the length must be the same'
+            )
+            self.logger.error(msg)
+            raise LengthMismatchException(msg)
+
+        for doc, chunks in zip(docs_pts, docs_chunks):
+            self._add_chunks(doc, chunks)
 
     @staticmethod
-    def _update(doc, ret):
-        for r in ret:
-            with Document(length=len(ret), **r) as c:
+    def _add_chunks(doc, chunks):
+        num_siblings = len(chunks)
+        for chunk in chunks:
+            with Document(length=num_siblings, **chunk) as c:
                 if not c.mime_type:
                     c.mime_type = doc.mime_type
             doc.chunks.append(c)
