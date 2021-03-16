8c7f59538bc0a7af7e5786360c5b7d598476358c
David Buchaca Prats <davidbuchaca@gmail.com>
2021-03-16 09:02:28 +0100

    fix: incorrect spacing

--- jina/drivers/rank/aggregate/__init__.py
+++ jina/drivers/rank/aggregate/__init__.py
@@ -89,7 +89,7 @@ class BaseAggregateMatchesRankerDriver(BaseRankDriver):
                 prev_index = i
                 prev_val = current_val
         if current_val == prev_val:
-           list_numpy_arrays.append(_sorted_m[prev_index:])
+            list_numpy_arrays.append(_sorted_m[prev_index:])
 
         return list_numpy_arrays
 
