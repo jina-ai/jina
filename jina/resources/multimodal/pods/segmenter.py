4248545dcf235b9e0f9bbdb02749b0c9315fc6b5
Joan Fontanals Martinez <joan.martinez@jina.ai>
2021-03-16 12:39:04 +0100

    fix: fix multimodal example

--- jina/resources/multimodal/pods/segmenter.py
+++ jina/resources/multimodal/pods/segmenter.py
@@ -1,11 +1,13 @@
 import os
 
 from jina import Segmenter, Crafter
+from jina.executors.decorators import single, single_multi_input
 
 
 class SimpleCrafter(Crafter):
     """Simple crafter for multimodal example."""
 
+    @single
     def craft(self, tags):
         """
         Read the data and add tags.
@@ -22,6 +24,7 @@ class SimpleCrafter(Crafter):
 class BiSegmenter(Segmenter):
     """Segmenter for multimodal example."""
 
+    @single_multi_input(num_data=2, flatten_output=False)
     def segment(self, text, uri):
         """
         Segment data into text and uri.
