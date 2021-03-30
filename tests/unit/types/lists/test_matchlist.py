6507a7f407676c9ec3d069ed9e08b25e41dc4182
mkhilai <nikita.khilay@gmail.com>
2021-03-30 20:22:57 +0200

    feat: add tests for lists type

---
+++ tests/unit/types/lists/test_matchlist.py
@@ -0,0 +1,63 @@
+import pytest
+from jina import Request
+
+from jina.types.document import Document
+from jina.types.lists.match import MatchList
+
+
+@pytest.fixture(scope='function')
+def document_factory():
+    class DocumentFactory(object):
+        def create(self, idx, text, mime_type=None):
+            with Document() as d:
+                d.tags['id'] = idx
+                d.text = text
+                d.mime_type = mime_type
+            return d
+
+    return DocumentFactory()
+
+
+@pytest.fixture
+def reference_doc(document_factory):
+    return document_factory.create(0, 'test ref', 'text/plain')
+
+
+@pytest.fixture
+def matches(document_factory):
+    req = Request()
+    req.request_type = 'index'
+    req.docs.extend(
+        [
+            document_factory.create(1, 'test 1'),
+            document_factory.create(2, 'test 1'),
+            document_factory.create(3, 'test 3'),
+        ]
+    )
+    return req.proto.index.docs
+
+
+@pytest.fixture
+def matchlist(matches, reference_doc):
+    return MatchList(docs_proto=matches, reference_doc=reference_doc)
+
+
+def test_append_from_documents(matchlist, document_factory, reference_doc):
+    match = document_factory.create(4, 'test 4')
+    rv = matchlist.append(match)
+    assert len(matchlist) == 4
+    assert matchlist[-1].text == 'test 4'
+    assert rv.text == match.text
+    assert rv.granularity == reference_doc.granularity
+    assert rv.adjacency == reference_doc.adjacency + 1
+    assert rv.mime_type == 'text/plain'
+    assert rv.score.ref_id == reference_doc.id
+
+
+def test_mime_type_not_reassigned():
+    d = Document()
+    m = Document()
+    assert m.mime_type == ''
+    d.mime_type = 'text/plain'
+    r = d.matches.append(m)
+    assert r.mime_type == ''
