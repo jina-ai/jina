bd11b4cfac8f4acd057b6f85ea3a978bb2049297
Han Xiao <han.xiao@jina.ai>
2021-04-04 00:02:58 +0800

    feat(flow): add reload api to flow

--- jina/__init__.py
+++ jina/__init__.py
@@ -60,7 +60,7 @@ __version__ = '1.1.2'
 
 # do not change this line manually
 # this is managed by proto/build-proto.sh and updated on every execution
-__proto_version__ = '0.0.80'
+__proto_version__ = '0.0.81'
 
 __uptime__ = _datetime.datetime.now().isoformat()
 
