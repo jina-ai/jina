abbb7e5eed8b3ccf1696fa51b67be372dd150ee3
Mohamed <mohamed.amaksoud@gmail.com>
2021-04-08 20:54:28 +0200

    fix: add parameters to docstrings

--- daemon/api/endpoints/logs.py
+++ daemon/api/endpoints/logs.py
@@ -42,7 +42,11 @@ class ConnectionManager:
         self.active_connections: List[WebSocket] = []
 
     async def connect(self, websocket: WebSocket):
-        """Register a new websocket"""
+        """
+        Register a new websocket
+
+        :param websocket:
+        """
         await websocket.accept()
         daemon_logger.info(
             '%s is connected to stream logs!' % _websocket_details(websocket)
@@ -50,13 +54,22 @@ class ConnectionManager:
         self.active_connections.append(websocket)
 
     async def disconnect(self, websocket: WebSocket):
-        """Disconnect a websocket"""
+        """
+        Disconnect a websocket
+
+        :param websocket:
+        """
         self.active_connections.remove(websocket)
         await websocket.close()
         daemon_logger.info('%s is disconnected' % _websocket_details(websocket))
 
     async def broadcast(self, message: dict):
-        """Send a json message to all registered websockets"""
+        """
+        Send a json message to all registered websockets
+
+
+        :param message: JSON-serializable message to be broadcast
+        """
         daemon_logger.debug('connections: %r', self.active_connections)
         for connection in self.active_connections:
             try:
