e6c2ec63eb546fc06ea45c5a12f9edb02be24732
mkhilai <nikita.khilay@gmail.com>
2021-04-18 15:24:49 +0200

    feat: deprecate missing classes

--- jina/types/sets/doc_groundtruth.py
+++ jina/types/sets/doc_groundtruth.py
@@ -1,10 +1,16 @@
 from typing import Sequence
 from .traversable import TraversableSequence
+from ..lists.doc_groundtruth import DocumentGroundtruthSequence
+from ...helper import deprecated_class
 
 if False:
     from ..document.helper import DocGroundtruthPair
 
 
+@deprecated_class(
+    new_class=DocumentGroundtruthSequence,
+    custom_msg="The class has been moved to '..types.lists', keeping its original name.",
+)
 class DocumentGroundtruthSequence(TraversableSequence):
     def __init__(self, pairs: Sequence['DocGroundtruthPair']):
         self._pairs = pairs
