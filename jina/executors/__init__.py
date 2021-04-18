39aa7987002ae946a2d88edffe63e4841aec0005
Florian Hönicke <hoenicke.florian@gmail.com>
2021-04-18 17:50:58 +0200

    refactor: introduce feedback

--- jina/executors/__init__.py
+++ jina/executors/__init__.py
@@ -368,7 +368,7 @@ class BaseExecutor(JAMLCompatible, metaclass=ExecutorType):
 
         :return: returns the workspace of the shard of this Executor.
         """
-        if replica_id == -1:
+        if replica_id == -1 or replica_id is None:
             return os.path.join(workspace_folder, f'{workspace_name}-{pea_id}')
         else:
             return os.path.join(
