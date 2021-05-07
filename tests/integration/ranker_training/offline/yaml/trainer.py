aa5dd6f4299e641fd91394a2cd09aa1ab283591a
bwanglzu <bo.wang@jina.ai>
2021-05-07 10:27:21 +0200

    feat: finish sgd regression ranker

--- tests/integration/ranker_training/offline/yaml/trainer.py
+++ tests/integration/ranker_training/offline/yaml/trainer.py
@@ -35,15 +35,18 @@ class SGDRegressorRankerTrainer(RankerTrainer):
 
     MODEL_FILENAME = 'model.pickle'
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, model_path: str, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.model_path = model_path
+        self.match_required_keys = {'tags__size', 'tags__price', 'tags__relevance'}
+        self.query_required_keys = None
 
     def post_init(self):
         from sklearn.linear_model import SGDRegressor
 
         self.regressor = SGDRegressor(warm_start=True)
 
-    def train(self, matches_metas, *args, **kwargs):
+    def train(self, query_metas, matches_metas, *args, **kwargs):
         sizes = []
         prices = []
         relevance = []
@@ -51,22 +54,20 @@ class SGDRegressorRankerTrainer(RankerTrainer):
             for m in match_meta:
                 sizes.append(m['tags__size'])
                 prices.append(m['tags__price'])
-                relevance.append(m['tags_relevance'])
+                relevance.append(m['tags__relevance'])
         X = np.column_stack((sizes, prices))
         y = np.asarray(relevance)
         self.regressor.partial_fit(X, y)
 
-    def save(self, filename: str):
+    def save(self):
         """
         Save the weights of the ranker model.
         """
-        path = Path(filename)
+        path = Path(self.model_path)
         model_path = path.joinpath(self.MODEL_FILENAME)
 
         if not path.exists():
             path.mkdir(parents=True)
-        else:
-            raise FileExistsError(f'{path} already exist, fail to save.')
 
         with open(model_path, mode='wb') as model_file_name:
             pickle.dump(self.regressor, model_file_name)
