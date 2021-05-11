6d73d56497ad762c0240db29bbb540e88b22e80d
David Buchaca Prats <davidbuchaca@gmail.com>
2021-05-11 16:19:43 +0200

    test: check parallel execution works properly

---
+++ tests/integration/gateway_non_blocking/test_gateway_non_blocking.py
@@ -0,0 +1,41 @@
+import os
+
+import pytest
+
+from jina.flow import Flow
+from jina import Executor, requests, DocumentArray, Document
+
+import time
+
+
+class FastSlowExecutor(Executor):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @requests(on=['/search'])
+    def encode(self, docs: DocumentArray, *args, **kwargs):
+        assert len(docs) == 1
+        if docs[0].text == 'slow':
+            time.sleep(2)
+
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+@pytest.mark.parametrize(
+    'parallel, expected_response', [(1, ['slow', 'fast']), (2, ['fast', 'slow'])]
+)
+def test_non_blocking_gateway(parallel, expected_response):
+    response = []
+
+    def fill_responses(resp):
+        assert len(resp.data.docs) == 1
+        response.append(resp.data.docs[0].text)
+
+    data = DocumentArray([Document(text='slow'), Document(text='fast')])
+
+    f = Flow().add(uses=FastSlowExecutor, parallel=parallel)
+    with f:
+        f.post(on='/search', inputs=data, request_size=1, on_done=fill_responses)
+
+    assert response == expected_response
