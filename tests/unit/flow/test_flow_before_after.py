d9a91b2b95c52237a674f08243045da965953636
Han Xiao <han.xiao@jina.ai>
2021-05-13 09:33:58 +0800

    refactor: make a mwu running

--- tests/unit/flow/test_flow_before_after.py
+++ tests/unit/flow/test_flow_before_after.py
@@ -1,8 +1,14 @@
 import pytest
 
+from jina import Executor, requests, __default_executor__
 from jina.flow import Flow
 from tests import random_docs
-from jina import __default_executor__
+
+
+class MyExec(Executor):
+    @requests
+    def foo(self, **kwargs):
+        pass
 
 
 @pytest.mark.parametrize('restful', [False, True])
@@ -20,7 +26,7 @@ def test_flow(restful):
 @pytest.mark.parametrize('restful', [False, True])
 def test_flow_before(restful):
     docs = random_docs(10)
-    f = Flow(restful=restful).add(uses_before=__default_executor__, name='p1')
+    f = Flow(restful=restful).add(uses_before=MyExec, name='p1')
 
     with f:
         f.index(docs)
@@ -32,7 +38,7 @@ def test_flow_before(restful):
 @pytest.mark.parametrize('restful', [False, True])
 def test_flow_after(restful):
     docs = random_docs(10)
-    f = Flow(restful=restful).add(uses_after=__default_executor__, name='p1')
+    f = Flow(restful=restful).add(uses_after=MyExec, name='p1')
 
     with f:
         f.index(docs)
@@ -42,11 +48,23 @@ def test_flow_after(restful):
 
 
 @pytest.mark.parametrize('restful', [False, True])
-def test_flow_before_after(restful):
+def test_flow_default_before_after_is_ignored(restful):
     docs = random_docs(10)
     f = Flow(restful=restful).add(
-        uses_before=__default_executor__, uses_after=__default_executor__, name='p1'
+        uses_after=__default_executor__, uses_before=__default_executor__, name='p1'
     )
+
+    with f:
+        f.index(docs)
+        assert f.num_pods == 2
+        assert f._pod_nodes['p1'].num_peas == 1
+        assert f.num_peas == 2
+
+
+@pytest.mark.parametrize('restful', [False, True])
+def test_flow_before_after(restful):
+    docs = random_docs(10)
+    f = Flow(restful=restful).add(uses_before=MyExec, uses_after=MyExec, name='p1')
 
     with f:
         f.index(docs)
