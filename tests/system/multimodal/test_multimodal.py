5c4c0b24c23119baf267144b5b8ed81f108a5d47
bwanglzu <bo.wang@jina.ai>
2021-05-14 18:11:16 +0200

    feat: adapt 2.0 api change in system test

--- tests/system/multimodal/test_multimodal.py
+++ tests/system/multimodal/test_multimodal.py
@@ -5,7 +5,7 @@ from pkg_resources import resource_filename
 
 from jina import Document
 from jina.flow import Flow
-from jina.helloworld.multimodal import hello_world
+from jina.helloworld.multimodal.app import hello_world
 from jina.parsers.helloworld import set_hw_multimodal_parser
 from tests import validate_callback
 
@@ -63,17 +63,18 @@ def test_multimodal(helloworld_args, query_document, mocker):
             assert len(doc.matches) == 10
 
     hello_world(helloworld_args)
-    flow_query_path = os.path.join(resource_filename('jina', 'resources'), 'multimodal')
 
     mock_on_done = mocker.Mock()
     mock_on_fail = mocker.Mock()
 
-    with Flow.load_config(os.path.join(flow_query_path, 'flow-query.yml')) as f:
-        f.search(
+    flow_query_path = os.path.join(resource_filename('jina', 'helloworld'), 'multimodal')
+    with Flow.load_config(os.path.join(flow_query_path, 'flow-search.yml')) as f:
+        f.post(
+            on='/search',
             inputs=[query_document],
             on_done=mock_on_done,
             on_fail=mock_on_fail,
-            top_k=10,
+            parameters={'top_k': 10},
         )
 
     mock_on_fail.assert_not_called()
