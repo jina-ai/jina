8244f308e997b4586d29482765e039aab9c37d9e
bwanglzu <bo.wang@jina.ai>
2021-05-15 16:55:39 +0200

    feat: remove test file

--- jina/helloworld/multimodal/test.py
+++
@@ -1,6 +0,0 @@
-from jina.helloworld.multimodal.app import hello_world
-from jina.parsers.helloworld import set_hw_multimodal_parser
-
-if __name__ == '__main__':
-    args = set_hw_multimodal_parser().parse_args()
-    hello_world(args)
