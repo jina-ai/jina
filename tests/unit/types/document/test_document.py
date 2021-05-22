761da651cfdaff8240be4250ba2180fc71ad1dd8
Han Xiao <han.xiao@jina.ai>
2021-05-22 19:22:44 +0800

    refactor: remove legacy siblings

--- tests/unit/types/document/test_document.py
+++ tests/unit/types/document/test_document.py
@@ -821,3 +821,10 @@ def test_doc_different_score_value_type(val):
     d = Document()
     d.score = val
     assert int(d.score.value) == 1
+
+
+def test_doc_match_score_assign():
+    d = Document(id='hello')
+    d1 = Document(d, copy=True, score=123)
+    d.matches = [d1]
+    assert d.matches[0].score.value == 123
