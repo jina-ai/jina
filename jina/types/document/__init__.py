64d391fb1fa12bc35af41b79aaf3976356467694
bwanglzu <bo.wang@jina.ai>
2021-05-22 19:15:21 +0200

    refactor: update api for document

--- jina/types/document/__init__.py
+++ jina/types/document/__init__.py
@@ -281,7 +281,7 @@ class Document(ProtoTypeMixin):
             self._pb_body.ClearField(k)
 
     def clear(self) -> None:
-        """Remove all values from all fields of this Document. """
+        """Remove all values from all fields of this Document."""
         self._pb_body.Clear()
 
     @property
@@ -342,8 +342,7 @@ class Document(ProtoTypeMixin):
     def _update(
         source: 'Document',
         destination: 'Document',
-        exclude_fields: Optional[Tuple[str]] = None,
-        include_fields: Optional[Tuple[str]] = None,
+        fields: Optional[Tuple[str]] = None,
         replace_message_field: bool = True,
         replace_repeated_field: bool = True,
     ) -> None:
@@ -351,105 +350,42 @@ class Document(ProtoTypeMixin):
 
         :param source: source :class:`Document` object.
         :param destination: the destination :class:`Document` object to be merged into.
-        :param exclude_fields: a tuple of field names that excluded from destination document
-        :param include_fields: a tuple of field names that included from source document
+        :param fields: a tuple of field names that included from destination document
         :param replace_message_field: Replace message field if True. Merge message
                   field if False.
         :param replace_repeated_field: Replace repeated field if True. Append
                   elements of repeated field if False.
 
         .. note::
-            *. if neither ``exclude_fields`` nor ``include_fields`` is given,
-                then destination is overridden by the source completely.
+            *. if ``fields`` is empty, then destination is overridden by the source completely.
             *. ``destination`` will be modified in place, ``source`` will be unchanged
         """
-
-        if not include_fields and not exclude_fields:
-            # same behavior as copy
-            destination.CopyFrom(source)
-        elif include_fields is not None and exclude_fields is None:
-            FieldMask(paths=include_fields).MergeMessage(
-                source.proto,
-                destination.proto,
-                replace_message_field=replace_message_field,
-                replace_repeated_field=replace_repeated_field,
-            )
-        elif exclude_fields is not None:
-            empty_doc = jina_pb2.DocumentProto()
-
-            _dest = jina_pb2.DocumentProto()
-            # backup exclude fields in destination
-            FieldMask(paths=exclude_fields).MergeMessage(
-                destination.proto,
-                _dest,
-                replace_repeated_field=True,
-                replace_message_field=True,
-            )
-
-            if include_fields is None:
-                # override dest with src
-                destination.CopyFrom(source)
-            else:
-                # only update include fields
-                FieldMask(paths=include_fields).MergeMessage(
-                    source.proto,
-                    destination.proto,
-                    replace_message_field=replace_message_field,
-                    replace_repeated_field=replace_repeated_field,
-                )
-
-            # clear the exclude fields
-            FieldMask(paths=exclude_fields).MergeMessage(
-                empty_doc,
-                destination.proto,
-                replace_repeated_field=True,
-                replace_message_field=True,
-            )
-
-            # recover exclude fields
-            destination.proto.MergeFrom(_dest)
+        if fields:
+            pass  # only include specified fields.
+        else:
+            destination.CopyFrom(source)  # merge fields completely
 
     def update(
         self,
         source: 'Document',
-        exclude_fields: Optional[Tuple[str, ...]] = None,
-        include_fields: Optional[Tuple[str, ...]] = None,
+        fields: Optional[Tuple[str, ...]] = None,
     ) -> None:
         """Updates fields specified in ``include_fields`` from the source to current Document.
 
         :param source: source :class:`Document` object.
-        :param exclude_fields: a tuple of field names that excluded from the current document,
-                when not given the non-empty fields of the current document is considered as ``exclude_fields``
-        :param include_fields: a tuple of field names that included from the source document
+        :param fields: a tuple of field names that included from the current document,
+                if not specified, merge all fields.
 
         .. note::
             *. ``destination`` will be modified in place, ``source`` will be unchanged
         """
-        if (include_fields and not isinstance(include_fields, tuple)) or (
-            exclude_fields and not isinstance(exclude_fields, tuple)
-        ):
-            raise TypeError('include_fields and exclude_fields must be tuple of str')
-
-        if exclude_fields is None:
-            if include_fields:
-                exclude_fields = tuple(
-                    f for f in self.non_empty_fields if f not in include_fields
-                )
-            else:
-                exclude_fields = self.non_empty_fields
-
-        if include_fields and exclude_fields:
-            _intersect = set(include_fields).intersection(exclude_fields)
-            if _intersect:
-                raise ValueError(
-                    f'{_intersect} is in both `include_fields` and `exclude_fields`'
-                )
+        if fields and not isinstance(fields, tuple):
+            raise TypeError('Parameter `fields` must be tuple of str')
 
         self._update(
             source,
             self,
-            exclude_fields=exclude_fields,
-            include_fields=include_fields,
+            fields=fields,
             replace_message_field=True,
             replace_repeated_field=True,
         )
@@ -1034,7 +970,7 @@ class Document(ProtoTypeMixin):
         self.blob = np.frombuffer(self.buffer, dtype, count, offset)
 
     def convert_blob_to_buffer(self):
-        """Convert blob to buffer """
+        """Convert blob to buffer"""
         self.buffer = self.blob.tobytes()
 
     def convert_uri_to_buffer(self):
