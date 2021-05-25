8980f75c835a62d665d3857e01b0242c18086a0b
Han Xiao <han.xiao@jina.ai>
2021-05-25 12:08:56 +0800

    chore(docs): add api reference docs

--- docs/makedoc.sh
+++ docs/makedoc.sh
@@ -10,14 +10,3 @@ docker run --rm \
   pseudomuto/protoc-gen-doc --doc_opt=markdown,docs.md
 
 make dirhtml
-exit
-echo docs.jina.ai > CNAME
-git init
-git config --local user.email "dev-bot@jina.ai"
-git config --local user.name "Jina Dev Bot"
-touch .nojekyll
-git add .
-git commit -m "$2" -a
-git status
-cd -
-
