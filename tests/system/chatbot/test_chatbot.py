fc912dc7b094c041bbd34b41c97975d21001895e
bwanglzu <bo.wang@jina.ai>
2021-05-26 23:28:26 +0200

    test: fix chatbot test

--- tests/system/chatbot/test_chatbot.py
+++ tests/system/chatbot/test_chatbot.py
@@ -1,25 +1,17 @@
 import pytest
-import requests
-from xprocess import ProcessStarter
 
+from jina import Document, Flow
+from jina.helloworld.chatbot.app import hello_world
+from jina.helloworld.chatbot.executors import MyTransformer, MyIndexer
 from jina.parsers.helloworld import set_hw_chatbot_parser
+from tests import validate_callback
 
 
 @pytest.fixture
-def chatbot_args(tmpdir):
+def helloworld_args(tmpdir):
     return set_hw_chatbot_parser().parse_args(
-        ['--workdir', str(tmpdir), '--port-expose', '8080']
+        ['--workdir', str(tmpdir), '--unblock-query-flow']
     )
-
-
-@pytest.fixture
-def payload():
-    return {'top_k': 1, 'data': ['text:Is my dog safe from virus']}
-
-
-@pytest.fixture
-def post_uri():
-    return 'http://localhost:8080/search'
 
 
 @pytest.fixture
@@ -27,20 +19,30 @@ def expected_result():
     return '''no evidence from the outbreak that eating garlic, sipping water every 15 minutes or taking vitamin C will protect people from the new coronavirus.'''
 
 
-@pytest.fixture(autouse=True)
-def start_server(xprocess, chatbot_args):
-    class Starter(ProcessStarter):
-        pattern = "You should see a demo page opened in your browser"
-        args = ["jina", "hello", "chatbot"]
-        max_read_lines = 10000
-
-    xprocess.ensure("server", Starter)
-    yield
-    xprocess.getinfo("server").terminate()
+def search(query_document, on_done_callback, on_fail_callback, top_k):
+    with Flow().add(uses=MyTransformer).add(uses=MyIndexer) as f:
+        f.search(
+            inputs=query_document,
+            on_done=on_done_callback,
+            on_fail=on_fail_callback,
+            parameters={'top_k': top_k},
+        )
 
 
-def test_chatbot(payload, post_uri, expected_result):
-    """Regression test for chatbot example."""
-    resp = requests.post(post_uri, json=payload)
-    assert resp.status_code == 200
-    assert expected_result in resp.text
+def test_multimodal(helloworld_args, expected_result, mocker):
+    """Regression test for multimodal example."""
+
+    def validate_response(resp):
+        assert len(resp.data.docs) == 1
+        for doc in resp.data.docs:
+            assert len(doc.matches) == 10
+
+    hello_world(helloworld_args)
+
+    mock_on_done = mocker.Mock()
+    mock_on_fail = mocker.Mock()
+
+    search(Document(text='Is my dog safe from virus'), mock_on_done, mock_on_fail, 1)
+
+    mock_on_fail.assert_not_called()
+    validate_callback(mock_on_done, validate_response)
