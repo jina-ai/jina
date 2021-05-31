99def8d99288fca15b60a7af843c52d85f32bde5
Joan Fontanals Martinez <joan.martinez@jina.ai>
2021-05-31 08:44:11 +0200

    test: test different configurations

--- tests/integration/external_pod/test_external_pod.py
+++ tests/integration/external_pod/test_external_pod.py
@@ -1,6 +1,7 @@
 import pytest
 
-from jina.peapods.pods import Pod
+from jina.peapods.pods.factory import PodFactory
+from jina.peapods.pods import PodRoleType
 from jina.parsers import set_pod_parser
 
 from jina import Flow, Executor, requests, Document, DocumentArray
@@ -45,7 +46,7 @@ def external_pod_args(port_in_external, port_out_external):
 
 @pytest.fixture
 def external_pod(external_pod_args):
-    return Pod(
+    return PodFactory.build_pod(
         external_pod_args,
     )
 
@@ -108,7 +109,7 @@ def external_pod_parallel_1_args(port_in_external, port_out_external):
 
 @pytest.fixture
 def external_pod_parallel_1(external_pod_parallel_1_args):
-    return Pod(
+    return PodFactory.build_pod(
         external_pod_parallel_1_args,
     )
 
@@ -138,7 +139,7 @@ def external_pod_parallel_2_args(port_in_external, port_out_external):
 
 @pytest.fixture
 def external_pod_parallel_2(external_pod_parallel_2_args):
-    return Pod(
+    return PodFactory.build_pod(
         external_pod_parallel_2_args,
     )
 
@@ -199,6 +200,149 @@ def test_flow_with_external_pod_parallel(
     validate_callback(mock, validate_response)
 
 
+@pytest.fixture(scope='function')
+def external_pod_pre_parallel_args(port_in_external, port_out_external):
+    args = [
+        '--uses',
+        'MyExternalExecutor',
+        '--name',
+        'external_real',
+        '--port-in',
+        str(port_in_external),
+        '--host-in',
+        '0.0.0.0',
+        '--socket-in',
+        'PULL_BIND',
+        '--port-out',
+        str(port_out_external),
+        '--host-out',
+        '0.0.0.0',
+        '--socket-out',
+        'PUB_BIND',
+    ]
+    return set_pod_parser().parse_args(args)
+
+
+@pytest.fixture
+def external_pod_pre_parallel(external_pod_pre_parallel_args):
+    return PodFactory.build_pod(
+        external_pod_pre_parallel_args,
+    )
+
+
+def test_flow_with_external_pod_pre_parallel(
+    external_pod_pre_parallel, external_pod_pre_parallel_args, input_docs, mocker
+):
+    def validate_response(resp):
+        assert len(resp.data.docs) == 50
+        for doc in resp.data.docs:
+            assert doc.tags['name'] == 'external_real'
+
+    with external_pod_pre_parallel:
+        external_args = vars(external_pod_pre_parallel_args)
+        del external_args['name']
+        del external_args['external']
+        del external_args['pod_role']
+        del external_args['freeze_network_settings']
+        flow = (
+            Flow()
+            .add(
+                **external_args,
+                name='external_fake',
+                external=True,
+                freeze_network_settings=True,
+            )
+            .add(
+                name='pod1',
+                needs=['external_fake'],
+            )
+            .add(
+                name='pod2',
+                needs=['external_fake'],
+            )
+            .join(needs=['pod1', 'pod2'])
+        )
+        mock = mocker.Mock()
+        with flow:
+            flow.index(inputs=input_docs, on_done=mock)
+
+    validate_callback(mock, validate_response)
+
+
+@pytest.fixture(scope='function')
+def external_pod_join_args(port_in_external, port_out_external):
+    args = [
+        '--uses',
+        'MyExternalExecutor',
+        '--name',
+        'external_real',
+        '--port-in',
+        str(port_in_external),
+        '--host-in',
+        '0.0.0.0',
+        '--socket-in',
+        'PULL_BIND',
+        '--port-out',
+        str(port_out_external),
+        '--host-out',
+        '0.0.0.0',
+        '--socket-out',
+        'PUSH_BIND',
+        '--pod-role',
+        'JOIN',
+    ]
+    return set_pod_parser().parse_args(args)
+
+
+@pytest.fixture
+def external_pod_join(external_pod_join_args):
+    return PodFactory.build_pod(
+        external_pod_join_args,
+    )
+
+
+def test_flow_with_external_pod_join(
+    external_pod_join, external_pod_join_args, input_docs, mocker
+):
+    def validate_response(resp):
+        assert len(resp.data.docs) == 50
+        for doc in resp.data.docs:
+            assert doc.tags['name'] == 'external_real'
+
+    with external_pod_join:
+        external_args = vars(external_pod_join_args)
+        del external_args['name']
+        del external_args['external']
+        del external_args['freeze_network_settings']
+        del external_args['pod_role']
+        flow = (
+            Flow()
+            .add(
+                **external_args,
+                external=True,
+            )
+            .add(
+                name='pod1',
+                needs=['pod0'],
+            )
+            .add(
+                name='pod2',
+                needs=['pod0'],
+            )
+            .join(
+                **external_args,
+                external=True,
+                needs=['pod1', 'pod2'],
+                freeze_network_settings=True,
+            )
+        )
+        mock = mocker.Mock()
+        with flow:
+            flow.index(inputs=input_docs, on_done=mock)
+
+    validate_callback(mock, validate_response)
+
+
 def test_flow_with_external_pod_parallel_error(
     external_pod_parallel_1,
     external_pod_parallel_2,
@@ -208,7 +352,6 @@ def test_flow_with_external_pod_parallel_error(
     port_out_external,
     input_docs,
 ):
-
     with external_pod_parallel_1, external_pod_parallel_2:
         external_args_1 = vars(external_pod_parallel_1_args)
         external_args_2 = vars(external_pod_parallel_2_args)
