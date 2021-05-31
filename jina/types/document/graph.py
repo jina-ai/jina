77d18c76d08bbfe8f38bbbcf833109db0006d5b9
Joan Fontanals Martinez <joan.martinez@jina.ai>
2021-05-31 12:49:55 +0200

    test: add tests

--- jina/types/document/graph.py
+++ jina/types/document/graph.py
@@ -1,8 +1,9 @@
-from typing import Optional, Iterator, Tuple, Dict
+from typing import Optional, Iterator, Tuple, Dict, List
 
 import numpy as np
 
 from . import Document, DocumentSourceType
+from ..arrays import ChunkArray
 from ..struct import StructView
 from ..ndarray.sparse.scipy import SparseNdArray
 
@@ -22,9 +23,6 @@ class GraphDocument(Document):
 
     .. warning::
         - It assumes that every ``chunk`` of a ``document`` is a node of a graph.
-        - It assumes that every :class:`MultimodalDocument` have at least two chunks.
-        - Build :class:`MultimodalDocument` from :attr:`modality_content_mapping` expects you assign
-          :attr:`Document.content` as the value of the dictionary.
 
     :param document: the document to construct from. If ``bytes`` is given
             then deserialize a :class:`DocumentProto`; ``dict`` is given then
@@ -44,7 +42,7 @@ class GraphDocument(Document):
         **kwargs,
     ):
         super().__init__(document=document, copy=copy, **kwargs)
-        self._chunk_offset_to_chunk_index = {
+        self._chunk_id_to_offset = {
             chunk.id: offset for offset, chunk in enumerate(self.chunks)
         }
 
@@ -58,17 +56,30 @@ class GraphDocument(Document):
         :param doc2: the ending node for this edge
         :param features: Optional features dictionary to be added to this new created edge
         """
-        for doc in {doc1, doc2}:
-            if doc.id not in self._chunk_offset_to_chunk_index:
+        from scipy.sparse import coo_matrix
+
+        for doc in [doc1, doc2]:
+            if doc.id not in self._chunk_id_to_offset:
+                self._chunk_id_to_offset[doc.id] = len(self.chunks)
                 self.chunks.append(doc)
-                self._chunk_offset_to_chunk_index[doc.id] = len(self.chunks)
         current_adjacency = self.adjacency
-        row = current_adjacency.row
-        col = current_adjacency.col
-        data = current_adjacency.data
-        row = np.append(row, self._chunk_offset_to_chunk_index[doc1.id])
-        col = np.append(col, self._chunk_offset_to_chunk_index[doc2.id])
-        data = np.append(data, 1)
+        doc1_node_offset = self._chunk_id_to_offset[doc1.id]
+        doc2_node_offset = self._chunk_id_to_offset[doc2.id]
+        row = (
+            np.append(current_adjacency.row, doc1_node_offset)
+            if current_adjacency is not None
+            else np.array([doc1_node_offset])
+        )
+        col = (
+            np.append(current_adjacency.col, doc2_node_offset)
+            if current_adjacency is not None
+            else np.array([doc2_node_offset])
+        )
+        data = (
+            np.append(current_adjacency.data, 1)
+            if current_adjacency is not None
+            else np.array([1])
+        )
         self.adjacency = coo_matrix(
             (data, (row, col)), shape=(len(self.chunks), len(self.chunks))
         )
@@ -109,13 +120,47 @@ class GraphDocument(Document):
 
         :param value: the float weight of the document.
         """
-        from ... import JINA_GLOBAL
+        SparseNdArray(self._pb_body.graph_info.adjacency, sp_format='coo').value = value
 
-        if JINA_GLOBAL.scipy_installed:
-            SparseNdArray(
-                self._pb_body.graph_info.adjacency, sp_format='coo'
-            ).value = value
+    def get_outgoing_nodes(self, doc: 'Document') -> Optional[ChunkArray]:
+        """
+        Get all the outgoing edges from `doc`
+
+        .. # noqa: DAR201
+        :param doc: the document node from which to extract the outgoing nodes.
+        """
+        if self.adjacency is not None and doc.id in self._chunk_id_to_offset:
+            offset = self._chunk_id_to_offset[doc.id]
+            return ChunkArray(
+                [
+                    self.chunks[col.item()]
+                    for (row, col) in zip(self.adjacency.row, self.adjacency.col)
+                    if row.item() == offset
+                ],
+                reference_doc=self,
+            )
+
+    def get_incoming_nodes(self, doc: 'Document') -> Optional[ChunkArray]:
+        """
+        Get all the outgoing edges from `doc`
+
+        .. # noqa: DAR201
+        :param doc: the document node from which to extract the incoming nodes.
+        """
+        if self.adjacency is not None and doc.id in self._chunk_id_to_offset:
+            offset = self._chunk_id_to_offset[doc.id]
+            return ChunkArray(
+                [
+                    self.chunks[row.item()]
+                    for (row, col) in zip(self.adjacency.row, self.adjacency.col)
+                    if col.item() == offset
+                ],
+                reference_doc=self,
+            )
 
     def __iter__(self) -> Iterator[Tuple['Document']]:
-        for (row, col) in self.adjacency:
-            yield self.chunks[row], self.chunks[col]
+        for (row, col) in zip(self.adjacency.row, self.adjacency.col):
+            yield self.chunks[row.item()], self.chunks[col.item()]
+
+    def __len__(self) -> int:
+        return self.adjacency.getnnz()
---
+++ tests/unit/types/document/test_graph_document.py
@@ -0,0 +1,101 @@
+import pytest
+
+from jina.types.document.graph import GraphDocument
+from jina.types.document import Document
+
+
+@pytest.fixture()
+def graph():
+    graph = GraphDocument()
+
+    doc0 = Document(text='Document0')
+    doc1 = Document(text='Document1')
+    doc2 = Document(text='Document2')
+    doc3 = Document(text='Document3')
+
+    graph.add_edge(doc0, doc1)
+    graph.add_edge(doc0, doc2)
+    graph.add_edge(doc2, doc1)
+    graph.add_edge(doc1, doc3)
+    graph.add_edge(doc2, doc3)
+    return graph
+
+
+def validate_graph(graph):
+    doc0 = graph.chunks[0]
+    assert doc0.text == 'Document0'
+    doc1 = graph.chunks[1]
+    assert doc1.text == 'Document1'
+    doc2 = graph.chunks[2]
+    assert doc2.text == 'Document2'
+    doc3 = graph.chunks[3]
+    assert doc3.text == 'Document3'
+
+    assert len(graph) == 5
+    for i, (d1, d2) in enumerate(graph):
+        if i == 0:
+            assert d1.text == 'Document0'
+            assert d2.text == 'Document1'
+        if i == 1:
+            assert d1.text == 'Document0'
+            assert d2.text == 'Document2'
+        if i == 2:
+            assert d1.text == 'Document2'
+            assert d2.text == 'Document1'
+        if i == 3:
+            assert d1.text == 'Document1'
+            assert d2.text == 'Document3'
+        if i == 4:
+            assert d1.text == 'Document2'
+            assert d2.text == 'Document3'
+
+    outgoing_0 = graph.get_outgoing_nodes(doc0)
+    assert len(outgoing_0) == 2
+    assert outgoing_0[0].text == 'Document1'
+    assert outgoing_0[1].text == 'Document2'
+
+    incoming_0 = graph.get_incoming_nodes(doc0)
+    assert len(incoming_0) == 0
+
+    outgoing_1 = graph.get_outgoing_nodes(doc1)
+    assert len(outgoing_1) == 1
+    assert outgoing_1[0].text == 'Document3'
+
+    incoming_1 = graph.get_incoming_nodes(doc1)
+    assert len(incoming_1) == 2
+    assert incoming_1[0].text == 'Document0'
+    assert incoming_1[1].text == 'Document2'
+
+    outgoing_2 = graph.get_outgoing_nodes(doc2)
+    assert len(outgoing_2) == 2
+    assert outgoing_2[0].text == 'Document1'
+    assert outgoing_2[1].text == 'Document3'
+
+    incoming_2 = graph.get_incoming_nodes(doc2)
+    assert len(incoming_2) == 1
+    assert incoming_2[0].text == 'Document0'
+
+    outgoing_3 = graph.get_outgoing_nodes(doc3)
+    assert len(outgoing_3) == 0
+
+    incoming_3 = graph.get_incoming_nodes(doc3)
+    assert len(incoming_3) == 2
+    assert incoming_3[0].text == 'Document1'
+    assert incoming_3[1].text == 'Document2'
+
+    assert graph.get_incoming_nodes(Document()) is None
+    assert graph.get_outgoing_nodes(Document()) is None
+
+
+def test_graph_document_add_edges(graph):
+    validate_graph(graph)
+
+
+def test_graph_document_from_graph(graph):
+    graph2 = GraphDocument(graph)
+    validate_graph(graph2)
+
+
+def test_graph_document_from_proto(graph):
+    graph2 = GraphDocument(graph._pb_body)
+    validate_graph(graph2)
