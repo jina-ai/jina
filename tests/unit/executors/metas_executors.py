c801a5babdc6ce38bb537b595920e30dbe48ade8
jakob1111996 <jakob.kruse@jina.ai>
2021-06-07 15:53:43 +0200

    style: fix test files style

--- tests/unit/executors/metas_executors.py
+++ tests/unit/executors/metas_executors.py
@@ -2,7 +2,6 @@ from jina import Executor, requests, DocumentArray
 
 
 class TestExecutor(Executor):
-
     @requests
     def process(self, docs: DocumentArray, **kwargs):
         return docs
--- tests/unit/executors/test_set_metas.py
+++ tests/unit/executors/test_set_metas.py
@@ -59,14 +59,17 @@ def test_name_python_jaml_identical():
     # First way of import using py_modules argument in jaml file
     from jina.jaml.helper import load_py_modules
     from jina.helper import typename
+
     load_py_modules({'py_modules': ['metas_executors.py']})
     from metas_executors import TestExecutor
+
     jaml_executor = TestExecutor()
     jaml_metas_name = jaml_executor.metas.name
     jaml_typename = typename(jaml_executor)
 
     # Second way of importing directly via path in python
     from .metas_executors import TestExecutor
+
     py_executor = TestExecutor()
     py_metas_name = py_executor.metas.name
     py_typename = typename(py_executor)
