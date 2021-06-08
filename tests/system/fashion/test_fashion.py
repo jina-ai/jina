3b987b6d11e186070ad4cb14db18ab77ecad61f0
Han Xiao <artex.xh@gmail.com>
2021-06-08 22:55:47 +0800

    refactor: use memmap in hello world (#2584)
    * refactor: use memmap in hello world
    
    * refactor: use memmap in hello world
    
    * refactor: use memmap in hello world
    
    * test: fix broken test
    
    Co-authored-by: Maximilian Werk <maximilian.werk@jina.ai>

--- tests/system/fashion/test_fashion.py
+++ tests/system/fashion/test_fashion.py
@@ -4,7 +4,7 @@ import pytest
 
 import jina
 from jina.helloworld.fashion.app import hello_world
-from jina.helloworld.fashion.executors import *
+from jina.helloworld.fashion.my_executors import *
 from jina.parsers.helloworld import set_hw_parser
 
 
