5995ec5ab423ce1e8b9c940d67cb91a8cbd614f8
felix.wang <felix.wang@jina.ai>
2021-06-08 19:01:50 +0800

    deleting all legacy jina hub methods in 1.x

--- jina/parsers/hub/new.py
+++ jina/parsers/hub/new.py
@@ -15,19 +15,6 @@ def mixin_hub_new_parser(parser):
         help='where to output the generated project dir into.',
     )
     gp.add_argument(
-        '--template',
-        type=str,
-        default='https://github.com/jina-ai/cookiecutter-jina-hub.git',
-        help='The cookiecutter template directory containing a project template directory, or a URL to a git repository. Only used when `--type template`',
-    )
-    gp.add_argument(
-        '--type',
-        type=str,
-        default='pod',
-        choices=['pod', 'app', 'template'],
-        help='The template type for executor hub pod or app using cookiecutter.',
-    )
-    gp.add_argument(
         '--overwrite',
         action='store_true',
         default=False,
