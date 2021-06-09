652842383ef6fa2772922e0bfc47d9ee0dd515a3
bwanglzu <bo.wang@jina.ai>
2021-06-09 18:29:26 +0200

    test: fix chatbot test

--- tests/system/chatbot/test_chatbot.py
+++ tests/system/chatbot/test_chatbot.py
@@ -17,50 +17,54 @@ def helloworld_args(tmpdir):
         [
             '--workdir',
             str(tmpdir),
+            '--port-expose',
+            '8080',
             '--unblock-query-flow',
+            '--parallel',
+            '1',
         ]
     )
 
 
-@pytest.fixture
-def expected_result():
-    return '''It's not completely up to you.'''
-
-
-@pytest.fixture
-def flow(tmpdir):
-    return Flow().add(uses=MyTransformer, parallel=1).add(uses=MyIndexer, workspace=str(tmpdir))
-
-
-def test_chatbot(helloworld_args, expected_result, flow, mocker):
+def test_chatbot(helloworld_args, tmpdir, mocker):
     """Regression test for chatbot example."""
     hello_world(helloworld_args)
-    # def validate_response(resp):
-    #     assert len(resp.data.docs) == 1
-    #     for doc in resp.data.docs:
-    #         assert len(doc.matches) == 10
-    #
-    #
-    # targets = {
-    #     'covid-csv': {
-    #         'url': helloworld_args.index_data_url,
-    #         'filename': os.path.join(helloworld_args.workdir, 'dataset.csv'),
-    #     }
-    # }
-    #
-    # mock_on_done = mocker.Mock()
-    # mock_on_fail = mocker.Mock()
-    #
-    # # download the data
-    # download_data(targets, helloworld_args.download_proxy, task_name='download csv data')
-    # with flow, open(targets['covid-csv']['filename']) as fp:
-    #     flow.index(from_csv(fp, field_resolver={'question': 'text'}))
-    #     flow.search(
-    #         inputs=Document(text='Is my dog safe from virus'),
-    #         on_done=mock_on_done,
-    #         on_fail=mock_on_fail,
-    #         parameters={'top_k': 10},
-    #     )
-    #
-    #     mock_on_fail.assert_not_called()
-    #     validate_callback(mock_on_done, validate_response)
+
+    def validate_response(resp):
+        assert len(resp.data.docs) == 1
+        for doc in resp.data.docs:
+            assert len(doc.matches) == 1
+            assert (
+                'there is currently no evidence that pets such as dogs or cats can be'
+                in doc.matches[0].tags['answer']
+            )
+
+    targets = {
+        'covid-csv': {
+            'url': helloworld_args.index_data_url,
+            'filename': os.path.join(helloworld_args.workdir, 'dataset.csv'),
+        }
+    }
+
+    mock_on_done = mocker.Mock()
+    mock_on_fail = mocker.Mock()
+
+    download_data(
+        targets, helloworld_args.download_proxy, task_name='download csv data'
+    )
+    f = (
+        Flow()
+        .add(uses=MyTransformer, parallel=1)
+        .add(uses=MyIndexer, workspace=str(tmpdir))
+    )
+    with f, open(targets['covid-csv']['filename']) as fp:
+        f.index(from_csv(fp, field_resolver={'question': 'text'}))
+        f.search(
+            inputs=Document(content='Is my dog safe from virus'),
+            on_done=mock_on_done,
+            on_fail=mock_on_fail,
+            parameters={'top_k': 10},
+        )
+
+        mock_on_fail.assert_not_called()
+        validate_callback(mock_on_done, validate_response)
