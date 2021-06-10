12ae3f66cbf44a87b89ea4b70c1c9d9eddb34c4b
bwanglzu <bo.wang@jina.ai>
2021-06-10 09:08:52 +0200

    test: do not download data again

--- tests/system/chatbot/test_chatbot.py
+++ tests/system/chatbot/test_chatbot.py
@@ -1,12 +1,8 @@
-import os
-
 import pytest
 
 from jina import Flow, Document
-from jina.types.document.generators import from_csv
 from jina.helloworld.chatbot.app import hello_world
 from jina.parsers.helloworld import set_hw_chatbot_parser
-from jina.helloworld.chatbot.app import download_data
 from jina.helloworld.chatbot.my_executors import MyTransformer, MyIndexer
 from tests import validate_callback
 
@@ -35,35 +31,34 @@ def test_chatbot(helloworld_args, tmpdir, mocker):
         for doc in resp.data.docs:
             assert len(doc.matches) == 1
             assert (
-                'there is currently no evidence that pets such as dogs or cats can be'
+                'testing positive means that you have the virus'
                 in doc.matches[0].tags['answer']
             )
-
-    targets = {
-        'covid-csv': {
-            'url': helloworld_args.index_data_url,
-            'filename': os.path.join(helloworld_args.workdir, 'dataset.csv'),
-        }
-    }
 
     mock_on_done = mocker.Mock()
     mock_on_fail = mocker.Mock()
 
-    download_data(
-        targets, helloworld_args.download_proxy, task_name='download csv data'
-    )
-    f = (
+    flow = (
         Flow()
         .add(uses=MyTransformer, parallel=1)
         .add(uses=MyIndexer, workspace=str(tmpdir))
     )
-    with f, open(targets['covid-csv']['filename']) as fp:
-        f.index(from_csv(fp, field_resolver={'question': 'text'}))
+    with flow as f:
+        f.index(
+            inputs=Document(
+                text='Does testing positive mean that I have the virus and that I will develop symptoms?',
+                tags={
+                    "wrong_answer": "You should take precaution with any containers, Elliott says. \"The plasticgrocery bags I’d throw out right away, wash your hands and then clean yourfood. Chances (of infection) are low,\" she said. \"But better yet, bring yourown bags! It’s better for the environment anyway.\"",
+                    "answer": "Yes, testing positive means that you have the virus, but it does not mean that you will develop symptoms. Some people who have the virus don't have any symptoms at all.\n\nAt the same time, testing negative does not necessarily mean that you don't have the virus.",
+                },
+            ),
+        )
         f.search(
-            inputs=Document(content='Is my dog safe from virus'),
+            inputs=Document(
+                content='Does testing positive mean that I have the virus and that I will develop symptoms?'
+            ),
             on_done=mock_on_done,
             on_fail=mock_on_fail,
-            parameters={'top_k': 10},
         )
 
         mock_on_fail.assert_not_called()
