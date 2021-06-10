ae9fa982ff60cf021cef4766b9dedc12564d5f60
Wang Bo <bo.wang@jina.ai>
2021-06-10 09:43:50 +0200

    test: fix chatbot codecov missing (#2491)
    * test: fix chatbot test
    
    * feat: debug validate response
    
    * test: threading the helloworld
    
    * test: threading the helloworld
    
    * test: remove xprocess from requirements
    
    * test: join thread with timeout
    
    * test: replace thread with mp
    
    * test: rename process
    
    * test: fix test name
    
    * test: remove unused comments
    
    * test: join after terminate
    
    * test: join after terminate
    
    * test: kill process
    
    * test: call terminate manually
    
    * test: refine chatbot test without multiprocessing
    
    * test: fix chatbot test
    
    * test: revert remove of xprocess
    
    * test: do not download data again
    
    * test: do not download data again
    
    * test: create the same flow for test

--- tests/system/chatbot/test_chatbot.py
+++ tests/system/chatbot/test_chatbot.py
@@ -1,48 +1,62 @@
 import pytest
-import requests
-from xprocess import ProcessStarter
 
+from jina import Document
+from jina.helloworld.chatbot.app import hello_world, create_chatbot_flow
 from jina.parsers.helloworld import set_hw_chatbot_parser
+from tests import validate_callback
 
 
 @pytest.fixture
-def chatbot_args(tmpdir):
+def helloworld_args(tmpdir):
     return set_hw_chatbot_parser().parse_args(
-        ['--workdir', str(tmpdir), '--port-expose', '8080']
+        [
+            '--workdir',
+            str(tmpdir),
+            '--port-expose',
+            '8080',
+            '--unblock-query-flow',
+            '--parallel',
+            '1',
+        ]
     )
 
 
-@pytest.fixture
-def payload():
-    return {'top_k': 1, 'data': ['text:Is my dog safe from virus']}
+def test_chatbot_helloworld(helloworld_args):
+    hello_world(helloworld_args)  # ensure no error is raised.
 
 
-@pytest.fixture
-def post_uri():
-    return 'http://localhost:8080/search'
+def test_chatbot(tmpdir, mocker, helloworld_args):
+    #  test the index and query flow.
+    def validate_response(resp):
+        assert len(resp.data.docs) == 1
+        for doc in resp.data.docs:
+            assert len(doc.matches) == 1
+            assert (
+                'testing positive means that you have the virus'
+                in doc.matches[0].tags['answer']
+            )
 
+    mock_on_done = mocker.Mock()
+    mock_on_fail = mocker.Mock()
 
-@pytest.fixture
-def expected_result():
-    return '''It's not completely up to you.'''
+    flow = create_chatbot_flow(helloworld_args)
+    with flow as f:
+        f.index(
+            inputs=Document(
+                text='Does testing positive mean that I have the virus and that I will develop symptoms?',
+                tags={
+                    "wrong_answer": "You should take precaution with any containers, Elliott says. \"The plasticgrocery bags I’d throw out right away, wash your hands and then clean yourfood. Chances (of infection) are low,\" she said. \"But better yet, bring yourown bags! It’s better for the environment anyway.\"",
+                    "answer": "Yes, testing positive means that you have the virus, but it does not mean that you will develop symptoms. Some people who have the virus don't have any symptoms at all.\n\nAt the same time, testing negative does not necessarily mean that you don't have the virus.",
+                },
+            ),
+        )
+        f.search(
+            inputs=Document(
+                content='Does testing positive mean that I have the virus and that I will develop symptoms?'
+            ),
+            on_done=mock_on_done,
+            on_fail=mock_on_fail,
+        )
 
-
-@pytest.fixture(autouse=True)
-def start_server(xprocess, chatbot_args):
-    class Starter(ProcessStarter):
-        pattern = "You should see a demo page opened in your browser"
-        args = ["jina", "hello", "chatbot"]
-        max_read_lines = 10000
-
-    xprocess.ensure("server", Starter)
-    yield
-    xprocess.getinfo("server").terminate()
-
-
-def test_chatbot(payload, post_uri, expected_result):
-    """Regression test for chatbot example."""
-    resp = requests.post(post_uri, json=payload)
-    assert resp.status_code == 200
-    print('-------------------------1111111----')
-    print(resp.text)
-    assert expected_result in resp.text
+        mock_on_fail.assert_not_called()
+        validate_callback(mock_on_done, validate_response)
