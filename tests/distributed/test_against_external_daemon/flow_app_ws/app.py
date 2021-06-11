868bb8683613a4fcef226b8df4287909721e215b
Jacobowitz <tobias.jacobowitz@jina.ai>
2021-06-11 11:08:28 +0200

    fix(daemon): fix broken dummy test app

--- tests/distributed/test_against_external_daemon/flow_app_ws/app.py
+++ tests/distributed/test_against_external_daemon/flow_app_ws/app.py
@@ -1,12 +1,9 @@
 import sys
 from jina import Flow
-from .helper import foo, bar
 
 
 def main(port_expose):
     f = Flow(port_expose=port_expose).add()
-    foo()
-    bar()
     with f:
         f.block()
 
