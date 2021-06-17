581c3e911ccac15aecedb6e198e065fc72c7308e
Joan Fontanals Martinez <joan.martinez@jina.ai>
2021-06-17 07:56:45 +0200

    fix: fallback to proto sort if not valid interface

--- tests/unit/types/arrays/test_documentarray.py
+++ tests/unit/types/arrays/test_documentarray.py
@@ -272,6 +272,18 @@ def test_da_sort_by_document_interface_not_in_proto():
     assert da[0].embedding.shape == (1,)
 
 
+def test_da_sort_by_document_interface_in_proto():
+    docs = [Document(embedding=np.array([1] * (10 - i))) for i in range(10)]
+    da = DocumentArray(
+        [docs[i] if (i % 2 == 0) else docs[i].proto for i in range(len(docs))]
+    )
+    assert len(da) == 10
+    assert da[0].embedding.shape == (10,)
+
+    da.sort(key=lambda d: d.embedding.dense.shape[0])
+    assert da[0].embedding.shape == (1,)
+
+
 def test_da_reverse():
     docs = [Document(embedding=np.array([1] * (10 - i))) for i in range(10)]
     da = DocumentArray(
