0a7a2ba345c030e73a456ab53bfdb8729680cc74
Deepankar Mahapatro <deepankar.mahapatro@jina.ai>
2021-06-18 21:25:36 +0530

    docs: added docstrings

--- tests/daemon/unit/test_files.py
+++ tests/daemon/unit/test_files.py
@@ -1,10 +1,17 @@
 import os
+import tempfile
+from fastapi import UploadFile
+
+from daemon import daemon_logger
+from daemon.models import DaemonID
 from daemon.files import DaemonFile
+from daemon.files import workspace_files
 from daemon.excepts import Runtime400Exception
 
 import pytest
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
+cur_filename = os.path.basename(__file__)
 
 
 @pytest.mark.parametrize(
@@ -30,3 +37,17 @@ def test_jinad_good_ws(workdir, expected_response):
 def test_jinad_bad_ws(workdir):
     with pytest.raises(Runtime400Exception):
         DaemonFile(workdir=f'{cur_dir}/{workdir}')
+
+
+def _test_workspace_files():
+    with tempfile.NamedTemporaryFile() as fp1, tempfile.NamedTemporaryFile() as fp2:
+        fp1.write(b'Hello world1!')
+        fp2.write(b'Hello world2!')
+        fp1.flush()
+        fp2.flush()
+        fp1.seek(0, 0)
+        fp2.seek(0, 0)
+        id = DaemonID('jworkspace')
+        print(fp1.read())
+        files = [UploadFile(filename='a.txt'), UploadFile(filename='b.txt', file=fp2)]
+        workspace_files(id, files, daemon_logger)
