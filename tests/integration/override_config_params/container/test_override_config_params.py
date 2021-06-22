c79242264d54723d63d22e1c3678967e2e7e8244
Joan Fontanals Martinez <joan.martinez@jina.ai>
2021-06-22 15:41:51 +0200

    test: add test with flow

--- tests/integration/override_config_params/container/test_override_config_params.py
+++ tests/integration/override_config_params/container/test_override_config_params.py
@@ -3,7 +3,6 @@ import time
 import pytest
 
 from jina import Flow, Document
-from tests import validate_callback
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -17,21 +16,26 @@ def docker_image():
     os.system(f"docker rmi $(docker images | grep 'override-config-test')")
 
 
-def test_override_config_params(mocker, docker_image):
-    def validate_response(resp):
-        doc = resp.docs[0]
-        assert doc.tags['param1'] == 50
-        assert doc.tags['param2'] == 30
-        assert doc.tags['param3'] == 10  # not overriden
-        assert doc.tags['name'] == 'name'  # not override
-        assert doc.tags['workspace'] == 'different_workspace'
+@pytest.fixture()
+def flow(request):
+    flow_src = request.param
+    if flow_src == 'yml':
+        return Flow().load_config(os.path.join(cur_dir, 'flow.yml'))
+    elif flow_src == 'python':
+        return Flow().add(
+            uses='docker://override-config-test',
+            override_with_params={'param1': 50, 'param2': 30},
+            override_metas_params={'workspace': 'different_workspace'},
+        )
 
-    mock = mocker.Mock()
-    f = Flow().add(
-        uses='docker://override-config-test',
-        override_with_params={'param1': 50, 'param2': 30},
-        override_metas_params={'workspace': 'different_workspace'},
-    )
-    with f:
-        f.search(inputs=[Document()], on_done=mock)
-    validate_callback(mock, validate_response)
+
+@pytest.mark.parametrize('flow', ['yml', 'python'], indirect=['flow'])
+def test_override_config_params(docker_image, flow):
+    with flow:
+        resps = flow.search(inputs=[Document()], return_results=True)
+    doc = resps[0].docs[0]
+    assert doc.tags['param1'] == 50
+    assert doc.tags['param2'] == 30
+    assert doc.tags['param3'] == 10  # not overriden
+    assert doc.tags['name'] == 'name'  # not override
+    assert doc.tags['workspace'] == 'different_workspace'
--- tests/integration/override_config_params/zed/test_override_config_params.py
+++ tests/integration/override_config_params/zed/test_override_config_params.py
@@ -1,26 +1,31 @@
 import os
+import pytest
 
 from jina import Flow, Document
-from tests import validate_callback
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 
 
-def test_override_config_params(mocker):
-    def validate_response(resp):
-        doc = resp.docs[0]
-        assert doc.tags['param1'] == 50
-        assert doc.tags['param2'] == 30
-        assert doc.tags['param3'] == 10  # not overriden
-        assert doc.tags['name'] == 'name'  # not override
-        assert doc.tags['workspace'] == 'different_workspace'
+@pytest.fixture()
+def flow(request):
+    flow_src = request.param
+    if flow_src == 'yml':
+        return Flow().load_config(os.path.join(cur_dir, 'flow.yml'))
+    elif flow_src == 'python':
+        return Flow(return_results=True).add(
+            uses=os.path.join(cur_dir, 'default_config.yml'),
+            override_with_params={'param1': 50, 'param2': 30},
+            override_metas_params={'workspace': 'different_workspace'},
+        )
 
-    mock = mocker.Mock()
-    f = Flow().add(
-        uses=os.path.join(cur_dir, 'default_config.yml'),
-        override_with_params={'param1': 50, 'param2': 30},
-        override_metas_params={'workspace': 'different_workspace'},
-    )
-    with f:
-        f.search(inputs=[Document()], on_done=mock)
-    validate_callback(mock, validate_response)
+
+@pytest.mark.parametrize('flow', ['yml', 'python'], indirect=['flow'])
+def test_override_config_params(flow):
+    with flow:
+        resps = flow.search(inputs=[Document()], return_results=True)
+    doc = resps[0].docs[0]
+    assert doc.tags['param1'] == 50
+    assert doc.tags['param2'] == 30
+    assert doc.tags['param3'] == 10  # not overriden
+    assert doc.tags['name'] == 'name'  # not override
+    assert doc.tags['workspace'] == 'different_workspace'
