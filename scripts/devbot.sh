73d7e660203471b09a7fc34e55129ab98169d0aa
Han Xiao <han.xiao@jina.ai>
2021-06-29 15:02:41 +0800

    feat(cli): call version check in another thread

---
+++ scripts/devbot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# this script is run by jina-dev-bot
+
+# inject @overload info && black it
+arr=( $(python inject-cli-as-overload.py) ) && black -S "${arr[@]}"
+
+# update autocomplete info && black it
+python update-autocomplete-cli.py && black -S ../cli/autocomplete.py
