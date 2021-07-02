22199d32347adfa4d2a20f620ef2e5e3823584d7
numb3r3 <wangfelix87@gmail.com>
2021-07-02 18:21:08 +0800

    fix(tests): add prefetch parameter

--- tests/storm/echo_flow.py
+++ tests/storm/echo_flow.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import time
 import random
 import sys
@@ -16,8 +17,9 @@ class MyExecutor(Executor):
 @click.option('--polling', default='ANY')
 @click.option('--parallel', default=15)
 @click.option('--protocal', default='http')
-def cli(port, protocal, parallel, polling):
-    f = Flow(protocol=protocal, port_expose=port).add(
+@click.option('--prefetch', default=50)
+def cli(port, protocal, parallel, polling, prefetch):
+    f = Flow(protocol=protocal, port_expose=port, prefetch=prefetch).add(
         uses=MyExecutor, parallel=parallel, polling=polling
     )
     with f:
