1cb78224384dfd68d582b408c3aecc91a76ef743
Joan Fontanals Martinez <joan.martinez@jina.ai>
2021-07-07 10:21:31 +0200

    test: run only rolling update

--- scripts/get-all-test-paths.sh
+++ scripts/get-all-test-paths.sh
@@ -10,6 +10,9 @@ declare -a array1=( "tests/daemon/unit/*.py" )
 declare -a array2=( $(ls -d tests/daemon/{unit,integration}/*/ | grep -v '__pycache__' ))
 dest2=( "${array1[@]}" "${array2[@]}" )
 
-dest=( "${dest1[@]}" "${dest2[@]}" )
+dest=( "tests/integration/rolling_update/*" )
 
+#printf '%s\n' "${dest[@]}" | jq -R . | jq -cs .
+#printf "tests/integration/rolling_update/test_rolling_update.py"
+#["tests/integration/rolling_update/*"]
 printf '%s\n' "${dest[@]}" | jq -R . | jq -cs .
