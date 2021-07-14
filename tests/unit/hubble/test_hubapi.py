6d6f80e68f0356028ab9496bc6c0d564d7b69574
numb3r3 <wangfelix87@gmail.com>
2021-07-14 14:28:48 +0800

    chore(hubble): remove unused codes

--- tests/unit/hubble/test_hubapi.py
+++ tests/unit/hubble/test_hubapi.py
@@ -18,21 +18,6 @@ def test_executor():
     return HubExecutor(uuid='hello', alias=None, sn=0, tag='v0')
 
 
-# @pytest.fixture
-# def local_hub_executor(test_envs, executor_zip_file, test_executor):
-#     hubapi.install_local(executor_zip_file, test_executor)
-
-
-# def test_exist_local(test_envs, local_hub_executor):
-#     assert hubapi.exist_local('hello', 'v0')
-
-
-# def test_resolve_local(test_envs, local_hub_executor, test_executor):
-#     pkg, pkg_dist = hubapi.resolve_local(test_executor)
-#     assert pkg is not None
-#     assert pkg_dist is not None
-
-
 def test_install_local(test_envs, executor_zip_file, test_executor):
     assert not hubapi.exist_local(test_executor.uuid, test_executor.tag)
     hubapi.install_local(executor_zip_file, test_executor)
@@ -40,11 +25,6 @@ def test_install_local(test_envs, executor_zip_file, test_executor):
 
     hubapi.uninstall_local(test_executor.uuid)
     assert not hubapi.exist_local(test_executor.uuid, test_executor.tag)
-
-
-# def test_uninstall_locall(test_envs, local_hub_executor):
-#     hubapi.uninstall_local('hello')
-#     assert not hubapi.exist_local('hello', None)
 
 
 def test_load_dump_secret(test_envs):
