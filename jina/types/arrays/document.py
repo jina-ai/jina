58843df4e2f3ecef84031720993e4653c1da9132
bwanglzu <bo.wang@jina.ai>
2021-07-16 15:21:47 +0200

    feat: filter in documentarray

--- jina/types/arrays/document.py
+++ jina/types/arrays/document.py
@@ -1,4 +1,6 @@
+import re
 import json
+import operator
 import warnings
 from abc import abstractmethod
 from collections.abc import MutableSequence, Iterable as Itr
@@ -14,6 +16,7 @@ from typing import (
     Generator,
     BinaryIO,
     TypeVar,
+    Dict,
 )
 
 from .traversable import TraversableSequence
@@ -439,3 +442,43 @@ class DocumentArray(
             dap.ParseFromString(fp.read())
             da = DocumentArray(dap.docs)
             return da
+
+    def fuzzy_filter(
+        self, regexes: List[Dict[str]], traversal_paths: Iterable[str] = ['r']
+    ):
+        """Filter document array by tags attribute."""
+        filtered = DocumentArray()
+        docs = self.traverse_flat(traversal_paths)
+        for tag_name, regex in regexes:
+            pattern = re.compile(regex)
+            for doc in docs:
+                if re.match(pattern, doc.get(tag_name, '')):
+                    filtered.append(doc)
+        return filtered
+
+    def hard_filter(
+        self,
+        conditions: List[tuple(str, str, str)],
+        traversal_paths: Iterable[str] = ['r'],
+    ):
+        """Filter documents by hard match on tags."""
+        filtered = DocumentArray()
+        docs = self.traverse_flat(traversal_paths)
+        operators_map = {
+            'gt': operator.gt,
+            'lt': operator.lt,
+            'eq': operator.eq,
+            'ne': operator.ne,
+            'ge': operator.ge,
+            'ne': operator.ne,
+        }
+        for condition in conditions:
+            operator_instance = operators_map.get(condition[1], None)
+            if operator_instance:
+                for doc in docs:
+                    tag_name = doc.get(condition[0], None)
+                    tag_value = doc.get(condition[2], None)
+                    if tag_name and tag_value:
+                        if operator_instance(tag_name, tag_value):
+                            filtered.append(doc)
+        return filtered
