22dc5bc429e5c5999c3c2f2ca4421839495d72ba
David Buchaca Prats <davidbuchaca@gmail.com>
2021-07-16 12:45:57 +0200

    fix: update match to allways compute top k as distances

--- jina/types/arrays/document.py
+++ jina/types/arrays/document.py
@@ -3,7 +3,6 @@ import warnings
 from abc import abstractmethod
 from collections.abc import MutableSequence, Iterable as Itr
 from contextlib import nullcontext
-import numpy as np
 from typing import (
     Union,
     Iterable,
@@ -21,6 +20,9 @@ from .traversable import TraversableSequence
 from ..document import Document
 from ...helper import typename, cached_property, cache_invalidate
 from ...proto import jina_pb2
+
+import numpy as np
+from math import inf
 
 try:
     # when protobuf using Cpp backend
@@ -300,76 +302,112 @@ class DocumentArray(
         else:
             self._pb_body.sort(*args, **kwargs)
 
-    def _compute_dist(
+    def compute_distances(
         self, X: 'np.ndarray', Y: 'np.ndarray', metric: str
     ) -> 'np.ndarray':
-        """Computes the distance between each row of X and each row on Y.
+        """Computes the distance between each row of X and each row on Y according to `metric`.
 
         - Let `n_X = X.shape[0]`
         - Let `n_Y = Y.shape[0]`
-        - Returns a matrix `dist` of shape `(n_X, n_Y)`.
-          `dist[i,j] = metric(X[i], X[j])`.
+        - Returns a matrix `dist` of shape `(n_X, n_Y)` with `dist[i,j] = metric(X[i], X[j])`.
 
         :param X: np.ndarray of ndim 2
         :param Y:  np.ndarray of ndim 2
         :param metric: string describing the metric type
-        :return:  np.ndarray of ndim 2
+        :return: np.ndarray of ndim 2
         """
+        assert metric in [
+            'cosine',
+            'euclidean_squared',
+            'euclidean',
+        ], f'Input metric={metric} not valid'
+
         if metric == 'cosine':
             dists = cosine_distance(X, Y)
-        if metric == 'euclidean':
+        if metric == 'euclidean_squared':
             dists = euclidean_distance_squared(X, Y)
+        if metric == 'euclidean':
+            dists = np.sqrt(euclidean_distance_squared(X, Y))
         return dists
 
     def match(
         self,
         darray: 'DocumentArray',
         metric: str = 'cosine',
-        limit: Optional[int] = None,
+        limit: Optional[int] = inf,
+        is_distance: bool = False,
     ) -> None:
         """Compute embedding based nearest neighbour in `another` for each Document in `self`,
         and store results in `matches`.
 
         Note:
-            - If metric is 'cosine' it uses the cosine **similarity**.
-            - IF metric is 'euclidean' it uses the euclidean **distance**.
+            - If metric is 'cosine' it uses the cosine **distance**.
+            - If metric is 'euclidean' it uses the euclidean **distance**.
+            - If metric is 'euclidean_squared' it uses the euclidean **distance** squared.
 
         :param darray: the other DocumentArray to match against
         :param metric: the distance metric
         :param limit: the maximum number of matches, when not given
                       all Documents in `another` are considered as matches
+        :param is_distance: Boolean flag informing if `metric` values want to be considered as distances or scores.
         """
+
+        def invert_if_score(
+            X: 'np.ndarray', metric: str, is_distance: bool
+        ) -> 'np.ndarray':
+            """Invert values to scores if `is_distance=False` according to the metric that is passed.
+
+            :param X: input np.ndarray
+            :param metric: string defining distance function ['cosine', 'euclidean', lidean_squared'].
+            :param is_distance: Boolean flag that describes if values in X need to be erted to similarities.
+            :return: A np.ndarray with distances if `is_distance=True`, scores if distance=False`.
+            """
+            if metric == 'cosine':
+                if is_distance:
+                    X = X
+                else:
+                    X = 1 - X
+            if metric == 'euclidean_squared' or metric == 'euclidean':
+                if is_distance:
+                    X = X
+                else:
+                    X = 1 / X
+            return X
+
         X = np.stack(self.get_attributes('embedding'))
         Y = np.stack(darray.get_attributes('embedding'))
-        if limit is None:
-            limit = len(darray)
-        dists = self._compute_dist(X, Y, metric)
-        idx, dist = self._get_sorted_top_k(dists, limit)
+        limit = min(limit, len(darray))
+
+        dists = self.compute_distances(X, Y, metric)
+        idx, dist = self._get_sorted_smallest_k(dists, limit)
 
         for _q, _ids, _dists in zip(self, idx, dist):
             for _id, _dist in zip(_ids, _dists):
                 d = Document(darray[int(_id)], copy=True)
-                if metric == 'cosine':
-                    d.scores[metric] = 1 - _dist
-                else:
-                    d.scores[metric] = _dist
+                d.scores[metric] = invert_if_score(_dist, metric, is_distance)
                 _q.matches.append(d)
 
     @staticmethod
-    def _get_sorted_top_k(
-        dist: 'np.ndarray', top_k: int
+    def _get_sorted_smallest_k(
+        dists: 'np.ndarray', top_k: int
     ) -> Tuple['np.ndarray', 'np.ndarray']:
-        if top_k >= dist.shape[1]:
-            idx = dist.argsort(axis=1)[:, :top_k]
-            dist = np.take_along_axis(dist, idx, axis=1)
-        else:
-            idx_ps = dist.argpartition(kth=top_k, axis=1)[:, :top_k]
-            dist = np.take_along_axis(dist, idx_ps, axis=1)
-            idx_fs = dist.argsort(axis=1)
-            idx = np.take_along_axis(idx_ps, idx_fs, axis=1)
-            dist = np.take_along_axis(dist, idx_fs, axis=1)
+        """Finds the smallest `top_k` values (and its indices) from `dists`. Returns both indices and values.
 
-        return idx, dist
+        :param dists: np.ndarray of distances
+        :param top_k: number of values to retrieve
+        :return: Lowest values in dists and the indices of those values
+        """
+        if top_k >= dists.shape[1]:
+            indices = dists.argsort(axis=1)[:, :top_k]
+            dists = np.take_along_axis(dists, indices, axis=1)
+        else:
+            indices_ps = dists.argpartition(kth=top_k, axis=1)[:, :top_k]
+            dists = np.take_along_axis(dists, indices_ps, axis=1)
+            indices_fs = dists.argsort(axis=1)
+            indices = np.take_along_axis(indices_ps, indices_fs, axis=1)
+            dists = np.take_along_axis(dists, indices_fs, axis=1)
+
+        return indices, dists
 
     def __bool__(self):
         """To simulate ```l = []; if l: ...```
