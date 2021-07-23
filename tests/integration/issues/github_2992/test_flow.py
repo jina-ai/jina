15ca7358a8f10aa00cf55b2f99ba9a35f1a63b70
Alaeddine Abdessalem <alaeddine-13@live.fr>
2021-07-23 08:03:09 +0100

    chore: remove on_always from integration test

--- tests/integration/issues/github_2992/test_flow.py
+++ tests/integration/issues/github_2992/test_flow.py
@@ -3,17 +3,10 @@ from jina.types.arrays.memmap import DocumentArrayMemmap
 
 
 def test_dam_flow(tmpdir):
-    def _assert_called_once(response):
-        assert len(response.docs) == 1
-        assert not _assert_called_once.called
-        _assert_called_once.called = True
-
-    _assert_called_once.called = False
-
     f = Flow().add()
     dam = DocumentArrayMemmap(tmpdir)
     dam.append(Document())
     with f:
-        response = f.post('/', dam, on_always=_assert_called_once, return_results=True)
+        response = f.post('/', dam, return_results=True)
     assert len(response) == 1
     assert len(response[0].docs) == 1
