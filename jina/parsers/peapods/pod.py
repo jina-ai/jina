2fcc94af365a74aa3ea45d05ab595e5c34ba1b4a
Maximilian Werk <maximilian.werk@jina.ai>
2021-08-02 10:12:42 +0200

    fix: removed unnecessary argument

--- jina/parsers/peapods/pod.py
+++ jina/parsers/peapods/pod.py
@@ -90,15 +90,6 @@ The polling strategy of the Pod (when `parallel>1`)
     )
 
     parser.add_argument(
-        '--dynamic-routing',
-        action='store_true',
-        default=True,
-        help='The Pod will setup the socket types of the HeadPea and TailPea depending on this argument.'
-        if _SHOW_ALL_ARGS
-        else argparse.SUPPRESS,
-    )
-
-    parser.add_argument(
         '--no-dynamic-routing',
         action='store_false',
         dest='dynamic_routing',
