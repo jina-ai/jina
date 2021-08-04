f31e337e2a7eaf6580f46ab621fdf321118833f4
Florian Hönicke <hoenicke.florian@gmail.com>
2021-08-04 14:41:54 +0200

    fix: imports

--- jina/kubernetes/k8s-deploy.py
+++ jina/kubernetes/k8s-deploy.py
@@ -2,32 +2,32 @@ from jina import Flow
 
 dump_path = '/shared'
 
-index_flow = (
-    Flow(name='index-flow')
-    .add(
-        name='cliptext',
-        uses='jinahub+docker://CLIPTextEncoder',
-        shards=1,
-        polling='any',
-    )
-    .add(
-        name='cliptext2',
-        uses='jinahub+docker://CLIPTextEncoder',
-        needs='gateway',
-        shards=1,
-        polling='any',
-    )
-    .add(
-        name='textindexer',
-        uses='jinahub+docker://PostgreSQLStorage',
-        needs=['cliptext'],
-    )
-    .add(
-        name='textindexer2',
-        uses='jinahub+docker://PostgreSQLStorage',
-        needs=['cliptext2'],
-    )
-)
+# index_flow = (
+#     Flow(name='index-flow')
+#     .add(
+#         name='cliptext',
+#         uses='jinahub+docker://CLIPTextEncoder',
+#         shards=1,
+#         polling='any',
+#     )
+#     .add(
+#         name='cliptext2',
+#         uses='jinahub+docker://CLIPTextEncoder',
+#         needs='gateway',
+#         shards=1,
+#         polling='any',
+#     )
+#     .add(
+#         name='textindexer',
+#         uses='jinahub+docker://PostgreSQLStorage',
+#         needs=['cliptext'],
+#     )
+#     .add(
+#         name='textindexer2',
+#         uses='jinahub+docker://PostgreSQLStorage',
+#         needs=['cliptext2'],
+#     )
+# )
 
 search_flow = (
     Flow(name='search-flow')
@@ -37,37 +37,37 @@ search_flow = (
         shards=1,
         polling='any',
     )
-    .add(
-        name='cliptext2',
-        uses='jinahub+docker://CLIPTextEncoder',
-        needs='gateway',
-        shards=1,
-        polling='any',
-    )
-    .add(
-        name='searcher1',
-        shards=2,
-        polling='all',
-        uses='jinahub+docker://AnnoySearcher',
-        uses_with={'dump_path': dump_path},
-        uses_after='gcr.io/jina-showcase/match-merger',
-        needs=['cliptext'],
-    )
-    .add(
-        name='searcher2',
-        shards=2,
-        polling='all',
-        uses='jinahub+docker://AnnoySearcher',
-        uses_with={'dump_path': dump_path},
-        uses_after='gcr.io/jina-showcase/match-merger',
-        needs=['cliptext2'],
-    )
-    .add(
-        name='ranker',
-        uses='jinahub+docker://MinRanker',
-        uses_with={'metric': 'cosine'},
-        needs=['searcher1', 'searcher2'],
-    )
+    # .add(
+    #     name='cliptext2',
+    #     uses='jinahub+docker://CLIPTextEncoder',
+    #     needs='gateway',
+    #     shards=1,
+    #     polling='any',
+    # )
+    # .add(
+    #     name='searcher1',
+    #     shards=2,
+    #     polling='all',
+    #     uses='jinahub+docker://AnnoySearcher',
+    #     uses_with={'dump_path': dump_path},
+    #     uses_after='gcr.io/jina-showcase/match-merger',
+    #     needs=['cliptext'],
+    # )
+    # .add(
+    #     name='searcher2',
+    #     shards=2,
+    #     polling='all',
+    #     uses='jinahub+docker://AnnoySearcher',
+    #     uses_with={'dump_path': dump_path},
+    #     uses_after='gcr.io/jina-showcase/match-merger',
+    #     needs=['cliptext2'],
+    # )
+    # .add(
+    #     name='ranker',
+    #     uses='jinahub+docker://MinRanker',
+    #     uses_with={'metric': 'cosine'},
+    #     needs=['searcher1', 'searcher2'],
+    # )
 )
 
 # print('deploy index flow')
