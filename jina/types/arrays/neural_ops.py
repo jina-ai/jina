dbbdf7bde13da381c429ab86109fda8408e9c51e
David Buchaca Prats <davidbuchaca@gmail.com>
2021-08-09 10:51:23 +0200

    feat: match with batching capability (#3113)
    * feat: match with batching capability
    
    * fix: update helper
    
    * fix: allow args in example
    
    * fix: add types
    
    * fix: interface avoid repetition code
    
    * fix: update varible names helper
    
    * feat: add tests row update
    
    * fix: update test with args
    
    * fix: assert type
    
    * feat: add test to check batching results match non batching
    
    * fix: behaviour with limit when none

--- jina/types/arrays/neural_ops.py
+++ jina/types/arrays/neural_ops.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from ... import Document
 from ...importer import ImportExtensions
-from ...math.helper import top_k, minmax_normalize
+from ...math.helper import top_k, minmax_normalize, update_rows_x_mat_best
 from ...logging.predefined import default_logger
 
 if False:
@@ -22,82 +22,59 @@ class DocumentArrayNeuralOpsMixin:
         metric: Union[
             str, Callable[['np.ndarray', 'np.ndarray'], 'np.ndarray']
         ] = 'cosine',
-        limit: Optional[int] = inf,
+        limit: Optional[int] = 20,
         normalization: Optional[Tuple[int, int]] = None,
         use_scipy: bool = False,
         metric_name: Optional[str] = None,
+        batch_size: Optional[int] = None,
     ) -> None:
         """Compute embedding based nearest neighbour in `another` for each Document in `self`,
         and store results in `matches`.
-
         .. note::
             'cosine', 'euclidean', 'sqeuclidean' are supported natively without extra dependency.
-
             You can use other distance metric provided by ``scipy``, such as ‘braycurtis’, ‘canberra’, ‘chebyshev’,
             ‘cityblock’, ‘correlation’, ‘cosine’, ‘dice’, ‘euclidean’, ‘hamming’, ‘jaccard’, ‘jensenshannon’,
             ‘kulsinski’, ‘mahalanobis’, ‘matching’, ‘minkowski’, ‘rogerstanimoto’, ‘russellrao’, ‘seuclidean’,
             ‘sokalmichener’, ‘sokalsneath’, ‘sqeuclidean’, ‘wminkowski’, ‘yule’.
-
             To use scipy metric, please set ``use_scipy=True``.
-
         - To make all matches values in [0, 1], use ``dA.match(dB, normalization=(0, 1))``
         - To invert the distance as score and make all values in range [0, 1],
             use ``dA.match(dB, normalization=(1, 0))``. Note, how ``normalization`` differs from the previous.
-
         :param darray: the other DocumentArray or DocumentArrayMemmap to match against
         :param metric: the distance metric
-        :param limit: the maximum number of matches, when not given
-                      all Documents in `another` are considered as matches
+        :param limit: the maximum number of matches, when not given defaults to 20.
         :param normalization: a tuple [a, b] to be used with min-max normalization,
                                 the min distance will be rescaled to `a`, the max distance will be rescaled to `b`
                                 all values will be rescaled into range `[a, b]`.
         :param use_scipy: use Scipy as the computation backend
         :param metric_name: if provided, then match result will be marked with this string.
+        :param batch_size: if provided, then `darray` is loaded in chunks of, at most, batch_size elements. This option
+                           will be slower but more memory efficient. Specialy indicated if `darray` is a big
+                           DocumentArrayMemmap.
         """
-        is_sparse = False
 
-        if isinstance(darray[0].embedding, np.ndarray):
-            X = np.stack(self.get_attributes('embedding'))
-            Y = np.stack(darray.get_attributes('embedding'))
-        else:
-            import scipy.sparse as sp
-
-            if sp.issparse(darray[0].embedding):
-                X = sp.vstack(self.get_attributes('embedding'))
-                Y = sp.vstack(darray.get_attributes('embedding'))
-                is_sparse = True
-
-        limit = min(limit, len(darray))
-        if isinstance(metric, str):
-
-            if use_scipy and is_sparse is False:
-                # cdist from scipy does not support sparse arrays
-                from scipy.spatial.distance import cdist
-
-                dists = cdist(X, Y, metric)
+        if callable(metric):
+            cdist = metric
+        elif isinstance(metric, str):
+            if use_scipy:
+                from scipy.spatial.distance import cdist as cdist
             else:
-                if use_scipy:
-                    default_logger.info(
-                        f'Scipy cdist does not support sparse arrays, using Jina.math.distances sparse cdist'
-                    )
-                from ...math.distance import cdist
-
-                dists = cdist(X, Y, metric, is_sparse=is_sparse)
-
-        elif callable(metric):
-            dists = metric(X, Y)
+                from ...math.distance import cdist as cdist
         else:
             raise TypeError(
                 f'metric must be either string or a 2-arity function, received: {metric!r}'
             )
 
-        dist, idx = top_k(dists, min(limit, len(darray)), descending=False)
+        metric_name = metric_name or (metric.__name__ if callable(metric) else metric)
+        limit = len(darray) if limit is None else limit
 
-        if normalization is not None:
-            if isinstance(normalization, (tuple, list)):
-                dist = minmax_normalize(dist, normalization)
+        if batch_size:
+            dist, idx = self._match_online(
+                darray, cdist, limit, normalization, metric_name, batch_size
+            )
+        else:
+            dist, idx = self._match(darray, cdist, limit, normalization, metric_name)
 
-        m_name = metric_name or (metric.__name__ if callable(metric) else metric)
         for _q, _ids, _dists in zip(self, idx, dist):
             _q.matches.clear()
             for _id, _dist in zip(_ids, _dists):
@@ -108,7 +85,99 @@ class DocumentArrayNeuralOpsMixin:
                 if d.id in self:
                     d = Document(d, copy=True)
                     d.pop('matches')
-                _q.matches.append(d, scores={m_name: _dist}, copy=False)
+                _q.matches.append(d, scores={metric_name: _dist}, copy=False)
+
+    def _match(self, darray, cdist, limit, normalization, metric_name):
+        """
+        Computes the matches between self and `darray` loading `darray` into main memory.
+        :param darray: the other DocumentArray or DocumentArrayMemmap to match against
+        :param cdist: the distance metric
+        :param limit: the maximum number of matches, when not given
+                      all Documents in `darray` are considered as matches
+        :param normalization: a tuple [a, b] to be used with min-max normalization,
+                                the min distance will be rescaled to `a`, the max distance will be rescaled to `b`
+                                all values will be rescaled into range `[a, b]`.
+        :param metric_name: if provided, then match result will be marked with this string.
+        :return: distances and indices
+        """
+        is_sparse = False
+
+        if isinstance(darray[0].embedding, np.ndarray):
+            x_mat = np.stack(self.get_attributes('embedding'))
+            y_mat = np.stack(darray.get_attributes('embedding'))
+        else:
+            import scipy.sparse as sp
+
+            if sp.issparse(darray[0].embedding):
+                x_mat = sp.vstack(self.get_attributes('embedding'))
+                y_mat = sp.vstack(darray.get_attributes('embedding'))
+                is_sparse = True
+
+        if is_sparse:
+            dists = cdist(x_mat, y_mat, metric_name, is_sparse=is_sparse)
+        else:
+            dists = cdist(x_mat, y_mat, metric_name)
+
+        dist, idx = top_k(dists, min(limit, len(darray)), descending=False)
+        if isinstance(normalization, (tuple, list)) and normalization is not None:
+            dist = minmax_normalize(dist, normalization)
+
+        return dist, idx
+
+    def _match_online(
+        self, darray, cdist, limit, normalization, metric_name, batch_size
+    ):
+        """
+        Computes the matches between self and `darray` loading `darray` into main memory in chunks of size `batch_size`.
+
+        :param darray: the other DocumentArray or DocumentArrayMemmap to match against
+        :param cdist: the distance metric
+        :param limit: the maximum number of matches, when not given
+                      all Documents in `another` are considered as matches
+        :param normalization: a tuple [a, b] to be used with min-max normalization,
+                                the min distance will be rescaled to `a`, the max distance will be rescaled to `b`
+                                all values will be rescaled into range `[a, b]`.
+        :param batch_size: length of the chunks loaded into memory from darray.
+        :param metric_name: if provided, then match result will be marked with this string.
+        :return: distances and indices
+        """
+        assert isinstance(
+            darray[0].embedding, np.ndarray
+        ), f'expected embedding of type np.ndarray but received {type(darray[0].embedding)}'
+
+        x_mat = np.stack(self.get_attributes('embedding'))
+        n_x = x_mat.shape[0]
+
+        def batch_generator(y_darray: 'DocumentArrayMemmap', n_batch: int):
+            n_max = len(y_darray)
+            for i in range(0, len(y_darray), n_batch):
+                y_mat = np.vstack(
+                    [y_darray[j].embedding for j in range(i, min(i + n_batch, n_max))]
+                )
+                yield y_mat, i
+
+        y_batch_generator = batch_generator(darray, batch_size)
+        top_dists = np.inf * np.ones((n_x, limit))
+        top_inds = np.zeros((n_x, limit), dtype=int)
+
+        for y_batch, y_batch_start_pos in y_batch_generator:
+            distances = cdist(x_mat, y_batch, metric_name)
+            dists, inds = top_k(distances, limit, descending=False)
+
+            if isinstance(normalization, (tuple, list)) and normalization is not None:
+                dists = minmax_normalize(dists, normalization)
+
+            inds = y_batch_start_pos + inds
+            top_dists, top_inds = update_rows_x_mat_best(
+                top_dists, top_inds, dists, inds, limit
+            )
+
+        # sort final the final `top_dists` and `top_inds` per row
+        permutation = np.argsort(top_dists, axis=1)
+        dist = np.take_along_axis(top_dists, permutation, axis=1)
+        idx = np.take_along_axis(top_inds, permutation, axis=1)
+
+        return dist, idx
 
     def visualize(
         self,
