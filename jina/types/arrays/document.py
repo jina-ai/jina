a2edba7a8257b59d07707ec0c9e2f24e3c6f811d
bwanglzu <bo.wang@jina.ai>
2021-08-09 16:00:21 +0200

    perf: split get attribute mixin

--- jina/types/arrays/document.py
+++ jina/types/arrays/document.py
@@ -46,6 +46,58 @@ DocumentArraySourceType = TypeVar(
 )
 
 
+class DocumentArrayMemmapGetAttrMixin:
+    """A mixin that provides attributes getter in bulk """
+
+    @abstractmethod
+    def __iter__(self):
+        ...
+
+    def get_attributes(self, *fields: str) -> Union[List, List[List]]:
+        """Return all nonempty values of the fields from all docs this array contains
+
+        :param fields: Variable length argument with the name of the fields to extract
+        :return: Returns a list of the values for these fields.
+            When `fields` has multiple values, then it returns a list of list.
+        """
+        fields = list(fields)
+        if 'embedding' in fields:
+            embeddings = self.get_cached_embedding()
+            index = fields.index('embedding')
+            fields = fields.remove('embedding')
+        if fields:
+            contents = [doc.get_attributes(*fields) for doc in self]
+            if len(fields) > 1:
+                contents = list(map(list, zip(*contents)))
+            if index:
+                contents.insert(index, embeddings)
+
+        return contents
+
+    def get_attributes_with_docs(
+        self,
+        *fields: str,
+    ) -> Tuple[Union[List, List[List]], 'DocumentArray']:
+        """Return all nonempty values of the fields together with their nonempty docs
+
+        :param fields: Variable length argument with the name of the fields to extract
+        :return: Returns a tuple. The first element is  a list of the values for these fields.
+            When `fields` has multiple values, then it returns a list of list. The second element is the non-empty docs.
+        """
+
+        contents = []
+        docs_pts = []
+
+        for doc in self:
+            contents.append(doc.get_attributes(*fields))
+            docs_pts.append(doc)
+
+        if len(fields) > 1:
+            contents = list(map(list, zip(*contents)))
+
+        return contents, DocumentArray(docs_pts)
+
+
 class DocumentArrayGetAttrMixin:
     """A mixin that provides attributes getter in bulk """
 
