f10b7e79f8e10b2b6c979451dc11f54d0d29fe35
slettner <sebastian.lettner@jina.ai>
2021-08-24 10:56:52 +0200

    tests: add wrapper for kind

--- tests/kubernetes_jina/test_example_index_flow.py
+++ tests/kubernetes_jina/test_example_index_flow.py
@@ -1,18 +1,19 @@
+import time
+
 import pytest
-from typing import Generator, Dict, List
+from typing import Dict, List
 from http import HTTPStatus
 
 import requests
 
-from jina import Flow, Document, DocumentArray
-import pykube
+from jina import Flow
 
 
 @pytest.fixture()
 def products() -> List[Dict]:
     data = [
         {
-            "name": f"Gucci Handbag {i+100}",
+            "name": f"Gucci Handbag {i+1}",
             "description": "Black handbag from Gucci with golden decorations.",
             "uri": "https://media.gucci.com/style/"
                    "DarkGray_Center_0_0_1200x1200/"
@@ -27,7 +28,7 @@ def products() -> List[Dict]:
 def k8s_index_flow(executor_image) -> Flow:
     # TODO: Fix: implicitly pulls Gateway and JoinAll executor from remote should be avoided
     index_flow = (
-        Flow(name='index-flow', port_expose=8080, protocol='http')
+        Flow(name='index-flow', port_expose=8080, infrastructure='K8S', protocol='http')
         .add(
             name='segmenter',
             uses=executor_image,
@@ -56,38 +57,45 @@ def k8s_index_flow(executor_image) -> Flow:
             needs='imageencoder',
             uses_with={'name': 'imagestorage'},
         )
-    ).needs_all()
+    )
+    # ).needs_all()
     return index_flow
 
 
-def test_deploy(k8s_cluster, executor_image, k8s_index_flow: Flow, products: List[Dict]):
-    expected_running_pods = 7
-    valid_phases = ['Pending', 'ContainerCreating', 'Running']
+@pytest.mark.timeout(360)
+def test_deploy(k8s_cluster, executor_image, k8s_index_flow: Flow, products: List[Dict], logger):
+    expected_running_pods = 6  # TODO: set to 7 when joiner is finished
+
     # image pull anyways must be Never or IfNotPresent otherwise kubernetes will try to pull the image anyway
-    k8s_cluster.load_docker_image(executor_image)
+    logger.debug(f'Loading docker image {executor_image} into kind cluster...')
+    k8s_cluster.needs_docker_image(executor_image)
+    logger.debug(f'Done loading docker image {executor_image} into kind cluster...')
 
-    k8s_index_flow.deploy('k8s')
+    logger.debug(f'Starting flow on kind cluster...')
+    k8s_index_flow.start()
+    logger.debug(f'Done starting flow on kind cluster...')
 
-    api = pykube.HTTPClient(pykube.KubeConfig.from_file())
-    running_pods = []
     # TODO: Is there a better way to wait for pods to reach Running phase?
+    logger.debug(f'Starting to wait for pods in kind cluster to reach "RUNNING" state...')
     waiting = True
     while waiting:
-        for pod in list(pykube.Pod.objects(api, namespace='index-flow')):
-            assert pod.obj['status']['phase'] in valid_phases, f'Pod {pod.name} is in invalid phase' \
-                                                               f' {pod.obj["status"]["phase"]}.'
-            if pod.obj['status']['phase'] == 'Running' and pod.name not in running_pods:
-                running_pods.append(pod.name)
-        if len(running_pods) == expected_running_pods:
+        num_running_pods = len(k8s_cluster.list_ready_pods('index-flow'))
+        if num_running_pods == expected_running_pods:
             waiting = False
+        time.sleep(3)
+        logger.debug(f'Still waiting for pods to reach running state '
+                     f'(Current Status: {num_running_pods}/{expected_running_pods}).')
 
     expected_traversed_executors = ['segmenter', 'imageencoder', 'textencoder', 'imagestorage', 'textstorage']
 
-    # TODO: Fix Connection Refused Error (appears inconsistently)
-    with k8s_cluster.port_forward('service/gateway', 8080, '-n', 'index-flow', local_port=8080) as port:
+    logger.debug(f'Starting port-forwarding to gateway service...')
+    with k8s_cluster.port_forward('service/gateway', 8080, 8080, 'index-flow') as _:
+        logger.debug(f'Port-forward running...')
 
         resp = requests.post(f'http://localhost:8080/index', json={'data': products})
-        assert resp.status_code == HTTPStatus.OK
-        docs = resp.json()['data']['docs']
-        for doc in docs:
-            assert sorted(list(doc['tags']['traversed-executors'])) == sorted(expected_traversed_executors)
+        logger.debug(resp.status_code)
+
+    assert resp.status_code == HTTPStatus.OK
+    docs = resp.json()['data']['docs']
+    for doc in docs:
+        assert sorted(list(doc['tags']['traversed-executors'])) == sorted(expected_traversed_executors)
