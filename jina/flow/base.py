d97edf56e5ef60ca13924ebbc6e75262fc3f02dc
Florian Hönicke <hoenicke.florian@gmail.com>
2021-08-24 22:45:41 +0200

    refactor: reactivate dns resolving feature

--- jina/flow/base.py
+++ jina/flow/base.py
@@ -893,12 +893,8 @@ class Flow(PostMixin, JAMLCompatible, ExitStack, metaclass=FlowType):
             if len(pod.deployments) > 0:
                 deployments = pod.deployments
                 for deployment in deployments[1:-1]:
-                    graph.add_edge(
-                        deployments[0]['name'], deployment['name']
-                    )
-                    graph.add_edge(
-                        deployment['name'], deployments[-1]['name']
-                    )
+                    graph.add_edge(deployments[0]['name'], deployment['name'])
+                    graph.add_edge(deployment['name'], deployments[-1]['name'])
 
         graph.active_pod = f'start-{GATEWAY_NAME}'
         return graph
@@ -982,10 +978,9 @@ class Flow(PostMixin, JAMLCompatible, ExitStack, metaclass=FlowType):
 
         op_flow._set_initial_dynamic_routing_table()
 
-        # TODO skipped for now to have faster runtime (k8s hosts can not be resolved locally)
-        # for pod in op_flow._pod_nodes.values():
-        #     pass
-        # pod.args.host = self._resolve_host(pod.args.host)
+        for pod in op_flow._pod_nodes.values():
+            pass
+        pod.args.host = self._resolve_host(pod.args.host)
 
         hanging_pods = _hanging_pods(op_flow)
         if hanging_pods:
