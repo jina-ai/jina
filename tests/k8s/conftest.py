beb07a9b064f9a0cbcfea51a568d7ef79d691480
Florian Hönicke <hoenicke.florian@gmail.com>
2021-08-25 22:51:42 +0200

    refactor: format

--- tests/k8s/conftest.py
+++ tests/k8s/conftest.py
@@ -1,11 +1,10 @@
-# Contains reusable fixtures for the kubernetes testing module
 import operator
 import os
 from typing import List
 
+import docker
 import pykube
 import pytest
-import docker
 from pytest_kind import KindCluster
 
 from jina.logging.logger import JinaLogger
@@ -22,7 +21,9 @@ def logger():
 
 @pytest.fixture()
 def test_executor_image(logger: JinaLogger):
-    image, build_logs = client.images.build(path=os.path.join(cur_dir, 'test-executor'), tag='test-executor:0.13.0')
+    image, build_logs = client.images.build(
+        path=os.path.join(cur_dir, 'test-executor'), tag='test-executor:0.13.0'
+    )
     for chunk in build_logs:
         if 'stream' in chunk:
             for line in chunk['stream'].splitlines():
@@ -32,7 +33,9 @@ def test_executor_image(logger: JinaLogger):
 
 @pytest.fixture()
 def executor_merger_image(logger: JinaLogger):
-    image, build_logs = client.images.build(path=os.path.join(cur_dir, 'executor-merger'), tag='merger-executor:0.1.0')
+    image, build_logs = client.images.build(
+        path=os.path.join(cur_dir, 'executor-merger'), tag='merger-executor:0.1.0'
+    )
     for chunk in build_logs:
         if 'stream' in chunk:
             for line in chunk['stream'].splitlines():
@@ -42,7 +45,9 @@ def executor_merger_image(logger: JinaLogger):
 
 @pytest.fixture()
 def dummy_dumper_image(logger: JinaLogger):
-    image, build_logs = client.images.build(path=os.path.join(cur_dir, 'dummy-dumper'), tag='dummy-dumper:0.1.0')
+    image, build_logs = client.images.build(
+        path=os.path.join(cur_dir, 'dummy-dumper'), tag='dummy-dumper:0.1.0'
+    )
     for chunk in build_logs:
         if 'stream' in chunk:
             for line in chunk['stream'].splitlines():
@@ -51,12 +56,12 @@ def dummy_dumper_image(logger: JinaLogger):
 
 
 class KindClusterWrapper:
-
     def __init__(self, kind_cluster: KindCluster, logger: JinaLogger):
         self._cluster = kind_cluster
         self._cluster.ensure_kubectl()
         self._kube_config_path = os.path.join(
-            os.getcwd(), '.pytest-kind/pytest-kind/kubeconfig')
+            os.getcwd(), '.pytest-kind/pytest-kind/kubeconfig'
+        )
         self._kind_exec_path = os.path.join(
             os.getcwd(), '.pytest-kind/pytest-kind/kind'
         )
@@ -77,11 +82,11 @@ class KindClusterWrapper:
         return self._kind_exec_path
 
     def port_forward(
-            self,
-            service_name: str,
-            local_port: int,
-            service_port: int,
-            namespace: str = None
+        self,
+        service_name: str,
+        local_port: int,
+        service_port: int,
+        namespace: str = None,
     ):
         if namespace:
             return self._cluster.port_forward(
@@ -90,14 +95,11 @@ class KindClusterWrapper:
                 '-n',
                 namespace,
                 local_port=local_port,
-                retries=20
+                retries=20,
             )
         else:
             return self._cluster.port_forward(
-                service_name,
-                service_port,
-                local_port=local_port,
-                retries=20
+                service_name, service_port, local_port=local_port, retries=20
             )
 
     def list_pods(self, namespace: str = None) -> List:
