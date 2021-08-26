a4404887d73f0f93194880bd7b5e3c428486a199
Florian Hönicke <hoenicke.florian@gmail.com>
2021-08-26 16:33:25 +0200

    test: more readable

--- tests/k8s/test_example_index_flow.py
+++ tests/k8s/test_example_index_flow.py
@@ -44,35 +44,45 @@ def k8s_flow_with_needs(test_executor_image: str, executor_merger_image: str) ->
     return flow
 
 
-@pytest.mark.timeout(360)
-def test_flow_with_needs(
-    k8s_cluster_namespaced,
-    test_executor_image,
-    executor_merger_image,
-    k8s_flow_with_needs: Flow,
-    logger,
-):
-    expected_running_pods = 7
-
+def pull_images(images, cluster, logger):
     # image pull anyways must be Never or IfNotPresent otherwise kubernetes will try to pull the image anyway
     logger.debug(f'Loading docker image into kind cluster...')
-    k8s_cluster_namespaced.needs_docker_image(test_executor_image)
-    k8s_cluster_namespaced.needs_docker_image(executor_merger_image)
+    for image in images:
+        cluster.needs_docker_image(image)
     logger.debug(f'Done loading docker image into kind cluster...')
 
-    logger.debug(f'Starting flow on kind cluster...')
-    k8s_flow_with_needs.start()
-    logger.debug(f'Done starting flow on kind cluster...')
 
+def run_test(images, cluster, flow, logger, expected_running_pods, endpoint):
+    pull_images(images, cluster, logger)
+    start_flow(expected_running_pods, cluster, flow, logger)
+    resp = send_dummy_request(endpoint, cluster, flow, logger)
+    return resp
+
+
+def send_dummy_request(endpoint, k8s_cluster_namespaced, k8s_flow_with_needs, logger):
+    logger.debug(f'Starting port-forwarding to gateway service...')
+    with k8s_cluster_namespaced.port_forward(
+        'service/gateway', 8080, 8080, k8s_flow_with_needs.args.name
+    ) as _:
+        logger.debug(f'Port-forward running...')
+
+        resp = requests.post(
+            f'http://localhost:8080/{endpoint}', json={'data': [{} for _ in range(10)]}
+        )
+    return resp
+
+
+def start_flow(expected_running_pods, k8s_cluster_namespaced, flow, logger):
+    logger.debug(f'Starting flow on kind cluster...')
+    flow.start()
+    logger.debug(f'Done starting flow on kind cluster...')
     logger.debug(
         f'Starting to wait for pods in kind cluster to reach "RUNNING" state...'
     )
     waiting = True
     while waiting:
         num_running_pods = len(
-            k8s_cluster_namespaced.list_ready_pods(
-                namespace=k8s_flow_with_needs.args.name
-            )
+            k8s_cluster_namespaced.list_ready_pods(namespace=flow.args.name)
         )
         if num_running_pods == expected_running_pods:
             waiting = False
@@ -81,30 +91,6 @@ def test_flow_with_needs(
             f'Still waiting for pods to reach running state '
             f'(Current Status: {num_running_pods}/{expected_running_pods}).'
         )
-
-    expected_traversed_executors = {
-        'segmenter',
-        'imageencoder',
-        'textencoder',
-        'imagestorage',
-        'textstorage',
-    }
-
-    logger.debug(f'Starting port-forwarding to gateway service...')
-    with k8s_cluster_namespaced.port_forward(
-        'service/gateway', 8080, 8080, k8s_flow_with_needs.args.name
-    ) as _:
-        logger.debug(f'Port-forward running...')
-
-        resp = requests.post(
-            f'http://localhost:8080/index', json={'data': [{} for _ in range(10)]}
-        )
-
-    assert resp.status_code == HTTPStatus.OK
-    docs = resp.json()['data']['docs']
-    assert len(docs) == 10
-    for doc in docs:
-        assert set(doc['tags']['traversed-executors']) == expected_traversed_executors
 
 
 @pytest.fixture()
@@ -123,64 +109,6 @@ def k8s_flow_with_init_container(
     return flow
 
 
-@pytest.mark.timeout(3600)
-def test_flow_with_init(
-    k8s_cluster_namespaced,
-    test_executor_image,
-    dummy_dumper_image: str,
-    k8s_flow_with_init_container: Flow,
-    logger,
-):
-    expected_running_pods = 2
-
-    # image pull anyways must be Never or IfNotPresent otherwise kubernetes will try to pull the image anyway
-    logger.debug(f'Loading docker image {test_executor_image} into kind cluster...')
-    k8s_cluster_namespaced.needs_docker_image(test_executor_image)
-    k8s_cluster_namespaced.needs_docker_image(dummy_dumper_image)
-    logger.debug(
-        f'Done loading docker image {test_executor_image} into kind cluster...'
-    )
-
-    logger.debug(f'Starting flow on kind cluster...')
-    k8s_flow_with_init_container.start()
-    logger.debug(f'Done starting flow on kind cluster...')
-
-    logger.debug(
-        f'Starting to wait for pods in kind cluster to reach "RUNNING" state...'
-    )
-    waiting = True
-    while waiting:
-        num_running_pods = len(
-            k8s_cluster_namespaced.list_ready_pods(
-                namespace=k8s_flow_with_init_container.args.name
-            )
-        )
-        if num_running_pods == expected_running_pods:
-            waiting = False
-        time.sleep(3)
-        logger.debug(
-            f'Still waiting for pods to reach running state '
-            f'(Current Status: {num_running_pods}/{expected_running_pods}).'
-        )
-
-    logger.debug(f'Starting port-forwarding to gateway service...')
-    with k8s_cluster_namespaced.port_forward(
-        'service/gateway', 8080, 8080, k8s_flow_with_init_container.args.name
-    ) as _:
-        logger.debug(f'Port-forward running...')
-
-        # TODO: change to custom endpoint once https://github.com/jina-ai/jina/issues/3265 is fixed
-        resp = requests.post(
-            'http://localhost:8080/search', json={'data': [{} for _ in range(10)]}
-        )
-
-    assert resp.status_code == HTTPStatus.OK
-    docs = resp.json()['data']['docs']
-    assert len(docs) == 10
-    for doc in docs:
-        assert doc['tags']['file'] == ['1\n', '2\n', '3']
-
-
 @pytest.fixture()
 def k8s_flow_with_sharding(
     test_executor_image: str, executor_merger_image: str, dummy_dumper_image: str
@@ -189,15 +117,72 @@ def k8s_flow_with_sharding(
         name='test-flow', port_expose=8080, infrastructure='K8S', protocol='http'
     ).add(
         name='test_executor',
-        shards=3,
-        replicas=2,
+        # shards=3,
+        # replicas=2,
         uses=test_executor_image,
-        uses_after=executor_merger_image,
+        # uses_after=executor_merger_image,
     )
     return flow
 
 
-# @pytest.mark.timeout(360)
+@pytest.mark.timeout(3600)
+def test_flow_with_needs(
+    k8s_cluster_namespaced,
+    test_executor_image,
+    executor_merger_image,
+    k8s_flow_with_needs: Flow,
+    logger,
+):
+    resp = run_test(
+        [test_executor_image, executor_merger_image],
+        k8s_cluster_namespaced,
+        k8s_flow_with_needs,
+        logger,
+        expected_running_pods=7,
+        endpoint='index',
+    )
+
+    expected_traversed_executors = {
+        'segmenter',
+        'imageencoder',
+        'textencoder',
+        'imagestorage',
+        'textstorage',
+    }
+
+    assert resp.status_code == HTTPStatus.OK
+    docs = resp.json()['data']['docs']
+    assert len(docs) == 10
+    for doc in docs:
+        assert set(doc['tags']['traversed-executors']) == expected_traversed_executors
+
+
+@pytest.mark.timeout(3600)
+def test_flow_with_init(
+    k8s_cluster_namespaced,
+    test_executor_image,
+    dummy_dumper_image: str,
+    k8s_flow_with_init_container: Flow,
+    logger,
+):
+    resp = run_test(
+        [test_executor_image, dummy_dumper_image],
+        k8s_cluster_namespaced,
+        k8s_flow_with_init_container,
+        logger,
+        expected_running_pods=2,
+        endpoint='search',
+    )
+
+    assert resp.status_code == HTTPStatus.OK
+    docs = resp.json()['data']['docs']
+    assert len(docs) == 10
+    for doc in docs:
+        assert doc['tags']['file'] == ['1\n', '2\n', '3']
+
+
+#
+# @pytest.mark.timeout(3600)
 # def test_flow_with_sharding(
 #     k8s_cluster_namespaced,
 #     test_executor_image,
@@ -205,35 +190,15 @@ def k8s_flow_with_sharding(
 #     k8s_flow_with_sharding: Flow,
 #     logger,
 # ):
-#     expected_running_pods = 9
 #
-#     # image pull anyways must be Never or IfNotPresent otherwise kubernetes will try to pull the image anyway
-#     logger.debug(f'Loading docker image into kind cluster...')
-#     k8s_cluster_namespaced.needs_docker_image(test_executor_image)
-#     k8s_cluster_namespaced.needs_docker_image(executor_merger_image)
-#     logger.debug(f'Done loading docker image into kind cluster...')
-#
-#     logger.debug(f'Starting flow on kind cluster...')
-#     k8s_flow_with_sharding.start()
-#     logger.debug(f'Done starting flow on kind cluster...')
-#
-#     logger.debug(
-#         f'Starting to wait for pods in kind cluster to reach "RUNNING" state...'
+#     resp = run_test(
+#         [test_executor_image, executor_merger_image],
+#         k8s_cluster_namespaced,
+#         k8s_flow_with_sharding,
+#         logger,
+#         expected_running_pods=9,
+#         endpoint='index'
 #     )
-#     waiting = True
-#     while waiting:
-#         num_running_pods = len(
-#             k8s_cluster_namespaced.list_ready_pods(
-#                 namespace=k8s_flow_with_sharding.args.name
-#             )
-#         )
-#         if num_running_pods == expected_running_pods:
-#             waiting = False
-#         time.sleep(3)
-#         logger.debug(
-#             f'Still waiting for pods to reach running state '
-#             f'(Current Status: {num_running_pods}/{expected_running_pods}).'
-#         )
 #
 #     expected_traversed_executors = {
 #         'segmenter',
@@ -242,17 +207,6 @@ def k8s_flow_with_sharding(
 #         'imagestorage',
 #         'textstorage',
 #     }
-#
-#     logger.debug(f'Starting port-forwarding to gateway service...')
-#     with k8s_cluster_namespaced.port_forward(
-#         'service/gateway', 8080, 8080, k8s_flow_with_sharding.args.name
-#     ) as _:
-#         logger.debug(f'Port-forward running...')
-#
-#         # TODO: change to custom endpoint once https://github.com/jina-ai/jina/issues/3265 is fixed
-#         resp = requests.post(
-#             f'http://localhost:8080/index', json={'data': [{} for _ in range(10)]}
-#         )
 #
 #     assert resp.status_code == HTTPStatus.OK
 #     docs = resp.json()['data']['docs']
