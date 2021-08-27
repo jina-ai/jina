4ac2eadb55ab8f6a87f588afe6aa7d2dac4bcc67
Florian Hönicke <hoenicke.florian@gmail.com>
2021-08-27 13:49:45 +0200

    ci: add file

---
+++ tests/k8s/test_k8s.sh
@@ -0,0 +1,13 @@
+set -ex
+
+docker build --build-arg PIP_TAG="[devel]" -f Dockerfiles/pip.Dockerfile -t jinaai/jina:test-pip .
+if [ "${PWD##*/}" != "jina" ]
+  then
+    echo "test_k8s.sh should only be run from the jina base directory"
+    exit 1
+fi
+
+pip install pytest
+
+pytest -sv ./tests/k8s
+
