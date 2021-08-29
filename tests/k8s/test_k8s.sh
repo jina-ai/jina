50b805b9b5ab44d8c80ab65fc4bf0714ef82e5dd
Florian Hönicke <hoenicke.florian@gmail.com>
2021-08-29 19:05:48 +0200

    ci: use existing gh action

--- tests/k8s/test_k8s.sh
+++ tests/k8s/test_k8s.sh
@@ -3,6 +3,7 @@ set -ex
 
 echo "kubeconfig: ${KUBECONFIG}"
 ls -a ~/
+ls -a ~/.kube
 kubectl config current-context
 
 docker build --build-arg PIP_TAG="[devel]" -f Dockerfiles/pip.Dockerfile -t jinaai/jina:test-pip .
