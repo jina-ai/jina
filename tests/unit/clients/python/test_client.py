564c7f0153f42bcd266641a572b81107909a4e71
bwanglzu <bo.wang@jina.ai>
2021-09-03 14:50:12 +0200

    test: clarify client tests

--- tests/unit/clients/python/test_client.py
+++ tests/unit/clients/python/test_client.py
@@ -167,7 +167,7 @@ def test_client_from_kwargs(protocol):
 def test_independent_client(protocol):
     with Flow(protocol=protocol) as f:
         c = Client(host='localhost', port=f.port_expose, protocol=protocol)
-        assert type(c) == type(f.client)
+        assert isinstance(c, f.client)
         c.post('/')
 
 
@@ -188,10 +188,11 @@ def test_all_sync_clients(protocol, mocker):
     m3 = mocker.Mock()
     m4 = mocker.Mock()
     with f:
-        f.post('/', on_done=m1)
-        f.post('/foo', docs, on_done=m2)
-        f.post('/foo', on_done=m3)
-        f.post('/foo', docs, parameters={'hello': 'world'}, on_done=m4)
+        c = Client(host='localhost', port=f.port_expose, protocol=protocol)
+        c.post('/', on_done=m1)
+        c.post('/foo', docs, on_done=m2)
+        c.post('/foo', on_done=m3)
+        c.post('/foo', docs, parameters={'hello': 'world'}, on_done=m4)
 
     m1.assert_called_once()
     m2.assert_called()
