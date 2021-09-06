6033edb35d2ef105402912e06a554ff0090aaca2
Florian Hönicke <hoenicke.florian@gmail.com>
2021-09-06 19:34:45 +0200

    fix(k8s): uses with

--- tests/unit/peapods/pods/test_k8s_pod.py
+++ tests/unit/peapods/pods/test_k8s_pod.py
@@ -6,6 +6,7 @@ import jina
 from jina.parsers import set_pod_parser
 from jina.peapods.pods.k8s import K8sPod
 from jina.peapods.pods.k8slib import kubernetes_tools
+from jina.peapods.pods.k8slib.kubernetes_deployment import dictionary_to_cli_param
 
 
 @pytest.mark.parametrize('is_master', (True, False))
@@ -32,3 +33,10 @@ def test_version(is_master, requests_mock):
             assert pod.version == 'master'
         else:
             assert pod.version == jina.__version__
+
+
+def test_dictionary_to_cli_param():
+    assert (
+        dictionary_to_cli_param({'k1': 'v1', 'k2': {'k3': 'v3'}})
+        == '{\\"k1\\": \\"v1\\", \\"k2\\": {\\"k3\\": \\"v3\\"}}'
+    )
