cbe578dc3a1560704580be848b55de3f6703c663
Florian Hönicke <hoenicke.florian@gmail.com>
2021-09-06 16:01:11 +0200

    feat: k8s parallel for shards (#3350)
    * feat: k8s parallel for shards
    
    * feat: k8s shards parameter

--- jina/peapods/pods/k8slib/kubernetes_deployment.py
+++ jina/peapods/pods/k8slib/kubernetes_deployment.py
@@ -120,8 +120,6 @@ def get_cli_params(arguments: Namespace, skip_list: Tuple[str] = ()) -> str:
         'uses_after',
         'uses_before',
         'replicas',
-        'shards',
-        'parallel',
         'polling',
         'port_in',
         'port_out',
