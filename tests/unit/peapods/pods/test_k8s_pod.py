75610eca2f4225f078f0b17657f06e8c364a49cf
slettner <sebastian.lettner@jina.ai>
2021-09-07 11:00:21 +0200

    test: parse args unit test

--- tests/unit/peapods/pods/test_k8s_pod.py
+++ tests/unit/peapods/pods/test_k8s_pod.py
@@ -1,6 +1,8 @@
+from typing import Union, Dict, Tuple
 from unittest.mock import Mock
 
 import pytest
+from jinja2.utils import Namespace
 
 import jina
 from jina.parsers import set_pod_parser
@@ -40,3 +42,62 @@ def test_dictionary_to_cli_param():
         dictionary_to_cli_param({'k1': 'v1', 'k2': {'k3': 'v3'}})
         == '{\\"k1\\": \\"v1\\", \\"k2\\": {\\"k3\\": \\"v3\\"}}'
     )
+
+
+def test_parse_args_no_parallel():
+    args = set_pod_parser().parse_args(['--parallel', '1'])
+    pod = K8sPod(args)
+
+    assert pod.deployment_args['head_deployment'] is None
+    assert pod.deployment_args['tail_deployment'] is None
+    assert pod.deployment_args['deployments'] == [args]
+
+
+@pytest.mark.parametrize('parallel', [2, 3, 4, 5])
+def test_parse_args_parallel(parallel):
+    args = set_pod_parser().parse_args(['--parallel', str(parallel)])
+    pod = K8sPod(args)
+
+    assert pod.deployment_args['head_deployment'] == args
+    assert pod.deployment_args['head_deployment'].uses == jina.__default_executor__
+    assert pod.deployment_args['tail_deployment'] == args
+    assert pod.deployment_args['tail_deployment'].uses == jina.__default_executor__
+    assert pod.deployment_args['deployments'] == [args] * parallel
+
+
+@pytest.mark.parametrize('parallel', [2, 3, 4, 5])
+def test_parse_args_parallel_custom_exectuor(parallel):
+    args = set_pod_parser().parse_args(
+        [
+            '--parallel',
+            str(parallel),
+            '--uses-before',
+            'custom-executor',
+            '--uses-after',
+            'custom-executor',
+        ]
+    )
+    pod = K8sPod(args)
+
+    assert namespace_equal(
+        args, pod.deployment_args['head_deployment'], skip_attr=('uses',)
+    )
+    assert pod.deployment_args['head_deployment'].uses == 'custom-executor'
+    assert namespace_equal(
+        args, pod.deployment_args['tail_deployment'], skip_attr=('uses',)
+    )
+    assert pod.deployment_args['tail_deployment'].uses == 'custom-executor'
+    assert pod.deployment_args['deployments'] == [args] * parallel
+
+
+def namespace_equal(
+    n1: Union[Namespace, Dict], n2: Union[Namespace, Dict], skip_attr: Tuple = ()
+) -> bool:
+    """
+    Checks that two `Namespace` object have equal public attributes.
+    It skips attributes that start with a underscore and additional `skip_attr`.
+    """
+    for attr in filter(lambda x: x not in skip_attr and not x.startswith('_'), dir(n1)):
+        if not getattr(n1, attr) == getattr(n2, attr):
+            return False
+    return True
