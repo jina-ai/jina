f31561b895bb11cb64007993e3e8ab91225cfbd1
Deepankar Mahapatro <deepankarmahapatro@gmail.com>
2021-09-13 19:14:55 +0530

    ci: set python encoding

--- scripts/get-all-test-paths.sh
+++ scripts/get-all-test-paths.sh
@@ -2,9 +2,10 @@
 
 set -ex
 
-if [[ $1 == 'windows' ]]; then
-    declare -a array1=( "tests/system/*.py" )
-    dest1=( "${array1[@]}" )
+if [[ $1 == "windows" ]]; then
+    declare -a array1=( "tests/system/*.py" "" )
+    declare -a array2=( $(ls -d tests/system/*/ | grep -v '__pycache__' ))
+    dest1=( "${array1[@]}" "${array2[@]}" )
     printf '%s\n' "${dest[@]}" | jq -R . | jq -cs .
 else
     declare -a array1=( "tests/unit/*.py" "tests/integration/*.py" "tests/distributed/*.py" "tests/system/*.py")
