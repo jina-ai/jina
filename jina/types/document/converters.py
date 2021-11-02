e95a600ba2c966a47110023242a1f44393f36970
Han Xiao <artex.xh@gmail.com>
2021-11-02 18:38:07 +0100

    docs: add general tutorial on 3d mesh data (#3853)

--- jina/types/document/converters.py
+++ jina/types/document/converters.py
@@ -274,6 +274,35 @@ class ContentConversionMixin:
             self.blob = audio_normalised
         return self
 
+    def convert_uri_to_point_cloud_blob(
+        self, samples: int, as_chunks: bool = False
+    ) -> 'Document':
+        """Convert a 3d mesh-like :attr:`.uri` into :attr:`.blob`
+
+        :param samples: number of points to sample from the mesh
+        :param as_chunks: when multiple geometry stored in one mesh file,
+            then store each geometry into different :attr:`.chunks`
+
+        :return: itself after processed
+        """
+        import trimesh
+
+        mesh = trimesh.load_mesh(self.uri).deduplicated()
+
+        pcs = []
+        for geo in mesh.geometry.values():
+            geo: trimesh.Trimesh
+            pcs.append(geo.sample(samples))
+
+        if as_chunks:
+            from . import Document
+
+            for p in pcs:
+                self.chunks.append(Document(blob=p))
+        else:
+            self.blob = np.stack(pcs).squeeze()
+        return self
+
     def convert_uri_to_image_blob(
         self,
         width: Optional[int] = None,
