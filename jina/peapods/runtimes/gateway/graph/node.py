e380654c8f5117fc025639c3d77179d061a03776
Joan Fontanals Martinez <joan.martinez@jina.ai>
2021-11-05 12:59:12 +0100

    feat: seems to work for bifurcation graph

--- jina/peapods/runtimes/gateway/graph/node.py
+++ jina/peapods/runtimes/gateway/graph/node.py
@@ -18,11 +18,11 @@ class ConnectionPool:
 
 
 class Node:
-    def __init__(self, pod_name, connection_pool, outgoing_nodes: List['Node']):
+    def __init__(self, pod_name, connection_pool, outgoing_nodes: List['Node'], number_of_parts=1):
         self.pod_name = pod_name
         self.connection_pool = connection_pool
         self.outgoing_nodes = outgoing_nodes
-        self.futures = []
+        self.number_of_parts = number_of_parts
 
     async def _send_message(self, msg):
         # ask connection pool to send to `self.pod_name`
@@ -32,19 +32,33 @@ class Node:
     def last(self):
         return len(self.outgoing_nodes) == 0
 
-    def send_and_forward(self, msg_to_send: Optional[str], previous_task: Optional[asyncio.Task]) -> asyncio.Task:
+    def send_and_forward(
+            self, msg_to_send: Optional[str], previous_task: Optional[asyncio.Task]
+    ) -> List[asyncio.Task]:
+        print(f' {self.pod_name} - send_and_forward')
+
         async def _wait_previous_and_send(msg, t):
             if t is not None:
                 msg = await t
+            # this is a specific needs
             resp = await self._send_message(msg)
             resp = f'from {self.pod_name} ' + resp
             return resp
 
-        wait_and_send_task = asyncio.create_task(_wait_previous_and_send(msg_to_send, previous_task))
-        if self.last:
-            return wait_and_send_task  # I am the last in the chain
-        t = self.outgoing_nodes[0].send_and_forward(None, wait_and_send_task)
-        return t
+        wait_and_send_task = asyncio.create_task(
+            _wait_previous_and_send(msg_to_send, previous_task)
+        )
+        if self.last:  # I am like a leaf
+            return [wait_and_send_task]  # I am the last in the chain
+        tasks = []
+        print(f' self.pod_name {self.pod_name} len outgoing {len(self.outgoing_nodes)}')
+        for outgoing_node in self.outgoing_nodes:
+            print(f' self.pod_name {self.pod_name} call send_and_forward from {outgoing_node.pod_name}')
+            t = outgoing_node.send_and_forward(None, wait_and_send_task)
+            print(f' self.pod_name {self.pod_name} extracting {len(t)} from {outgoing_node.pod_name}')
+            tasks.append(t[-1])
+        print(f' self.pod_name {self.pod_name} tasks {len(tasks)}')
+        return tasks
 
 
 # pod6 -> needs: pod5, pod4
@@ -71,17 +85,39 @@ class Graph:
 
     @property
     def origin_nodes(self):
+        merge_node = Node(pod_name='merger', connection_pool=self.connection_pool, number_of_parts=2, outgoing_nodes=[
+            Node(pod_name='pod_last', connection_pool=self.connection_pool, outgoing_nodes=[])])
         return [
             Node(
                 pod_name='pod0',
                 connection_pool=self.connection_pool,
                 outgoing_nodes=[
                     Node(
-                        pod_name='pod1', connection_pool=self.connection_pool, outgoing_nodes=[
+                        pod_name='pod1',
+                        connection_pool=self.connection_pool,
+                        outgoing_nodes=[],
+                    ),
+                    Node(
+                        pod_name='pod2',
+                        connection_pool=self.connection_pool,
+                        outgoing_nodes=[
                             Node(
-                                pod_name='pod2', connection_pool=self.connection_pool, outgoing_nodes=[]
+                                pod_name='pod3',
+                                connection_pool=self.connection_pool,
+                                outgoing_nodes=[],
                             )
-                        ]
+                        ],
+                    )
+                ],
+            ),
+            Node(
+                pod_name='pod4',
+                connection_pool=self.connection_pool,
+                outgoing_nodes=[
+                    Node(
+                        pod_name='pod5',
+                        connection_pool=self.connection_pool,
+                        outgoing_nodes=[],
                     )
                 ],
             )
@@ -95,9 +131,11 @@ class GatewayRuntime:
 
     async def receive_from_client(self, msg):
         graph = copy.deepcopy(self.graph)
-        origin_node = graph.origin_nodes[0]
-        t = origin_node.send_and_forward(msg, None)
-        resp = await t
+        tasks = []
+        for origin_node in graph.origin_nodes:
+            ts = origin_node.send_and_forward(msg, None)
+            tasks.extend(ts)
+        resp = await asyncio.gather(*tasks)
         return resp
         # merge responses and send back to client
 
@@ -105,7 +143,12 @@ class GatewayRuntime:
 if __name__ == '__main__':
     async def main():
         runtime = GatewayRuntime()
-        resps = await asyncio.gather(runtime.receive_from_client('Request-1'), runtime.receive_from_client('Request-2'), runtime.receive_from_client('Request-3'))
+        resps = await asyncio.gather(
+            runtime.receive_from_client('Request-1'),
+            runtime.receive_from_client('Request-2'),
+            runtime.receive_from_client('Request-3'),
+        )
         print(f' resps {resps}')
+
 
     asyncio.run(main())
