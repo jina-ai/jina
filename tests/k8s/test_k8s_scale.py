dff0227e8c687e0cb06f8ad94e8756bc2b87728b
bwanglzu <bo.wang@jina.ai>
2021-11-11 22:56:52 +0100

    feat: add scale up down and replicas

--- tests/k8s/test_k8s_scale.py
+++ tests/k8s/test_k8s_scale.py
@@ -6,7 +6,7 @@ DEFAULT_REPLICAS = 2
 
 
 @pytest.mark.timeout(3600)
-@pytest.mark.parametrize('shards', [1])
+@pytest.mark.parametrize('shards', [1, 2])
 def test_k8s_scale(k8s_cluster, load_images_in_kind, set_test_pip_version, shards):
     flow = Flow(
         name='test-flow-scale',
@@ -21,33 +21,33 @@ def test_k8s_scale(k8s_cluster, load_images_in_kind, set_test_pip_version, shard
         replicas=DEFAULT_REPLICAS,
         timeout_ready=12000,
     )
-    # with flow as f:
-    #     from jina.peapods.pods.k8slib.kubernetes_client import K8sClients
-    #
-    #     deployment = K8sClients().apps_v1.read_namespaced_deployment(
-    #         name='test-executor', namespace='test-flow-scale-ns'
-    #     )
-    #     replica = deployment.status.replicas
-    #
-    #     assert replica == DEFAULT_REPLICAS
-    #
-    #     f.scale(pod_name='test_executor', replicas=3)  # scale up to 3
-    #
-    #     deployment = K8sClients().apps_v1.read_namespaced_deployment(
-    #         name='test-executor', namespace='test-flow-scale-ns'
-    #     )
-    #     replica = deployment.status.replicas
-    #
-    #     assert replica == 3
-    #
-    #     f.scale(pod_name='test_executor', replicas=1)  # scale down to 1
-    #
-    #     deployment = K8sClients().apps_v1.read_namespaced_deployment(
-    #         name='test-executor', namespace='test-flow-scale-ns'
-    #     )
-    #     replica = deployment.status.replicas
-    #
-    #     assert replica == 1
+    with flow as f:
+        from jina.peapods.pods.k8slib.kubernetes_client import K8sClients
+
+        deployment = K8sClients().apps_v1.read_namespaced_deployment(
+            name='test-executor', namespace='test-flow-scale-ns'
+        )
+        replica = deployment.status.replicas
+
+        assert replica == DEFAULT_REPLICAS
+
+        f.scale(pod_name='test_executor', replicas=3)  # scale up to 3
+
+        deployment = K8sClients().apps_v1.read_namespaced_deployment(
+            name='test-executor', namespace='test-flow-scale-ns'
+        )
+        replica = deployment.status.replicas
+
+        assert replica == 3
+
+        f.scale(pod_name='test_executor', replicas=1)  # scale down to 1
+
+        deployment = K8sClients().apps_v1.read_namespaced_deployment(
+            name='test-executor', namespace='test-flow-scale-ns'
+        )
+        replica = deployment.status.replicas
+
+        assert replica == 1
 
 
 # def test_k8s_scale_fail(
