a0896f9f8d94a5f9c3151bff0972911d50d34626
Joan Fontanals <joan.martinez@jina.ai>
2021-11-11 10:01:08 +0100

    test: test runtimes fast slow request (#3910)

--- tests/integration/runtimes/test_runtimes.py
+++ tests/integration/runtimes/test_runtimes.py
@@ -410,6 +410,81 @@ async def test_runtimes_with_executor():
         assert process.exitcode == 0
 
 
+@pytest.mark.asyncio
+async def test_runtimes_with_replicas_advance_faster():
+    head_port = random_port()
+    port_expose = random_port()
+    graph_description = '{"start-gateway": ["pod0"], "pod0": ["end-gateway"]}'
+    pod_addresses = f'{{"pod0": ["0.0.0.0:{head_port}"]}}'
+
+    # create a single head runtime
+    head_process = multiprocessing.Process(
+        target=_create_head_runtime, args=(head_port, 'head')
+    )
+    head_process.start()
+
+    # create the shards
+    replica_processes = []
+    for i in range(10):
+        # create worker
+        worker_port = random_port()
+        # create a single worker runtime
+        worker_process = multiprocessing.Process(
+            target=_create_worker_runtime,
+            args=(worker_port, f'pod0/{i}', 'FastSlowExecutor'),
+        )
+        replica_processes.append(worker_process)
+        worker_process.start()
+
+        await asyncio.sleep(0.1)
+
+        # this would be done by the Pod, its adding the worker to the head
+        activate_msg = ControlMessage(command='ACTIVATE')
+        activate_msg.add_related_entity('worker', '127.0.0.1', worker_port)
+        assert GrpcConnectionPool.send_message_sync(
+            activate_msg, f'127.0.0.1:{head_port}'
+        )
+
+    # create a single gateway runtime
+    gateway_process = multiprocessing.Process(
+        target=_create_gateway_runtime,
+        args=(graph_description, pod_addresses, port_expose),
+    )
+    gateway_process.start()
+
+    await asyncio.sleep(1.0)
+
+    c = Client(host='localhost', port=port_expose, asyncio=True)
+    input_docs = [Document(text='slow'), Document(text='fast')]
+    responses = c.post('/', inputs=input_docs, request_size=1, return_results=True)
+    response_list = []
+    async for response in responses:
+        response_list.append(response)
+
+    # clean up runtimes
+    gateway_process.terminate()
+    head_process.terminate()
+    for replica_process in replica_processes:
+        replica_process.terminate()
+
+    gateway_process.join()
+    head_process.join()
+    for replica_process in replica_processes:
+        replica_process.join()
+
+    assert len(response_list) == 2
+    for response in response_list:
+        assert len(response.docs) == 1
+
+    assert response_list[0].docs[0].text == 'fast'
+    assert response_list[1].docs[0].text == 'slow'
+
+    assert gateway_process.exitcode == 0
+    assert head_process.exitcode == 0
+    for replica_process in replica_processes:
+        assert replica_process.exitcode == 0
+
+
 class NameChangeExecutor(Executor):
     def __init__(self, runtime_args, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -420,6 +495,14 @@ class NameChangeExecutor(Executor):
         print(f'{self.name} doc count {len(docs)}')
         docs.append(Document(text=self.name))
         return docs
+
+
+class FastSlowExecutor(Executor):
+    @requests
+    def foo(self, docs, **kwargs):
+        for doc in docs:
+            if doc.text == 'slow':
+                time.sleep(1.0)
 
 
 async def _activate_worker(head_port, worker_port, shard_id=None):
