73a6aa04d05e2dd3d91ecefe085edf332d7cfdaf
jacobowitz <tobias.jacobowitz@posteo.de>
2021-11-12 15:28:52 +0100

    feat(pea): remove _inner_pea function

--- jina/peapods/peas/__init__.py
+++ jina/peapods/peas/__init__.py
@@ -360,11 +360,3 @@ class BasePea:
 
         .. #noqa: DAR201"""
         return self.args.pea_role
-
-    @property
-    def _is_inner_pea(self) -> bool:
-        """Determine whether this is a inner pea or a head
-
-
-        .. #noqa: DAR201"""
-        return self.role is PeaRoleType.WORKER
