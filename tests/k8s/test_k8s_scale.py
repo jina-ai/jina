ff0901a87ee7a075734161b0998e8dcdbbbe1971
bwanglzu <bo.wang@jina.ai>
2021-11-14 21:58:17 +0100

    feat: fix deployment name shard over 2

--- tests/k8s/test_k8s_scale.py
+++ tests/k8s/test_k8s_scale.py
@@ -22,28 +22,33 @@ def test_k8s_scale(k8s_cluster, load_images_in_kind, set_test_pip_version, shard
         timeout_ready=12000,
     )
     with flow as f:
+        k8s_deployment_name = 'test-executor'
+        jina_pod_name = 'test_executor'
+        if shards > 1:
+            k8s_deployment_name += '-0'
+
         from jina.peapods.pods.k8slib.kubernetes_client import K8sClients
 
         deployment = K8sClients().apps_v1.read_namespaced_deployment(
-            name='test-executor', namespace='test-flow-scale-ns'
+            name=k8s_deployment_name, namespace='test-flow-scale-ns'
         )
         replica = deployment.status.replicas
 
         assert replica == DEFAULT_REPLICAS
 
-        f.scale(pod_name='test_executor', replicas=3)  # scale up to 3
+        f.scale(pod_name=jina_pod_name, replicas=3)  # scale up to 3
 
         deployment = K8sClients().apps_v1.read_namespaced_deployment(
-            name='test-executor', namespace='test-flow-scale-ns'
+            name=k8s_deployment_name, namespace='test-flow-scale-ns'
         )
         replica = deployment.status.replicas
 
         assert replica == 3
 
-        f.scale(pod_name='test_executor', replicas=1)  # scale down to 1
+        f.scale(pod_name=jina_pod_name, replicas=1)  # scale down to 1
 
         deployment = K8sClients().apps_v1.read_namespaced_deployment(
-            name='test-executor', namespace='test-flow-scale-ns'
+            name=k8s_deployment_name, namespace='test-flow-scale-ns'
         )
         replica = deployment.status.replicas
 
