961f48c0567ddc37f38b02e464b5f1704c83d331
Han Xiao <artex.xh@gmail.com>
2021-11-17 14:12:39 +0100

    fix(evaluate): fix length and hash check on evaluate mixin (#3943)

--- tests/unit/types/arrays/mixins/test_eval_class.py
+++ tests/unit/types/arrays/mixins/test_eval_class.py
@@ -1,3 +1,5 @@
+import copy
+
 import numpy as np
 import pytest
 
@@ -46,13 +48,60 @@ def test_eval_mixin_zero_match(metric_fn, kwargs):
     da1.embeddings = np.random.random([10, 256])
     da1.match(da1, exclude_self=True)
 
-    da2 = DocumentArray.empty(10)
+    da2 = copy.deepcopy(da1)
     da2.embeddings = np.random.random([10, 256])
     da2.match(da2, exclude_self=True)
 
     r = da1.evaluate(da2, metric=metric_fn, **kwargs)
     assert isinstance(r, float)
-    assert r == 0.0
+    assert r == 1.0
     for d in da1:
         d: Document
-        assert d.evaluations[metric_fn].value == 0.0
+        assert d.evaluations[metric_fn].value == 1.0
+
+
+def test_diff_len_should_raise():
+    da1 = DocumentArray.empty(10)
+    da2 = DocumentArray.empty(5)
+    with pytest.raises(ValueError):
+        da1.evaluate(da2, metric='precision_at_k')
+
+
+def test_diff_hash_fun_should_raise():
+    da1 = DocumentArray.empty(10)
+    da2 = DocumentArray.empty(10)
+    with pytest.raises(ValueError):
+        da1.evaluate(da2, metric='precision_at_k')
+
+
+def test_same_hash_same_len_fun_should_work():
+    da1 = DocumentArray.empty(10)
+    da1.embeddings = np.random.random([10, 3])
+    da1.match(da1)
+    da2 = DocumentArray.empty(10)
+    da2.embeddings = np.random.random([10, 3])
+    da2.match(da2)
+    with pytest.raises(ValueError):
+        da1.evaluate(da2, metric='precision_at_k')
+    for d1, d2 in zip(da1, da2):
+        d1.id = d2.id
+
+    da1.evaluate(da2, metric='precision_at_k')
+
+
+def test_adding_noise():
+    da = DocumentArray.empty(10)
+
+    da.embeddings = np.random.random([10, 3])
+    da.match(da, exclude_self=True)
+
+    da2 = copy.deepcopy(da)
+
+    for d in da2:
+        d.matches.extend(DocumentArray.empty(10))
+        d.matches = d.matches.shuffle()
+
+    assert da2.evaluate(da, metric='precision_at_k', k=10) < 1.0
+
+    for d in da2:
+        assert 0.0 < d.evaluations['precision_at_k'].value < 1.0
