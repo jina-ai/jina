132659fcd45c8e44545044d6868639385d4e06d1
Tobias Jacobowitz <tobias.jacobowitz@jina.ai>
2021-11-23 10:32:02 +0100

    feat(k8s): adapt K8s Pod for StarRouting (#3964)
    * test: optimize k8s test
    
    * test: increase timeout and use different namespace
    
    * test: optimize k8s test
    
    * test: build and load image when needed
    
    * test: refactor k8s test
    
    * test: fix image name error
    
    * test: fix k8s image load
    
    * test: fix typoe port expose
    
    * test: update tests in connection pool and handling
    
    * test: remove unused fixture
    
    * test: parameterize docker images
    
    * test: parameterize docker images
    
    * test: parameterize docker images
    
    * feat(k8s): adapt k8s pod for star routing
    
    * fix(k8s): dont overwrite add/remove function in pool
    
    * fix(k8s): some fixes
    
    * fix(k8s): some more fixes
    
    * fix(k8s): linting
    
    * fix(tests): fix tests
    
    * fix(tests): fix k8s unit tests
    
    * feat(k8s): complete k8s integration test
    
    * feat(k8s): finish k8s tests
    
    * feat(k8s): fix test
    
    * fix(tests): fix test with no name
    
    * feat(k8s): unify create/replace interface
    
    * feat(k8s): extract k8s port constants
    
    * fix(tests): fix tests
    
    * fix(tests): wait for runtime being ready in tests
    
    * feat(k8s): address comments
    
    Co-authored-by: bwanglzu <bo.wang@jina.ai>

--- tests/k8s/conftest.py
+++ tests/k8s/conftest.py
@@ -24,14 +24,11 @@ class KindClusterWrapper:
         self._log.debug(f'Setting KUBECONFIG to {self._kube_config_path}')
         os.environ['KUBECONFIG'] = self._kube_config_path
 
-    def load_docker_image(self, image_name: str):
-        self._cluster.load_docker_image(image_name)
-
-
-@pytest.fixture()
-def logger():
-    logger = JinaLogger('kubernetes-testing')
-    return logger
+    def load_docker_images(self, images, image_tag_map):
+        for image in images:
+            if image != 'alpine' and image != 'jinaai/jina':
+                build_docker_image(image, image_tag_map)
+            self._cluster.load_docker_image(image + ':' + image_tag_map[image])
 
 
 @pytest.fixture()
@@ -39,15 +36,35 @@ def test_dir() -> str:
     return cur_dir
 
 
-@pytest.fixture()
+@pytest.fixture
+def logger():
+    return JinaLogger('kubernetes-testing')
+
+
+@pytest.fixture
 def k8s_cluster(kind_cluster: KindCluster, logger: JinaLogger) -> KindClusterWrapper:
     return KindClusterWrapper(kind_cluster, logger)
 
 
-@pytest.fixture()
-def reload_executor_image(logger: JinaLogger):
+@pytest.fixture
+def image_name_tag_map():
+    return {
+        'reload-executor': '0.13.1',
+        'test-executor': '0.13.1',
+        'executor-merger': '0.1.1',
+        'dummy-dumper': '0.1.1',
+        'slow-process-executor': '0.14.1',
+        'slow-init-executor': '0.13.1',
+        'alpine': '3.14',
+        'jinaai/jina': 'test-pip',
+    }
+
+
+def build_docker_image(image_name, image_name_tag_map):
+    logger = JinaLogger('kubernetes-testing')
+    image_tag = image_name + ':' + image_name_tag_map[image_name]
     image, build_logs = client.images.build(
-        path=os.path.join(cur_dir, 'reload-executor'), tag='reload-executor:0.13.1'
+        path=os.path.join(cur_dir, image_name), tag=image_tag
     )
     for chunk in build_logs:
         if 'stream' in chunk:
@@ -56,103 +73,18 @@ def reload_executor_image(logger: JinaLogger):
     return image.tags[-1]
 
 
-@pytest.fixture()
-def test_executor_image(logger: JinaLogger):
-    image, build_logs = client.images.build(
-        path=os.path.join(cur_dir, 'test-executor'), tag='test-executor:0.13.1'
-    )
-    for chunk in build_logs:
-        if 'stream' in chunk:
-            for line in chunk['stream'].splitlines():
-                logger.debug(line)
-    return image.tags[-1]
-
-
-@pytest.fixture()
-def executor_merger_image(logger: JinaLogger):
-    image, build_logs = client.images.build(
-        path=os.path.join(cur_dir, 'executor-merger'), tag='merger-executor:0.1.1'
-    )
-    for chunk in build_logs:
-        if 'stream' in chunk:
-            for line in chunk['stream'].splitlines():
-                logger.debug(line)
-    return image.tags[-1]
-
-
-@pytest.fixture()
-def dummy_dumper_image(logger: JinaLogger):
-    image, build_logs = client.images.build(
-        path=os.path.join(cur_dir, 'dummy-dumper'), tag='dummy-dumper:0.1.1'
-    )
-    for chunk in build_logs:
-        if 'stream' in chunk:
-            for line in chunk['stream'].splitlines():
-                logger.debug(line)
-    return image.tags[-1]
-
-
-@pytest.fixture()
-def slow_process_executor_image(logger: JinaLogger):
-    image, build_logs = client.images.build(
-        path=os.path.join(cur_dir, 'slow-process-executor'),
-        tag='slow-process-executor:0.14.1',
-    )
-    for chunk in build_logs:
-        if 'stream' in chunk:
-            for line in chunk['stream'].splitlines():
-                logger.debug(line)
-    return image.tags[-1]
-
-
-@pytest.fixture()
-def slow_init_executor_image(logger: JinaLogger):
-    image, build_logs = client.images.build(
-        path=os.path.join(cur_dir, 'slow-init-executor'),
-        tag='slow-init-executor:0.13.1',
-    )
-    for chunk in build_logs:
-        if 'stream' in chunk:
-            for line in chunk['stream'].splitlines():
-                logger.debug(line)
-    return image.tags[-1]
-
-
-@pytest.fixture()
-def alpine_image():
-    return 'alpine:3.14'
-
-
-@pytest.fixture()
-def load_images_in_kind(
-    logger,
-    test_executor_image,
-    executor_merger_image,
-    dummy_dumper_image,
-    reload_executor_image,
-    slow_process_executor_image,
-    slow_init_executor_image,
-    alpine_image,
-    k8s_cluster,
-):
-    logger.debug(f'Loading docker image into kind cluster...')
-    for image in [
-        test_executor_image,
-        executor_merger_image,
-        dummy_dumper_image,
-        reload_executor_image,
-        slow_process_executor_image,
-        slow_init_executor_image,
-        'jinaai/jina:test-pip',
-    ]:
-        k8s_cluster.load_docker_image(image)
-    logger.debug(f'Done loading docker image into kind cluster...')
-
-
-@pytest.fixture()
+@pytest.fixture(autouse=True)
 def set_test_pip_version():
-    import os
-
     os.environ['JINA_K8S_USE_TEST_PIP'] = 'True'
     yield
     del os.environ['JINA_K8S_USE_TEST_PIP']
+
+
+@pytest.fixture
+def docker_images(request, image_name_tag_map, k8s_cluster):
+    image_names = request.param
+    k8s_cluster.load_docker_images(image_names, image_name_tag_map)
+    images = [
+        image_name + ':' + image_name_tag_map[image_name] for image_name in image_names
+    ]
+    return images
