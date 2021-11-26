0e06ae5033672a8cc4d2f90c6e7cdcf1fcd4fe33
bwanglzu <bo.wang@jina.ai>
2021-11-26 10:56:45 +0100

    feat(daemon): refine test structure

--- tests/distributed/test_env_setting/test_env_setting.py
+++ tests/distributed/test_env_setting/test_env_setting.py
@@ -1,42 +1,45 @@
 import os
+from contextlib import contextmanager
 
 import pytest
 
 from daemon.clients import JinaDClient
-from jina import Flow, Document, Client, __default_host__
+from jina import Document, Client, __default_host__
 
 CLOUD_HOST = 'localhost:8000'
 NUM_DOCS = 10
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
+jinad_client = JinaDClient(host=__default_host__, port=8000)
 
 
-@pytest.mark.parametrize('replicas', [1, 2])
-def test_remote_flow_local_executors(mocker, replicas):
-    client = JinaDClient(host=__default_host__, port=8000)
-    workspace_id = client.workspaces.create(paths=[cur_dir])
-    envs = {'key1': 'val1', 'key2': 'val2', 'REPLICAS': replicas}
+@contextmanager
+def RemoteFlow(filename, envs):
+    flow_id = None
+    try:
+        workspace_id = jinad_client.workspaces.create(paths=[cur_dir])
+        print(f'\n\n{workspace_id}\n\n')
+        flow_id = jinad_client.flows.create(
+            workspace_id=workspace_id, filename=filename, envs=envs
+        )
+        yield
+    finally:
+        if flow_id:
+            assert jinad_client.flows.delete(flow_id), 'Flow termination failed'
+            print(f'Remote Flow {flow_id} successfully terminated')
 
-    flow_yaml = 'flow_with_env.yml'
-    response_mock = mocker.Mock()
-    flow_id = client.flows.create(
-        workspace_id=workspace_id, filename=flow_yaml, envs=envs
-    )
-    args = client.flows.get(flow_id)['arguments']['object']['arguments']
-    resp = Client(
-        host=__default_host__,
-        port=args['port_expose'],
-        protocol=args['protocol'],
-    ).post(
-        on='/',
-        inputs=[Document(id=idx) for idx in range(NUM_DOCS)],
-        on_done=response_mock,
-        show_progress=True,
-        return_results=True,
-    )
-    for doc in resp[0].data.docs:
-        print(doc)
-    # response_mock.assert_called()
-    # assert client.flows.delete(flow_id)
-    #
-    # assert client.workspaces.delete(workspace_id)
+
+@pytest.mark.parametrize('replicas', [1])
+def test_remote_flow_local_executors(replicas):
+    with RemoteFlow(
+        filename='flow_with_env.yml',
+        envs={'key1': 'val1', 'key2': 'val2', 'REPLICAS': replicas},
+    ):
+        resp = Client(port=12345).post(
+            on='/',
+            inputs=[Document(id=idx) for idx in range(NUM_DOCS)],
+            return_results=True,
+        )
+        for doc in resp[0].data.docs:
+            print(doc)
+        # assert resp[0].data.docs[0].text == directory * mul
