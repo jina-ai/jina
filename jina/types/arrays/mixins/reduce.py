4d41c7b7891bf74b580c924f327b2a320d800fef
Alaeddine Abdessalem <alaeddine-13@live.fr>
2021-12-04 19:49:31 +0100

    chore: apply review suggestions

--- jina/types/arrays/mixins/reduce.py
+++ jina/types/arrays/mixins/reduce.py
@@ -10,13 +10,13 @@ if TYPE_CHECKING:
 class ReduceMixin:
     """A mixing that provides reducing logic for :class:`DocumentArray` or :class:`DocumentArrayMemmap`"""
 
-    def reduce(self: 'T', da: 'T'):
+    def reduce(self: 'T', other: 'T') -> 'T':
         """
-        Reduces da and the current DocumentArray into one DocumentArray in-place. Changes are applied to the current
+        Reduces other and the current DocumentArray into one DocumentArray in-place. Changes are applied to the current
         DocumentArray
-        :param da: DocumentArray
+        :param other: DocumentArray
         """
-        for doc in da:
+        for doc in other:
             if doc.id in self:
                 self._reduce_doc(self[doc.id], doc)
             else:
@@ -35,13 +35,13 @@ class ReduceMixin:
         if len(doc2.chunks) > 0:
             doc1.chunks.reduce(doc2.chunks)
 
-    def reduce_mat(self: 'T', da_matrix: List['T']) -> 'T':
+    def reduce_all(self: 'T', others: List['T']) -> 'T':
         """
         Reduces a list of DocumentArrays and this  DocumentArray into one DocumentArray. Changes are applied to this
         DocumentArray
-        :param da_matrix: List of DocumentArray to be reduced
+        :param others: List of DocumentArray to be reduced
         :return: the resulting DocumentArray
         """
-        for da in da_matrix:
+        for da in others:
             self.reduce(da)
         return self
