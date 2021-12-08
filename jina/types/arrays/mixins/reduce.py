56df12f7815dd15122da19384ed1b5b959a859d1
Alaeddine Abdessalem <alaeddine-13@live.fr>
2021-12-08 10:43:14 +0100

    chore: apply review suggestions

--- jina/types/arrays/mixins/reduce.py
+++ jina/types/arrays/mixins/reduce.py
@@ -29,7 +29,11 @@ class ReduceMixin:
     def reduce(self: 'T', other: 'T') -> 'T':
         """
         Reduces other and the current DocumentArray into one DocumentArray in-place. Changes are applied to the current
-        DocumentArray
+        DocumentArray.
+        Reducing 2 DocumentArrays consists in adding Documents in the second DocumentArray to the first DocumentArray
+        if they do not exist. If a Document exists in both DocumentArrays, the data properties are merged with priority
+        to the first Document (that is, to the current DocumentArray's Document). The matches and chunks are also
+        reduced.
         :param other: DocumentArray
         :return: DocumentArray
         """
@@ -44,7 +48,10 @@ class ReduceMixin:
     @staticmethod
     def _reduce_doc(doc1: 'Document', doc2: 'Document'):
         """
-        Reduces doc1 and doc2 into one Document in-place. Changes are applied to doc1
+        Reduces doc1 and doc2 into one Document in-place. Changes are applied to doc1.
+        Reducing 2 Documents consists in setting data properties of the second Document to the first Document if they
+        are empty and reducing the matches and the chunks of both documents.
+        Reduction of matches and chunks relies on :class:`DocumentArray`.:method:`reduce`.
         :param doc1: first Document
         :param doc2: second Document
         """
@@ -57,8 +64,23 @@ class ReduceMixin:
 
     def reduce_all(self: 'T', others: List['T']) -> 'T':
         """
-        Reduces a list of DocumentArrays and this  DocumentArray into one DocumentArray. Changes are applied to this
-        DocumentArray
+        Reduces a list of DocumentArrays and this DocumentArray into one DocumentArray. Changes are applied to this
+        DocumentArray in-place.
+
+        Reduction consists in reducing this DocumentArray with every DocumentArray in others sequentially using
+        :class:`DocumentArray`.:method:`reduce`.
+        The resulting DocumentArray contains Documents of all DocumentArrays.
+        If a Document exists in many DocumentArrays, data properties are merged with priority to the left-most
+        DocumentArrays (that is, if a data attribute is set in a Document belonging to many DocumentArrays, the
+        attribute value of the left-most DocumentArray is kept.
+        Non-data properties are ignored.
+        Matches and chunks of a Document belonging to many DocumentArrays are also reduced in the same way.
+
+        .. note::
+            - Matches are not kept in a sorted order when they are reduced. You might want to re-sort them in a later
+                step.
+            - The final result depends on the order of DocumentArrays when applying reduction.
+
         :param others: List of DocumentArray to be reduced
         :return: the resulting DocumentArray
         """
