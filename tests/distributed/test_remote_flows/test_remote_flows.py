19ee5b0d61d6a6abd3f2d7a4e2d073eb4fce3b13
bwanglzu <bo.wang@jina.ai>
2021-12-16 15:36:36 +0100

    test(daemon): test list and clear

--- tests/distributed/test_remote_flows/test_remote_flows.py
+++ tests/distributed/test_remote_flows/test_remote_flows.py
@@ -12,7 +12,8 @@ from jina.parsers.flow import set_flow_parser
 HOST = __default_host__
 PORT = 8000
 PROTOCOL = 'HTTP'
-EXECUTOR_PORT = 9000
+MINI_FLOW1_PORT = 9000
+MINI_FLOW2_PORT = 9001
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -31,41 +32,64 @@ def async_jinad_client():
     return AsyncJinaDClient(host=HOST, port=PORT)
 
 
-def test_remote_jinad_flow(flow_args, jinad_client):
+def test_remote_jinad_flow(jinad_client):
     workspace_id = jinad_client.workspaces.create(
         paths=[os.path.join(cur_dir, cur_dir)]
     )
     assert jinad_client.flows.alive()
     # create flow
-    flow_id = jinad_client.flows.create(workspace_id=workspace_id, filename='flow.yml')
+    flow_id = jinad_client.flows.create(workspace_id=workspace_id, filename='flow1.yml')
     assert flow_id
     # get flow status
     remote_flow_args = jinad_client.flows.get(DaemonID(flow_id))
     remote_flow_args = remote_flow_args['arguments']['object']['arguments']
-    assert remote_flow_args['port_expose'] == EXECUTOR_PORT
+    assert remote_flow_args['port_expose'] == MINI_FLOW1_PORT
     assert remote_flow_args['protocol'] == PROTOCOL
     assert jinad_client.flows.delete(flow_id)
     assert jinad_client.workspaces.delete(workspace_id)
 
 
 @pytest.mark.asyncio
-async def test_remote_jinad_flow_async(flow_args, async_jinad_client):
+async def test_remote_jinad_flow_async(async_jinad_client):
     workspace_id = await async_jinad_client.workspaces.create(
         paths=[os.path.join(cur_dir, cur_dir)]
     )
     assert await async_jinad_client.flows.alive()
     # create flow
     flow_id = await async_jinad_client.flows.create(
-        workspace_id=workspace_id, filename='flow.yml'
+        workspace_id=workspace_id, filename='flow1.yml'
     )
     assert flow_id
     # get flow status
     remote_flow_args = await async_jinad_client.flows.get(DaemonID(flow_id))
     remote_flow_args = remote_flow_args['arguments']['object']['arguments']
-    assert remote_flow_args['port_expose'] == EXECUTOR_PORT
+    assert remote_flow_args['port_expose'] == MINI_FLOW1_PORT
     assert remote_flow_args['protocol'] == PROTOCOL
     assert await async_jinad_client.flows.delete(flow_id)
     assert await async_jinad_client.workspaces.delete(workspace_id)
+
+
+@pytest.mark.asyncio
+async def test_remote_jinad_flow_get_delete_all(async_jinad_client):
+    workspace_id = await async_jinad_client.workspaces.create(
+        paths=[os.path.join(cur_dir, cur_dir)]
+    )
+    assert await async_jinad_client.flows.alive()
+    # create flow
+    flow_id1 = await async_jinad_client.flows.create(
+        workspace_id=workspace_id, filename='flow1.yml'
+    )
+    assert flow_id1
+    flow_id2 = await async_jinad_client.flows.create(
+        workspace_id=workspace_id, filename='flow2.yml'
+    )
+    assert flow_id2
+    # get all flows
+    remote_flow_args = await async_jinad_client.flows.list()
+    assert len(remote_flow_args.keys()) == 2
+    await async_jinad_client.flows.clear()
+    remote_flow_args = await async_jinad_client.flows.list()
+    assert len(remote_flow_args.keys()) == 0
 
 
 @pytest.mark.asyncio
