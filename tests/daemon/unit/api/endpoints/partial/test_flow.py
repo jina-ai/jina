5c5e257e0d60b1365f362953c789b395f9cd26f5
bwanglzu <bo.wang@jina.ai>
2021-12-21 12:05:36 +0100

    test(daemon): remove client from flow unit test

--- tests/daemon/unit/api/endpoints/partial/test_flow.py
+++ tests/daemon/unit/api/endpoints/partial/test_flow.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 from daemon.models import FlowModel
-from jina import Client, Document
 
 cur_dir = Path(__file__).parent
 api = '/flow'
@@ -16,10 +15,6 @@ def test_flow_api(monkeypatch, partial_flow_client):
 
     get_response = partial_flow_client.get(api)
 
-    endpoint_responses = Client(port=56789).post(
-        on='/any_endpoint', inputs=Document(), return_results=True
-    )
-
     rolling_update_response = partial_flow_client.put(
         f'{api}/rolling_update',
         params={
@@ -33,6 +28,6 @@ def test_flow_api(monkeypatch, partial_flow_client):
     assert create_response
     assert get_response
     assert get_response.json()['arguments']['port_expose'] == 56789
-    assert endpoint_responses[0].docs[0].content == 'https://jina.ai'
     assert rolling_update_response.status_code == 200
     assert delete_response
+    partial_flow_client.wait_shutdown()
