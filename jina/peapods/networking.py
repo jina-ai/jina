70b7e120769cbea839c30558aea1856f955a784b
Zhaofeng Miao <522856232@qq.com>
2021-12-23 15:26:08 +0800

    fix: update jina/peapods/networking.py
    Co-authored-by: Joan Fontanals <joan.martinez@jina.ai>

--- jina/peapods/networking.py
+++ jina/peapods/networking.py
@@ -621,7 +621,7 @@ class GrpcConnectionPool:
     async def send_request_async(
         request: Request,
         target: str,
-        timeout=1.0,
+        timeout: float=1.0,
         https: bool =False,
     ) -> Request:
         """
