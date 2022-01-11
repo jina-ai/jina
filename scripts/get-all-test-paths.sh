36c13dd0f757c5b0fb794ed576f33fef7c90b55a
Joan Fontanals Martinez <joan.martinez@jina.ai>
2022-01-11 08:56:07 +0100

    fix: do not change script

--- scripts/get-all-test-paths.sh
+++ scripts/get-all-test-paths.sh
@@ -1,25 +1,19 @@
 #!/usr/bin/env bash
-
 set -ex
-
 if [[ $1 == "windows" ]]; then
     # declare -a array1=( "tests/system/*.py" )
     declare -a array2=( $(ls -d tests/{unit,system,docarray}/*/ | grep -v '__pycache__' ))
     # dest=( "${array1[@]}" "${array2[@]}" )
     printf '%s\n' "${array2[@]}" | jq -R . | jq -cs .
 else
-    # TODO: enable all distributed/daemon unit tests
-    declare -a array1=( "tests/unit/*.py" "tests/integration/*.py" "tests/system/*.py" "tests/docarray/*.py" "tests/distributed/test_remote_pods/" "tests/distributed/test_remote_flows/")
-    # declare -a array1=( "tests/unit/*.py" "tests/integration/*.py" "tests/distributed/*.py" "tests/system/*.py" "tests/docarray/*.py")
-    declare -a array2=( $(ls -d tests/{unit,integration,system,docarray}/*/ | grep -v '__pycache__' ))
-    # declare -a array2=( $(ls -d tests/{unit,integration,distributed,system,docarray}/*/ | grep -v '__pycache__' ))
+    declare -a array1=( "tests/unit/*.py" "tests/integration/*.py" "tests/distributed/*.py" "tests/system/*.py" "tests/docarray/*.py")
+    declare -a array2=( $(ls -d tests/{unit,integration,distributed,system,docarray}/*/ | grep -v '__pycache__' ))
     dest1=( "${array1[@]}" "${array2[@]}" )
 
-    # declare -a array1=( "tests/daemon/unit/*.py" )
-    # declare -a array2=( $(ls -d tests/daemon/{unit,integration}/*/ | grep -v '__pycache__' ))
+    declare -a array1=( "tests/daemon/unit/*.py" )
+    declare -a array2=( $(ls -d tests/daemon/{unit,integration}/*/ | grep -v '__pycache__' ))
     dest2=( "${array1[@]}" "${array2[@]}" )
 
     dest=( "${dest1[@]}" "${dest2[@]}" )
-
     printf '%s\n' "${dest[@]}" | jq -R . | jq -cs .
 fi
