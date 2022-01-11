b5297daf6410127dffb4353a3da8039c9684fdc5
Johannes Messner <messnerjo@gmail.com>
2022-01-11 09:30:16 +0100

    refactor(Client): remove Optional in boolean type hint

--- jina/clients/__init__.py
+++ jina/clients/__init__.py
@@ -21,8 +21,8 @@ def Client(
     https: Optional[bool] = False,
     port: Optional[int] = None,
     protocol: Optional[str] = 'GRPC',
-    proxy: Optional[bool] = False,
-    results_as_docarray: Optional[bool] = False,
+    proxy: bool = False,
+    results_as_docarray: bool = False,
     **kwargs
 ) -> Union[
     'AsyncWebSocketClient',
