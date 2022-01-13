7c13cec69552389d2fe057ae97914c7d1e57b910
Johannes Messner <messnerjo@gmail.com>
2022-01-13 15:41:26 +0100

    test: adjust remote scale tests to now missing replica_id

--- tests/distributed/test_scale_remote_executors/test_remote_executors.py
+++ tests/distributed/test_scale_remote_executors/test_remote_executors.py
@@ -82,22 +82,23 @@ def test_scale_success(remote_flow_with_runtime: Flow, pod_params):
         )
 
         assert len(ret1) == 20
-        replica_ids = set()
+        replicas = set()
         for r in ret1:
             assert len(r.docs) == 10
-            for replica_id in r.docs.get_attributes('tags__replica_id'):
-                replica_ids.add(replica_id)
+            # replicas are identified via their docker id
+            for id in r.docs.get_attributes('tags__docker_id'):
+                replicas.add(id)
 
-        assert replica_ids == set(range(num_replicas))
+        assert len(replicas) == num_replicas * shards
 
         assert len(ret2) == 20
-        replica_ids = set()
+        replicas = set()
         for r in ret2:
             assert len(r.docs) == 10
-            for replica_id in r.docs.get_attributes('tags__replica_id'):
-                replica_ids.add(replica_id)
+            for id in r.docs.get_attributes('tags__docker_id'):
+                replicas.add(id)
 
-        assert replica_ids == set(range(scale_to))
+        assert len(replicas) == scale_to * shards
 
 
 @pytest.mark.parametrize(
