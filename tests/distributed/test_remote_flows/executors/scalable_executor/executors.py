72c8de684e51767c183f8e9be9c04bcf19e06c0f
Johannes Messner <messnerjo@gmail.com>
2022-01-14 14:47:00 +0100

    test: adjust remote flow tests to missing replica_id

--- tests/distributed/test_remote_flows/executors/scalable_executor/executors.py
+++ tests/distributed/test_remote_flows/executors/scalable_executor/executors.py
@@ -1,14 +1,15 @@
 from jina import Executor, requests
+import os
 
 
 class ScalableExecutor(Executor):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.replica_id = self.runtime_args.replica_id
+        self.process_id = os.getpid()
         self.shard_id = self.runtime_args.shard_id
 
     @requests
     def foo(self, docs, **kwargs):
         for doc in docs:
-            doc.tags['replica_id'] = self.replica_id
             doc.tags['shard_id'] = self.shard_id
+            doc.tags['process_id'] = self.process_id
