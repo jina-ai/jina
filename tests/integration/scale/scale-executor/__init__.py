1b7711d3d237dae90e5297fccd9e22c3a6c24661
Tobias Jacobowitz <tobias.jacobowitz@jina.ai>
2022-01-25 17:32:46 +0100

    fix: replace docker id with uids (#4217)

--- tests/integration/scale/scale-executor/__init__.py
+++ tests/integration/scale/scale-executor/__init__.py
@@ -1,21 +1,16 @@
+import uuid
+
 from jina import Executor, requests
 
 
 class ScalableExecutor(Executor):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.docker_id = self.get_docker_id()
-        print(self.docker_id)
+        self.uid = str(uuid.uuid4())
         self.shard_id = self.runtime_args.shard_id
 
     @requests
     def foo(self, docs, **kwargs):
         for doc in docs:
-            doc.tags['docker_id'] = self.docker_id
+            doc.tags['uid'] = self.uid
             doc.tags['shard_id'] = self.shard_id
-
-    def get_docker_id(self):
-        import subprocess
-
-        bash_command = """head -1 /proc/self/cgroup|cut -d/ -f3"""
-        return subprocess.check_output(['bash', '-c', bash_command]).decode("utf-8")
