5f75519174012164f61c66eed78d107133e24426
Johannes Messner <messnerjo@gmail.com>
2022-01-31 11:15:24 +0100

    test: add test checking 'uses_requests' is only applied to 'uses' executor

--- tests/integration/v2_api/test_override_requests.py
+++ tests/integration/v2_api/test_override_requests.py
@@ -25,3 +25,33 @@ def test_override_requests():
 
     assert resp1[0].docs[0].text == ''
     assert resp2[0].docs[0].text == 'foo called'
+
+
+def test_override_requests_uses_after():
+    class FooExecutor(Executor):
+        @requests(on='/bar')
+        def foo(self, docs, **kwargs):
+            for doc in docs:
+                doc.text = 'foo called'
+
+    class AfterExecutor(Executor):
+        @requests(on='/bar')
+        def bar(self, docs, **kwargs):
+            for doc in docs:
+                doc.text = 'bar called'
+
+    with Flow(port_expose=exposed_port).add(
+        uses=FooExecutor, uses_requests={'/foo': 'foo'}, uses_after=AfterExecutor
+    ) as f:
+        c = Client(port=exposed_port)
+        resp1 = c.post(
+            on='/foo', inputs=DocumentArray([Document(text='')]), return_results=True
+        )
+        resp2 = c.post(
+            on='/non_foo',
+            inputs=DocumentArray([Document(text='')]),
+            return_results=True,
+        )
+
+    assert resp1[0].docs[0].text == 'foo called'
+    assert resp2[0].docs[0].text == ''
