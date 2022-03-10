ff9b4eb4d6e3dc9bd5ee6c5c4564b9597c69972c
Johannes Messner <messnerjo@gmail.com>
2022-03-10 11:08:00 +0100

    refactor: per default do not expose graphql endpoint

--- jina/orchestrate/flow/base.py
+++ jina/orchestrate/flow/base.py
@@ -40,15 +40,15 @@ from jina.excepts import (
     RuntimeFailToStart,
 )
 from jina.helper import (
+    GRAPHQL_MIN_DOCARRAY_VERSION,
     ArgNamespace,
     CatchAllCleanupContextManager,
     colored,
+    docarray_graphql_compatible,
     download_mermaid_url,
     get_internal_ip,
     get_public_ip,
     typename,
-    docarray_graphql_compatible,
-    GRAPHQL_MIN_DOCARRAY_VERSION,
 )
 from jina.jaml import JAMLCompatible
 from jina.logging.logger import JinaLogger
@@ -142,7 +142,7 @@ class Flow(PostMixin, JAMLCompatible, ExitStack, metaclass=FlowType):
         native: Optional[bool] = False,
         no_crud_endpoints: Optional[bool] = False,
         no_debug_endpoints: Optional[bool] = False,
-        no_graphql_endpoint: Optional[bool] = False,
+        expose_graphql_endpoint: Optional[bool] = False,
         polling: Optional[str] = 'ANY',
         port: Optional[int] = None,
         prefetch: Optional[int] = 0,
@@ -197,7 +197,7 @@ class Flow(PostMixin, JAMLCompatible, ExitStack, metaclass=FlowType):
 
                   Any executor that has `@requests(on=...)` bind with those values will receive data requests.
         :param no_debug_endpoints: If set, /status /post endpoints are removed from HTTP interface.
-        :param no_graphql_endpoint: If set, /graphql endpoint is removed from HTTP interface.
+        :param expose_graphql_endpoint: If set, /graphql endpoint is added to HTTP interface.
         :param polling: The polling strategy of the Deployment and its endpoints (when `shards>1`).
               Can be defined for all endpoints of a Deployment or by endpoint.
               Define per Deployment:
@@ -262,7 +262,7 @@ class Flow(PostMixin, JAMLCompatible, ExitStack, metaclass=FlowType):
         inspect: Optional[str] = 'COLLECT',
         log_config: Optional[str] = None,
         name: Optional[str] = None,
-        no_graphql_endpoint: Optional[bool] = False,
+        expose_graphql_endpoint: Optional[bool] = False,
         polling: Optional[str] = 'ANY',
         quiet: Optional[bool] = False,
         quiet_error: Optional[bool] = False,
@@ -287,7 +287,7 @@ class Flow(PostMixin, JAMLCompatible, ExitStack, metaclass=FlowType):
           - ...
 
           When not given, then the default naming strategy will apply.
-        :param no_graphql_endpoint: If set, /graphql endpoint is removed from HTTP interface.
+        :param expose_graphql_endpoint: If set, /graphql endpoint is added to HTTP interface.
         :param polling: The polling strategy of the Deployment and its endpoints (when `shards>1`).
               Can be defined for all endpoints of a Deployment or by endpoint.
               Define per Deployment:
@@ -325,13 +325,13 @@ class Flow(PostMixin, JAMLCompatible, ExitStack, metaclass=FlowType):
         self._update_args(args, **kwargs)
         if (
             self.protocol == GatewayProtocolType.HTTP
-            and not self.args.no_graphql_endpoint
+            and self.args.expose_graphql_endpoint
             and not docarray_graphql_compatible()
         ):
-            self.args.no_graphql_endpoint = True
+            self.args.expose_graphql_endpoint = False
             warnings.warn(
                 'DocArray version is incompatible with GraphQL features. '
-                'Automatically setting no_graphql_endpoint=True. '
+                'Automatically setting expose_graphql_endpoint=False. '
                 f'To use GraphQL features, install docarray>={GRAPHQL_MIN_DOCARRAY_VERSION}'
             )
 
@@ -449,8 +449,8 @@ class Flow(PostMixin, JAMLCompatible, ExitStack, metaclass=FlowType):
         kwargs.update(self._common_kwargs)
         args = ArgNamespace.kwargs2namespace(kwargs, set_gateway_parser())
         args.noblock_on_start = True
-        args.no_graphql_endpoint = (
-            self.args.no_graphql_endpoint
+        args.expose_graphql_endpoint = (
+            self.args.expose_graphql_endpoint
         )  # also used in Flow, thus not in kwargs
         args.graph_description = json.dumps(graph_description)
         args.deployments_addresses = json.dumps(deployments_addresses)
@@ -1508,7 +1508,7 @@ class Flow(PostMixin, JAMLCompatible, ExitStack, metaclass=FlowType):
                     attrs='underline',
                 )
             )
-            if not self.args.no_graphql_endpoint:
+            if self.args.expose_graphql_endpoint:
                 address_table.append(
                     f'\t💬 GraphQL UI:\t\t'
                     + colored(
