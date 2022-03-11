832b64257f54507928cdecd22e793c5815affd20
Sami Jaghouar <sami.jaghouar@jina.ai>
2022-03-11 15:23:42 +0100

    fix: using grpc connection pool check health

--- jina/resources/health_check/pod.py
+++ jina/resources/health_check/pod.py
@@ -1,21 +1,20 @@
 import grpc
 
-from jina.proto import jina_pb2_grpc
+from jina.serve.networking import GrpcConnectionPool
 from jina.types.request.control import ControlRequest
 
 
-def check_health_pod(addr: str, timeout: int = 600):
+def check_health_pod(addr: str):
     """check if a pods is healthy
 
     :param addr: the address on which the pod is serving ex : localhost:1234
-    :param timeout: timeout for the health check
     """
 
-    channel = grpc.insecure_channel(addr)
-    stub = jina_pb2_grpc.JinaControlRequestRPCStub(channel)
-    request = ControlRequest(command="STATUS")
     try:
-        stub.process_control(request, timeout=timeout)
+        GrpcConnectionPool.send_request_sync(
+            request=ControlRequest('STATUS'),
+            target=addr,
+        )
     except grpc.RpcError as e:
         print('The pod is unhealthy')
         print(e)
@@ -37,6 +36,4 @@ if __name__ == '__main__':
         raise ValueError('You need to specify a address to check health')
 
     addr = sys.argv[1]
-    timeout = sys.argv[2] if len(sys.argv) > 2 else 600
-
-    check_health_pod(addr, timeout)
+    check_health_pod(addr)
