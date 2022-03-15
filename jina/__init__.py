aabdfeda7e784ee89a93d972f2d2527a5e473619
Joan Fontanals Martinez <joan.martinez@jina.ai>
2022-03-15 18:10:36 +0100

    chore: bump version number

--- jina/__init__.py
+++ jina/__init__.py
@@ -14,8 +14,8 @@ import signal as _signal
 import sys as _sys
 import types as _types
 import warnings as _warnings
-import docarray as _docarray
 
+import docarray as _docarray
 
 if _sys.version_info < (3, 7, 0):
     raise OSError(f'Jina requires Python >= 3.7, but yours is {_sys.version_info}')
@@ -60,7 +60,7 @@ elif _sys.version_info >= (3, 8, 0) and _platform.system() == 'Darwin':
 # this is managed by git tag and updated on every release
 # NOTE: this represents the NEXT release version
 
-__version__ = '3.1.7'
+__version__ = '3.2.0'
 
 # do not change this line manually
 # this is managed by proto/build-proto.sh and updated on every execution
@@ -188,19 +188,19 @@ _set_nofile()
 
 # ONLY FIRST CLASS CITIZENS ARE ALLOWED HERE, namely Document, Executor Flow
 
-# Client
-from jina.clients import Client
-
 # Document
 from docarray import Document, DocumentArray
+
+# Client
+from jina.clients import Client
+from jina.orchestrate.flow.asyncio import AsyncFlow
+
+# Flow
+from jina.orchestrate.flow.base import Flow
 
 # Executor
 from jina.serve.executors import BaseExecutor as Executor
 from jina.serve.executors.decorators import requests
-
-# Flow
-from jina.orchestrate.flow.base import Flow
-from jina.orchestrate.flow.asyncio import AsyncFlow
 
 __all__ = [_s for _s in dir() if not _s.startswith('_')]
 __all__.extend(_names_with_underscore)
