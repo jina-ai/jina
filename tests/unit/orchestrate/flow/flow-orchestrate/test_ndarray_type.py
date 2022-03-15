006eb3df918fcb555998bcb20788ac64f6a0c04f
Johannes Messner <messnerjo@gmail.com>
2022-03-15 11:14:41 +0100

    test: fix tests

--- tests/unit/orchestrate/flow/flow-orchestrate/test_ndarray_type.py
+++ tests/unit/orchestrate/flow/flow-orchestrate/test_ndarray_type.py
@@ -1,8 +1,15 @@
 import numpy as np
 import pytest
+import torch
 from docarray import Document, DocumentArray
 
 from jina import Executor, Flow, requests
+
+
+class MyExec(Executor):
+    @requests
+    def foo(self, docs, **kwargs):
+        pass
 
 
 class ListInExec(Executor):
@@ -37,9 +44,9 @@ class NparrayInEec(Executor):
 def linear_flow():
     f = (
         Flow()
-        .add(output_array_type='list')
-        .add(uses=ListInExec, output_array_type='numpy')
-        .add(uses=NparrayInEec)
+        .add(uses=MyExec, output_array_type='numpy')
+        .add(uses=NparrayInEec, output_array_type='list')
+        .add(uses=ListInExec)
     )
     return f
 
@@ -47,16 +54,16 @@ def linear_flow():
 def test_array_conversion(linear_flow):
     docs = DocumentArray.empty(5)
     for doc in docs:
-        doc.embedding = np.random.randn(5)
-        doc.tensor = np.random.randn(3, 3)
+        doc.embedding = torch.tensor(np.random.randn(5))
+        doc.tensor = torch.tensor(np.random.randn(3, 3))
 
     with linear_flow as f:
         resp = f.post(on='/foo', inputs=docs)
     for doc in resp:
-        assert doc.tags['listcheck_embedding']
-        assert doc.tags['listcheck_tensor']
         assert doc.tags['nparraycheck_embedding']
         assert doc.tags['nparraycheck_tensor']
+        assert doc.tags['listcheck_embedding']
+        assert doc.tags['listcheck_tensor']
 
 
 def test_empty_arrays(linear_flow):
