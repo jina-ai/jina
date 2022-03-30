ffdae546026a434fb1b441e7948bc672fe4b4267
Han Xiao <artex.xh@gmail.com>
2022-03-30 10:35:35 +0200

    feat(grpc): add os env bytes counter (#4560)

---
+++ tests/integration/grpc_bytes_census/test_bytes_census.py
@@ -0,0 +1,25 @@
+import os
+
+import pytest
+
+from docarray import DocumentArray
+from jina import Flow
+
+
+@pytest.mark.parametrize('inputs', [None, DocumentArray.empty(10)])
+def test_grpc_census(inputs):
+    assert int(os.environ.get('JINA_GRPC_SEND_BYTES', 0)) == 0
+    assert int(os.environ.get('JINA_GRPC_RECV_BYTES', 0)) == 0
+    with Flow().add().add() as f:
+        f.post(
+            on='/',
+            inputs=inputs,
+        )
+    assert int(os.environ['JINA_GRPC_SEND_BYTES']) > 0
+    assert int(os.environ['JINA_GRPC_RECV_BYTES']) > 0
+    # add some route info, so size must be larger
+    assert int(os.environ['JINA_GRPC_SEND_BYTES']) < int(
+        os.environ['JINA_GRPC_RECV_BYTES']
+    )
+    del os.environ['JINA_GRPC_SEND_BYTES']
+    del os.environ['JINA_GRPC_RECV_BYTES']
