f6325ee33e9bdbc6f05281e2a27492fef4db5e65
Han Xiao <artex.xh@gmail.com>
2022-03-30 15:31:25 +0200

    chore: update setup.py to include py310

--- setup.py
+++ setup.py
@@ -174,6 +174,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Unix Shell',
         'Environment :: Console',
         'License :: OSI Approved :: Apache Software License',
