b9f2f305339a4a0c4f3e45ef94598f2294168108
Sami Jaghouar <sami.jaghouar@jina.ai>
2022-03-31 11:12:40 +0200

    refactor(tests): move test to issues

---
+++ tests/integration/issues/github_4555/test_port.py
@@ -0,0 +1,21 @@
+import time
+from threading import Thread
+
+import pytest
+
+from jina import Flow
+from jina.excepts import PortAlreadyUsed
+
+
+def test_two_flow_using_on_same_port_failing(port_generator):
+    # hacky test but there is now good way to wait that a flow exit
+
+    port = port_generator()
+
+    f1 = Flow(port=port)
+    f2 = Flow(port=port)
+
+    with f1:
+        with pytest.raises(PortAlreadyUsed):
+            with f2:
+                pass
