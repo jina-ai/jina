ca636a9d195ed21e1d443bd89d7569b488e6bfd6
Sami Jaghouar <sami.jaghouar@jina.ai>
2022-04-07 11:18:24 +0200

    feat: close monitoring server in teardown

--- jina/serve/runtimes/mixin.py
+++ jina/serve/runtimes/mixin.py
@@ -1,3 +1,39 @@
+import threading
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from prometheus_client import CollectorRegistry
+
+
+def _start_monitoring_server(port, registry: 'CollectorRegistry', addr=''):
+    """Starts a WSGI server for prometheus metrics as a daemon thread.
+    :param port: port on which to expose
+    :param registry: prometheus registry that will feed to http server with metrics
+    :param addr: address on which to expose
+    :return: httpd server
+    """
+    from wsgiref.simple_server import make_server
+
+    from prometheus_client import make_wsgi_app
+    from prometheus_client.exposition import ThreadingWSGIServer, _SilentHandler
+
+    app = make_wsgi_app(registry)
+    httpd = make_server(
+        addr, port, app, ThreadingWSGIServer, handler_class=_SilentHandler
+    )
+    t = threading.Thread(target=httpd.serve_forever)
+    t.daemon = True
+    t.start()
+
+    return httpd
+
+
+def _close_monitoring_server(httpd):
+    t = threading.Thread(target=httpd.shutdown())
+    t.daemon = True
+    t.start()
+
+
 class MonitoringMixin:
     """The Monitoring Mixin for pods"""
 
@@ -15,10 +51,11 @@ class MonitoringMixin:
 
         if self.args.monitoring:
 
-            from prometheus_client import start_http_server
-
-            start_http_server(self.args.port_monitoring, registry=self.metrics_registry)
+            self.httpd_monitoring = _start_monitoring_server(
+                self.args.port_monitoring, registry=self.metrics_registry
+            )
 
     def teardown_monitoring(self):
         """clean up monitoring resources during teardown."""
-        pass  # todo find a way to correctly kill to the prometheus thread
+        if self.args.monitoring:
+            _close_monitoring_server(self.httpd_monitoring)
