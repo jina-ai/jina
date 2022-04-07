d7a07992ad9f4f18f4f3571b824d2f8717e41dea
Sami Jaghouar <sami.jaghouar@jina.ai>
2022-04-07 13:12:23 +0200

    Revert "feat: add placeholder for prometheus teardon which is not impl yet"
    This reverts commit 6dfc767b0bb1a8e6af5c763851eb9926ca74f823.

--- jina/serve/runtimes/monitoring.py
+++ jina/serve/runtimes/monitoring.py
@@ -18,7 +18,3 @@ class MonitoringMixin:
             from prometheus_client import start_http_server
 
             start_http_server(self.args.port_monitoring, registry=self.metrics_registry)
-
-    def teardown_monitoring(self):
-        """clean up monitoring resources during teardown."""
-        pass  # todo find a way to correctly kill to the prometheus thread
