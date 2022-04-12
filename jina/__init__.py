58547ea43416260c9c92a869c0a1247b0b8f06c0
Joan Fontanals <joan.martinez@jina.ai>
2022-04-12 14:43:59 +0200

    chore: bump version (#4604)

--- jina/__init__.py
+++ jina/__init__.py
@@ -12,7 +12,6 @@ import os as _os
 import platform as _platform
 import signal as _signal
 import sys as _sys
-import types as _types
 import warnings as _warnings
 
 import docarray as _docarray
@@ -60,7 +59,7 @@ elif _sys.version_info >= (3, 8, 0) and _platform.system() == 'Darwin':
 # this is managed by git tag and updated on every release
 # NOTE: this represents the NEXT release version
 
-__version__ = '3.2.11'
+__version__ = '3.3.0'
 
 # do not change this line manually
 # this is managed by proto/build-proto.sh and updated on every execution
