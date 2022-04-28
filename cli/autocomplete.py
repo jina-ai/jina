19e5cd7e5be84f755c418c2c7c57a4e0f9f49f6c
Yanlong Wang <yanlong.wang@naiver.org>
2022-04-28 21:56:12 +0800

    feat: immutable tags for executors (#4676)
    * feat(hubio): immutble tags
    
    * docs(hub): mention about the immutable-tag option
    
    * docs(hub): rewrite sentense for immutable tags
    
    * style: fix overload and cli autocomplete
    
    * style: apply black style to hubio changes
    
    * fix: remove useless space
    
    * docs: polish immutable tags
    
    * docs(immutable-tags): add reason and rephrase
    
    Co-authored-by: Jina Dev Bot <dev-bot@jina.ai>
    Co-authored-by: Zhaofeng Miao <522856232@qq.com>

--- cli/autocomplete.py
+++ cli/autocomplete.py
@@ -183,6 +183,7 @@ ac_table = {
             '--verbose',
             '--dockerfile',
             '--tag',
+            '--immutable-tag',
             '--force-update',
             '--force',
             '--secret',
