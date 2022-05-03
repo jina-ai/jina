23fb2a3985824d7a6a092de8bc0e69fe8e2ec647
Johannes Messner <messnerjo@gmail.com>
2022-05-03 11:43:57 +0200

    test: add test for headful flow

--- tests/integration/runtimes/test_network_failures.py
+++ tests/integration/runtimes/test_network_failures.py
@@ -10,11 +10,57 @@ from jina.serve.runtimes.asyncio import AsyncNewLoopRuntime
 from jina.types.request.control import ControlRequest
 
 from .test_runtimes import (
+    _activate_runtimes,
     _create_gateway_runtime,
     _create_head_runtime,
     _create_worker_runtime,
     async_inputs,
 )
+
+
+def _create_worker(port):
+    # create a single worker runtime
+    p = multiprocessing.Process(target=_create_worker_runtime, args=(port,))
+    p.start()
+    time.sleep(0.1)
+    return p
+
+
+def _create_gateway(port, graph, pod_addr, protocol):
+    # create a single worker runtime
+    # create a single gateway runtime
+    p = multiprocessing.Process(
+        target=_create_gateway_runtime,
+        args=(graph, pod_addr, port, protocol),
+    )
+    p.start()
+    time.sleep(0.1)
+    return p
+
+
+def _create_head(port, polling):
+    p = multiprocessing.Process(
+        target=_create_head_runtime, args=(port, 'head', polling)
+    )
+    p.start()
+    time.sleep(0.1)
+    return p
+
+
+def _send_request(gateway_port, protocol):
+    """send request to gateway and see what happens"""
+    c = Client(host='localhost', port=gateway_port, protocol=protocol)
+    return c.post(
+        '/', inputs=[Document(text='hi')], request_size=1, return_responses=True
+    )
+
+
+def _test_error(gateway_port, error_port, protocol):
+    with pytest.raises(ConnectionError) as err_info:
+        _send_request(gateway_port, protocol)
+    # assert error message contains useful info
+    assert 'pod0' in err_info.value.args[0]
+    assert str(error_port) in err_info.value.args[0]
 
 
 @pytest.mark.parametrize('protocol', ['http', 'websocket', 'grpc'])
@@ -25,21 +71,10 @@ def test_runtimes_headless_topology(port_generator, protocol):
     graph_description = '{"start-gateway": ["pod0"], "pod0": ["end-gateway"]}'
     pod_addresses = f'{{"pod0": ["0.0.0.0:{worker_port}"]}}'
 
-    def _create_worker():
-        # create a single worker runtime
-        p = multiprocessing.Process(target=_create_worker_runtime, args=(worker_port,))
-        p.start()
-        time.sleep(1)
-        return p
-
-    worker_process = _create_worker()
-
-    # create a single gateway runtime
-    gateway_process = multiprocessing.Process(
-        target=_create_gateway_runtime,
-        args=(graph_description, pod_addresses, gateway_port, protocol),
+    worker_process = _create_worker(worker_port)
+    gateway_process = _create_gateway(
+        gateway_port, graph_description, pod_addresses, protocol
     )
-    gateway_process.start()
 
     time.sleep(1.0)
 
@@ -58,28 +93,76 @@ def test_runtimes_headless_topology(port_generator, protocol):
     worker_process.terminate()  # kill worker
     worker_process.join()
 
-    def _send_request():
-        """send request to gateway and see what happens"""
-        c = Client(host='localhost', port=gateway_port, protocol=protocol)
-        return c.post(
-            '/', inputs=[Document(text='hi')], request_size=1, return_responses=True
-        )
-
-    def _test_error():
-        with pytest.raises(ConnectionError) as err_info:
-            _send_request()
-        # assert error message contains useful info
-        assert 'pod0' in err_info.value.args[0]
-        assert str(worker_port) in err_info.value.args[0]
-
     try:
         # ----------- 1. test that useful errors are given -----------
-        _test_error()
+        _test_error(gateway_port, worker_port, protocol)
         # ----------- 2. test that gateways remain alive -----------
-        _test_error()  # just repeat the test, expecting the same result
-
+        _test_error(
+            gateway_port, worker_port, protocol
+        )  # just repeat the test, expecting the same result
+    except Exception:
+        assert False
     finally:  # clean up runtimes
         gateway_process.terminate()
         worker_process.terminate()
         gateway_process.join()
         worker_process.join()
+
+
+@pytest.mark.parametrize('terminate_head', [True, False])
+@pytest.mark.parametrize('protocol', ['http', 'websocket', 'grpc'])
+def test_runtimes_headful_topology(port_generator, protocol, terminate_head):
+    # create gateway and workers manually, then terminate worker process to provoke an error
+    worker_port = port_generator()
+    gateway_port = port_generator()
+    head_port = port_generator()
+    graph_description = '{"start-gateway": ["pod0"], "pod0": ["end-gateway"]}'
+    pod_addresses = f'{{"pod0": ["0.0.0.0:{head_port}"]}}'
+
+    head_process = _create_head(head_port, 'ANY')
+    worker_process = _create_worker(worker_port)
+    asyncio.run(_activate_runtimes(head_port, [worker_port]))
+    gateway_process = _create_gateway(
+        gateway_port, graph_description, pod_addresses, protocol
+    )
+
+    time.sleep(1.0)
+
+    AsyncNewLoopRuntime.wait_for_ready_or_shutdown(
+        timeout=5.0,
+        ctrl_address=f'0.0.0.0:{worker_port}',
+        ready_or_shutdown_event=multiprocessing.Event(),
+    )
+
+    AsyncNewLoopRuntime.wait_for_ready_or_shutdown(
+        timeout=5.0,
+        ctrl_address=f'0.0.0.0:{gateway_port}',
+        ready_or_shutdown_event=multiprocessing.Event(),
+    )
+
+    # terminate pod, either head or worker behind the head
+    if terminate_head:
+        head_process.terminate()
+        head_process.join()
+        error_port = head_port
+    else:
+        worker_process.terminate()  # kill worker
+        worker_process.join()
+        error_port = worker_port
+
+    try:
+        # ----------- 1. test that useful errors are given -----------
+        _test_error(gateway_port, error_port, protocol)
+        # ----------- 2. test that gateways remain alive -----------
+        _test_error(
+            gateway_port, error_port, protocol
+        )  # just repeat the test, expecting the same result
+    except Exception:
+        assert False
+    finally:  # clean up runtimes
+        gateway_process.terminate()
+        worker_process.terminate()
+        head_process.terminate()
+        gateway_process.join()
+        worker_process.join()
+        head_process.join()
