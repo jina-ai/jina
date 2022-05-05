5e789f9a6e3201a4e43eb66cb0e74424865eb7ee
jacobowitz <tobias.jacobowitz@posteo.de>
2022-05-05 15:25:29 +0200

    fix: better loop closing

--- jina/helper.py
+++ jina/helper.py
@@ -1001,10 +1001,20 @@ def _update_policy():
             )
 
 
+def _close_loop():
+    try:
+        loop = asyncio.get_event_loop()
+        if not loop.is_closed():
+            loop.close()
+    except RuntimeError:
+        # there is no loop, so nothing to do here
+        pass
+
+
 # workaround for asyncio loop and fork issue: https://github.com/python/cpython/issues/66197
 # we close the loop after forking to avoid reusing the parents process loop
 # a new loop should be created in the child process
-os.register_at_fork(after_in_child=lambda: get_or_reuse_loop().close())
+os.register_at_fork(after_in_child=_close_loop)
 
 
 def get_or_reuse_loop():
