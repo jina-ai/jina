eab7b276ef98b440d210dfe0f2d9b76522f267e6
Alaeddine Abdessalem <alaeddine-13@live.fr>
2022-06-10 16:46:46 +0200

    chore: raise warning if user is port_monitoring is default and already in use

--- jina/serve/runtimes/monitoring.py
+++ jina/serve/runtimes/monitoring.py
@@ -1,3 +1,6 @@
+from jina import __default_port_monitoring__
+
+
 class MonitoringMixin:
     """The Monitoring Mixin for pods"""
 
@@ -17,4 +20,19 @@ class MonitoringMixin:
 
             from prometheus_client import start_http_server
 
-            start_http_server(self.args.port_monitoring, registry=self.metrics_registry)
+            try:
+                start_http_server(
+                    self.args.port_monitoring, registry=self.metrics_registry
+                )
+            except OSError as e:
+                if (
+                    e.args[0] == 98
+                    and self.args.port_monitoring == __default_port_monitoring__
+                ):
+                    self.logger.warning(
+                        f'The port monitoring {self.args.port_monitoring} is already in use. If you are running Jina '
+                        f'Flow natively, you may have not set different ports for each Executor'
+                    )
+                    raise
+                else:
+                    raise
