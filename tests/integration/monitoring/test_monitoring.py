f0636efb47ad69274bb2499676e6bdd7683b23ba
Joan Fontanals Martinez <joan.martinez@jina.ai>
2022-06-28 11:41:14 +0200

    test: test reproduce issue

--- tests/integration/monitoring/test_monitoring.py
+++ tests/integration/monitoring/test_monitoring.py
@@ -4,8 +4,14 @@ import time
 import pytest
 import requests as req
 
-from docarray import DocumentArray
+from docarray import Document, DocumentArray
 from jina import Executor, Flow, requests
+from jina.parsers import set_gateway_parser, set_pod_parser
+from jina.serve.runtimes.asyncio import AsyncNewLoopRuntime
+from jina.serve.runtimes.gateway.grpc import GRPCGatewayRuntime
+from jina.serve.runtimes.gateway.http import HTTPGatewayRuntime
+from jina.serve.runtimes.gateway.websocket import WebSocketGatewayRuntime
+from jina.serve.runtimes.worker import WorkerRuntime
 
 
 @pytest.fixture()
@@ -189,54 +195,154 @@ def test_requests_size(port_generator, executor):
         assert measured_request_bytes_sum > measured_request_bytes_sum_init
 
 
-@pytest.mark.parametrize('failure', [False, True])
-def test_pending_request(port_generator, failure):
+def _assert_pending_value(val: str, runtime_name, port):
+    resp = req.get(f'http://localhost:{port}/')
+    assert resp.status_code == 200
+    assert (
+        f'jina_number_of_pending_requests{{runtime_name="{runtime_name}"}} {val}'
+        in str(resp.content)
+    )
+
+
+@pytest.mark.parametrize('protocol', ['grpc', 'http', 'websocket'])
+@pytest.mark.parametrize('failure_in_executor', [False, True])
+def test_pending_request(port_generator, failure_in_executor, protocol):
     port0 = port_generator()
     port1 = port_generator()
+    runtime_name = (
+        'gateway/rep-0/GRPCGatewayRuntime' if protocol == 'grpc' else 'gateway/rep-0'
+    )
 
     class SlowExecutor(Executor):
         @requests
         def foo(self, docs, **kwargs):
             time.sleep(5)
-            if failure:
+            if failure_in_executor:
                 raise Exception
 
-    with Flow(monitoring=True, port_monitoring=port0).add(
+    with Flow(monitoring=True, port_monitoring=port0, protocol=protocol).add(
         uses=SlowExecutor, port_monitoring=port1
     ) as f:
 
         def _send_request():
             f.search(inputs=DocumentArray.empty(size=1), continue_on_error=True)
 
-        def _assert_pending_value(val: str):
-            resp = req.get(f'http://localhost:{port0}/')
-            assert resp.status_code == 200
-            assert (
-                f'jina_number_of_pending_requests{{runtime_name="gateway/rep-0/GRPCGatewayRuntime"}} {val}'
-                in str(resp.content)
-            )
-
         p_send = multiprocessing.Process(target=_send_request)
-        p_before = multiprocessing.Process(target=_assert_pending_value, args=('0.0',))
-        p_while = multiprocessing.Process(target=_assert_pending_value, args=('1.0',))
+        _assert_pending_value('0.0', runtime_name, port0)
 
-        p_before.start()
-        time.sleep(1)
         p_send.start()
         time.sleep(1)
-        p_while.start()
+        _assert_pending_value('1.0', runtime_name, port0)
 
-        for p in [p_before, p_send, p_while]:
-            p.join()
+        p_send.join()
+        assert p_send.exitcode == 0
+        _assert_pending_value('0.0', runtime_name, port0)
 
-        exitcodes = []
-        for p in [p_before, p_send, p_while]:
-            p.terminate()
-            exitcodes.append(
-                p.exitcode
-            )  # collect the exit codes and assert after all of them have been terminated, to avoid timeouts
 
-        for code in exitcodes:
-            assert code == 0
+def _create_worker_runtime(port, name='', executor=None):
+    args = set_pod_parser().parse_args([])
+    args.port = port
+    args.name = name
+    if executor:
+        args.uses = executor
+    with WorkerRuntime(args) as runtime:
+        runtime.run_forever()
 
-        _assert_pending_value('0.0')
+
+def _create_gateway_runtime(
+    graph_description, pod_addresses, port, port_monitoring, protocol='grpc', retries=-1
+):
+    if protocol == 'http':
+        gateway_runtime = HTTPGatewayRuntime
+    elif protocol == 'websocket':
+        gateway_runtime = WebSocketGatewayRuntime
+    else:
+        gateway_runtime = GRPCGatewayRuntime
+    with gateway_runtime(
+        set_gateway_parser().parse_args(
+            [
+                '--graph-description',
+                graph_description,
+                '--deployments-addresses',
+                pod_addresses,
+                '--port',
+                str(port),
+                '--retries',
+                str(retries),
+                '--monitoring',
+                '--port-monitoring',
+                str(port_monitoring),
+            ]
+        )
+    ) as runtime:
+        runtime.run_forever()
+
+
+def _create_worker(port):
+    # create a single worker runtime
+    p = multiprocessing.Process(target=_create_worker_runtime, args=(port,))
+    p.start()
+    time.sleep(0.1)
+    return p
+
+
+def _create_gateway(port, port_monitoring, graph, pod_addr, protocol, retries=-1):
+    # create a single worker runtime
+    # create a single gateway runtime
+    p = multiprocessing.Process(
+        target=_create_gateway_runtime,
+        args=(graph, pod_addr, port, port_monitoring, protocol, retries),
+    )
+    p.start()
+    time.sleep(0.1)
+    return p
+
+
+def _send_request(gateway_port, protocol):
+    """send request to gateway and see what happens"""
+    from jina.clients import Client
+
+    c = Client(host='localhost', port=gateway_port, protocol=protocol)
+    return c.post(
+        '/foo',
+        inputs=[Document(text='hi') for _ in range(2)],
+        request_size=1,
+        return_responses=True,
+        continue_on_error=True,
+    )
+
+
+@pytest.mark.parametrize('protocol', ['grpc', 'http', 'websocket'])
+def test_pending_requests_with_connection_error(port_generator, protocol):
+    runtime_name = (
+        'gateway/rep-0/GRPCGatewayRuntime' if protocol == 'grpc' else 'gateway/rep-0'
+    )
+    gateway_port = port_generator()
+    worker_port = port_generator()
+    port_monitoring = port_generator()
+    graph_description = '{"start-gateway": ["pod0"], "pod0": ["end-gateway"]}'
+    pod_addresses = f'{{"pod0": ["0.0.0.0:{worker_port}"]}}'
+
+    gateway_process = _create_gateway(
+        gateway_port, port_monitoring, graph_description, pod_addresses, protocol
+    )
+
+    time.sleep(1.0)
+
+    AsyncNewLoopRuntime.wait_for_ready_or_shutdown(
+        timeout=5.0,
+        ctrl_address=f'0.0.0.0:{gateway_port}',
+        ready_or_shutdown_event=multiprocessing.Event(),
+    )
+
+    try:
+        p = multiprocessing.Process(target=_send_request, args=(gateway_port, protocol))
+        p.start()
+        p.join()
+        time.sleep(2)
+        _assert_pending_value('0.0', runtime_name, port_monitoring)
+    except Exception:
+        assert False
+    finally:  # clean up runtimes
+        gateway_process.terminate()
+        gateway_process.join()
