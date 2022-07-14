ec068bcab738cde63615e0f6fe55095f7d705df5
numb3r3 <wangfelix87@gmail.com>
2022-07-14 15:26:20 +0800

    fix: map multiple endpoints to same func

--- jina/serve/executors/__init__.py
+++ jina/serve/executors/__init__.py
@@ -7,6 +7,8 @@ import warnings
 from types import SimpleNamespace
 from typing import TYPE_CHECKING, Any, Dict, Optional, Type, Union
 
+import catch as catch
+
 from jina import __args_executor_init__, __default_endpoint__
 from jina.enums import BetterEnum
 from jina.helper import ArgNamespace, T, iscoroutinefunction, typename
@@ -179,14 +181,14 @@ class BaseExecutor(JAMLCompatible, metaclass=ExecutorType):
     def _add_requests(self, _requests: Optional[Dict]):
         if not hasattr(self, 'requests'):
             self.requests = {}
-
         if _requests:
             func_names = {f.__name__: e for e, f in self.requests.items()}
             for endpoint, func in _requests.items():
                 # the following line must be `getattr(self.__class__, func)` NOT `getattr(self, func)`
                 # this to ensure we always have `_func` as unbound method
                 if func in func_names:
-                    del self.requests[func_names[func]]
+                    if func_names[func] in self.requests:
+                        del self.requests[func_names[func]]
 
                 _func = getattr(self.__class__, func)
                 if callable(_func):
