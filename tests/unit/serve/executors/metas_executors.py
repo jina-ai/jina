064763c2fd398b1dffb95a3bedf51eae40d285ab
numb3r3 <wangfelix87@gmail.com>
2022-07-17 13:05:46 +0800

    fix: test meta executor

--- tests/unit/serve/executors/metas_executors.py
+++ tests/unit/serve/executors/metas_executors.py
@@ -2,10 +2,6 @@ from jina import DocumentArray, Executor, requests
 
 
 class TestExecutor(Executor):
-    def __init__(self, bar, **kwargs):
-        super().__init__(**kwargs)
-        self.bar = bar
-
     @requests
     def process(self, docs: DocumentArray, **kwargs):
         return docs
