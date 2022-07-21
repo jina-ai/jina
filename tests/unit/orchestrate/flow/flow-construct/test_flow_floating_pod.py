c2c896b8d20a4abd69d0cf60913d5e5a9ea44688
Joan Fontanals Martinez <joan.martinez@jina.ai>
2022-07-21 13:35:35 +0200

    ci: divide unit orchestrate flow slow tests

--- tests/unit/orchestrate/flow/flow-construct/test_flow_floating_pod.py
+++ tests/unit/orchestrate/flow/flow-construct/test_flow_floating_pod.py
@@ -3,20 +3,19 @@ import pytest
 from jina import Document, Executor, Flow, requests
 
 
-class MyExecutor(Executor):
-    @requests
-    async def add_text(self, docs, **kwargs):
-        docs[0].text = 'hello'
-
-
 @pytest.mark.parametrize('floating, expect_str', [(True, 'world'), (False, 'hello')])
 def test_floating_pod(floating, expect_str):
+    class MyFloatingExecutor(Executor):
+        @requests
+        async def add_text(self, docs, **kwargs):
+            docs[0].text = 'hello'
+
     f = (
         Flow()
-        .add(replicas=3, name='a1')
-        .add(name='a2', floating=floating, uses=MyExecutor)
-        .add(name='a3')
-        .add(floating=floating, uses=MyExecutor)
+            .add(replicas=3, name='a1')
+            .add(name='a2', floating=floating, uses=MyFloatingExecutor)
+            .add(name='a3')
+            .add(floating=floating, uses=MyFloatingExecutor)
     )
 
     with f:
