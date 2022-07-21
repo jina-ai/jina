d797dc32fa42e25c13fb2790771234c43907fcc6
Joan Fontanals Martinez <joan.martinez@jina.ai>
2022-07-21 13:44:28 +0200

    feat: remove envs from telemetry data

--- jina/serve/helper.py
+++ jina/serve/helper.py
@@ -10,9 +10,9 @@ def _telemetry_run_in_thread(event: str) -> None:
         url = 'https://telemetry.jina.ai/'
         try:
             from jina.helper import get_full_version
-            metas, envs = get_full_version()
+            metas, _ = get_full_version()
             data = base64.urlsafe_b64encode(
-                json.dumps({**metas, **envs, 'event': event}).encode('utf-8')
+                json.dumps({**metas, 'event': event}).encode('utf-8')
             )
             req = urllib.request.Request(
                 url, data=data, headers={'User-Agent': 'Mozilla/5.0'}
