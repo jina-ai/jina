527beb85a62eef9ec239406b986e43fcec28c2e7
AlaeddineAbdessalem <alaeddine-13@live.fr>
2022-08-26 17:03:47 +0100

    feat: bump protobuf version (#5082)
    * feat: add proto compiler version 3.21
    
    * feat: dynamically load the appropriate proto modules
    
    * chore: update configs
    
    * chore: update scripts
    
    * fix: fix serializer import
    
    * fix: restore file structure to keep compatibility
    
    * fix: restore file structure to keep compatibility
    
    * chore: free protobuf version
    
    * ci: install modified docarray version
    
    * style: fix overload and cli autocomplete
    
    * fix: use proper type method
    
    Co-authored-by: Jina Dev Bot <dev-bot@jina.ai>

---
+++ Dockerfiles/protogen-3.21.Dockerfile
@@ -0,0 +1,23 @@
+FROM python:3.7-slim
+
+RUN apt-get update && apt-get install --no-install-recommends -y
+
+WORKDIR /builder/
+
+RUN apt-get install --no-install-recommends -y build-essential git autoconf libtool wget unzip zlib1g-dev pkg-config cmake
+RUN wget https://github.com/protocolbuffers/protobuf/releases/download/v21.5/protoc-21.5-linux-x86_64.zip  -O protobuf.zip \
+    && unzip protobuf.zip && rm protobuf.zip && \
+    cp bin/protoc /usr/local/bin/ && \
+    cp -r include/* /usr/local/include/ && \
+    git clone --depth 1 https://github.com/grpc/grpc.git && \
+    cd grpc && git submodule update --depth 1 --init && \
+    mkdir -p cmake/build && cd cmake/build && cmake ../.. && make -j12
+
+WORKDIR /
+ADD jina ./jina/
+
+WORKDIR /jina/proto
+
+ENTRYPOINT ["bash", "./build-proto.sh", "/builder/grpc/cmake/build/grpc_python_plugin", "pb"]
+
+
--- jina/proto/build-proto.sh
+++ jina/proto/build-proto.sh
@@ -2,16 +2,26 @@
 set -e
 
 # Do NOT use this directly, use jinaai/protogen image
+# use jinaai/protogen:3.21 in order to use compiler version == 21 (creates pb/docarray_pb2.py)
+# and use jinaai/protogen:latest to use compiler version <= 20 (creates pb2/docarray_pb2.py)
+# make sure to use jinaai/protogen:3.21 to avoid overriting the module
 #
 # current dir: jina root (the one with README.md)
 # run the following in bash:
-# docker run -v $(pwd)/jina/:/jina/ jinaai/protogen
+# docker run -v $(pwd)/jina/proto:/jina/proto jinaai/protogen
+# finally, set back owner of the generated files using: sudo chown -R $(id -u ${USER}):$(id -g ${USER}) ./jina/proto
 
 SRC_DIR=./
-SRC_NAME=jina.proto
+MODULE=jina
+SRC_NAME="${MODULE}.proto"
+
+COMP_PROTO_OUT_NAME="${MODULE}_pb2.py"
+COMP_GRPC_OUT_NAME="${MODULE}_pb2_grpc.py"
+OUT_FOLDER="${2:-pb2}/"
+
 VER_FILE=../__init__.py
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 1 ] && [ "$#" -ne 2 ]; then
     echo "Error: Please specify the [PATH_TO_GRPC_PYTHON_PLUGIN], refer more details at " \
       "https://docs.jina.ai/"
     printf "\n"
@@ -25,7 +35,7 @@ PLUGIN_PATH=${1}  # /Volumes/TOSHIBA-4T/Documents/grpc/bins/opt/grpc_python_plug
 
 printf "\e[1;33mgenerating protobuf and grpc python interface\e[0m\n"
 
-protoc -I ${SRC_DIR} --python_out=${SRC_DIR} --grpc_python_out=${SRC_DIR} --plugin=protoc-gen-grpc_python=${PLUGIN_PATH} ${SRC_DIR}${SRC_NAME}
+protoc -I ${SRC_DIR} --python_out="${SRC_DIR}${OUT_FOLDER}" --grpc_python_out="${SRC_DIR}${OUT_FOLDER}" --plugin=protoc-gen-grpc_python=${PLUGIN_PATH} ${SRC_DIR}${SRC_NAME}
 
 printf "\e[1;33mfixing grpc import\e[0m\n"
 printf "using linux sed syntax, if you are running this on mac, you may want to comment out the sed for linux"
@@ -33,8 +43,8 @@ printf "using linux sed syntax, if you are running this on mac, you may want to 
 # for mac
 # sed -i '' -e 's/import\ jina_pb2\ as\ jina__pb2/from\ \.\ import\ jina_pb2\ as\ jina__pb2/' ${SRC_DIR}jina_pb2_grpc.py
 # for linux
-sed -i 's/import\ docarray_pb2/import\ docarray.proto.docarray_pb2/' ${SRC_DIR}jina_pb2.py
-sed -i 's/import\ jina_pb2\ as\ jina__pb2/from\ \.\ import\ serializer\ as\ jina__pb2/' ${SRC_DIR}jina_pb2_grpc.py
+sed -i 's/import\ docarray_pb2/import\ docarray.proto.docarray_pb2/' "${SRC_DIR}${OUT_FOLDER}jina_pb2.py"
+sed -i 's/import\ jina_pb2\ as\ jina__pb2/from\ \.\.\ import\ serializer\ as\ jina__pb2/' "${SRC_DIR}${OUT_FOLDER}jina_pb2_grpc.py"
 
 OLDVER=$(sed -n 's/^__proto_version__ = '\''\(.*\)'\''$/\1/p' $VER_FILE)
 printf "current proto version:\t\e[1;33m$OLDVER\e[0m\n"
@@ -46,6 +56,8 @@ printf "bump proto version to:\t\e[1;32m$NEWVER\e[0m\n"
 # sed -i '' -e 's/^__proto_version__ = '\''\(.*\)'\''/__proto_version__ = '\'"$NEWVER"\''/' $VER_FILE
 # for linux
 sed -i 's/^__proto_version__ = '\''\(.*\)'\''/__proto_version__ = '\'"$NEWVER"\''/' $VER_FILE
+
+
 
 printf "\e[1;32mAll done!\e[0m\n"
 printf "if you are running this inside Docker container, you may manually bump proto version to:\t\e[1;32m$NEWVER\e[0m\n"
