c6f8cafa1322df349997a120d4f9f0cd0bef483a
Joan Fontanals Martinez <joan.martinez@jina.ai>
2022-09-16 13:24:53 +0200

    refactor: continue on error affect aiorpc error

--- jina/clients/base/grpc.py
+++ jina/clients/base/grpc.py
@@ -73,6 +73,7 @@ class GRPCBaseClient(BaseClient):
 
             self.inputs = inputs
             req_iter = self._get_requests(**kwargs)
+            continue_on_error = self.continue_on_error
             async with GrpcConnectionPool.get_grpc_channel(
                 f'{self.args.host}:{self.args.port}',
                 asyncio=True,
@@ -84,58 +85,64 @@ class GRPCBaseClient(BaseClient):
                 with ProgressBar(
                     total_length=self._inputs_length, disable=not self.show_progress
                 ) as p_bar:
-                    async for resp in stub.Call(
-                        req_iter,
-                        compression=self.compression,
-                        metadata=kwargs.get('metadata', None),
-                        credentials=kwargs.get('credentials', None),
-                        timeout=kwargs.get('timeout', None),
-                    ):
-                        callback_exec(
-                            response=resp,
-                            on_error=on_error,
-                            on_done=on_done,
-                            on_always=on_always,
-                            continue_on_error=self.continue_on_error,
-                            logger=self.logger,
-                        )
-                        if self.show_progress:
-                            p_bar.update()
-                        yield resp
+                    try:
+                        async for resp in stub.Call(
+                            req_iter,
+                            compression=self.compression,
+                            metadata=kwargs.get('metadata', None),
+                            credentials=kwargs.get('credentials', None),
+                            timeout=kwargs.get('timeout', None),
+                        ):
+                            callback_exec(
+                                response=resp,
+                                on_error=on_error,
+                                on_done=on_done,
+                                on_always=on_always,
+                                continue_on_error=continue_on_error,
+                                logger=self.logger,
+                            )
+                            if self.show_progress:
+                                p_bar.update()
+                            yield resp
+                    except (grpc.aio._call.AioRpcError, InternalNetworkError) as err:
+                        my_code = err.code()
+                        my_details = err.details()
+                        msg = f'gRPC error: {my_code} {my_details}'
+
+                        if my_code == grpc.StatusCode.UNAVAILABLE:
+                            self.logger.error(
+                                f'{msg}\nThe ongoing request is terminated as the server is not available or closed already.'
+                            )
+                            if not continue_on_error:
+                                raise ConnectionError(my_details) from None
+                        elif my_code == grpc.StatusCode.DEADLINE_EXCEEDED:
+                            self.logger.error(
+                                f'{msg}\nThe ongoing request is terminated due to a server-side timeout.'
+                            )
+                            if not continue_on_error:
+                                raise ConnectionError(my_details) from None
+                        elif my_code == grpc.StatusCode.INTERNAL:
+                            self.logger.error(f'{msg}\ninternal error on the server side')
+                            if not continue_on_error:
+                                raise err
+                        elif (
+                                my_code == grpc.StatusCode.UNKNOWN
+                                and 'asyncio.exceptions.TimeoutError' in my_details
+                        ):
+                            if not continue_on_error:
+                                raise BadClientInput(
+                                    f'{msg}\n'
+                                    'often the case is that you define/send a bad input iterator to jina, '
+                                    'please double check your input iterator'
+                                ) from err
+                        else:
+                            if not continue_on_error:
+                                raise BadServerFlow(msg) from err
 
         except KeyboardInterrupt:
             self.logger.warning('user cancel the process')
         except asyncio.CancelledError as ex:
             self.logger.warning(f'process error: {ex!r}')
-        except (grpc.aio._call.AioRpcError, InternalNetworkError) as err:
-            my_code = err.code()
-            my_details = err.details()
-            msg = f'gRPC error: {my_code} {my_details}'
-
-            if my_code == grpc.StatusCode.UNAVAILABLE:
-                self.logger.error(
-                    f'{msg}\nThe ongoing request is terminated as the server is not available or closed already.'
-                )
-                raise ConnectionError(my_details) from None
-            elif my_code == grpc.StatusCode.DEADLINE_EXCEEDED:
-                self.logger.error(
-                    f'{msg}\nThe ongoing request is terminated due to a server-side timeout.'
-                )
-                raise ConnectionError(my_details) from None
-            elif my_code == grpc.StatusCode.INTERNAL:
-                self.logger.error(f'{msg}\ninternal error on the server side')
-                raise err
-            elif (
-                my_code == grpc.StatusCode.UNKNOWN
-                and 'asyncio.exceptions.TimeoutError' in my_details
-            ):
-                raise BadClientInput(
-                    f'{msg}\n'
-                    'often the case is that you define/send a bad input iterator to jina, '
-                    'please double check your input iterator'
-                ) from err
-            else:
-                raise BadServerFlow(msg) from err
         except:
             # Not sure why, adding this line helps in fixing a hanging test
             raise
