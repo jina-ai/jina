60be04429328a5a16096aa4b974973294254cfeb
Girish Chandrashekar <girish.chandrashekar@jina.ai>
2022-09-27 14:45:04 +0200

    test(instrumentation): document spans in the grpc and http gateway instrumentation

--- tests/unit/serve/runtimes/gateway/grpc/test_instrumentation.py
+++ tests/unit/serve/runtimes/gateway/grpc/test_instrumentation.py
@@ -15,6 +15,12 @@ class TestGrpcGatewayTracing(InstrumentationTestBase):
     def tearDown(self):
         super().tearDown()
 
+    @staticmethod
+    def print_spans(spans):
+        for span in spans:
+            print(f'--->span: {span}')
+        print()
+
     def test_http_span_attributes_default_args(self):
         f = Flow(protocol='grpc').add()
 
@@ -28,8 +34,22 @@ class TestGrpcGatewayTracing(InstrumentationTestBase):
             )
             # give some time for the tracing and metrics exporters to finish exporting.
             time.sleep(1)
-
-            self.assertEqual(5, len(self.get_finished_spans()))
+            (
+                server_spans,
+                client_spans,
+                internal_spans,
+            ) = self.partition_spans_by_kind()
+            self.assertEqual(0, len(server_spans))
+            # There are currently 5 SpanKind.CLIENT spans produced by the
+            # a. /grpc.reflection.v1alpha.ServerReflection/ServerReflectionInfo (2)
+            # b. /jina.JinaDiscoverEndpointsRPC/endpoint_discovery (1)
+            # c. /jina.JinaSingleDataRequestRPC/process_single_data (1)
+            # d. /jina.JinaRPC/Call (1)
+            # that cannot yet be completely disabled because the GrpcConnectionPool.get_grpc_channel
+            # method is a static method. This means that until a concrete '.set_tracer_provider' gets executed, the tracer will
+            # be a default ProxyTracerProvider.
+            self.assertEqual(5, len(client_spans))
+            self.assertEqual(0, len(internal_spans))
 
     def test_http_span_attributes_with_executor(self):
         f = Flow(
@@ -52,6 +72,8 @@ class TestGrpcGatewayTracing(InstrumentationTestBase):
                 client_spans,
                 internal_spans,
             ) = self.partition_spans_by_kind()
+
+            # There only 1 dummy span created in the Executor method
             self.assertEqual(1, len(internal_spans))
             for internal_span in internal_spans:
                 if internal_span.get('name', '') == 'dummy':
@@ -59,8 +81,10 @@ class TestGrpcGatewayTracing(InstrumentationTestBase):
                         len(self.docs_input), internal_span['attributes']['len_docs']
                     )
 
-            self.assertEqual(5, len(server_spans))
+            # The 5 spans are as described in the above test.
             self.assertEqual(5, len(client_spans))
+            # The SpanKind.SERVER spans for each of the above 5 SpanKind.CLIENT requests
+            self.assertEqual(5, len(server_spans))
             self.assertEqual(11, len(self.get_finished_spans()))
 
             self.assertGreater(
--- tests/unit/serve/runtimes/gateway/http/test_instrumentation.py
+++ tests/unit/serve/runtimes/gateway/http/test_instrumentation.py
@@ -4,7 +4,6 @@ import requests as req
 
 from jina import Flow
 from tests.unit.serve.runtimes.gateway import (
-    CustomMetricExporter,
     ExecutorTestWithTracing,
     InstrumentationTestBase,
 )
@@ -28,13 +27,27 @@ class TestHttpGatewayTracing(InstrumentationTestBase):
             )
             # give some time for the tracing and metrics exporters to finish exporting.
             time.sleep(1)
-
+            (
+                server_spans,
+                client_spans,
+                internal_spans,
+            ) = self.partition_spans_by_kind()
+            self.assertEqual(0, len(server_spans))
+            # There are currently 4 SpanKind.CLIENT spans produced by the
+            # a. /grpc.reflection.v1alpha.ServerReflection/ServerReflectionInfo (2)
+            # b. /jina.JinaDiscoverEndpointsRPC/endpoint_discovery (1)
+            # c. /jina.JinaSingleDataRequestRPC/process_single_data (1)
+            # that cannot yet be completely disabled because the GrpcConnectionPool.get_grpc_channel
+            # method is a static method. This means that until a concrete '.set_tracer_provider' gets executed, the tracer will
+            # be a default ProxyTracerProvider.
+            self.assertEqual(4, len(client_spans))
+            self.assertEqual(0, len(internal_spans))
             self.assertEqual(4, len(self.get_finished_spans()))
 
     def test_http_span_attributes_with_executor(self):
         f = Flow(
             protocol='http', opentelemetry_tracing=True, opentelemetry_metrics=True
-        ).add(uses=ExecutorTestWithTracing)
+        ).add(uses=ExecutorTestWithTracing, name='executortest')
 
         with f:
             req.post(
@@ -49,6 +62,7 @@ class TestHttpGatewayTracing(InstrumentationTestBase):
                 client_spans,
                 internal_spans,
             ) = self.partition_spans_by_kind()
+
             self.assertEqual(4, len(internal_spans))
             for internal_span in internal_spans:
                 if internal_span.get('name', '') == 'dummy':
@@ -56,8 +70,18 @@ class TestHttpGatewayTracing(InstrumentationTestBase):
                         len(self.docs_input), internal_span['attributes']['len_docs']
                     )
 
-            self.assertEqual(5, len(server_spans))
+            # There are the usual 4 SpanKind.CLIENT spans as mentioned above.
             self.assertEqual(4, len(client_spans))
+            # There are 5 total spans. The CLIENT spans from the above are being traced correctly by the server. Apart
+            # from the spans mentioned in te above test, there is an extra span (expected) for the '/index'
+            # request from the client that is handled by the gateway http server>
+            self.assertEqual(5, len(server_spans))
+            # The FASTApi app instrumentation tracks the server spans as SpanKind.INTERNAL. There are 4 spans for:
+            # 1. /index http receive
+            # 2. dummy operation in the Executor method
+            # 3. /index http send
+            # 4. /index http send
+            self.assertEqual(4, len(internal_spans))
             self.assertEqual(13, len(self.get_finished_spans()))
 
             self.assertGreater(
