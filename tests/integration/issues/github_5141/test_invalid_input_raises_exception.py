7378d91c95396b04c3f9e98dc4fce723b5a0f7c5
Joan Fontanals Martinez <joan.martinez@jina.ai>
2022-10-07 11:17:00 +0200

    fix: fix raise proper exception

--- tests/integration/issues/github_5141/test_invalid_input_raises_exception.py
+++ tests/integration/issues/github_5141/test_invalid_input_raises_exception.py
@@ -16,7 +16,7 @@ def test_invalid_input_raise(protocol):
             da = DocumentArray([Document(text='hello', tags={'date': datetime.now()})])
             try:
                 f.post(on='/', inputs=da)  # process should stop here and raise an exception
-            except:
+            except Exception:
                 raise MyOwnException
             assert False
     except MyOwnException:
