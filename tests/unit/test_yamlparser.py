4a6b21f8a11965f51345af29cf1006347f2312ec
Girish Chandrashekar <girish.chandrashekar@jina.ai>
2022-10-10 15:27:15 +0200

    fix: remove redundant env var in tests

--- tests/unit/test_yamlparser.py
+++ tests/unit/test_yamlparser.py
@@ -121,13 +121,11 @@ def set_test_encoder_env_var():
 
 
 def test_encoder_name_env_replace(set_test_encoder_env_var):
-    os.environ['BE_TEST_NAME'] = 'hello123'
     with BaseExecutor.load_config('yaml/test-encoder-env.yml') as be:
         assert be.metas.name == 'hello123'
 
 
 def test_encoder_name_dict_replace(set_test_encoder_env_var):
-    os.environ['BE_TEST_NAME'] = 'hello123'
     d = {'BE_TEST_NAME': 'hello123'}
     with BaseExecutor.load_config('yaml/test-encoder-env.yml', context=d) as be:
         assert be.metas.name == 'hello123'
