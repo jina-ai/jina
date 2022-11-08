81c1e343d0569ca2c9a6d91e4265abbacc501375
Jackmin801 <ongjackm@gmail.com>
2022-11-08 17:26:59 +0800

    test: move assertions out of context manager

--- tests/unit/serve/instrumentation/test_gateway_metric_labels.py
+++ tests/unit/serve/instrumentation/test_gateway_metric_labels.py
@@ -34,84 +34,84 @@ def test_gateway_metric_labels(monkeypatch_metric_exporter):
             i['name']: i['data']['data_points'] for i in gateway_metrics
         }
 
-        assert (
-            'address'
-            in gateway_metric_data_point['jina_sending_request_seconds'][0][
-                'attributes'
-            ]
-        )
-        assert (
-            'address'
-            in gateway_metric_data_point['jina_sent_request_bytes'][0]['attributes']
-        )
-        assert (
-            'address'
-            in gateway_metric_data_point['jina_received_response_bytes'][0][
-                'attributes'
-            ]
-        )
-        assert (
-            'address'
-            in gateway_metric_data_point['jina_sending_request_seconds'][1][
-                'attributes'
-            ]
-        )
-        assert (
-            'address'
-            in gateway_metric_data_point['jina_sent_request_bytes'][1]['attributes']
-        )
-        assert (
-            'address'
-            in gateway_metric_data_point['jina_received_response_bytes'][1][
-                'attributes'
-            ]
-        )
+    assert (
+        'address'
+        in gateway_metric_data_point['jina_sending_request_seconds'][0][
+            'attributes'
+        ]
+    )
+    assert (
+        'address'
+        in gateway_metric_data_point['jina_sent_request_bytes'][0]['attributes']
+    )
+    assert (
+        'address'
+        in gateway_metric_data_point['jina_received_response_bytes'][0][
+            'attributes'
+        ]
+    )
+    assert (
+        'address'
+        in gateway_metric_data_point['jina_sending_request_seconds'][1][
+            'attributes'
+        ]
+    )
+    assert (
+        'address'
+        in gateway_metric_data_point['jina_sent_request_bytes'][1]['attributes']
+    )
+    assert (
+        'address'
+        in gateway_metric_data_point['jina_received_response_bytes'][1][
+            'attributes'
+        ]
+    )
 
-        assert (
-            'deployment'
-            in gateway_metric_data_point['jina_sending_request_seconds'][0][
-                'attributes'
-            ]
-        )
-        assert (
-            'deployment'
-            in gateway_metric_data_point['jina_sent_request_bytes'][0]['attributes']
-        )
-        assert (
-            'deployment'
-            in gateway_metric_data_point['jina_received_response_bytes'][0][
-                'attributes'
-            ]
-        )
-        assert (
-            'deployment'
-            in gateway_metric_data_point['jina_sending_request_seconds'][1][
-                'attributes'
-            ]
-        )
-        assert (
-            'deployment'
-            in gateway_metric_data_point['jina_sent_request_bytes'][1]['attributes']
-        )
-        assert (
-            'deployment'
-            in gateway_metric_data_point['jina_received_response_bytes'][1][
-                'attributes'
-            ]
-        )
+    assert (
+        'deployment'
+        in gateway_metric_data_point['jina_sending_request_seconds'][0][
+            'attributes'
+        ]
+    )
+    assert (
+        'deployment'
+        in gateway_metric_data_point['jina_sent_request_bytes'][0]['attributes']
+    )
+    assert (
+        'deployment'
+        in gateway_metric_data_point['jina_received_response_bytes'][0][
+            'attributes'
+        ]
+    )
+    assert (
+        'deployment'
+        in gateway_metric_data_point['jina_sending_request_seconds'][1][
+            'attributes'
+        ]
+    )
+    assert (
+        'deployment'
+        in gateway_metric_data_point['jina_sent_request_bytes'][1]['attributes']
+    )
+    assert (
+        'deployment'
+        in gateway_metric_data_point['jina_received_response_bytes'][1][
+            'attributes'
+        ]
+    )
 
-        assert {'first_exec', 'second_exec'} == {
-            i['attributes']['deployment']
-            for i in gateway_metric_data_point['jina_received_response_bytes']
-        }
-        assert {'first_exec', 'second_exec'} == {
-            i['attributes']['deployment']
-            for i in gateway_metric_data_point['jina_sent_request_bytes']
-        }
-        assert {'first_exec', 'second_exec'} == {
-            i['attributes']['deployment']
-            for i in gateway_metric_data_point['jina_sending_request_seconds']
-        }
+    assert {'first_exec', 'second_exec'} == {
+        i['attributes']['deployment']
+        for i in gateway_metric_data_point['jina_received_response_bytes']
+    }
+    assert {'first_exec', 'second_exec'} == {
+        i['attributes']['deployment']
+        for i in gateway_metric_data_point['jina_sent_request_bytes']
+    }
+    assert {'first_exec', 'second_exec'} == {
+        i['attributes']['deployment']
+        for i in gateway_metric_data_point['jina_sending_request_seconds']
+    }
 
 
 def test_merge_with_no_reduce(monkeypatch_metric_exporter):
@@ -141,15 +141,15 @@ def test_merge_with_no_reduce(monkeypatch_metric_exporter):
             i['name']: i['data']['data_points'] for i in gateway_metrics
         }
 
-        assert {'name1', 'name2', 'name3'} == {
-            i['attributes']['deployment']
-            for i in gateway_metric_data_point['jina_received_response_bytes']
-        }
-        assert {'name1', 'name2', 'name3'} == {
-            i['attributes']['deployment']
-            for i in gateway_metric_data_point['jina_sent_request_bytes']
-        }
-        assert {'name1', 'name2', 'name3'} == {
-            i['attributes']['deployment']
-            for i in gateway_metric_data_point['jina_sending_request_seconds']
-        }
+    assert {'name1', 'name2', 'name3'} == {
+        i['attributes']['deployment']
+        for i in gateway_metric_data_point['jina_received_response_bytes']
+    }
+    assert {'name1', 'name2', 'name3'} == {
+        i['attributes']['deployment']
+        for i in gateway_metric_data_point['jina_sent_request_bytes']
+    }
+    assert {'name1', 'name2', 'name3'} == {
+        i['attributes']['deployment']
+        for i in gateway_metric_data_point['jina_sending_request_seconds']
+    }
