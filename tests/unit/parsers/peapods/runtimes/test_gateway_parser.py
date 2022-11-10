b10eacd628c8865e077fd7ae7328d7aa081b1d8e
Alaeddine Abdessalem <alaeddine-13@live.fr>
2022-11-10 10:15:05 +0100

    test: cover gateway parser output

---
+++ tests/unit/parsers/peapods/runtimes/test_gateway_parser.py
@@ -0,0 +1,33 @@
+import pytest
+
+from jina.enums import GatewayProtocolType
+from jina.helper import ArgNamespace
+from jina.parsers import set_gateway_parser
+
+# TODO: make sure this file is covered in CI
+
+
+@pytest.mark.parametrize(
+    'port,expected_port',
+    [
+        ('12345', ['12345']),
+        ([12345], ['12345']),
+        ([12345, 12344], ['12345', '12344']),
+    ],
+)
+@pytest.mark.parametrize(
+    'protocol,expected_protocol',
+    [
+        ('http', [GatewayProtocolType.HTTP]),
+        (['GRPC'], [GatewayProtocolType.GRPC]),
+        (['grpc', 'http'], [GatewayProtocolType.GRPC, GatewayProtocolType.HTTP]),
+    ],
+)
+def test_multiple_port_protocol_gateway_kwargs(
+    port, protocol, expected_port, expected_protocol
+):
+    args = ArgNamespace.kwargs2namespace(
+        {'port': port, 'protocol': protocol}, set_gateway_parser()
+    )
+    assert args.port == expected_port
+    assert args.protocol == expected_protocol
