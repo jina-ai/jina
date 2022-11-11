b44d767f22bd862cdb75926ba388c14f5db0323c
Joan Fontanals <joan.martinez@jina.ai>
2022-11-11 16:15:28 +0100

    fix: fix bug inheritance, requests nested dict (#5380)

--- jina/serve/executors/__init__.py
+++ jina/serve/executors/__init__.py
@@ -3,6 +3,7 @@ import inspect
 import multiprocessing
 import os
 import threading
+import copy
 import warnings
 from types import SimpleNamespace
 from typing import TYPE_CHECKING, Any, Dict, Optional, Type, Union
@@ -13,12 +14,12 @@ from jina.helper import ArgNamespace, T, iscoroutinefunction, typename
 from jina.importer import ImportExtensions
 from jina.jaml import JAML, JAMLCompatible, env_var_regex, internal_var_regex
 from jina.logging.logger import JinaLogger
-from jina.serve.executors.decorators import avoid_concurrent_lock_cls
+from jina.serve.executors.decorators import avoid_concurrent_lock_cls, _init_requests_by_class
 from jina.serve.executors.metas import get_executor_taboo
 from jina.serve.helper import store_init_kwargs, wrap_func
 from jina.serve.instrumentation import MetricsTimer
 
-if TYPE_CHECKING: # pragma: no cover
+if TYPE_CHECKING:  # pragma: no cover
     from opentelemetry.context.context import Context
 
 __dry_run_endpoint__ = '_jina_dry_run_'
@@ -37,6 +38,8 @@ class ExecutorType(type(JAMLCompatible), type):
         :return: Executor class
         """
         _cls = super().__new__(cls, *args, **kwargs)
+        # this needs to be here, in the case where Executors inherited do not define new `requests`
+        _init_requests_by_class(_cls)
         return cls.register_class(_cls)
 
     @staticmethod
@@ -47,7 +50,6 @@ class ExecutorType(type(JAMLCompatible), type):
         :param cls: The class.
         :return: The class, after being registered.
         """
-
         reg_cls_set = getattr(cls, '_registered_class', set())
 
         cls_id = f'{cls.__module__}.{cls.__name__}'
@@ -55,7 +57,7 @@ class ExecutorType(type(JAMLCompatible), type):
             arg_spec = inspect.getfullargspec(cls.__init__)
 
             if not arg_spec.varkw and not __args_executor_init__.issubset(
-                arg_spec.args
+                    arg_spec.args
             ):
                 raise TypeError(
                     f'{cls.__init__} does not follow the full signature of `Executor.__init__`, '
@@ -115,12 +117,12 @@ class BaseExecutor(JAMLCompatible, metaclass=ExecutorType):
     """
 
     def __init__(
-        self,
-        metas: Optional[Dict] = None,
-        requests: Optional[Dict] = None,
-        runtime_args: Optional[Dict] = None,
-        workspace: Optional[str] = None,
-        **kwargs,
+            self,
+            metas: Optional[Dict] = None,
+            requests: Optional[Dict] = None,
+            runtime_args: Optional[Dict] = None,
+            workspace: Optional[str] = None,
+            **kwargs,
     ):
         """`metas` and `requests` are always auto-filled with values from YAML config.
 
@@ -158,12 +160,12 @@ class BaseExecutor(JAMLCompatible, metaclass=ExecutorType):
 
     def _init_monitoring(self):
         if (
-            hasattr(self.runtime_args, 'metrics_registry')
-            and self.runtime_args.metrics_registry
+                hasattr(self.runtime_args, 'metrics_registry')
+                and self.runtime_args.metrics_registry
         ):
             with ImportExtensions(
-                required=True,
-                help_text='You need to install the `prometheus_client` to use the montitoring functionality of jina',
+                    required=True,
+                    help_text='You need to install the `prometheus_client` to use the montitoring functionality of jina',
             ):
                 from prometheus_client import Summary
 
@@ -214,9 +216,25 @@ class BaseExecutor(JAMLCompatible, metaclass=ExecutorType):
             self.meter_provider = None
             self.meter = None
 
+    @property
+    def requests(self):
+        """
+        Get the request dictionary corresponding to this specific class
+
+        :return: Returns the requests corresponding to the specific Executor instance class
+        """
+        if hasattr(self, '_requests'):
+            return self._requests
+        else:
+            if not hasattr(self, 'requests_by_class'):
+                self.requests_by_class = {}
+            if self.__class__.__name__ not in self.requests_by_class:
+                self.requests_by_class[self.__class__.__name__] = {}
+            # we need to copy so that different instances with different (requests) in input do not disturb one another
+            self._requests = copy.copy(self.requests_by_class[self.__class__.__name__])
+            return self._requests
+
     def _add_requests(self, _requests: Optional[Dict]):
-        if not hasattr(self, 'requests'):
-            self.requests = {}
         if _requests:
             func_names = {f.__name__: e for e, f in self.requests.items()}
             for endpoint, func in _requests.items():
@@ -278,7 +296,7 @@ class BaseExecutor(JAMLCompatible, metaclass=ExecutorType):
                 if not hasattr(target, k):
                     if isinstance(v, str):
                         if not (
-                            env_var_regex.findall(v) or internal_var_regex.findall(v)
+                                env_var_regex.findall(v) or internal_var_regex.findall(v)
                         ):
                             setattr(target, k, v)
                         else:
@@ -323,16 +341,17 @@ class BaseExecutor(JAMLCompatible, metaclass=ExecutorType):
         # noqa: DAR102
         # noqa: DAR201
         """
+
         if req_endpoint in self.requests:
             return await self.__acall_endpoint__(req_endpoint, **kwargs)
         elif __default_endpoint__ in self.requests:
             return await self.__acall_endpoint__(__default_endpoint__, **kwargs)
 
     async def __acall_endpoint__(
-        self, req_endpoint, tracing_context: Optional['Context'], **kwargs
+            self, req_endpoint, tracing_context: Optional['Context'], **kwargs
     ):
         async def exec_func(
-            summary, histogram, histogram_metric_labels, tracing_context
+                summary, histogram, histogram_metric_labels, tracing_context
         ):
             with MetricsTimer(summary, histogram, histogram_metric_labels):
                 if iscoroutinefunction(func):
@@ -390,10 +409,10 @@ class BaseExecutor(JAMLCompatible, metaclass=ExecutorType):
         :return: returns the workspace of the current shard of this Executor.
         """
         workspace = (
-            getattr(self.runtime_args, 'workspace', None)
-            or getattr(self.metas, 'workspace')
-            or self._init_workspace
-            or __cache_path__
+                getattr(self.runtime_args, 'workspace', None)
+                or getattr(self.metas, 'workspace')
+                or self._init_workspace
+                or __cache_path__
         )
         if workspace:
             complete_workspace = os.path.join(workspace, self.metas.name)
@@ -416,13 +435,13 @@ class BaseExecutor(JAMLCompatible, metaclass=ExecutorType):
 
     @classmethod
     def from_hub(
-        cls: Type[T],
-        uri: str,
-        context: Optional[Dict[str, Any]] = None,
-        uses_with: Optional[Dict] = None,
-        uses_metas: Optional[Dict] = None,
-        uses_requests: Optional[Dict] = None,
-        **kwargs,
+            cls: Type[T],
+            uri: str,
+            context: Optional[Dict[str, Any]] = None,
+            uses_with: Optional[Dict] = None,
+            uses_metas: Optional[Dict] = None,
+            uses_requests: Optional[Dict] = None,
+            **kwargs,
     ) -> T:
         """Construct an Executor from Hub.
 
@@ -474,12 +493,12 @@ class BaseExecutor(JAMLCompatible, metaclass=ExecutorType):
 
     @classmethod
     def serve(
-        cls,
-        uses_with: Optional[Dict] = None,
-        uses_metas: Optional[Dict] = None,
-        uses_requests: Optional[Dict] = None,
-        stop_event: Optional[Union[threading.Event, multiprocessing.Event]] = None,
-        **kwargs,
+            cls,
+            uses_with: Optional[Dict] = None,
+            uses_metas: Optional[Dict] = None,
+            uses_requests: Optional[Dict] = None,
+            stop_event: Optional[Union[threading.Event, multiprocessing.Event]] = None,
+            **kwargs,
     ):
         """Serve this Executor in a temporary Flow. Useful in testing an Executor in remote settings.
 
@@ -512,16 +531,16 @@ class BaseExecutor(JAMLCompatible, metaclass=ExecutorType):
 
     @staticmethod
     def to_kubernetes_yaml(
-        uses: str,
-        output_base_path: str,
-        k8s_namespace: Optional[str] = None,
-        executor_type: Optional[
-            StandaloneExecutorType
-        ] = StandaloneExecutorType.EXTERNAL,
-        uses_with: Optional[Dict] = None,
-        uses_metas: Optional[Dict] = None,
-        uses_requests: Optional[Dict] = None,
-        **kwargs,
+            uses: str,
+            output_base_path: str,
+            k8s_namespace: Optional[str] = None,
+            executor_type: Optional[
+                StandaloneExecutorType
+            ] = StandaloneExecutorType.EXTERNAL,
+            uses_with: Optional[Dict] = None,
+            uses_metas: Optional[Dict] = None,
+            uses_requests: Optional[Dict] = None,
+            **kwargs,
     ):
         """
         Converts the Executor into a set of yaml deployments to deploy in Kubernetes.
@@ -549,23 +568,23 @@ class BaseExecutor(JAMLCompatible, metaclass=ExecutorType):
             output_base_path=output_base_path,
             k8s_namespace=k8s_namespace,
             include_gateway=executor_type
-            == BaseExecutor.StandaloneExecutorType.EXTERNAL,
+                            == BaseExecutor.StandaloneExecutorType.EXTERNAL,
         )
 
     to_k8s_yaml = to_kubernetes_yaml
 
     @staticmethod
     def to_docker_compose_yaml(
-        uses: str,
-        output_path: Optional[str] = None,
-        network_name: Optional[str] = None,
-        executor_type: Optional[
-            StandaloneExecutorType
-        ] = StandaloneExecutorType.EXTERNAL,
-        uses_with: Optional[Dict] = None,
-        uses_metas: Optional[Dict] = None,
-        uses_requests: Optional[Dict] = None,
-        **kwargs,
+            uses: str,
+            output_path: Optional[str] = None,
+            network_name: Optional[str] = None,
+            executor_type: Optional[
+                StandaloneExecutorType
+            ] = StandaloneExecutorType.EXTERNAL,
+            uses_with: Optional[Dict] = None,
+            uses_metas: Optional[Dict] = None,
+            uses_requests: Optional[Dict] = None,
+            **kwargs,
     ):
         """
         Converts the Executor into a yaml file to run with `docker-compose up`
@@ -590,11 +609,11 @@ class BaseExecutor(JAMLCompatible, metaclass=ExecutorType):
             output_path=output_path,
             network_name=network_name,
             include_gateway=executor_type
-            == BaseExecutor.StandaloneExecutorType.EXTERNAL,
+                            == BaseExecutor.StandaloneExecutorType.EXTERNAL,
         )
 
     def monitor(
-        self, name: Optional[str] = None, documentation: Optional[str] = None
+            self, name: Optional[str] = None, documentation: Optional[str] = None
     ) -> Optional[MetricsTimer]:
         """
         Get a given prometheus metric, if it does not exist yet, it will create it and store it in a buffer.
