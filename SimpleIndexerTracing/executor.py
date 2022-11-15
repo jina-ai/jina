3197db6a7b1f30afc8a4e6288adfee5d1247f687
Girish Chandrashekar <girish.chandrashekar@jina.ai>
2022-11-15 09:35:23 +0100

    fix: revert debug code

--- SimpleIndexerTracing/executor.py
+++
@@ -1,9 +0,0 @@
-from jina import DocumentArray, Executor, requests
-
-
-class SimpleIndexerTracing(Executor):
-    """"""
-
-    @requests
-    def foo(self, docs: DocumentArray, **kwargs):
-        pass
