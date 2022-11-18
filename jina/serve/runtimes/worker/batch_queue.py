5956a29785770e6cee0b64e594e097388b578156
Jackmin801 <ongjackm@gmail.com>
2022-11-18 22:50:03 +0800

    feat: add timeout mechanism in batch queue
    Co-authored-by: Johannes Messner <messnerjo@gmail.com>

--- jina/serve/runtimes/worker/batch_queue.py
+++ jina/serve/runtimes/worker/batch_queue.py
@@ -1,4 +1,6 @@
-from typing import List, Dict
+import asyncio
+from asyncio import Event
+from typing import List, Dict, Any
 
 from jina import DocumentArray
 from jina.serve.executors import BaseExecutor
@@ -7,14 +9,20 @@ from jina.types.request.data import DataRequest
 # TODO: Make sure we dont deserialize the request twice
 # TODO: SIGINT/SIGTEM: Flush
 
+async def sleep_then_set(time_seconds: int, event: Event):
+    """Sleep for time_seconds and then set the event"""
+    asyncio.sleep(time_seconds)
+    event.set()
+
 class BatchQueue():
     """A batch queue that holds the data request and the executor."""
 
-    def __init__(self, executor: BaseExecutor, exec_endpoint: str, preferred_batch_size: int=4, timeout: int=10_000) -> None:
+    def __init__(self, executor: BaseExecutor, exec_endpoint: str, args: Any, preferred_batch_size: int=4, timeout: int=10_000) -> None:
         self._preferred_batch_size: int = preferred_batch_size
         self._timeout: int = timeout
         self._executor: BaseExecutor = executor
         self._exec_endpoint: str = exec_endpoint
+        self.args = args
 
         self._reset()
     
@@ -25,11 +33,38 @@ class BatchQueue():
         self.__repr__()
     
     def _reset(self) -> None:
+        """Set all events and reset the batch queue."""
+        if getattr(self, '_events', None) is not None:
+            [i.set() for i in self._events]
+        self._events: List[Event] = []
         self._requests: List[DataRequest] = []
         self._request_lens: List[int] = []
         self._big_doc: DocumentArray = DocumentArray.empty()
+
+        self._flush_trigger: Event = Event()
+        asyncio.create_task(self.flush(self._flush_trigger))
+        self._timer_started = False
     
-    async def push(self, request: DataRequest) -> None:
+    def _start_timer(self):
+        asyncio.create_task(sleep_then_set(self._timeout, self._flush_trigger))
+        self._timer_started = True
+    
+    def _reset_timeout(self) -> None:
+        """Reset the timeout event."""
+        self._timeout_event.set()
+        self._timeout_event = Event()
+    
+    async def push(self, request: DataRequest, event: Event) -> None:
+        """Append request to the queue. Once the request has been processed, the event will be set.
+        
+        :param request: The request to append to the queue.
+        :param event: The event that will be set once the request has been processed.
+        """
+        # TODO: What if timer ends and triggers the flush in the middle of this function? Is that possible?
+        # This function is blocking and thus should be atomic right?
+        if not self._timer_started:
+            self._start_timer()
+
         docs = BatchQueue.get_docs_from_request(
             [request],
             field='docs',
@@ -38,40 +73,37 @@ class BatchQueue():
         self._big_doc.extend(docs)
         self._requests.append(request)
         self._request_lens.append(len(docs))
+        self._events.append(event)
 
         if len(self._big_doc) > self._preferred_batch_size:
-            await self.flush()
+            self._flush_trigger.set()
     
-    async def flush(self) -> None:
-        # executor logic
-        # TODO: What if they have different params? Should we disallow params?
-        # params = self._parse_params(requests[0].parameters, self._executor.metas.name)
+    async def flush(self, trigger_event: Event) -> None:
+        """Process all requests in the queue once event is set."""
+        await trigger_event.wait()
+        # We need to get the executor to process the big doc
         return_data = await self._executor.__acall__(
             req_endpoint=self._exec_endpoint,
-            docs=docs,
+            docs=self._big_doc,
             parameters={},
-            #docs_matrix=BatchQueue.get_docs_matrix_from_request(
-                #requests,
-                #field='docs',
-            #),
-            docs_matrix=[self._big_doc], # TODO: What is docs_matrix? Another thing to ban?
+            docs_matrix=None,
             tracing_context=None, # TODO: Tracing?
         )
 
-        # TODO: fan out big doc to all requests
-        #docs = self._set_result(requests, return_data, docs)
+        # We need to reslice the big doc array into the original requests
         consumed_count: int = 0
         for request, request_len in zip(self._requests, self._request_lens):
-            # TODO: What is return_data? Can we slice this? Do we need to?
-            self._set_result([request], return_data, self._big_doc[consumed_count:consumed_count + request_len])
+            left = consumed_count
+            right = consumed_count + request_len
+            self._set_result([request], return_data[left: right], self._big_doc[left: right])
             consumed_count += request_len
 
         # TODO: Metrics?
         #self._record_docs_processed_monitoring(requests, docs)
         #self._record_response_size_monitoring(requests)
 
-        return requests[0]
-
+        # We need to set all events and reset the batch queue
+        self._reset()
 
     # TODO: Duplicate from WorkerRequestHandler
     # TODO: This is only ever called with field='docs'
@@ -113,47 +145,22 @@ class BatchQueue():
         return parsed_params
 
     # TODO: Duplicate from WorkerRequestHandler
-    # TODO: This is only ever called with field='docs'
-    @staticmethod
-    def get_docs_matrix_from_request(
-        requests: List['DataRequest'],
-        field: str,
-    ) -> List['DocumentArray']:
-        """
-        Returns a docs matrix from a list of DataRequest objects.
-        :param requests: List of DataRequest objects
-        :param field: field to be retrieved
-        :return: docs matrix: list of DocumentArray objects
-        """
-        # TODO: The if else is redundant
-        if len(requests) > 1:
-            result = [getattr(request, field) for request in requests]
-        else:
-            result = [getattr(requests[0], field)]
-
-        # to unify all length=0 DocumentArray (or any other results) will simply considered as None
-        # otherwise, the executor has to handle [None, None, None] or [DocArray(0), DocArray(0), DocArray(0)]
-        len_r = sum(len(r) for r in result)
-        if len_r:
-            return result
-
-
-    # TODO: Duplicate from WorkerRequestHandler
     # TODO: Missing some self stuff
     def _set_result(self, requests, return_data, docs):
         # assigning result back to request
         if return_data is not None:
             if isinstance(return_data, DocumentArray):
                 docs = return_data
-            elif isinstance(return_data, dict):
-                params = requests[0].parameters
-                results_key = self._KEY_RESULT
+            # TODO: We will handle this later
+            #elif isinstance(return_data, dict):
+                #params = requests[0].parameters
+                #results_key = self._KEY_RESULT
 
-                if not results_key in params.keys():
-                    params[results_key] = dict()
+                #if not results_key in params.keys():
+                    #params[results_key] = dict()
 
-                params[results_key].update({self.args.name: return_data})
-                requests[0].parameters = params
+                #params[results_key].update({self.args.name: return_data})
+                #requests[0].parameters = params
 
             else:
                 raise TypeError(
@@ -161,7 +168,20 @@ class BatchQueue():
                     f'but getting {return_data!r}'
                 )
 
-        WorkerRequestHandler.replace_docs(
+        BatchQueue.replace_docs(
             requests[0], docs, self.args.output_array_type
         )
         return docs
+
+    # TODO: Duplicate from WorkerRequestHandler
+    @staticmethod
+    def replace_docs(
+            request: List['DataRequest'], docs: 'DocumentArray', ndarrray_type: str = None
+    ) -> None:
+        """Replaces the docs in a message with new Documents.
+
+        :param request: The request object
+        :param docs: the new docs to be used
+        :param ndarrray_type: type tensor and embedding will be converted to
+        """
+        request.data.set_docs_convert_arrays(docs, ndarray_type=ndarrray_type)
