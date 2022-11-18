2057e7ddc2acc0e3ab9fab9a54f8f2fd16d437e8
Jackmin801 <ongjackm@gmail.com>
2022-11-18 18:54:56 +0800

    test: add better fail observability and make tracing test use health check

--- tests/k8s_otel/test_k8s_instrumentation.py
+++ tests/k8s_otel/test_k8s_instrumentation.py
@@ -3,10 +3,11 @@ import os
 import requests
 import asyncio
 from typing import List, Dict
+from jina.logging.logger import JinaLogger
 
 from jina import Flow
 from . import KindClusterWrapperV2
-from .util import parse_string_jaeger_tags
+from .util import parse_string_jaeger_tags, get_last_health_check_data
 
 
 @pytest.mark.asyncio
@@ -14,6 +15,7 @@ from .util import parse_string_jaeger_tags
 async def test_flow_resource_labeling(tmpdir, otel_test_namespace: str, k8s_cluster_v2: KindClusterWrapperV2):
     NAMESPACE = 'test-flow-resource-labeling'
     dump_path = os.path.join(tmpdir, NAMESPACE)
+    logger = JinaLogger(NAMESPACE)
 
     # Create k8s flow artifacts
     flow = Flow(
@@ -41,40 +43,60 @@ async def test_flow_resource_labeling(tmpdir, otel_test_namespace: str, k8s_clus
         [i async for i in Client(port=gateway_port, asyncio=True).post("/")]
 
     # Give grace period for metrics and traces to be exported
-    await asyncio.sleep(20)
+    await asyncio.sleep(60)
 
     # Check Jaeger API
     with k8s_cluster_v2.port_forward('svc/jaeger', otel_test_namespace, svc_port=16686) as jaeger_port:
-        # Gateway
-        trace_data = requests.get(f'http://localhost:{jaeger_port}/api/traces?service=gateway').json()['data']
-        assert trace_data[0]['processes']['p1']['serviceName'] == 'gateway'
-        tags: Dict[str, str] = parse_string_jaeger_tags(trace_data[0]['processes']['p1']['tags'])
-        assert tags['k8s.deployment.name'] == 'gateway'
-        assert tags['k8s.namespace.name'] == NAMESPACE
-        assert tags['k8s.pod.name'].startswith('gateway-')
+        try:
+            # Gateway
+            trace_data = get_last_health_check_data(jaeger_port=jaeger_port, service_name='gateway')
+            assert trace_data['processes']['p1']['serviceName'] == 'gateway'
+            tags: Dict[str, str] = parse_string_jaeger_tags(trace_data['processes']['p1']['tags'])
+            assert tags['k8s.deployment.name'] == 'gateway'
+            assert tags['k8s.namespace.name'] == NAMESPACE
+            assert tags['k8s.pod.name'].startswith('gateway-')
 
-        # Instrumentation Executor
-        trace_data = requests.get(f'http://localhost:{jaeger_port}/api/traces?service=instrumentation').json()['data']
-        assert trace_data[0]['processes']['p1']['serviceName'] == 'instrumentation'
-        tags: Dict[str, str] = parse_string_jaeger_tags(trace_data[0]['processes']['p1']['tags'])
-        assert tags['k8s.deployment.name'] == 'instrumentation'
-        assert tags['k8s.namespace.name'] == NAMESPACE
-        assert tags['k8s.pod.name'].startswith('instrumentation-')
+            # Instrumentation Executor
+            trace_data = get_last_health_check_data(jaeger_port=jaeger_port, service_name='instrumentation')
+            assert trace_data['processes']['p1']['serviceName'] == 'instrumentation'
+            tags: Dict[str, str] = parse_string_jaeger_tags(trace_data['processes']['p1']['tags'])
+            assert tags['k8s.deployment.name'] == 'instrumentation'
+            assert tags['k8s.namespace.name'] == NAMESPACE
+            assert tags['k8s.pod.name'].startswith('instrumentation-')
+        except AssertionError as e:
+            logger.error(trace_data)
+            raise e
 
     with k8s_cluster_v2.port_forward('svc/prometheus', otel_test_namespace, svc_port=9090) as prometheus_port:
-        # Check Prometheus Labels
-        prometheus_labels: List[str] = requests.get(f'http://localhost:{prometheus_port}/api/v1/labels').json()['data']
-        assert 'k8s_deployment_name' in prometheus_labels
-        assert 'k8s_namespace_name' in prometheus_labels
-        assert 'k8s_pod_name' in prometheus_labels
+        try:
+            # Check Prometheus Labels
+            prometheus_labels: List[str] = requests.get(f'http://localhost:{prometheus_port}/api/v1/labels').json()['data']
+            assert 'k8s_deployment_name' in prometheus_labels
+            assert 'k8s_namespace_name' in prometheus_labels
+            assert 'k8s_pod_name' in prometheus_labels
+        except AssertionError as e:
+            logger.error(prometheus_labels)
+            raise e
 
-        depl_values: List[str] = requests.get(f'http://localhost:{prometheus_port}/api/v1/label/k8s_deployment_name/values').json()['data']
-        assert 'gateway' in depl_values
-        assert 'instrumentation' in depl_values
+        try:
+            depl_values: List[str] = requests.get(f'http://localhost:{prometheus_port}/api/v1/label/k8s_deployment_name/values').json()['data']
+            assert 'gateway' in depl_values
+            assert 'instrumentation' in depl_values
+        except AssertionError as e:
+            logger.error(depl_values)
+            raise e
 
-        ns_values: List[str] = requests.get(f'http://localhost:{prometheus_port}/api/v1/label/k8s_namespace_name/values').json()['data']
-        assert NAMESPACE in ns_values
+        try:
+            ns_values: List[str] = requests.get(f'http://localhost:{prometheus_port}/api/v1/label/k8s_namespace_name/values').json()['data']
+            assert NAMESPACE in ns_values
+        except AssertionError as e:
+            logger.error(ns_values)
+            raise e
 
-        pod_values: List[str] = requests.get(f'http://localhost:{prometheus_port}/api/v1/label/k8s_pod_name/values').json()['data']
-        assert any(i.startswith('gateway-') for i in pod_values)
-        assert any(i.startswith('instrumentation-') for i in pod_values)
+        try:
+            pod_values: List[str] = requests.get(f'http://localhost:{prometheus_port}/api/v1/label/k8s_pod_name/values').json()['data']
+            assert any(i.startswith('gateway-') for i in pod_values)
+            assert any(i.startswith('instrumentation-') for i in pod_values)
+        except AssertionError as e:
+            logger.error(pod_values)
+            raise e
