93f5ff7ae8dd3e1e2fee4b23fdf66aa05e34281f
Alaeddine Abdessalem <alaeddine-13@live.fr>
2022-11-23 01:30:35 +0100

    test: adapt tests

--- tests/unit/jaml/test_gateway_parse.py
+++ tests/unit/jaml/test_gateway_parse.py
@@ -19,33 +19,6 @@ class MyDummyGateway(Gateway):
         pass
 
 
-@pytest.fixture
-def dummy_gateway_runtime_args():
-    return {
-        'name': None,
-        'port': None,
-        'host': __default_host__,
-        'protocol': None,
-        'tracing': None,
-        'tracer_provider': None,
-        'grpc_tracing_server_interceptors': None,
-        'graph_description': '{}',
-        'graph_conditions': '{}',
-        'deployments_addresses': '{}',
-        'deployments_metadata': '{}',
-        'deployments_no_reduce': '{}',
-        'timeout_send': -1,
-        'retries': 2,
-        'compression': None,
-        'runtime_name': 'test',
-        'prefetch': None,
-        'metrics_registry': None,
-        'meter': None,
-        'aio_tracing_client_interceptors': None,
-        'tracing_client_interceptor': None,
-    }
-
-
 def test_cls_from_tag():
     assert JAML.cls_from_tag('MyDummyGateway') == MyDummyGateway
     assert JAML.cls_from_tag('!MyDummyGateway') == MyDummyGateway
@@ -53,33 +26,28 @@ def test_cls_from_tag():
     assert JAML.cls_from_tag('Nonexisting') is None
 
 
-def test_base_jtype(tmpdir, dummy_gateway_runtime_args):
+def test_base_jtype(tmpdir):
     gateway_path = os.path.join(tmpdir, 'gateway.yml')
+    from jina.serve.runtimes.gateway import BaseGateway
 
-    g = Gateway.load_config('BaseGateway', runtime_args=dummy_gateway_runtime_args)
+    g = BaseGateway.load_config('BaseGateway')
     g.save_config(gateway_path)
     with open(gateway_path, 'r') as file:
         conf = yaml.safe_load(file)
         assert 'jtype' in conf
         assert conf['jtype'] == 'BaseGateway'
 
-    assert (
-        type(Gateway.load_config(gateway_path, runtime_args=dummy_gateway_runtime_args))
-        == Gateway
-    )
+    assert type(Gateway.load_config(gateway_path)) == Gateway
 
 
-def test_custom_jtype(tmpdir, dummy_gateway_runtime_args):
+def test_custom_jtype(tmpdir):
     gateway_path = os.path.join(tmpdir, 'gateway.yml')
 
-    e = Gateway.load_config('MyDummyGateway', runtime_args=dummy_gateway_runtime_args)
+    e = Gateway.load_config('MyDummyGateway')
     e.save_config(gateway_path)
     with open(gateway_path, 'r') as file:
         conf = yaml.safe_load(file)
         assert 'jtype' in conf
         assert conf['jtype'] == 'MyDummyGateway'
 
-    assert (
-        type(Gateway.load_config(gateway_path, runtime_args=dummy_gateway_runtime_args))
-        == MyDummyGateway
-    )
+    assert type(Gateway.load_config(gateway_path)) == MyDummyGateway
