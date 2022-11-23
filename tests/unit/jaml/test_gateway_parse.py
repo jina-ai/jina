beecc7863e5a6b2580a0e1f10095253549279614
AlaeddineAbdessalem <alaeddine-13@live.fr>
2022-11-23 11:46:32 +0100

    refactor: inject dependencies at contruction with runtime args (#5418)
    Co-authored-by: Joan Fontanals <joan.martinez@jina.ai>
    Co-authored-by: Jina Dev Bot <dev-bot@jina.ai>

--- tests/unit/jaml/test_gateway_parse.py
+++ tests/unit/jaml/test_gateway_parse.py
@@ -1,5 +1,6 @@
 import os
 
+import pytest
 import yaml
 
 from jina import Gateway
@@ -27,25 +28,32 @@ def test_cls_from_tag():
 
 def test_base_jtype(tmpdir):
     gateway_path = os.path.join(tmpdir, 'gateway.yml')
+    from jina.serve.runtimes.gateway import BaseGateway
 
-    g = Gateway()
+    g = BaseGateway.load_config('BaseGateway', runtime_args={'port': [12345]})
     g.save_config(gateway_path)
     with open(gateway_path, 'r') as file:
         conf = yaml.safe_load(file)
         assert 'jtype' in conf
         assert conf['jtype'] == 'BaseGateway'
 
-    assert type(Gateway.load_config(gateway_path)) == Gateway
+    assert (
+        type(Gateway.load_config(gateway_path, runtime_args={'port': [12345]}))
+        == Gateway
+    )
 
 
 def test_custom_jtype(tmpdir):
     gateway_path = os.path.join(tmpdir, 'gateway.yml')
 
-    e = MyDummyGateway()
+    e = Gateway.load_config('MyDummyGateway', runtime_args={'port': [12345]})
     e.save_config(gateway_path)
     with open(gateway_path, 'r') as file:
         conf = yaml.safe_load(file)
         assert 'jtype' in conf
         assert conf['jtype'] == 'MyDummyGateway'
 
-    assert type(Gateway.load_config(gateway_path)) == MyDummyGateway
+    assert (
+        type(Gateway.load_config(gateway_path, runtime_args={'port': [12345]}))
+        == MyDummyGateway
+    )
