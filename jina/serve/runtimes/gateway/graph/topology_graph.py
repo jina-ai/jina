bb27bc2b6f644fb604e15cf63d8358b39e1bb02e
Joan Fontanals <joan.martinez@jina.ai>
2022-11-24 10:09:08 +0100

    fix: protect when deployment not in gathering endpoints dictionary (#5440)

--- jina/serve/runtimes/gateway/graph/topology_graph.py
+++ jina/serve/runtimes/gateway/graph/topology_graph.py
@@ -147,7 +147,7 @@ class TopologyGraph:
 
                     # avoid sending to executor which does not bind to this endpoint
                     if endpoint is not None and executor_endpoint_mapping is not None:
-                        if (
+                        if (self.name in executor_endpoint_mapping and
                             endpoint not in executor_endpoint_mapping[self.name]
                             and __default_endpoint__
                             not in executor_endpoint_mapping[self.name]
