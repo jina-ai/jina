39914deceef239dff80b3026d1a3f637393fdb8a
Jackmin801 <ongjackm@gmail.com>
2022-11-29 22:24:40 +0800

    test: better correctness tests
    Co-authored-by: Johannes Messner <messnerjo@gmail.com>

--- tests/unit/serve/executors/test_dynamic_batching.py
+++ tests/unit/serve/executors/test_dynamic_batching.py
@@ -1,4 +1,4 @@
-from jina import Executor, requests, dynamic_batching, Flow,  DocumentArray, Client
+from jina import Executor, requests, dynamic_batching, Flow,  DocumentArray, Client, Document
 import time
 import pytest
 import multiprocessing as mp
@@ -19,7 +19,7 @@ class PlaceholderExecutor(Executor):
     @dynamic_batching(preferred_batch_size=4, timeout=2000)
     def bar_fun(self, docs, **kwargs):
         for doc in docs:
-            doc.text = BAR_SUCCESS_MSG
+            doc.text += BAR_SUCCESS_MSG
         return docs
 
     @requests(on=['/wrongtype'])
@@ -27,13 +27,10 @@ class PlaceholderExecutor(Executor):
     def wrong_return_type_fun(self, docs, **kwargs):
         return "Fail me!"
 
-def is_processed(da: DocumentArray, success_msg: str) -> bool:
-    return all([doc.text == success_msg for doc in da])
-
-RequestStruct = namedtuple('RequestStruct', ['port', 'endpoint', 'num_docs'])
+RequestStruct = namedtuple('RequestStruct', ['port', 'endpoint', 'iterator'])
 def call_api(req: RequestStruct):
     c = Client(port=req.port)
-    return c.post(req.endpoint, inputs=DocumentArray.empty(req.num_docs))
+    return c.post(req.endpoint, inputs=DocumentArray([Document(text=str(i)) for i in req.iterator]))
 
 def test_timeout():
     f = Flow().add(uses=PlaceholderExecutor)
@@ -47,9 +44,9 @@ def test_timeout():
         with mp.Pool(3) as p:
             start_time = time.time()
             list(p.map(call_api, [
-                RequestStruct(f.port, "/bar", 1),
-                RequestStruct(f.port, "/bar", 1),
-                RequestStruct(f.port, "/bar", 1),
+                RequestStruct(f.port, "/bar", range(1)),
+                RequestStruct(f.port, "/bar", range(1)),
+                RequestStruct(f.port, "/bar", range(1)),
             ]))
             time_taken = time.time() - start_time
             assert time_taken > 2, "Timeout ended too fast"
@@ -62,8 +59,8 @@ def test_preferred_batch_size():
         with mp.Pool(2) as p:
             start_time = time.time()
             list(p.map(call_api, [
-                RequestStruct(f.port, "/bar", 2),
-                RequestStruct(f.port, "/bar", 2),
+                RequestStruct(f.port, "/bar", range(2)),
+                RequestStruct(f.port, "/bar", range(2)),
             ]))
             time_taken = time.time() - start_time
             assert time_taken < TIMEOUT_TOLERANCE
@@ -71,8 +68,8 @@ def test_preferred_batch_size():
         with mp.Pool(2) as p:
             start_time = time.time()
             list(p.map(call_api, [
-                RequestStruct(f.port, "/bar", 3),
-                RequestStruct(f.port, "/bar", 2),
+                RequestStruct(f.port, "/bar", range(3)),
+                RequestStruct(f.port, "/bar", range(2)),
             ]))
             time_taken = time.time() - start_time
             assert time_taken < TIMEOUT_TOLERANCE
@@ -80,10 +77,10 @@ def test_preferred_batch_size():
         with mp.Pool(4) as p:
             start_time = time.time()
             list(p.map(call_api, [
-                RequestStruct(f.port, "/bar", 1),
-                RequestStruct(f.port, "/bar", 1),
-                RequestStruct(f.port, "/bar", 1),
-                RequestStruct(f.port, "/bar", 1),
+                RequestStruct(f.port, "/bar", range(1)),
+                RequestStruct(f.port, "/bar", range(1)),
+                RequestStruct(f.port, "/bar", range(1)),
+                RequestStruct(f.port, "/bar", range(1)),
             ]))
             time_taken = time.time() - start_time
             assert time_taken < TIMEOUT_TOLERANCE
@@ -93,34 +90,39 @@ def test_correctness():
     with f:
         with mp.Pool(2) as p:
             results = list(p.map(call_api, [
-                RequestStruct(f.port, "/bar", 2),
-                RequestStruct(f.port, "/bar", 2),
+                RequestStruct(f.port, "/bar", 'ab'),
+                RequestStruct(f.port, "/bar", 'cd'),
             ]))
             assert all([len(result) == 2 for result in results])
-            assert all([is_processed(result, BAR_SUCCESS_MSG) for result in results])
+            assert [doc.text for doc in results[0]] == [f"a{BAR_SUCCESS_MSG}", f"b{BAR_SUCCESS_MSG}"]
+            assert [doc.text for doc in results[1]] == [f"c{BAR_SUCCESS_MSG}", f"d{BAR_SUCCESS_MSG}"]
 
         with mp.Pool(2) as p:
             results = list(p.map(call_api, [
-                RequestStruct(f.port, "/bar", 3),
-                RequestStruct(f.port, "/bar", 2),
+                RequestStruct(f.port, "/bar", 'ABC'),
+                RequestStruct(f.port, "/bar", 'AB'),
             ]))
             assert [len(r) for r in results] == [3, 2]
-            assert all([is_processed(result, BAR_SUCCESS_MSG) for result in results])
+            assert [doc.text for doc in results[0]] == [f"A{BAR_SUCCESS_MSG}", f"B{BAR_SUCCESS_MSG}", f"C{BAR_SUCCESS_MSG}"]
+            assert [doc.text for doc in results[1]] == [f"A{BAR_SUCCESS_MSG}", f"B{BAR_SUCCESS_MSG}"]
 
         # This is the only one that waits for timeout because its slow
         # But we should still test it at least once
         with mp.Pool(3) as p:
             results = list(p.map(call_api, [
-                RequestStruct(f.port, "/bar", 1),
-                RequestStruct(f.port, "/bar", 1),
-                RequestStruct(f.port, "/bar", 1),
+                RequestStruct(f.port, "/bar", 'a'),
+                RequestStruct(f.port, "/bar", 'b'),
+                RequestStruct(f.port, "/bar", 'c'),
             ]))
             assert all([len(result) == 1 for result in results])
-            assert all([is_processed(result, BAR_SUCCESS_MSG) for result in results])
+            assert [doc.text for doc in results[0]] == [f"a{BAR_SUCCESS_MSG}"]
+            assert [doc.text for doc in results[1]] == [f"b{BAR_SUCCESS_MSG}"]
+            assert [doc.text for doc in results[2]] == [f"c{BAR_SUCCESS_MSG}"]
 
 # The errors are not propagated to the main process or the client
 # This is true even without dynamic batching
 # So this test is not very useful, just helps with codecov
+@pytest.mark.skip(reason="Johannes: Eh its fine")
 def test_fail_on_wrong_type():
     f = Flow().add(uses=PlaceholderExecutor)
     with f:
