fde433811cef93adbd08baa314f853977d6bae15
Alaeddine Abdessalem <alaeddine-13@live.fr>
2022-12-01 14:12:36 +0100

    chore: add get_streamer to demo

--- flow.py
+++ flow.py
@@ -1,4 +1,16 @@
-from jina import Flow
+import random
+
+from docarray import DocumentArray
+
+from jina import Executor, Flow, requests
+
+
+class MyExecutor(Executor):
+    @requests
+    def boo(self, docs: DocumentArray, **kwargs):
+        for doc in docs:
+            doc.text = f'my doc{random.randint(1, 100)}'
+
 
 flow = Flow().config_gateway(
     port=8501, protocol='http', uses='streamlit_gateway/config.yml'
--- streamlit_gateway/app.py
+++ streamlit_gateway/app.py
@@ -1,3 +1,23 @@
-import streamlit as st
+import asyncio
 
+import streamlit as st
+from docarray import Document, DocumentArray
+
+from jina.serve.streamer import GatewayStreamer
+
+streamer = GatewayStreamer.get_streamer()
 st.title('Streamlit app running on Jina Gateway')
+
+
+async def send_docs():
+    res = []
+    async for docs in streamer.stream_docs(
+        docs=DocumentArray.empty(10),
+        exec_endpoint='/',
+    ):
+        for doc in docs:
+            res.append(doc.text)
+    st.text('results:\n' + '\n'.join(res))
+
+
+asyncio.run(send_docs())
