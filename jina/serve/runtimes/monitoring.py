33132fc6ddcd2063c6a7ad45906278f8dfca9f6d
AnneY <evangeline-lun@foxmail.com>
2022-12-08 00:14:18 +0800

    fix: port_monitoring is a list

--- jina/serve/runtimes/monitoring.py
+++ jina/serve/runtimes/monitoring.py
@@ -32,7 +32,7 @@ class MonitoringMixin:
             from prometheus_client import start_http_server
 
             start_http_server(
-                int(self.args.port_monitoring), registry=self.metrics_registry
+                int(self.args.port_monitoring[0]), registry=self.metrics_registry
             )
 
 
