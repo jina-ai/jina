beec7cc08e18c3cd8bbe68bfa485d691e8abf27b
Alaeddine Abdessalem <alaeddine-13@live.fr>
2022-12-09 15:31:04 +0100

    fix: protect access to shared state with lock

--- jina/serve/runtimes/worker/batch_queue.py
+++ jina/serve/runtimes/worker/batch_queue.py
@@ -17,6 +17,7 @@ class BatchQueue:
         preferred_batch_size: int = 4,
         timeout: int = 10_000,
     ) -> None:
+        self._data_lock = asyncio.Lock()
         self.func = func
         if params is None:
             params = dict()
@@ -75,58 +76,64 @@ class BatchQueue:
 
         :return: The task that will be set once the request has been processed.
         """
-        if not self._flush_task:
-            self._flush_task = asyncio.create_task(self._await_then_flush())
-        if not self._timer_task:
-            self._start_timer()
-
         docs = request.docs
-        # TODO: is this protected/synchronous ?
-        self._big_doc.extend(docs)
-        self._requests.append(request)
-        self._request_lens.append(len(docs))
 
-        if len(self._big_doc) >= self._preferred_batch_size:
-            self._flush_trigger.set()
+        # writes to shared data between tasks need to be mutually exclusive
+        async with self._data_lock:
+            if not self._flush_task:
+                self._flush_task = asyncio.create_task(self._await_then_flush())
+            if not self._timer_task:
+                self._start_timer()
+
+            self._big_doc.extend(docs)
+            self._requests.append(request)
+            self._request_lens.append(len(docs))
+
+            if len(self._big_doc) >= self._preferred_batch_size:
+                self._flush_trigger.set()
 
         return self._flush_task
 
     async def _await_then_flush(self) -> None:
         """Process all requests in the queue once flush_trigger event is set."""
         await self._flush_trigger.wait()
-        try:
-            # The function might accidentally change the size
-            input_len_before_call: int = len(self._big_doc)
 
-            # We need to get the function to process the big doc
-            return_data = await self.func(
-                docs=self._big_doc,
-                parameters=self.params,
-                docs_matrix=None,  # joining manually with batch queue is not supported right now
-                tracing_context=None,
-            )
+        # writes to shared data between tasks need to be mutually exclusive
+        async with self._data_lock:
+            try:
 
-            # Output validation
-            if isinstance(return_data, DocumentArray):
-                if not len(return_data) == input_len_before_call:
-                    raise ValueError(
-                        f'Dynamic Batching requires input size to equal output size. Expected output size {input_len_before_call}, but got {len(return_data)}'
-                    )
-            elif return_data is None:
-                if not len(self._big_doc) == input_len_before_call:
-                    raise ValueError(
-                        f'Dynamic Batching requires input size to equal output size. Expected output size {input_len_before_call}, but got {len(self._big_doc)}'
-                    )
-            else:
-                raise TypeError(
-                    f'The return type must be DocumentArray / `None` when using dynamic batching, '
-                    f'but getting {return_data!r}'
+                # The function might accidentally change the size
+                input_len_before_call: int = len(self._big_doc)
+
+                # We need to get the function to process the big doc
+                return_data = await self.func(
+                    docs=self._big_doc,
+                    parameters=self.params,
+                    docs_matrix=None,  # joining manually with batch queue is not supported right now
+                    tracing_context=None,
                 )
 
-            # We need to re-slice the big doc array into the original requests
-            self._fan_out_return_data(return_data)
-        finally:
-            self._reset()
+                # Output validation
+                if isinstance(return_data, DocumentArray):
+                    if not len(return_data) == input_len_before_call:
+                        raise ValueError(
+                            f'Dynamic Batching requires input size to equal output size. Expected output size {input_len_before_call}, but got {len(return_data)}'
+                        )
+                elif return_data is None:
+                    if not len(self._big_doc) == input_len_before_call:
+                        raise ValueError(
+                            f'Dynamic Batching requires input size to equal output size. Expected output size {input_len_before_call}, but got {len(self._big_doc)}'
+                        )
+                else:
+                    raise TypeError(
+                        f'The return type must be DocumentArray / `None` when using dynamic batching, '
+                        f'but getting {return_data!r}'
+                    )
+
+                # We need to re-slice the big doc array into the original requests
+                self._fan_out_return_data(return_data)
+            finally:
+                self._reset()
 
     def _fan_out_return_data(self, return_data: Optional[DocumentArray]):
         consumed_count: int = 0
@@ -151,5 +158,4 @@ class BatchQueue:
             if self._flush_task:
                 await self._flush_task
             self._cancel_timer_if_pending()
-            # await tasks here
             self._is_closed = True
