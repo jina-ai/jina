47eb441177151a653ceae2c2598e2ce5b19ce307
Joan Fontanals Martinez <joan.martinez@jina.ai>
2022-12-15 16:07:53 +0100

    feat: add prefetch arg to client

--- jina/clients/base/grpc.py
+++ jina/clients/base/grpc.py
@@ -134,7 +134,7 @@ class GRPCBaseClient(BaseClient):
         streamer = RequestStreamer(
             request_handler=_request_handler,
             result_handler=_result_handler,
-            prefetch=getattr(self.args, 'prefetch', 0),
+            prefetch=self.args.prefetch,
             iterate_sync_in_thread=False,
             logger=self.logger,
             **vars(self.args),
