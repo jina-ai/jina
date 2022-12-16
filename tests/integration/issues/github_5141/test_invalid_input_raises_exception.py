d00dab3d786cd73354d4d7f550d47589aa37eca2
Jackmin801 <ongjackm@gmail.com>
2022-12-16 20:59:28 +0800

    test: use base exception instead of exception

--- tests/integration/issues/github_5141/test_invalid_input_raises_exception.py
+++ tests/integration/issues/github_5141/test_invalid_input_raises_exception.py
@@ -1,27 +1,14 @@
 from datetime import datetime
-
 import pytest
 
 from jina import Document, DocumentArray, Flow
-
-
-class MyOwnException(Exception):
-    pass
 
 
 @pytest.mark.parametrize('protocol', ['grpc', 'http', 'websocket'])
 def test_invalid_input_raise(protocol):
     f = Flow(protocol=protocol).add()
 
-    try:
+    with pytest.raises(BaseException):
         with f:
             da = DocumentArray([Document(text='hello', tags={'date': datetime.now()})])
-            try:
-                f.post(
-                    on='/', inputs=da
-                )  # process should stop here and raise an exception
-            except Exception:
-                raise MyOwnException
-            assert False
-    except MyOwnException:
-        pass
+            f.post(on='/', inputs=da)  # process should stop here and raise an exception
