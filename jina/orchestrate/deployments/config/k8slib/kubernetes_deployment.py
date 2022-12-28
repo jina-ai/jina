9601235faba6afe31b48a39b9f50e6c49beabad8
AnneY <evangeline-lun@foxmail.com>
2022-12-28 16:25:37 +0800

    fix: add docstring

--- jina/orchestrate/deployments/config/k8slib/kubernetes_deployment.py
+++ jina/orchestrate/deployments/config/k8slib/kubernetes_deployment.py
@@ -47,6 +47,7 @@ def get_template_yamls(
     :param shard_id: id of this shard, None if shards=1 or this is gateway/head
     :param port: port which will be exposed by the deployed containers
     :param env: environment variables to be passed into configmap.
+    :param env_from_secret: environment variables from secret to be passed to this pod
     :param gpus: number of gpus to use, for k8s requires you pass an int number, refers to the number of requested gpus.
     :param image_name_uses_before: image for uses_before container in the k8s deployment
     :param image_name_uses_after: image for uses_after container in the k8s deployment
