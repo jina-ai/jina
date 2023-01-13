f8d2aec3fd3fd6c23f874ff5244c6cc1b148bff7
Alaeddine Abdessalem <alaeddine-13@live.fr>
2023-01-13 18:05:17 +0100

    fix: enable timeout for jina ping executor and gateway

--- jina/checker.py
+++ jina/checker.py
@@ -31,15 +31,21 @@ class NetworkChecker:
                 ) as tc:
                     if args.target == 'executor':
                         hostname, port, protocol, _ = parse_host_scheme(args.host)
-                        r = WorkerRuntime.is_ready(ctrl_address=f'{hostname}:{port}')
+                        r = WorkerRuntime.is_ready(
+                            ctrl_address=f'{hostname}:{port}',
+                            timeout=args.timeout / 1000,
+                        )
                     elif args.target == 'gateway':
                         hostname, port, protocol, _ = parse_host_scheme(args.host)
                         r = GatewayRuntime.is_ready(
                             f'{hostname}:{port}',
-                            protocol=GatewayProtocolType.from_string(protocol)
+                            protocol=GatewayProtocolType.from_string(protocol),
+                            timeout=args.timeout / 1000,
                         )
                     elif args.target == 'flow':
-                        r = Client(host=args.host).is_flow_ready(timeout=args.timeout / 1000)
+                        r = Client(host=args.host).is_flow_ready(
+                            timeout=args.timeout / 1000
+                        )
                     if not r:
                         default_logger.warning(
                             'not responding, attempt (%d/%d) in 1s'
