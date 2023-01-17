722066c54b86fbf9e713a16ce799d3f2935764e2
Girish Chandrashekar <girish.chandrashekar@jina.ai>
2023-01-17 15:23:26 +0100

    fix: revert changes to Client constructor

--- jina/parsers/__init__.py
+++ jina/parsers/__init__.py
@@ -143,11 +143,15 @@ def set_client_cli_parser(parser=None):
         mixin_client_features_parser,
         mixin_client_protocol_parser,
     )
-    from jina.parsers.orchestrate.runtimes.remote import mixin_client_gateway_parser
+    from jina.parsers.orchestrate.runtimes.remote import (
+        mixin_client_gateway_parser,
+        mixin_prefetch_parser,
+    )
 
     mixin_client_gateway_parser(parser)
     mixin_client_features_parser(parser)
     mixin_client_protocol_parser(parser)
+    mixin_prefetch_parser(parser)
 
     return parser
 
