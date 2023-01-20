b8e4ee9992f98f23a4ad1109eef65aa605831b02
Alaeddine Abdessalem <alaeddine-13@live.fr>
2023-01-20 09:05:02 +0100

    test: fix client

--- tests/k8s/test_k8s.py
+++ tests/k8s/test_k8s.py
@@ -1838,10 +1838,10 @@ async def test_deployment_serve_k8s(logger, docker_images, tmpdir, k8s_cluster):
             namespace,
         ):
             client = Client(port=port, asyncio=True)
-            async for resp in client.post(
+            async for docs in client.post(
                 '/debug', inputs=DocumentArray.empty(3), stream=False
             ):
-                for doc in resp.docs:
+                for doc in docs:
                     assert doc.tags['shards'] == 1
                     assert doc.tags['parallel'] == 3
 
