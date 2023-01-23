310cb4f1f4488daa6a1be46d6c860f7e06d6f1fa
samsja <sami.jaghouar@hotmail.fr>
2023-01-23 17:59:27 +0100

    fix: remove useless setup py lines
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>

--- setup.py
+++ setup.py
@@ -160,11 +160,6 @@ if sys.version_info.major == 3 and sys.version_info.minor >= 11:
     final_deps.add('grpcio-health-checking>=1.49.0')
     final_deps.add('grpcio-reflection>=1.49.0')
 
-install_requires = list(final_deps)
-install_requires.append(
-    'docarray @ git+https://github.com/docarray/docarray@feat-jina-compatibility[common]'
-)
-
 setup(
     name=pkg_name,
     packages=find_packages(),
