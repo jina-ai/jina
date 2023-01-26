dede8a49c142faa874f519644240bf467c74f0b1
Alaeddine Abdessalem <alaeddine-13@live.fr>
2023-01-26 11:51:48 +0100

    fix: fix save config

--- jina/jaml/parsers/deployment/legacy.py
+++ jina/jaml/parsers/deployment/legacy.py
@@ -1,6 +1,9 @@
 from typing import TYPE_CHECKING, Any, Dict, Optional, Type
 
+from jina.helper import ArgNamespace
 from jina.jaml.parsers.base import BaseLegacyParser
+from jina.jaml.parsers.flow.v1 import _get_taboo
+from jina.parsers import set_deployment_parser
 
 if TYPE_CHECKING:
     from jina.orchestrate.deployments import Deployment
@@ -21,17 +24,11 @@ class DeploymentLegacyParser(BaseLegacyParser):
         :param runtime_args: Optional runtime_args to be directly passed without being parsed into a yaml config
         :return: the Deployment YAML parser given the syntax version number
         """
-        from jina.logging.predefined import default_logger
-
-        data['metas'] = {}
-
         cls._init_from_yaml = True
         # tmp_p = {kk: expand_env_var(vv) for kk, vv in data.get('with', {}).items()}
 
         obj = cls(
             **data.get('with', {}),
-            metas=data.get('metas', {}),
-            requests=data.get('requests', {}),
             runtime_args=runtime_args,
         )
         cls._init_from_yaml = False
@@ -44,9 +41,17 @@ class DeploymentLegacyParser(BaseLegacyParser):
         :param data: versioned deployment object
         :return: the dictionary given a versioned deployment object
         """
-        a = {k: v for k, v in data._init_kwargs_dict.items()}
         r = {}
-        if a:
-            r['with'] = a
+        r['with'] = {}
+        parser = set_deployment_parser()
+        non_default_kw = ArgNamespace.get_non_defaults_args(data.args, parser)
+        for t in _get_taboo(parser):
+            if t in non_default_kw:
+                non_default_kw.pop(t)
+
+        if non_default_kw:
+            r['with'].update(non_default_kw)
+        if data._gateway_kwargs:
+            r['with'].update(data._gateway_kwargs)
 
         return r
