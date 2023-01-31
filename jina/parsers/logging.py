5d8c5e8c9c6a1c778ade6f05a7d754a1d960eadc
Jake <www4628559@163.com>
2023-02-01 00:00:04 +0800

    fix: add docstring

--- jina/parsers/logging.py
+++ jina/parsers/logging.py
@@ -2,6 +2,10 @@
 
 
 def mixin_suppress_root_logging_parser(parser):
+    """Mixing in arguments required by every module into the given parser.
+    This parser extends logging-related arguments.
+    :param parser: the parser instance to which we add arguments
+    """
     parser.add_argument(
         '--suppress-root-logging',
         action='store_true',
