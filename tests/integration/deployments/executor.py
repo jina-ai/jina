61ebaed4b7c92a2148456e1a5a5c3fcd9fb1f8c7
Alaeddine Abdessalem <alaeddine-13@live.fr>
2023-02-01 15:01:22 +0100

    test: cover loading deployments on different scenarios

--- tests/integration/deployments/executor.py
+++ tests/integration/deployments/executor.py
@@ -4,6 +4,11 @@ from jina import Executor, requests
 
 
 class DummyExecutor(Executor):
+    def __init__(self, arg='hello', **kwargs):
+        super().__init__(**kwargs)
+        self.arg = arg
+
     @requests
-    def foo(self, **kwargs):
-        pass
+    def foo(self, docs, **kwargs):
+        for doc in docs:
+            doc.text = self.arg
