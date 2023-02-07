10e5c28200d8f869d7e704b45481428a72e20206
Alaeddine Abdessalem <alaeddine-13@live.fr>
2023-02-07 09:04:26 +0100

    test: cover loading flow from config with deployment config

--- tests/unit/orchestrate/flow/flow-construct/test_flow_deployment.py
+++ tests/unit/orchestrate/flow/flow-construct/test_flow_deployment.py
@@ -10,6 +10,12 @@ _deployment_yaml_path = os.path.join(cur_dir, '../../../yaml/test-deployment.yml
 _deployment_yaml_with_exec_config_path = os.path.join(
     cur_dir, '../../../yaml/test-deployment-exec-config.yml'
 )
+_flow_yaml_with_deployment_config = os.path.join(
+    cur_dir, '../../../yaml/test-flow-deployment-nested-config.yml'
+)
+_flow_yaml_with_deployment_exec_config = os.path.join(
+    cur_dir, '../../yaml/test-flow-deployment-exec-nested-config.yml'
+)
 
 
 @pytest.mark.parametrize(
@@ -19,6 +25,19 @@ _deployment_yaml_with_exec_config_path = os.path.join(
 def test_flow_deployment(deployment_config):
 
     flow = Flow().add(deployment=deployment_config)
+    with flow:
+        docs = flow.post(on='/', inputs=DocumentArray.empty(3))
+        assert len(docs) == 3
+        assert all([doc.text == 'indexed' for doc in docs])
+
+
+@pytest.mark.parametrize(
+    'flow_config',
+    [_flow_yaml_with_deployment_config, _flow_yaml_with_deployment_exec_config],
+)
+def test_flow_load_config_with_deployment(flow_config):
+
+    flow = Flow.load_config(flow_config)
     with flow:
         docs = flow.post(on='/', inputs=DocumentArray.empty(3))
         assert len(docs) == 3
