ae92aab2252013fc19eddd9687fe6df1753d8c84
AlaeddineAbdessalem <alaeddine-13@live.fr>
2023-02-08 17:53:05 +0100

    chore: deprecate executor serve and to_kubernetes_yaml methods (#5665)

--- jina/serve/executors/__init__.py
+++ jina/serve/executors/__init__.py
@@ -805,6 +805,11 @@ class BaseExecutor(JAMLCompatible, metaclass=ExecutorType):
         :param kwargs: other kwargs accepted by the Flow, full list can be found `here <https://docs.jina.ai/api/jina.orchestrate.flow.base/>`
 
         """
+        warnings.warn(
+            f'Executor.serve() is no more supported and will be deprecated soon. Use Deployment to serve an Executor instead: '
+            f'https://docs.jina.ai/concepts/executor/serve/',
+            DeprecationWarning,
+        )
         from jina.orchestrate.deployments import Deployment
 
         dep = Deployment(
@@ -857,6 +862,11 @@ class BaseExecutor(JAMLCompatible, metaclass=ExecutorType):
         :param uses_dynamic_batching: dictionary of parameters to overwrite from the default config's dynamic_batching field
         :param kwargs: other kwargs accepted by the Flow, full list can be found `here <https://docs.jina.ai/api/jina.orchestrate.flow.base/>`
         """
+        warnings.warn(
+            f'Executor.to_kubernetes_yaml() is no more supported and will be deprecated soon. Use Deployment to export kubernetes YAML files: '
+            f'https://docs.jina.ai/concepts/executor/serve/#serve-via-kubernetes',
+            DeprecationWarning,
+        )
         from jina.orchestrate.flow.base import Flow
 
         Flow(**kwargs).add(
