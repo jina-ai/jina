4659809e8bf7017ead52bcfc778d367e63efc0b8
Alaeddine Abdessalem <alaeddine-13@live.fr>
2023-03-22 16:30:21 +0100

    test: add integration test for stateful executor

---
+++ tests/integration/stateful/test_stateful.py
@@ -0,0 +1,113 @@
+import os
+import time
+
+import pytest
+
+from jina import Client, Document, DocumentArray, Executor, Flow, requests
+from jina.helper import random_port
+from jina.orchestrate.pods.factory import PodFactory
+from jina.parsers import set_pod_parser
+from jina.serve.executors.decorators import write
+
+os.environ['JINA_LOG_LEVEL'] = 'DEBUG'
+
+
+class MyStateExecutor(Executor):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._docs = DocumentArray()
+
+    @requests(on=['/index'])
+    @write
+    def index(self, docs, **kwargs):
+        time.sleep(0.2)
+        for doc in docs:
+            self.logger.debug(f' Indexing doc {doc.text}')
+            self._docs.append(doc)
+
+    @requests(on=['/search'])
+    def search(self, docs, **kwargs):
+        time.sleep(0.2)
+        for doc in docs:
+            doc.text = self._docs[doc.id].text
+
+    def snapshot(self, snapshot_file: str):
+        self.logger.warning(
+            f' Snapshotting to {snapshot_file} with {len(self._docs)} documents'
+        )
+        self.logger.warning(f'Snapshotting with order {[d.text for d in self._docs]}')
+        with open(snapshot_file, 'wb') as f:
+            self._docs.save_binary(f)
+
+    def restore(self, snapshot_file: str):
+        self._docs = DocumentArray.load_binary(snapshot_file)
+        self.logger.warning(
+            f' Restoring from {snapshot_file} with {len(self._docs)} documents'
+        )
+        self.logger.warning(f'Restoring with order {[d.text for d in self._docs]}')
+
+
+class MyStateExecutorNoSnapshot(Executor):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._docs = DocumentArray()
+
+    @requests(on=['/index'])
+    @write
+    def index(self, docs, **kwargs):
+        time.sleep(0.2)
+        for doc in docs:
+            self.logger.debug(f' Indexing doc {doc.text}')
+            self._docs.append(doc)
+
+    @requests(on=['/search'])
+    def search(self, docs, **kwargs):
+        time.sleep(0.2)
+        for doc in docs:
+            doc.text = self._docs[doc.id].text
+
+
+def assert_is_indexed(client, search_da):
+    docs = client.search(inputs=search_da)
+    for doc in docs:
+        assert doc.text == f'ID {doc.id}'
+
+
+# TODO: for now, there is hanging when we attempt to run both at the same time
+# only one test can be run at a time
+@pytest.mark.parametrize('executor_cls', [MyStateExecutor, MyStateExecutorNoSnapshot])
+def test_stateful_index_search(executor_cls, tmpdir):
+    gateway_port = random_port()
+    pod_ports = [random_port(), random_port(), random_port()]
+
+    flow = Flow(port=gateway_port).add(
+        uses=executor_cls,
+        replicas=3,
+        workspace=tmpdir,
+        pod_ports=pod_ports,
+        stateful=True,
+        raft_configuration={
+            'snapshot_interval': 10,
+            'snapshot_threshold': 5,
+            'trailing_logs': 10,
+            'LogLevel': 'INFO',
+        },
+    )
+
+    with flow:
+        index_da = DocumentArray(
+            [Document(id=f'{i}', text=f'ID {i}') for i in range(100)]
+        )
+        search_da = DocumentArray([Document(id=f'{i}') for i in range(100)])
+        flow.index(inputs=index_da)
+
+        # allowing some time for the state to be replicated
+        time.sleep(10)
+        # checking against the main read replica
+        assert_is_indexed(flow, search_da)
+
+        # performing manual requests to the underlying replicas
+        executor_ports = [(port + 1) for port in pod_ports]
+        for port in executor_ports:
+            client = Client(port=port)
+            assert_is_indexed(client, search_da)
