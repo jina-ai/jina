d2b36e5886eca820e34c3191478772bf3baf6f24
Alaeddine Abdessalem <alaeddine-13@live.fr>
2023-04-03 17:13:25 +0200

    ci: publish to pypi in sep workflow and rename distribution

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -68,6 +68,17 @@ elif [[ $1 == 'rc' ]]; then
   VER_TAG_NEXT=$VER_TAG\'${NEXT_VER}\'
   update_ver_line "$VER_TAG" "$VER_TAG_NEXT" "$INIT_FILE"
 
+elif [[ $1 == 'stateful' ]]; then
+  # as a prerelease, pypi update only, no back commit etc.
+  COMMITS_SINCE_LAST_VER=$(git rev-list $LAST_VER..HEAD --count)
+  NEXT_VER=$RELEASE_VER".stateful"$COMMITS_SINCE_LAST_VER
+  printf "this will be a stateful executor release: \e[1;33m$NEXT_VER\e[0m\n"
+
+  VER_TAG_NEXT=$VER_TAG\'${NEXT_VER}\'
+  update_ver_line "$VER_TAG" "$VER_TAG_NEXT" "$INIT_FILE"
+
+fi
+
 else
   # as a prerelease, pypi update only, no back commit etc.
   COMMITS_SINCE_LAST_VER=$(git rev-list $LAST_VER..HEAD --count)
