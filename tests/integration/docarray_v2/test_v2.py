f70e0b6d4decef9495c32d9bf94284384b442201
samsja <sami.jaghouar@hotmail.fr>
2023-04-04 13:00:43 +0200

    refactor: rename docarray to doclist in test
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>

--- tests/integration/docarray_v2/test_v2.py
+++ tests/integration/docarray_v2/test_v2.py
@@ -1,8 +1,7 @@
 from typing import Optional
 
 import numpy as np
-from docarray import BaseDoc
-from docarray import DocList as DocumentArray
+from docarray import BaseDoc, DocList
 from docarray.documents import ImageDoc
 from docarray.typing import AnyTensor, ImageUrl
 
@@ -16,7 +15,7 @@ def test_different_document_schema():
 
     class MyExec(Executor):
         @requests(on='/foo')
-        def foo(self, docs: DocumentArray[Image], **kwargs) -> DocumentArray[Image]:
+        def foo(self, docs: DocList[Image], **kwargs) -> DocList[Image]:
             for doc in docs:
                 doc.tensor = doc.url.load()
             return docs
@@ -24,10 +23,8 @@ def test_different_document_schema():
     with Flow().add(uses=MyExec) as f:
         docs = f.post(
             on='/foo',
-            inputs=DocumentArray[Image](
-                [Image(url='https://via.placeholder.com/150.png')]
-            ),
-            return_type=DocumentArray[Image],
+            inputs=DocList[Image]([Image(url='https://via.placeholder.com/150.png')]),
+            return_type=DocList[Image],
         )
         docs = docs.stack()
         assert docs.tensor.ndim == 4
@@ -39,13 +36,11 @@ def test_send_custom_doc():
 
     class MyExec(Executor):
         @requests(on='/foo')
-        def foo(self, docs: DocumentArray[MyDoc], **kwargs):
+        def foo(self, docs: DocList[MyDoc], **kwargs):
             docs[0].text = 'hello world'
 
     with Flow().add(uses=MyExec) as f:
-        doc = f.post(
-            on='/foo', inputs=MyDoc(text='hello'), return_type=DocumentArray[MyDoc]
-        )
+        doc = f.post(on='/foo', inputs=MyDoc(text='hello'), return_type=DocList[MyDoc])
         assert doc[0].text == 'hello world'
 
 
@@ -56,8 +51,8 @@ def test_input_response_schema():
     class MyExec(Executor):
         @requests(
             on='/foo',
-            request_schema=DocumentArray[MyDoc],
-            response_schema=DocumentArray[MyDoc],
+            request_schema=DocList[MyDoc],
+            response_schema=DocList[MyDoc],
         )
         def foo(self, docs, **kwargs):
             assert docs.__class__.doc_type == MyDoc
@@ -65,9 +60,7 @@ def test_input_response_schema():
             return docs
 
     with Flow().add(uses=MyExec) as f:
-        docs = f.post(
-            on='/foo', inputs=MyDoc(text='hello'), return_type=DocumentArray[MyDoc]
-        )
+        docs = f.post(on='/foo', inputs=MyDoc(text='hello'), return_type=DocList[MyDoc])
         assert docs[0].text == 'hello world'
         assert docs.__class__.doc_type == MyDoc
 
@@ -78,15 +71,13 @@ def test_input_response_schema_annotation():
 
     class MyExec(Executor):
         @requests(on='/bar')
-        def bar(self, docs: DocumentArray[MyDoc], **kwargs) -> DocumentArray[MyDoc]:
+        def bar(self, docs: DocList[MyDoc], **kwargs) -> DocList[MyDoc]:
             assert docs.__class__.doc_type == MyDoc
             docs[0].text = 'hello world'
             return docs
 
     with Flow().add(uses=MyExec) as f:
-        docs = f.post(
-            on='/bar', inputs=MyDoc(text='hello'), return_type=DocumentArray[MyDoc]
-        )
+        docs = f.post(on='/bar', inputs=MyDoc(text='hello'), return_type=DocList[MyDoc])
         assert docs[0].text == 'hello world'
         assert docs.__class__.doc_type == MyDoc
 
@@ -100,10 +91,8 @@ def test_different_output_input():
 
     class MyExec(Executor):
         @requests(on='/bar')
-        def bar(
-            self, docs: DocumentArray[InputDoc], **kwargs
-        ) -> DocumentArray[OutputDoc]:
-            docs_return = DocumentArray[OutputDoc](
+        def bar(self, docs: DocList[InputDoc], **kwargs) -> DocList[OutputDoc]:
+            docs_return = DocList[OutputDoc](
                 [OutputDoc(embedding=np.zeros((100, 1))) for _ in range(len(docs))]
             )
             return docs_return
@@ -112,7 +101,7 @@ def test_different_output_input():
         docs = f.post(
             on='/bar',
             inputs=InputDoc(img=ImageDoc(tensor=np.zeros((3, 224, 224)))),
-            return_type=DocumentArray[OutputDoc],
+            return_type=DocList[OutputDoc],
         )
         assert docs[0].embedding.shape == (100, 1)
         assert docs.__class__.doc_type == OutputDoc
@@ -127,10 +116,8 @@ def test_deployments():
 
     class MyExec(Executor):
         @requests(on='/bar')
-        def bar(
-            self, docs: DocumentArray[InputDoc], **kwargs
-        ) -> DocumentArray[OutputDoc]:
-            docs_return = DocumentArray[OutputDoc](
+        def bar(self, docs: DocList[InputDoc], **kwargs) -> DocList[OutputDoc]:
+            docs_return = DocList[OutputDoc](
                 [OutputDoc(embedding=np.zeros((100, 1))) for _ in range(len(docs))]
             )
             return docs_return
@@ -139,7 +126,7 @@ def test_deployments():
         docs = dep.post(
             on='/bar',
             inputs=InputDoc(img=ImageDoc(tensor=np.zeros((3, 224, 224)))),
-            return_type=DocumentArray[OutputDoc],
+            return_type=DocList[OutputDoc],
         )
         assert docs[0].embedding.shape == (100, 1)
         assert docs.__class__.doc_type == OutputDoc
