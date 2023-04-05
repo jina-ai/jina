f5c491ca8a06ff11b700b8f2334be895b1768a53
samsja <sami.jaghouar@hotmail.fr>
2023-04-05 11:43:19 +0200

    fix: remove absolute path
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>

--- jina/proto/jina_pb2.py
+++ jina/proto/jina_pb2.py
@@ -4,12 +4,12 @@ from jina._docarray import docarray_v2 as is_docarray_v2
 
 if __pb__version__.startswith('4'):
     if is_docarray_v2:
-        from jina.proto.docarray_v2.pb.jina_pb2 import *
+        from .docarray_v2.pb.jina_pb2 import *
     else:
-        from jina.proto.docarray_v1.pb.jina_pb2 import *
+        from .docarray_v1.pb.jina_pb2 import *
 
 else:
     if is_docarray_v2:
-        from jina.proto.docarray_v2.pb2.jina_pb2 import *
+        from .docarray_v2.pb2.jina_pb2 import *
     else:
-        from jina.proto.docarray_v1.pb2.jina_pb2 import *
+        from .docarray_v1.pb2.jina_pb2 import *
