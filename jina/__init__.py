e182d4f4d6f3303c2fb59a5666dcd0c0edd639e8
Joan Fontanals <joan.martinez@jina.ai>
2023-04-14 11:53:13 +0200

    chore: bump version for release (#5806)

--- jina/__init__.py
+++ jina/__init__.py
@@ -61,7 +61,7 @@ elif _sys.version_info >= (3, 8, 0) and _platform.system() == 'Darwin':
 # this is managed by git tag and updated on every release
 # NOTE: this represents the NEXT release version
 
-__version__ = '3.14.2'
+__version__ = '3.15.0'
 
 # do not change this line manually
 # this is managed by proto/build-proto.sh and updated on every execution
