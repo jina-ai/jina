cda45ccec4cbc3d31d9888596868efb9db53f212
Alaeddine Abdessalem <alaeddine-13@live.fr>
2023-06-07 00:59:00 +0200

    test: unit test for docarray v2 deployment streaming

---
+++ tests/integration/docarray_v2/test_streaming.py
@@ -0,0 +1,42 @@
+import pytest
+
+from jina import Client, Executor, requests
+from jina._docarray import Document
+from jina.helper import random_port
+
+
+class MyDocument(Document):
+    text: str
+    number: int
+
+
+class MyExecutor(Executor):
+    @requests(on='/hello')
+    async def task(self, doc: MyDocument, **kwargs):
+        for i in range(100):
+            yield MyDocument(text=f'{doc.text} {doc.number + i}')
+
+
+@pytest.mark.asyncio
+async def test_streaming_sse_http_deployment():
+    from jina import Deployment
+
+    port = random_port()
+
+    with Deployment(
+        uses=MyExecutor,
+        timeout_ready=-1,
+        protocol='http',
+        cors=True,
+        port=port,
+        include_gateway=False,
+    ):
+        client = Client(port=port, protocol='http', cors=True, asyncio=True)
+        i = 10
+        async for doc in client.stream_doc(
+            on='/hello',
+            inputs=MyDocument(text='hello world', number=i),
+            return_type=MyDocument,
+        ):
+            assert doc.text == f'hello world {i}'
+            i += 1
