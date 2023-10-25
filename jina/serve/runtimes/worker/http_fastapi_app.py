524aba4aa76629fb51d475f3ff2990c9b5954a63
nareka <amirbekiann@gmail.com>
2023-10-25 10:09:13 -0700

    fix: remove post

--- jina/serve/runtimes/worker/http_fastapi_app.py
+++ jina/serve/runtimes/worker/http_fastapi_app.py
@@ -133,7 +133,7 @@ def get_fastapi_app(
 
         @app.api_route(
             path=f'/{endpoint_path.strip("/")}',
-            methods=['GET', 'POST'],
+            methods=['GET'],
             summary=f'Streaming Endpoint {endpoint_path}',
             tags=["streaming"],
         )
