ab2cc19039b9bfcf3957ffb3f1c04ed366fd16cc
Narek Amirbekian <NarekA@users.noreply.github.com>
2023-10-26 23:09:15 -0700

    fix: use body for streaming instead of params (#6098)

--- jina/serve/runtimes/gateway/http_fastapi_app_docarrayv2.py
+++ jina/serve/runtimes/gateway/http_fastapi_app_docarrayv2.py
@@ -252,35 +252,13 @@ def get_fastapi_app(
             methods=['GET'],
             summary=f'Streaming Endpoint {endpoint_path}',
         )
-        async def streaming_get(request: Request):
-            query_params = dict(request.query_params)
+        async def streaming_get(request: Request, body: input_doc_model = None):
+            body = body or dict(request.query_params)
+            body = input_doc_model.parse_obj(body)
 
             async def event_generator():
                 async for doc, error in streamer.stream_doc(
-                    doc=input_doc_model(**query_params), exec_endpoint=endpoint_path
-                ):
-                    if error:
-                        raise HTTPException(status_code=499, detail=str(error))
-                    yield {
-                        'event': 'update',
-                        'data': doc.dict()
-                    }
-                yield {
-                    'event': 'end'
-                }
-
-            return EventSourceResponse(event_generator())
-
-        @app.api_route(
-            path=f'/{endpoint_path.strip("/")}',
-            methods=['POST'],
-            summary=f'Streaming Endpoint {endpoint_path}',
-        )
-        async def streaming_post(body: dict):
-
-            async def event_generator():
-                async for doc, error in streamer.stream_doc(
-                    doc=input_doc_model.parse_obj(body), exec_endpoint=endpoint_path
+                    doc=body, exec_endpoint=endpoint_path
                 ):
                     if error:
                         raise HTTPException(status_code=499, detail=str(error))
