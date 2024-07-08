8ca8c27c5634db725ab6253f57a85db08da4546d
Joan Martinez <joan.fontanals.martinez@jina.ai>
2024-07-08 16:55:10 +0200

    test: test CSP parameters

--- tests/integration/docarray_v2/csp/SampleExecutor/executor.py
+++ tests/integration/docarray_v2/csp/SampleExecutor/executor.py
@@ -1,7 +1,7 @@
 import numpy as np
 from docarray import BaseDoc, DocList
 from docarray.typing import NdArray
-from pydantic import Field
+from pydantic import Field, BaseModel
 
 from jina import Executor, requests
 
@@ -19,6 +19,11 @@ class EmbeddingResponseModel(TextDoc):
         json_encoders = {NdArray: lambda v: v.tolist()}
 
 
+class Parameters(BaseModel):
+    emb_dim: int
+
+
+
 class SampleExecutor(Executor):
     @requests(on="/encode")
     def foo(self, docs: DocList[TextDoc], **kwargs) -> DocList[EmbeddingResponseModel]:
@@ -29,6 +34,19 @@ class SampleExecutor(Executor):
                     id=doc.id,
                     text=doc.text,
                     embeddings=np.random.random((1, 64)),
+                )
+            )
+        return DocList[EmbeddingResponseModel](ret)
+
+    @requests(on="/encode_parameter")
+    def foo(self, docs: DocList[TextDoc], parameters: Parameters, **kwargs) -> DocList[EmbeddingResponseModel]:
+        ret = []
+        for doc in docs:
+            ret.append(
+                EmbeddingResponseModel(
+                    id=doc.id,
+                    text=doc.text,
+                    embeddings=np.random.random((1, parameters.emb_dim)),
                 )
             )
         return DocList[EmbeddingResponseModel](ret)
